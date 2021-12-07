28de6d96a70362d28cd1cfc87df845de26f2099d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-07 10:33:34 -0500

    Update 1296. [Medium] Divide Array in Sets of K Consecutive Numbers.py

--- 1000 - 1999/1296. [Medium] Divide Array in Sets of K Consecutive Numbers.py
+++ 1000 - 1999/1296. [Medium] Divide Array in Sets of K Consecutive Numbers.py
@@ -1,20 +1,22 @@
 # https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
 
 class Solution:
-    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        n = len(hand)
-        if n % groupSize != 0: 
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        
+        if n % k != 0: 
             return False
         
-        hand.sort()
-        while hand:
-            minElem = hand[0]
-            for k in range(groupSize):
-                idx = bisect_left(hand, minElem)
-                if idx != len(hand):
-                    if hand[idx] == minElem: 
-                        minElem += 1
-                        hand.pop(idx)
+        nums.sort()
+        
+        while nums:
+            num = nums[0]
+            for _ in range(k):
+                idx = bisect_left(nums, num)
+                if idx != len(nums):
+                    if nums[idx] == num: 
+                        num += 1
+                        nums.pop(idx)
                     else: 
                         return False
                 else: 
