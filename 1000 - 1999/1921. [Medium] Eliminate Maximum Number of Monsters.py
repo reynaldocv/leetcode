7f3b5e473305d0fe772b722ee53e718da4d44dd4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-07 10:13:44 -0500

    Update 1921. [Medium] Eliminate Maximum Number of Monsters.py

--- 1000 - 1999/1921. [Medium] Eliminate Maximum Number of Monsters.py
+++ 1000 - 1999/1921. [Medium] Eliminate Maximum Number of Monsters.py
@@ -23,5 +23,24 @@ class Solution:
             
         return maxDay
             
+        
+class Solution2:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        arr = []
+        n = len(dist)
+        for i in range(n):
+            days, res = divmod(dist[i], speed[i])
+            days += 1 if res != 0 else 0 
+            insort(arr, days)
+        
+        maxDay = max(arr)
+        
+        day = 0
+        for arriveDay in arr:     
+            if arriveDay < day + 1: 
+                return day 
             
+            day += 1
+            
+        return day
         
