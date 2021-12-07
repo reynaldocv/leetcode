f00933a7f33db8aacfe575e9ccd68eed769bb238
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-07 10:34:38 -0500

    Create 0846. [Medium] Hand of Straights.py

---
+++ 0001 - 0999/0846. [Medium] Hand of Straights.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/hand-of-straights/
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        n = len(hand)
+        
+        if n % groupSize != 0: 
+            return False
+        
+        hand.sort()
+        
+        while hand:
+            minElem = hand[0]
+            for k in range(groupSize):
+                idx = bisect_left(hand, minElem)
+                if idx != len(hand):
+                    if hand[idx] == minElem: 
+                        minElem += 1
+                        hand.pop(idx)
+                    else: 
+                        return False
+                else: 
+                    return False
+                
+        return True
+        
