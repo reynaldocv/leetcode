80d8471a6fe4815c40c279a43db96c41acaf072a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-08 16:28:07 -0500

    Update 0894. [Medium] All Possible Full Binary Trees.py

--- 0001 - 0999/0894. [Medium] All Possible Full Binary Trees.py
+++ 0001 - 0999/0894. [Medium] All Possible Full Binary Trees.py
@@ -6,32 +6,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
-        def allTrees(n):
-            if n in seen:
-                return seen[n]
+        @cache
+        def helper(n):
+            if n == 0: 
+                return [None]
             elif n == 1: 
-                seen[1] = [TreeNode(0)]
-                return seen[1]
-            else:
+                return [TreeNode(0)]
+            else: 
                 ans = []
                 for i in range(1, n, 2):
-                    leftTrees = allTrees(i)
-                    rightTrees = allTrees(n - i - 1)
-                    for left in leftTrees: 
-                        for right in rightTrees: 
-                            root = TreeNode(0)
-                            root.left = left
-                            root.right = right
+                    for left in helper(i):
+                        for right in helper(n - i - 1):
+                            root = TreeNode(0, left, right)
                             ans.append(root)
-                seen[n] = ans
-                return seen[n]
+                            
+                return ans
         
-        seen  = {}
-        if n % 2 == 0: 
+        if n % 2 == 0:
             return []
-        else: 
-            return allTrees(n)
+        
+        return helper(n)
+                            
+            
+            
             
         
