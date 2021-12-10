111347d9f514191c9288ab8be392a85b6881708c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-10 09:55:04 -0500

    Create 0817. [Medium] Linked List Components.py

---
+++ 1000 - 1999/0817. [Medium] Linked List Components.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/linked-list-components/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:        
+        ans = 0 
+        prev = False
+        while head: 
+            if head.val in nums: 
+                if prev == False: 
+                    ans += 1
+                    prev = True
+            else: 
+                prev = False
+                
+            head = head.next
+            
+        return ans
+            
+ 
