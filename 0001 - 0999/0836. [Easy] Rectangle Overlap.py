9dd1bbb34be37e9894be02d0476d0acb6ccbd797
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-11 09:02:04 -0500

    Create 0836. [Easy] Rectangle Overlap.py

---
+++ 0001 - 0999/0836. [Easy] Rectangle Overlap.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/rectangle-overlap/
+
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        if rec1[0] >= rec2[2] or rec1[2] <= rec2[0]:
+            return False
+        
+        if rec1[1] >= rec2[3] or rec1[3] <= rec2[1]:
+            return False
+        
+        if rec1[2] > rec2[0] or rec1[3] > rec2[1]:
+            return True
+        
+        if rec1[0] < rec2[2] or rec1[1] < rec2[3]:
+            return True
+        
+        return False
+
