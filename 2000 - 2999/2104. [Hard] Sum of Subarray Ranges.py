ba0d19052db4ced710b25ac05c8029f9afa1099d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-11 23:28:37 -0500

    Create 2104. [Hard] Sum of Subarray Ranges.py

---
+++ 2000 - 2999/2104. [Hard] Sum of Subarray Ranges.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/sum-of-subarray-ranges/
+
+class Solution:
+    def maxTotalFruits(self, fruits: List[List[int]], startPos: int, k: int) -> int:
+        limit = max(max([x for (x, _) in fruits]), startPos) + k
+        
+        n = len(fruits)
+        prefix = [0 for _ in range(limit + 1)]
+        
+        for (x, fruit) in fruits: 
+            prefix[x] = fruit
+        
+        for i in range(1, limit + 1):
+            prefix[i] += prefix[i - 1]
+        
+        ans = 0 
+        for i in range(max(0, startPos - k), startPos + k + 1):
+            if i <= startPos: 
+                res = k - (startPos - i)
+                right = max(i + res, startPos)                
+                left = prefix[i - 1] if i >= 1 else 0 
+                ans = max(ans, prefix[right] - left)
+            else: 
+                res = k - (i - startPos)
+                idx = min(i - res, startPos)                
+                left = prefix[idx - 1] if idx >= 1 else 0                
+                ans = max(ans, prefix[i] - left)
+            
+        return ans
+            
+        
