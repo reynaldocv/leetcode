2deead0d89d672352d0ff6baa1bfe9233985ad8b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-13 08:40:03 -0500

    Create 1558. [Medium] Minimum Numbers of Function Calls to Make Target Array.py

---
+++ 1000 - 1999/1558. [Medium] Minimum Numbers of Function Calls to Make Target Array.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/minimum-numbers-of-function-calls-to-make-target-array/
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        aSum = sum(nums)
+        ans = 0
+        
+        while aSum != 0:
+            for i in range(n):
+                if nums[i] % 2 != 0:
+                    ans += 1
+                    aSum -= 1
+                    
+            if aSum != 0:
+                for i in range(n):
+                    if nums[i] != 0:
+                        nums[i] //= 2
+                        aSum -= nums[i]
+                        
+                ans += 1
+                
+        return ans
+        
