a86c66a02b3b032ce659c721a930b941e604084c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-15 08:31:09 -0500

    Update 0148. [Medium] Sort List.py

--- 0001 - 0999/0148. [Medium] Sort List.py
+++ 0001 - 0999/0148. [Medium] Sort List.py
@@ -5,11 +5,6 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head: 
