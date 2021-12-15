46b5a28d202e1180de1e7be25dcec9d666102d86
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-14 23:09:31 -0500

    Create 0926. [Medium] Flip String to Monotone Increasing.py

---
+++ 0001 - 0999/0926. [Medium] Flip String to Monotone Increasing.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/flip-string-to-monotone-increasing/
+
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        n = len(s)
+        left = [0 for _ in range(n)]
+        right = [0 for _ in range(n)]
+        
+        prev = 0 
+        for i in range(n):
+            left[i] = prev + 1 if s[i] == "1" else prev
+            prev = left[i]
+            
+        prev = 0 
+        for i in range(n - 1, -1, -1):
+            right[i] = prev + 1 if s[i] == "0" else prev
+            prev = right[i]
+        
+        ans = inf
+        
+        for i in range(n + 1):
+            prefix = left[i - 1] if i - 1 >= 0 else 0 
+            suffix = right[i] if i < n else 0 
+            
+            ans = min(ans, prefix + suffix)
+    
+        return ans
+            
+        
