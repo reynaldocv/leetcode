cdb5efadaaa3a07ec4cf81a53443609ebaba44bf
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-18 23:09:53 -0500

    Create 2108. [Easy] Find First Palindromic String in the Array.py

---
+++ 2000 - 2999/2108. [Easy] Find First Palindromic String in the Array.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for word in words: 
+            if word == word[::-1]:
+                return word
+            
+        return ""
+        
