22b6cf8661a824fe6f043d99cf30431dff4063fd
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-24 15:17:43 -0500

    Create 1987. [Hard] Number of Unique Good Subsequences.py

---
+++ 0001 - 0999/1987. [Hard] Number of Unique Good Subsequences.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/number-of-unique-good-subsequences/
+
+class Solution:
+    def numberOfUniqueGoodSubsequences(self, binary: str) -> int:
+        MOD = 10**9 + 7
+        dp = [1]
+        last = {}
+        
+        for (i, char) in enumerate(binary):
+            aux = 2*dp[-1]
+            if char in last: 
+                aux -= dp[last[char]]
+            elif char == "0":
+                aux -= 1
+            
+            last[char] = i
+            dp.append(aux)
+            
+        return (dp[-1] - 1 + ("0" in binary)) % MOD
+            
+        
+        
