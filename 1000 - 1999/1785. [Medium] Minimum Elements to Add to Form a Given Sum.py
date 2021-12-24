bfbd63c2b65b5d4c7ad8cdcb1ecb04375c025fd1
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-24 17:57:59 -0500

    Create 1785. [Medium] Minimum Elements to Add to Form a Given Sum.py

---
+++ 1000 - 1999/1785. [Medium] Minimum Elements to Add to Form a Given Sum.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/minimum-elements-to-add-to-form-a-given-sum/
+
+class Solution:
+    def minElements(self, nums: List[int], limit: int, goal: int) -> int:
+        aSum = sum(nums)
+        aux = abs(goal - aSum)
+        
+        if aux % limit == 0:
+            return aux//limit
+        else: 
+            return aux//limit + 1
+        
+        
