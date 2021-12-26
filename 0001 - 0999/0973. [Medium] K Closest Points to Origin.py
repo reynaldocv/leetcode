59b39a64a4ac2c27c1fa8749b8659a647542bfa6
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-25 19:05:41 -0500

    Update 0973. [Medium] K Closest Points to Origin.py

--- 0001 - 0999/0973. [Medium] K Closest Points to Origin.py
+++ 0001 - 0999/0973. [Medium] K Closest Points to Origin.py
@@ -1,13 +1,12 @@
 # https://leetcode.com/problems/k-closest-points-to-origin/
-
-class Solution:
+    
+class Solution2:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        def distance(x, y):
-            return x**2 + y**2
-        
-        points.sort(key = lambda item: distance(item[0], item[1]))
+        points.sort(key = lambda item: item[0]**2 + item[1]**2)
         
         return points[:k]
+        
+        
             
         
         
