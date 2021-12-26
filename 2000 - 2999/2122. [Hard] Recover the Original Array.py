18b2c8cea33c1ba655ccb8ce74d8c58ebc368d86
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2021-12-25 23:49:17 -0500

    Update 2122. [Hard] Recover the Original Array.py

--- 2000 - 2999/2122. [Hard] Recover the Original Array.py
+++ 2000 - 2999/2122. [Hard] Recover the Original Array.py
@@ -4,25 +4,21 @@ class Solution:
     def recoverArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
         nums.sort()
-        
         for num in nums[1:]:
             k = num - nums[0]
             
-            if k == 0 or k % 2 == 1: 
-                continue
+            if k != 0 and k % 2 == 0: 
                 
-            seen = defaultdict(lambda: 0)
-            ans = []
-            
-            for num in nums:
-                if seen[num] > 0:
-                    seen[num] -= 1
-                else:
-                    ans.append(num)
-                    seen[num + k] += 1
-                    
-            if len(ans)*2 == n:
-                k //= 2
-                return [num + k for num in ans]
-        
+                seen = defaultdict(lambda: 0)
+                ans = []
+                for num in nums: 
+                    if seen[num] > 0: 
+                        seen[num]-= 1
+                    else: 
+                        ans.append(num)
+                        seen[num + k] += 1
+                        
+                if len(ans)*2 == n:
+                    return [num + k//2 for num in ans]
+                
         return []
