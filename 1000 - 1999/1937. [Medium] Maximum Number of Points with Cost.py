bfd7e81dc8e090ccecc897a81e2b3d2a99bae8f0
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-01-02 20:36:44 -0500

    Create 1937. [Medium] Maximum Number of Points with Cost.py

---
+++ 1000 - 1999/1937. [Medium] Maximum Number of Points with Cost.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/maximum-number-of-points-with-cost/
+
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        def helper(arr):            
+            ans = [num for num in arr]
+            for i in range(1, n):
+                ans[i] = max(ans[i], ans[i - 1] - 1)
+                
+            return ans
+        
+        m, n = len(points), len(points[0])
+        
+        ans = [0 for i in range(n)]
+        for row in points: 
+            left = helper(ans)
+            right = helper(ans[::-1])[::-1]            
+            for i in range(n):
+                ans[i] = row[i] + max(left[i], right[i])
+            
+        return max(ans)
