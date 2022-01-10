f365096b445292ab680c31aedb77ca90fdf22e92
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-01-10 01:00:02 -0500

    Update 0984. [Medium] String Without AAA or BBB.py

--- 0001 - 0999/0984. [Medium] String Without AAA or BBB.py
+++ 0001 - 0999/0984. [Medium] String Without AAA or BBB.py
@@ -3,6 +3,24 @@
 class Solution:
     def strWithout3a3b(self, a: int, b: int) -> str:
         ans = ""
+        while a > 0 or b > 0:             
+            if len(ans) >= 2 and ans[-1] == ans[-2]:
+                writeA = ans[-1] == "b"
+            else: 
+                writeA = a >= b
+                
+            if writeA: 
+                ans += "a"
+                a -= 1
+            else: 
+                ans += "b"
+                b -= 1 
+           
+        return ans
+    
+class Solution2:
+    def strWithout3a3b(self, a: int, b: int) -> str:
+        ans = ""
         twice = False
         while a > 0 or b > 0: 
             if twice: 
