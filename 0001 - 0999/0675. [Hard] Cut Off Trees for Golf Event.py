495784bb0cf82755012641d58ea297fddeafbd1b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-01-12 00:39:11 -0500

    Create 0675. [Hard] Cut Off Trees for Golf Event.py

---
+++ 0001 - 0999/0675. [Hard] Cut Off Trees for Golf Event.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/cut-off-trees-for-golf-event/
+
+class Solution:
+    def judgePoint24(self, cards: List[int]) -> bool:
+        def helper(cards): 
+            if len(cards) == 1: 
+                return round(cards[0], 4) == 24
+            else: 
+                arr = [(i, card) for (i, card) in enumerate(cards)]
+                
+                for ((i, card1), (j, card2)) in combinations(arr, 2):
+                    newCards = {card1 + card2, card1*card2, abs(card1 - card2)}
+                    if card2 != 0:
+                        newCards.add(card1/card2)
+                    if card1 != 0:
+                        newCards.add(card2/card1)
+                        
+                    oldCards = [card for (k, card) in enumerate(cards) if k != i  and k != j]
+                    
+                    if any (helper(oldCards + [card]) for card in newCards):
+                        return True
+                    
+                return False
+                
+        return helper(cards)
