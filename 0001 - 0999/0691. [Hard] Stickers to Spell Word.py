20f8aba4f375838e84d4e59450a196728d9f5e61
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-01-12 23:12:50 -0500

    Create 0691. [Hard] Stickers to Spell Word.py

---
+++ 0001 - 0999/0691. [Hard] Stickers to Spell Word.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/stickers-to-spell-word/
+
+class Solution:
+    def minStickers(self, stickers: List[str], target: str) -> int:
+        def helper(t):
+            k = tuple(t.items())
+            
+            if k not in memo:
+                if not t: 
+                    return 0
+                
+                ans = inf 
+                for s in stk: 
+                    if t & s: 
+                        ans = min(ans, 1 + helper(t - s))
+                
+                memo[k] = ans
+            
+            return memo[k]
+
+        if set(target) - reduce(lambda a, b: a|b, map(set,stickers)): 
+            return -1
+
+        stk, memo = list(map(Counter,stickers)), {}
+        
+        return helper(Counter(target))
