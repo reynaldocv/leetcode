834fd3d9b65cc0473ee9765e5519a5e7efc1386d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-01-13 19:33:36 -0500

    Update 0008. [Medium] String to Integer (atoi).py

--- 0001 - 0999/0008. [Medium] String to Integer (atoi).py
+++ 0001 - 0999/0008. [Medium] String to Integer (atoi).py
@@ -2,6 +2,32 @@
 
 class Solution:
     def myAtoi(self, s: str) -> int:
+        ans = 0 
+        sign = 1
+        idx = 0 
+        n = len(s)
+        
+        while idx < n and s[idx] == " ":
+            idx += 1 
+        
+        if idx < n and s[idx] in "+-":
+            sign = 1 if s[idx] == "+" else -1
+            idx += 1 
+            
+        while idx < n and s[idx].isdigit():
+            ans = ans*10 + int(s[idx])
+            idx += 1
+            
+        if sign*ans > 2**31 - 1: 
+            return 2**31 - 1
+        
+        if sign*ans < -2**31: 
+            return -2**31
+        
+        return sign*ans
+
+class Solution2:
+    def myAtoi(self, s: str) -> int:
         n = len(s)        
         i, state = 0, 0
         
