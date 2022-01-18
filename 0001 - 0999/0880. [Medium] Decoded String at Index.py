705c7ed00e80c37fd1c7018770e06cea0ed7fbdb
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-01-18 01:03:39 -0500

    Create 0880. [Medium] Decoded String at Index.py

---
+++ 0001 - 0999/0880. [Medium] Decoded String at Index.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/decoded-string-at-index/
+
+class Solution:
+    def decodeAtIndex(self, s: str, k: int) -> str:
+        size = 0 
+        
+        for char in s: 
+            if char.isdigit():
+                size *= int(char)
+            else: 
+                size += 1 
+                
+        for char in s[::-1]:
+            k %= size
+            
+            if k == 0 and char.isalpha():
+                return char
+            
+            if char.isdigit():
+                size /= int(char)
+            else: 
+                size -= 1 
+                
+        
