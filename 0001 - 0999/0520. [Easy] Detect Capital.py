ae368fd7f96376576349a0bd34b68dcded2cba50
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-01-23 22:36:22 -0500

    Update 0520. [Easy] Detect Capital.py

--- 0001 - 0999/0520. [Easy] Detect Capital.py
+++ 0001 - 0999/0520. [Easy] Detect Capital.py
@@ -2,25 +2,6 @@
 
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        lowercases = {chr(ord("a") +i) for i in range(26)}
-        uppercases = {chr(ord("A") +i) for i in range(26)}
-        n = len(word)
-        if word[0] in lowercases:
-            for i in range(1, n):
-                if word[i] in uppercases:
-                    return False
-            return True            
-        else:
-            if n == 1: 
-                return True
-            if word[1] in lowercases: 
-                for i in range(2, n):
-                    if word[i] in uppercases:
-                        return False
-                return True
-            else:
-                for i in range(2, n):
-                    if word[i] in lowercases:
-                        return False
-                return True
+        return word in [word.upper(), word.lower(), word.capitalize()]
+        
         
