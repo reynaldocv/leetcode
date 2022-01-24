016fe1bf7e89c4ccd63c2c1f331655c9e3d0ef96
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-01-24 00:02:58 -0500

    Create 1402. [Hard] Reducing Dishes.py

---
+++ 1000 - 1999/1402. [Hard] Reducing Dishes.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/reducing-dishes/
+
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        ans = total = 0
+        satisfaction.sort()
+        while satisfaction and satisfaction[-1] + total > 0:
+            total += satisfaction.pop()
+            ans += total
+            
+        return ans
+                    
+            
+        
+            
+            
