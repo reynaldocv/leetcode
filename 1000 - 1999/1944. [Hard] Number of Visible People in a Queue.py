f7ce161d8f6bf307a5d1e3cfb3bfb04e952ebaa9
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-01-24 01:11:44 -0500

    Create 1944. [Hard] Number of Visible People in a Queue.py

---
+++ 1000 - 1999/1944. [Hard] Number of Visible People in a Queue.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/number-of-visible-people-in-a-queue/
+
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        n = len(heights)
+        
+        ans = [0 for _ in range(n)]
+        popCount = 0 
+        
+        stack = [heights[-1]]
+        for i in range(n - 2, -1, -1):
+            while stack and stack[-1] < heights[i]:
+                stack.pop()
+                popCount += 1 
+            
+            totalCount = popCount + 1 if stack else popCount
+            ans[i] = totalCount
+               
+            stack.append(heights[i])
+            popCount = 0
+            
+        return ans
+        
