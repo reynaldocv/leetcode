4774b6dcb4129f899fff8a7a3081c72e1414fbd4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-01-26 20:24:55 -0500

    Update 1864. [Medium] Minimum Number of Swaps to Make the Binary String Alternating.py

--- 1000 - 1999/1864. [Medium] Minimum Number of Swaps to Make the Binary String Alternating.py
+++ 1000 - 1999/1864. [Medium] Minimum Number of Swaps to Make the Binary String Alternating.py
@@ -3,37 +3,33 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
         n = len(s)
-        index = defaultdict(lambda: [])
         counter = [0, 0]
         for (i, bit) in enumerate(s):
             counter[int(bit)] += 1 
         
         if abs(counter[0] - counter[1]) > 1: 
             return -1
-        
-        ans = 0             
-        if counter[0] > counter[1]:
-            for i in range(0, n, 2):
-                if s[i] != "0":
-                    ans += 1 
-                    
-        elif counter[0] < counter[1]:
-            for i in range(0, n, 2):
-                if s[i] != "1":
-                    ans += 1 
-        else: 
-            swaps1, swaps2 = 0, 0
-            for i in range(0, n, 2):
-                if s[i] != "0":
-                    swaps1 += 1 
-        
-            for i in range(0, n, 2):
-                if s[i] != "1":
-                    swaps2 += 1 
                 
-            ans = min(swaps1, swaps2)
+        swaps1, swaps2 = 0, 0
+        for i in range(0, n, 2):
+            if s[i] != "0":
+                swaps1 += 1 
+
+        for i in range(0, n, 2):
+            if s[i] != "1":
+                swaps2 += 1 
+                
+        if counter[0] > counter[1]:
+            return swaps1
+        elif counter[1] > counter[0]:
+            return swaps2
+        else: 
+            return min(swaps1, swaps2)
+
             
-        return ans
+                
+                
+        
             
                 
                 
