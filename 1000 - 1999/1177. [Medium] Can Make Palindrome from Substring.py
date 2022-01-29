aa287adff5f4f0fe562d0ed2501b97a9aac670fc
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-01-28 23:04:51 -0500

    Update 1177. [Medium] Can Make Palindrome from Substring.py

--- 1000 - 1999/1177. [Medium] Can Make Palindrome from Substring.py
+++ 1000 - 1999/1177. [Medium] Can Make Palindrome from Substring.py
@@ -3,27 +3,36 @@
 class Solution:
     def canMakePaliQueries(self, s: str, queries: List[List[int]]) -> List[bool]:
         def helper(i, j):
-            ans = [0]*26
-            for k in range(26):
-                ans[k] = counter[i][k] - counter[j][k]
+            ans = {}
+            for key in counter[i]:
+                diff = counter[i][key] - counter[j][key]
+                if diff > 0:
+                    ans[key] = diff
                 
-            return tuple(ans)
+            return ans
             
-        cnt = (0, )*26
+        cnt = defaultdict(lambda: 0)
         counter = [cnt]
         for char in s: 
-            i = ord(char) - ord("a")
-            cnt = cnt[:i] + (cnt[i] + 1, ) + cnt[i + 1:]
+            cnt = counter[-1].copy()
+            cnt[char] += 1 
             counter.append(cnt)
-        
+                
         ans = []
         for (start, end, k) in queries: 
             end += 1 
             cnt = helper(end, start)
+            
             total = 0 
-            for i in range(26):
-                total += cnt[i] % 2
+            for key in cnt:
+                total += cnt[key] % 2
                 
             ans.append(total//2 <= k)
             
         return ans
+                
+            
+            
+            
+            
+        
