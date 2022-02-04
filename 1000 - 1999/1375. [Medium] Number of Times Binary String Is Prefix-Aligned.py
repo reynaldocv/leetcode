1f140874704b5f3562073cd860439ce5ac7ef3e4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-02-04 00:59:14 -0500

    Update 1375. [Medium] Number of Times Binary String Is Prefix-Aligned.py

--- 1000 - 1999/1375. [Medium] Number of Times Binary String Is Prefix-Aligned.py
+++ 1000 - 1999/1375. [Medium] Number of Times Binary String Is Prefix-Aligned.py
@@ -2,6 +2,20 @@
 
 class Solution:
     def numTimesAllBlue(self, light: List[int]) -> int:
+        maxElem = 0 
+        aSum = 0 
+        ans = 0 
+        
+        for num in light:
+            aSum += num
+            maxElem = max(maxElem, num)
+            if maxElem*(maxElem + 1)//2 == aSum:
+                ans += 1 
+                
+        return ans    
+
+class Solution2:
+    def numTimesAllBlue(self, light: List[int]) -> int:
         n = len(light)
         seen = {i + 1: True for i in range(n)}
         heap = [i + 1 for i in range(n)]
