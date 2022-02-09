7c0170b7b2d0bd0c1a470a93527980fc6418252c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-02-08 21:09:30 -0500

    Update 0532. [Medium] K-diff Pairs in an Array.py

--- 0001 - 0999/0532. [Medium] K-diff Pairs in an Array.py
+++ 0001 - 0999/0532. [Medium] K-diff Pairs in an Array.py
@@ -2,37 +2,33 @@
 
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        nums.sort()
+        ans = set()
         seen = {}
-        ans = 0 
-        for (i, num) in enumerate(nums): 
-            idx = bisect_left(nums[:i], num - k)
-            if idx < i:
-                if nums[idx] == num - k:
-                    if (num - k, num) not in seen: 
-                        seen[(num - k, num)] = True
-                        ans += 1
-                    
-        return ans
+        for num in nums: 
+            if num + k in seen:
+                ans.add(num)
+            if num - k in seen: 
+                ans.add(num - k)
+            
+            seen[num] = True
         
- class Solution2:
+        return len(ans)
+    
+class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
         counter = defaultdict(lambda: 0)
-        
-        for num in nums: 
-            counter[num] += 1
-            
+
+        nums.sort()
         ans = 0 
         
-        if k == 0: 
-            for key in counter: 
-                if counter[key] > 1:
-                    ans += 1
+        for num in nums: 
+            if num not in counter:
+                if num - k in counter: 
+                    ans += 1 
+                    
+            counter[num] += 1  
             
-        else:
-            for key in counter:
-                if key - k in counter: 
-                    ans += 1
-        
-        return ans
-        
+        if k == 0: 
+            return sum(1 for key in counter if counter[key] > 1)
+        else: 
+            return ans 
