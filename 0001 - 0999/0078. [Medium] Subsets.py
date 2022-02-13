9e3b550073930988686d94f3c7ce0ed369234547
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-02-13 00:40:54 -0500

    Update 0078. [Medium] Subsets.py

--- 0001 - 0999/0078. [Medium] Subsets.py
+++ 0001 - 0999/0078. [Medium] Subsets.py
@@ -2,6 +2,18 @@
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = [[]]
+        for num in nums: 
+            aux = []
+            for subset in ans: 
+                aux.append(subset + [num])
+                
+            ans.extend(aux)
+            
+        return ans
+
+class Solution2:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         def backtracking(arr, i, n,  elems):
             if i <= n: 
                 self.ans.append(list(arr))
