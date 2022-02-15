ea90e97da41a76ac8052f3b9a5fbb79132584424
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-02-14 21:30:46 -0500

    Update 0136. [Easy] Single Number.py

--- 0001 - 0999/0136. [Easy] Single Number.py
+++ 0001 - 0999/0136. [Easy] Single Number.py
@@ -2,11 +2,9 @@
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        dic = {}
-        for num in nums: 
-            dic[num] = dic.get(num, 0) + 1
-        
-        for i in dic: 
-            if dic[i] == 1: 
-                return i
+        ans = 0
+        for num in nums:
+            ans ^= num
+            
+        return ans
         
