1006e4f09b6da7966a2f9dcd7a517b95f744cfa6
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-02-21 21:17:58 -0500

    Update 0171. [Easy] Excel Sheet Column Number.py

--- 0001 - 0999/0171. [Easy] Excel Sheet Column Number.py
+++ 0001 - 0999/0171. [Easy] Excel Sheet Column Number.py
@@ -2,10 +2,13 @@
 
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
-        dic = {chr(i): i - ord("A") + 1 for i in range(ord("A"), ord("Z") + 1)}
-        n = len(columnTitle) - 1
-        ans = 0
-        for i in columnTitle: 
-            ans += dic[i]*26**(n)
-            n -= 1
+        values = {chr(ord("A") + i): i + 1 for i in range(26)}
+        ans = 0 
+        p = 1
+        
+        for char in columnTitle[::-1]:
+            ans += p*values[char]
+            p *= 26
+            
         return ans
+            
