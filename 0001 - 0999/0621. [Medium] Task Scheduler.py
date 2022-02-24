5543d841b6af7c6e04da627e20a4e5907dafe85e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-02-24 03:12:13 -0500

    Create 0621. [Medium] Task Scheduler.py

---
+++ 0001 - 0999/0621. [Medium] Task Scheduler.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/task-scheduler/
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        counter = defaultdict(lambda: 0)
+        high = cnt = 0 
+        
+        for task in tasks: 
+            counter[task] += 1 
+            if counter[task] > high: 
+                high = counter[task]
+                cnt = 1
+            
+            elif counter[task] == high: 
+                cnt += 1 
+                
+        return max(len(tasks), (high - 1)*(n + 1) + cnt)
+        
