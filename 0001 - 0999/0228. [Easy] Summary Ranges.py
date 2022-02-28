11d6088f13a19696fbd83a52998668861a5b16da
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-02-27 22:55:42 -0500

    Update 0228. [Easy] Summary Ranges.py

--- 0001 - 0999/0228. [Easy] Summary Ranges.py
+++ 0001 - 0999/0228. [Easy] Summary Ranges.py
@@ -2,22 +2,22 @@
 
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        def rangeStartEnd(start, end):            
-            if start != end: 
-                return str(start) + "->" + str(end)
-            else:
-                return str(start)            
-            
-        n = len(nums)
-        if n == 0: return []
-        if n == 1: return [str(nums[0])] 
+        if not nums: 
+            return []
+        
+        start = prev = nums[0]        
         ans = []
-        start = end = nums[0]        
-        for i in range(1, n):             
-            if nums[i] == nums[i - 1] + 1:
-                end = nums[i]
-            else:
-                ans.append(rangeStartEnd(start, end))
-                start = end = nums[i]
-        ans.append(rangeStartEnd(start, end))
+        
+        for num in nums[1: ] + [inf]:
+            if prev + 1 == num: 
+                prev += 1 
+            else: 
+                if start == prev: 
+                    ans.append(str(start))
+                else:
+                    ans.append(str(start) + "->" + str(prev))
+                    
+                prev = start = num
+                
         return ans
+
