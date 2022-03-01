647a0356c0eddd0247222cd80a510dbcdfdaa8de
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-02-28 20:47:24 -0500

    Update 0338. [Easy] Counting Bits.py

--- 0001 - 0999/0338. [Easy] Counting Bits.py
+++ 0001 - 0999/0338. [Easy] Counting Bits.py
@@ -2,18 +2,21 @@
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        if n == 0: return [0]
-        if n == 1: return [0, 1]
-        if n >= 2: 
+        if n == 0: 
+            return [0]
+        elif n == 1: 
+            return [0, 1]
+        else:
             ans = [0, 1]
-        
-            aux, j = 2, 0
-            for i in range(2, n + 1):
-                ans.append(ans[i - aux] + 1)
-                j += 1
-                if j == aux:
-                    aux = 2*aux
-                    j = 0
+            start = 0 
+            limit = 2
+            for _ in range(2, n + 1):
+                if start >= limit: 
+                    start = 0
+                    limit *= 2 
+                
+                ans.append(ans[start] + 1)                    
+                start += 1
             
-            return ans
+            return ans 
         
