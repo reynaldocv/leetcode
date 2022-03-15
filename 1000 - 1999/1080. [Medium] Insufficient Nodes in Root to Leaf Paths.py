e85f2e3acce202b7c30eda93f95e6065d9b9b578
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-03-15 11:40:20 -0300

    Create 1080. [Medium] Insufficient Nodes in Root to Leaf Paths.py

---
+++ 1000 - 1999/1080. [Medium] Insufficient Nodes in Root to Leaf Paths.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/insufficient-nodes-in-root-to-leaf-paths/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sufficientSubset(self, root: Optional[TreeNode], limit: int) -> Optional[TreeNode]:
+        def helper(node, value):
+            if not node: 
+                return None
+            
+            value -= node.val
+            
+            if node.left == None and node.right == None: 
+                return None if value > 0 else node
+            else: 
+                node.left = helper(node.left, value)
+                node.right = helper(node.right, value)
+
+                if node.left == None and node.right == None: 
+                    return None
+                else: 
+                    return node
+
+        return helper(root, limit)
+            
