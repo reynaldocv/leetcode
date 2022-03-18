3cf18ccdc46bdca23b7fae62ad0c74506ef5cebb
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-03-18 20:25:54 -0300

    Update 1523. [Easy] Count Odd Numbers in an Interval Range.py

--- 1000 - 1999/1523. [Easy] Count Odd Numbers in an Interval Range.py
+++ 1000 - 1999/1523. [Easy] Count Odd Numbers in an Interval Range.py
@@ -1,10 +1,12 @@
 # https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
 
 class Solution:
-    def countOdds(self, low: int, high: int) -> int:
-        total = high - low + 1
+    def countOdds(self, low: int, high: int) -> int:        
         if low % 2 == 1:
-            return total//2 + 1 if (total % 2 == 1) else total//2
-        else: 
-            return total//2
+            low -= 1 
+            
+        if high % 2 == 1:            
+            high += 1 
+        
+        return (high - low)//2
             
