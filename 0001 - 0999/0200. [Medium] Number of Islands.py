81fda8095923966606e7b62a5404e4c9413f5be1
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-03-18 21:00:19 -0300

    Update 0200. [Medium] Number of Islands.py

--- 0001 - 0999/0200. [Medium] Number of Islands.py
+++ 0001 - 0999/0200. [Medium] Number of Islands.py
@@ -2,26 +2,32 @@
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def markIsland(grid, x, y, n, m, val):
-            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-            points = [(x, y)]
-            while len(points) > 0:                
-                (x, y) = points.pop()
-                grid[x][y] = val
-                for (i, j) in directions: 
-                    r, s = x + i, y + j
-                    if 0 <= r < n and 0 <= s < m:                        
-                        if grid[r][s] == "*":
-                            points.append((r, s))
-                            
-        n, m = len(grid), len(grid[0])
-        grid = [["*" if grid[i][j] == "1" else "0" for j in range(m)] for i in range(n)]
-      
+        def helper(x, y):
+            stack = [(x, y)]
+            grid[x][y] = 0 
+            
+            while stack: 
+                (x, y) = stack.pop()
+                for (r, s) in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    p, q = x + r, y + s
+                    if 0 <= p < m and 0 <= q < n: 
+                        if grid[p][q] == "1": 
+                            grid[p][q] = "0" 
+                            stack.append((p, q))
+            
+        m, n = len(grid), len(grid[0])
+        
         ans = 0
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == "*":
-                    ans += 1        
-                    markIsland(grid, i, j, n, m, ans)
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    ans += 1
+                    helper(i, j)
                     
         return ans 
+                    
+        
+        
+        
+        
