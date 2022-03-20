3323adfaa75f61d55de40dce163ae32a5dba9e80
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-03-20 10:55:26 -0300

    Update 1007. [Medium] Minimum Domino Rotations For Equal Row.py

--- 1000 - 1999/1007. [Medium] Minimum Domino Rotations For Equal Row.py
+++ 1000 - 1999/1007. [Medium] Minimum Domino Rotations For Equal Row.py
@@ -2,23 +2,28 @@
 
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        seen = defaultdict(lambda: 0)
+        topCounter = defaultdict(lambda: 0)
+        bottomCounter = defaultdict(lambda: 0)
         n = len(tops)
-        ans = inf
-        for val in range(1, 7):
-            cntTops, cntBottoms = 0, 0
-            go = True
-            for i in range(n):
-                if val == tops[i] or val == bottoms[i]:
-                    cntTops += 1 if val == tops[i] else 0
-                    cntBottoms += 1 if val == bottoms[i] else 0
-                else: 
-                    go = False
-                    break 
-                    
-            if go: 
-                ans = min(ans, cntTops, n - cntTops, cntBottoms, n - cntBottoms)
+        
+        for i in range(n):
+            if tops[i] != bottoms[i]:                
+                seen[tops[i]] += 1 
+                seen[bottoms[i]] += 1 
+            else: 
+                seen[tops[i]] += 1 
                 
-        return ans if ans != inf else -1
+            topCounter[tops[i]] += 1 
+            bottomCounter[bottoms[i]] += 1
+            
+        ans = inf
+        for key in seen: 
+            if seen[key] == n: 
+                ans = min(ans, n - topCounter[key])
+                ans = min(ans, n - bottomCounter[key])
+                
+        return -1 if ans == inf else ans
                     
             
                     
