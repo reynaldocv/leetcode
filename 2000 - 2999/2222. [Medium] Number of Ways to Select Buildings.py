ad9c66bd1b1a1908ffc714218b75fca589181c77
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-02 20:12:26 -0300

    Update 2222. [Medium] Number of Ways to Select Buildings.py

--- 2000 - 2999/2222. [Medium] Number of Ways to Select Buildings.py
+++ 2000 - 2999/2222. [Medium] Number of Ways to Select Buildings.py
@@ -11,25 +11,22 @@ class Solution:
                 
             return ans
         
-        left = helper(s)
-        right = helper(s[::-1])[::-1]
+        def collaborator(s):        
+            left = helper(s)
+            right = helper(s[::-1])[::-1]
+
+            ans = 0 
         
-        ans = 0 
+            for i in range(1, n - 1):
+                if s[i] == "1":
+                    ans += left[i - 1]*right[i + 1]
+                    
+            return ans 
         
         n = len(s)
         
-        for i in range(1, n - 1):
-            if s[i] == "1":
-                ans += left[i - 1]*right[i + 1]
-
-        s = "".join([str(1 - int(char)) for char in s])
+        t = "".join([str(1 - int(char)) for char in s])
         
-        left = helper(s)
-        right = helper(s[::-1])[::-1]
-        
-        
-        for i in range(1, n - 1):
-            if s[i] == "1":
-                ans += left[i - 1]*right[i + 1]
+        ans = collaborator(s) + collaborator(t)
         
         return ans 
