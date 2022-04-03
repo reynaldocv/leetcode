6f30913fda3bfce8e417c65f6f1e00a3a3b3987f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-03 00:53:09 -0300

    Update 1801. [Medium] Number of Orders in the Backlog.py

--- 1000 - 1999/1801. [Medium] Number of Orders in the Backlog.py
+++ 1000 - 1999/1801. [Medium] Number of Orders in the Backlog.py
@@ -2,25 +2,26 @@
 
 class Solution:
     def getNumberOfBacklogOrders(self, orders: List[List[int]]) -> int:
+        MOD = 10**9 + 7
+        
         buy, sell = [], []
         
-        for (p, q, t) in orders: 
-            if t: 
-                heappush(sell, [p, q])
+        for (price, amount, orderType) in orders: 
+            if orderType == 0: 
+                heappush(buy, [-price, amount])
             else: 
-                heappush(buy, [-p, q])
-            
-            while buy and sell and -buy[0][0] >= sell[0][0]: 
-                qty = min(buy[0][1], sell[0][1])
-                buy[0][1] -= qty
-                sell[0][1] -= qty
+                heappush(sell, [price, amount])
+                
+            while buy and sell and -buy[0][0] >= sell[0][0]:
+                cnt = min(buy[0][1], sell[0][1])
+                buy[0][1] -= cnt
+                sell[0][1] -= cnt
                 
                 if buy[0][1] == 0: 
                     heappop(buy)
+                    
                 if sell[0][1] == 0: 
                     heappop(sell)
-                    
-        MOD = 10**9 + 7
         
         return (sum(q for _, q in sell) + sum(q for _, q in buy)) % MOD
-    
+        
