b8c8691944adf53967189c87b1ae5f30b635188d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-02 22:27:46 -0300

    Update 1385. [Easy] Find the Distance Value Between Two Arrays.py

--- 1000 - 1999/1385. [Easy] Find the Distance Value Between Two Arrays.py
+++ 1000 - 1999/1385. [Easy] Find the Distance Value Between Two Arrays.py
@@ -2,14 +2,34 @@
 
 class Solution:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
-        ans = 0
-        for numA in arr1:
-            success = True
-            for numB in arr2:
-                if abs(numA - numB) <= d:
-                    success = False
-                    break
-            if success: 
-                ans += 1
-        return ans
+        def helper(target):
+            if target < arr2[0]:
+                return 0 
+            
+            if arr2[-1] < target: 
+                return n 
+            
+            start = -1
+            end = n - 1
+            
+            while end - start > 1: 
+                mid = (end + start)//2
+                if arr2[mid] < target: 
+                    start = mid
+                else: 
+                    end = mid
+                    
+            return end
+                    
+        n = len(arr2)
+        
+        arr2.sort()
+        
+        ans = 0 
+        
+        for num in arr1:         
+            if helper(num + d + 1) == helper(num - d):                
+                ans += 1 
+            
+        return ans 
         
