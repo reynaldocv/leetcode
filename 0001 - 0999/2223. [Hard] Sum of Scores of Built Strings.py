d0d529d1074f3eea71334de187f0f664b4289758
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-02 23:25:35 -0300

    Create 2223. [Hard] Sum of Scores of Built Strings.py

---
+++ 0001 - 0999/2223. [Hard] Sum of Scores of Built Strings.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/sum-of-scores-of-built-strings/
+
+class Solution:
+    def sumScores(self, s: str) -> int:
+        def helper(s):
+            dp = [0 for _ in range(n)]
+            left, right = 0, 0
+            
+            for i in range (1, n):
+                if i > right:
+                    left = right = i                    
+                    while right < n and s[right - left] == s[right]:
+                        right += 1
+                    right -= 1
+                    
+                    dp[i] = right - left + 1
+                else:
+                    val = i - left                    
+                    if dp[val] + i <= right:
+                        dp[i] = dp[val]
+                        
+                    else:
+                        left = i                        
+                        while right < n and s[right - left] == s[right]:
+                            right += 1
+                        right -= 1
+                        
+                        dp[i] = right - left + 1
+                        
+            return sum(dp)
+        
+        n = len(s)
+        
+        return helper(s) + n
