c106c1ab390131cb9a938f1a32cdb47127b78a54
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-03 21:17:53 -0300

    Update 0048. [Medium] Rotate Image.py

--- 0001 - 0999/0048. [Medium] Rotate Image.py
+++ 0001 - 0999/0048. [Medium] Rotate Image.py
@@ -5,14 +5,16 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
         n = len(matrix)
+        
         for i in range(n):
             for j in range(i + 1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
+                
         for i in range(n):
-            for j in range(n//2):          
-                matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n - 1 - j] = matrix[i][n - 1 - j], matrix[i][j]
+                
+        
             
         
