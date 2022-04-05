8067f8abdfa2685f95448488025c760b37e5f357
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-05 11:48:16 -0300

    Update 0054. [Medium] Spiral Matrix.py

--- 0001 - 0999/0054. [Medium] Spiral Matrix.py
+++ 0001 - 0999/0054. [Medium] Spiral Matrix.py
@@ -2,24 +2,39 @@
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        direction = [(0, 1), (1, 0), (0, -1), (-1, 0)]        
-        n = len(matrix)
-        m = len(matrix[0])        
-        visited = [[False for j in range(m)] for i in range(n)]
+        m, n = len(matrix), len(matrix[0])
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
         x, y = 0, 0 
-        ans = []
-        k = 0
-        while len(ans) < n*m:             
-            if (x < n and y < m) and visited[x][y] == False:
-                ans.append(matrix[x][y])
-                visited[x][y] = True
-            else: 
-                x -= direction[k][0]
-                y -= direction[k][1]
+        k = 0 
+        
+        seen = {(0, 0): True, (0, n): True, (m, n - 1): True, (m - 1, -1): True}
+        ans = [matrix[0][0]]
+        
+        i = 1 
+        
+        while i < m*n:
+            x += directions[k][0]
+            y += directions[k][1]
+            
+            if (x, y) in seen: 
+                x -= directions[k][0]
+                y -= directions[k][1]
                 k = (k + 1) % 4
-            x += direction[k][0]
-            y += direction[k][1]
+                
+            else: 
+                ans.append(matrix[x][y])
+                seen[(x, y)] = True
+                i += 1 
+                
         return ans
+                
+            
+                
+                
+                
+            
+        
                     
                                        
         
