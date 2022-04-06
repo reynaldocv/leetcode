59eee244eb3df04039dc9309f79b7bf043c4f2d4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-06 15:10:05 -0300

    Update 0441. [Easy] Arranging Coins.py

--- 0001 - 0999/0441. [Easy] Arranging Coins.py
+++ 0001 - 0999/0441. [Easy] Arranging Coins.py
@@ -2,9 +2,29 @@
 
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        if n == 1: return 1
+        if n == 1: 
+            return 1
+        
         m = int((2*n)**.5)        
+        
         while m*(m + 1)//2 <= n:
             m += 1
+            
         return m - 1
         
+class Solution2:
+    def arrangeCoins(self, n: int) -> int:
+        def helper(val):
+            return val*(val + 1)//2 <= n
+        
+        start = 0 
+        end = n + 1
+        
+        while end - start > 1:
+            mid = (end + start)//2
+            if helper(mid):
+                start = mid
+            else: 
+                end = mid
+                
+        return start 
