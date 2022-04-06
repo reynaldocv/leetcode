61e3496049f58a15626655cc39226886cd0b4a17
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-05 22:11:10 -0300

    Update 0923. [Medium] 3Sum With Multiplicity.py

--- 0001 - 0999/0923. [Medium] 3Sum With Multiplicity.py
+++ 0001 - 0999/0923. [Medium] 3Sum With Multiplicity.py
@@ -2,34 +2,36 @@
 
 class Solution:
     def threeSumMulti(self, arr: List[int], target: int) -> int:
-        ans = 0 
         MOD = 10**9 + 7
+        ans = 0 
         n = len(arr)
-        arr.sort()
-        for (i, numA) in enumerate(arr):
-            T = target - numA
-            j = i + 1
-            k = n - 1
-            while k - j > 0: 
-                if arr[j] + arr[k] < T: 
-                    j += 1
-                elif arr[j] + arr[k] > T:
-                    k -= 1
-                elif arr[j] != arr[k]:
-                    countLeft = countRight = 1
-                    while j + 1 < k and arr[j] == arr[j + 1]:
-                        countLeft, j = countLeft + 1, j + 1
-                    while j < k - 1 and arr[k - 1] == arr[k]:
-                        countRight, k = countRight + 1, k - 1   
-                    ans += countLeft*countRight
-                    ans %= MOD
-                    j += 1
-                    k -= 1
-                else: 
-                    ans += (k - j + 1)*(k - j)//2
-                    ans %= MOD
-                    break
         
-        return ans % MOD
+        arr.sort()
+        
+        for (i, num) in enumerate(arr):
+            aSum = target - num
+            start = i + 1
+            end = n - 1
+            while end > start:
+                if arr[start] + arr[end] < aSum: 
+                    start += 1 
+                elif arr[start] + arr[end] > aSum:
+                    end -= 1 
+                elif arr[start] != arr[end]:
+                    left = 0
+                    while start < end and arr[start] == arr[start + left]:
+                        left += 1 
                     
-      
+                    right = 0 
+                    while start < end and arr[end] == arr[end - right]:
+                        right += 1 
+                    
+                    ans = (ans + left*right) % MOD
+                    start += left
+                    end -= right
+                
+                else: 
+                    ans = (ans + (end - start + 1)*(end - start)//2)
+                    break 
+            
+        return ans % MOD
