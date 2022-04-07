21e2c3d432ea3eb632c0104ab1dfb97d7bb88d6d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-07 10:36:50 -0300

    Update 1046. [Easy] Last Stone Weight.py

--- 1000 - 1999/1046. [Easy] Last Stone Weight.py
+++ 1000 - 1999/1046. [Easy] Last Stone Weight.py
@@ -2,30 +2,16 @@
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        def indexToInsert(array, val):
-            i = 0
-            j = len(array)
-            while j - i > 1:
-                m = (j + i)//2
-                if array[m] > val:
-                    i = m
-                else:
-                    j = m
-            return j
-         
-        stones.sort(reverse = True)
-        print(stones)
-        while len(stones) >= 2: 
-            stoneA = stones[0]
-            stoneB = stones[1]
-            del stones[0]
-            del stones[0]
-            stoneC = abs(stoneA - stoneB)
-            if stoneC != 0:
-                index = indexToInsert(stones, stoneC)
-                stones.insert(index, stoneC)
-            print(stones)
-        return 0 if (len(stones) == 0) else stones[0]
+        heap = []
+        for stone in stones: 
+            heappush(heap, -stone)
+            
+        while len(heap) >= 2: 
+            stone1 = -heappop(heap)
+            stone2 = -heappop(heap)
+            if stone1 != stone2: 
+                stone3 = abs(stone1 - stone2)
+                heappush(heap, -stone3)
+                
+        return -heappop(heap) if heap else 0 
         
-            
-            
