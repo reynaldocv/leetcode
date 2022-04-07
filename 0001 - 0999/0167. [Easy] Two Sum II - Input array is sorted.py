036def84acb42a43a6eae84ea5e2dc0a07fed6df
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-07 11:17:01 -0300

    Update 0167. [Easy] Two Sum II - Input array is sorted.py

--- 0001 - 0999/0167. [Easy] Two Sum II - Input array is sorted.py
+++ 0001 - 0999/0167. [Easy] Two Sum II - Input array is sorted.py
@@ -2,12 +2,27 @@
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        dic = {}
         n = len(numbers)
-        for i in range(n):
+        start = 0 
+        end = n - 1
+    
+        while end - start > 0: 
+            if numbers[start] + numbers[end] == target: 
+                return [start + 1, end + 1]
+            elif numbers[start] + numbers[end] < target:
+                start += 1 
+            else: 
+                end -= 1 
+                
+class Solution2:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        seen = {}
+        n = len(numbers)
+        for i in range(n):            
             B = target - numbers[i]
-            if B in dic: 
-                return [dic[B], i + 1]
-            dic[numbers[i]] = i + 1
+            if B in seen: 
+                return [seen[B], i + 1]
+            
+            seen[numbers[i]] = i + 1
         
         
