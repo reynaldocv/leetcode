20325c498bd14171e94bb991ce6c461f43b03a01
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-07 11:41:12 -0300

    Update 1608. [Easy] Special Array With X Elements Greater Than or Equal X.py

--- 1000 - 1999/1608. [Easy] Special Array With X Elements Greater Than or Equal X.py
+++ 1000 - 1999/1608. [Easy] Special Array With X Elements Greater Than or Equal X.py
@@ -2,14 +2,26 @@
 
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
-        nums.sort()
-        aux, n = -inf, len(nums)
-        for i in range(n):
-            if aux != nums[i]:
-                x = n - i                
-                if x == nums[i] or (aux < x <= nums[i] and x not in nums):
-                    return x
-                aux = nums[i]
+        def helper(value):
+            ans = 0 
+            for num in nums: 
+                if num >= value: 
+                    ans += 1 
+                    
+            return ans 
+        
+        start = 0 
+        end = max(nums) + 1
+        
+        while end - start > 1: 
+            mid = (end + start)//2 
+            if helper(mid) == mid: 
+                return mid
+            elif helper(mid) < mid: 
+                end = mid
+            else: 
+                start = mid 
+                
         return -1
                     
                     
