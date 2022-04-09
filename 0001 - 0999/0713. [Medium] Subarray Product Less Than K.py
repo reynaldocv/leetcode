7082f998964a87dfc4906f0b0d07f26c5e839a64
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-08 22:57:23 -0300

    Update 0713. [Medium] Subarray Product Less Than K.py

--- 0001 - 0999/0713. [Medium] Subarray Product Less Than K.py
+++ 0001 - 0999/0713. [Medium] Subarray Product Less Than K.py
@@ -1,22 +1,26 @@
 # https://leetcode.com/problems/subarray-product-less-than-k/
 
 class Solution:
-    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:        
-        n = len(nums)
-        left = 0 
-        mult = 1
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1: 
+            return 0 
+        
+        prefix = 1
+        cnt = 0
         ans = 0
         
-        if k <= 0: 
-            return 0
+        idx = 0 
         
-        for i in range(n):
-            mult *= nums[i]
-            while mult >= k and left < i: 
-                mult //= nums[left]
-                left += 1       
-            if mult < k:
-                ans += i - left + 1
+        for (i, num) in enumerate(nums): 
+            prefix *= num
+            cnt += 1 
+                
+            while prefix >= k: 
+                prefix //= nums[idx]
+                idx += 1 
+                cnt -= 1 
+                
+            ans += cnt
         
-        return ans
+        return ans 
 
