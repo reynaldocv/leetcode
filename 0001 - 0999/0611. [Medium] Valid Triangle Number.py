5632f739b1c9286ef1acad1d328cd1fbf28f9e3d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-11 22:07:37 -0300

    Update 0611. [Medium] Valid Triangle Number.py

--- 0001 - 0999/0611. [Medium] Valid Triangle Number.py
+++ 0001 - 0999/0611. [Medium] Valid Triangle Number.py
@@ -3,15 +3,22 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        nums.sort()
-        ans = 0
+        
+        nums.sort()        
+        ans = 0 
         
         for i in range(n - 2):
-            if nums[i] != 0: 
-                k = i + 2
-                for j in range(i + 1, n - 1):
-                    while k < n and nums[i] + nums[j] > nums[k]:
-                        k += 1
-                    ans += k - j - 1
-
-        return ans
+            for j in range(i + 1, n - 1):
+                minElem = nums[j] - nums[i]
+                maxElem = nums[j] + nums[i]
+                
+                end = bisect_left(nums, maxElem)                
+                if end <= j: 
+                    break 
+                
+                start = max(j + 1, bisect_left(nums, minElem + 1))
+                
+                ans += end - start 
+        
+        return ans 
+    
