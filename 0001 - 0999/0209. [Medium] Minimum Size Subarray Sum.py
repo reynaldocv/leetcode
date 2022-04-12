821428af1f707eaa825c5c9da14f9d6dc2590f8d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-11 22:21:27 -0300

    Update 0209. [Medium] Minimum Size Subarray Sum.py

--- 0001 - 0999/0209. [Medium] Minimum Size Subarray Sum.py
+++ 0001 - 0999/0209. [Medium] Minimum Size Subarray Sum.py
@@ -15,6 +15,25 @@ class Solution:
                 
         return ans if ans != inf else 0 
         
+class Solution2:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        prefix = [0]
+        for num in nums: 
+            prefix.append(prefix[-1] + num)
+            
+        m = len(prefix)
+        
+        ans = inf
+        
+        for (i, num) in enumerate(prefix): 
+            val = num + target
+            idx = bisect_left(prefix, val)
+            
+            if idx < m: 
+                ans = min(ans, idx - i)
+                
+        return 0 if ans == inf else ans 
+            
         
                 
             
