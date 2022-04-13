5f8096f2f1ed82f39acccfd2e7f090a0c886f59d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-12 21:43:13 -0300

    Update 1894. [Medium] Find the Student that Will Replace the Chalk.py

--- 1000 - 1999/1894. [Medium] Find the Student that Will Replace the Chalk.py
+++ 1000 - 1999/1894. [Medium] Find the Student that Will Replace the Chalk.py
@@ -11,5 +11,29 @@ class Solution:
             else: 
                 k -= val
                 
+class Solution2:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        prefix = [0]
+        
+        for num in chalk: 
+            prefix.append(prefix[-1] + num)
+        
+        k = k % prefix[-1]
+        
+        start = 0       
+        end = len(prefix) - 1
+        
+        while end - start > 1: 
+            mid = (end + start)//2
+            if prefix[mid] < k: 
+                start = mid
+            else: 
+                end = mid
+                
+        if prefix[end] == k: 
+            end += 1 
+       
+        return end - 1
+                 
         
         
