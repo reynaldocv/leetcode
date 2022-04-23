804f14f7e5d274ae2acdd95c039a449f53e3ab48
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-23 11:41:13 -0300

    Update 1137. [Easy] N-th Tribonacci Number.py

--- 1000 - 1999/1137. [Easy] N-th Tribonacci Number.py
+++ 1000 - 1999/1137. [Easy] N-th Tribonacci Number.py
@@ -10,5 +10,18 @@ class Solution:
             fib3[i] = fib3[i - 1] + fib3[i - 2] + fib3[i - 3]
         
         return fib3[n]
+    
+class Solution2:
+    def tribonacci(self, n: int) -> int:
+        @cache
+        def helper(i):
+            if i <= 1: 
+                return i 
+            elif i == 2: 
+                return 1 
+            else: 
+                return helper(i - 1) + helper(i - 2) + helper(i - 3)
+            
+        return helper(n)
         
         
