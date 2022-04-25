3e781a8cbdfb668cfd890a8231791236633ec5d4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-04-25 00:59:13 -0300

    Update 1574. [Medium] Shortest Subarray to be Removed to Make Array Sorted.py

--- 1000 - 1999/1574. [Medium] Shortest Subarray to be Removed to Make Array Sorted.py
+++ 1000 - 1999/1574. [Medium] Shortest Subarray to be Removed to Make Array Sorted.py
@@ -28,3 +28,41 @@ class Solution:
                 break
                 
         return ans
+
+class Solution2:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        n = len(arr)
+        
+        left = []        
+        prev = -inf
+        
+        for num in arr: 
+            if prev <= num: 
+                left.append(num)
+                prev = num
+            else: 
+                break
+                
+        right = []        
+        prev = inf
+        
+        for num in arr[::-1]:
+            if prev >= num: 
+                right.insert(0, num)
+                prev = num
+            else: 
+                break
+                
+        ans = n - max(len(left), len(right))
+        
+        if ans == 0: 
+            return ans 
+        
+        for (i, num) in enumerate(left): 
+            idx = bisect_left(right, num)
+            if idx == len(right):
+                ans = min(ans, n - i + 1)
+            else: 
+                ans = min(ans, n - (len(right) - idx + 1) - i)
+
+        return ans 
