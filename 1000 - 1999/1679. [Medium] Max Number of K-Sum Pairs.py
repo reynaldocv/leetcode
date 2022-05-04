f0565f937810a3acb7124242cb64b477feb74345
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-05-04 14:39:38 -0300

    Update 1679. [Medium] Max Number of K-Sum Pairs.py

--- 1000 - 1999/1679. [Medium] Max Number of K-Sum Pairs.py
+++ 1000 - 1999/1679. [Medium] Max Number of K-Sum Pairs.py
@@ -2,29 +2,27 @@
 
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        counter = defaultdict(lambda: 0)
-        nums.sort()
+        n = len(nums) 
+        
+        nums.sort() 
+        
         ans = 0 
-        for num in nums: 
-            if num < k: 
-                if num*2 == k: 
-                    counter[num] += 1
-                elif k - num in counter: 
-                    ans += 1
-                    counter[k - num] -= 1
-                    if counter[k - num] == 0: 
-                        counter.pop(k - num)
-                else: 
-                    counter[num] += 1
+        
+        start = 0 
+        end = n - 1
+        
+        while end > start: 
+            if nums[start] + nums[end] == k:
+                ans += 1 
+                start += 1 
+                end -= 1 
+                
+            elif nums[start] + nums[end] < k:
+                start += 1 
+            
             else: 
-                break
+                end -= 1 
                 
-        if k % 2 == 0: 
-            ans += counter[k//2]//2
-            
-        return ans
-            
-                
-                
+        return ans 
     
             
