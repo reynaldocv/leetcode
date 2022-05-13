cc18744638bcf382e85b0845bb8ab075149dc855
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-05-13 13:18:31 -0300

    Update 2267. [Hard] Check if There Is a Valid Parentheses String Path.py

--- 2000 - 2999/2267. [Hard] Check if There Is a Valid Parentheses String Path.py
+++ 2000 - 2999/2267. [Hard] Check if There Is a Valid Parentheses String Path.py
@@ -3,25 +3,27 @@
 class Solution:
     def hasValidPath(self, grid: List[List[str]]) -> bool:
         @cache
-        def helper(i, j, cnt): 
-            if i == m - 1 and j == n - 1: 
+        def helper(x, y, cnt):
+            if x == m - 1 and y == n - 1:
                 return cnt == 0 
+            else: 
+                for (r, s) in [(x + 1, y), (x, y + 1)]:
+                    if 0 <= r < m and 0 <= s < n: 
+                        cnt2 = cnt                        
+                        if grid[r][s] == "(":
+                            cnt2 += 1 
+                        else:
+                            cnt2 -= 1 
+                            
+                        if cnt2 >= 0 and helper(r, s, cnt2):
+                            return True 
+                        
+                return False 
             
-            for (r, s) in [(i + 1, j), (i, j + 1)]: 
-                if 0 <= r < m and 0 <= s < n: 
-                    if grid[r][s] == '(': 
-                        tmp = cnt + 1
-                    else: 
-                        tmp = cnt - 1
-                    
-                    if tmp >= 0 and helper(r, s, tmp): 
-                        return True 
-                    
-            return False 
-        
         m, n = len(grid), len(grid[0])
-        
-        if grid[0][0] == "(":            
+            
+        if grid[0][0] == "(":
             return helper(0, 0, 1)
         else: 
-            return False
+            return False 
+            
