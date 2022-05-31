b896ee144d628469b1b74414a53fb0f7d6e9b1b8
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-05-30 21:12:51 -0300

    Create 2290. [Hard] Minimum Obstacle Removal to Reach Corner.py

---
+++ 2000 - 2999/2290. [Hard] Minimum Obstacle Removal to Reach Corner.py
@@ -0,0 +1,58 @@
+# https://leetcode.com/problems/minimum-obstacle-removal-to-reach-corner/
+
+class Solution:
+    def minimumObstacles(self, grid: List[List[int]]) -> int:
+        def helper(x, y):
+            ans = [(x, y)]
+            
+            stack = [(x, y)]
+            
+            while stack: 
+                (x, y) = stack.pop(0)
+                for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    p, q = x + r, y + s
+                    if 0 <= p < m and 0 <= q < n: 
+                        if grid[p][q] == 0 and (p, q) not in seen:
+                            stack.append((p, q))                            
+                            ans.append((p, q))
+                            seen.add((p, q))
+                       
+            return ans
+        
+        m, n = len(grid), len(grid[0])
+                
+        seen = {(0, 0)}            
+        stack = [(0, 0)]
+        
+        ans = 0 
+        
+        while stack: 
+            newStack = []
+            for (x, y) in stack: 
+                if x == m - 1 and y == n - 1: 
+                    return ans - 1
+                for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    p, q = x + r, y + s
+                    if 0 <= p < m and 0 <= q < n: 
+                        if grid[p][q] == 0 and (p, q) not in seen: 
+                            seen.add((p, q))                            
+                            newStack += helper(p, q)                            
+                            
+            stack = stack + newStack
+            newStack = []
+            
+            for (x, y) in stack: 
+                if x == m - 1 and y == n - 1: 
+                    return ans
+                
+                for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    p, q = x + r, y + s
+                    if 0 <= p < m and 0 <= q < n: 
+                        if (p, q) not in seen:                             
+                            newStack.append((p, q))
+                            seen.add((p, q))
+            stack = newStack
+            ans += 1 
+        
+            
+            
