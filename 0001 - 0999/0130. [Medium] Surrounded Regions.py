8f1dcd9a1775d6531645ef8bcb4c605a9a079d86
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-06-02 11:08:25 -0300

    Update 0130. [Medium] Surrounded Regions.py

--- 0001 - 0999/0130. [Medium] Surrounded Regions.py
+++ 0001 - 0999/0130. [Medium] Surrounded Regions.py
@@ -1,43 +1,36 @@
-# https://leetcode.com/problems/surrounded-regions/
-
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        def helper(x, y):
-            allPoints = {(x, y): True}
-            points = [(x, y)]
-            limited = True 
-            while points:
-                (x, y) = points.pop()
-                if x == 0 or x == n - 1 or y == 0 or y == m - 1:
-                    limited = False
-                for (i, j) in directions: 
-                    r, s = x + i, y + j
-                    if 0 <= r < n and 0 <= s < m:
-                         if board[r][s] == "O" and (r, s) not in allPoints:
-                            points.append((r, s))
-                            allPoints[(r, s)] = True
-                           
-            if limited: 
-                for (x, y) in allPoints: 
-                    board[x][y] = "X"
-            else:
-                for (x, y) in allPoints: 
-                    board[x][y] = "Y"
+        m, n = len(board), len(board[0])
         
-        n = len(board)
-        m = len(board[0])
+        seen = set()
         
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        
-        for i in range(1, n - 1):
-            for j in range(1, m - 1):
-                if board[i][j] == "O":
-                    helper(i, j)
+        for j in range(n):
+            if board[0][j] == "O":
+                seen.add((0, j))
+            if board[m - 1][j] == "O":
+                seen.add((m - 1, j))
+                
+        for i in range(m):
+            if board[i][0] == "O":
+                seen.add((i, 0))
+            if board[i][n - 1] == "O":
+                seen.add((i, n - 1))
+                
+        stack = [(x, y) for (x, y) in seen]
                     
-        for i in range(n):
-            for j in range(m):
-                board[i][j] = "O" if board[i][j] == "Y" else board[i][j]                    
-        
+        while stack: 
+            (x, y) = stack.pop()
+            for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                p, q = x + r, y + s 
+                if 0 <= p < m and 0 <= q < n: 
+                    if (p, q) not in seen and board[p][q] == "O": 
+                        stack.append((p, q))
+                        seen.add((p, q))
+                        
+        for i in range(m):
+            for j in range(n):
+                if (i, j) not in seen: 
+                    board[i][j] = "X"
