68c7c7bbfd67c79139464594bcaa1db1abbc82a1
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-06-20 16:28:43 -0300

    Create 2309. [Easy] Greatest English Letter in Upper and Lower Case.py

---
+++ 2000 - 2999/2309. [Easy] Greatest English Letter in Upper and Lower Case.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/greatest-english-letter-in-upper-and-lower-case/
+
+class Solution:
+    def greatestLetter(self, s: str) -> str:
+        seen = set()
+        for char in s: 
+            seen.add(char)
+            
+        ans = ""    
+        
+        for i in range(26):
+            lower = chr(ord("a") + i)
+            upper = chr(ord("A") + i)
+            
+            if lower in seen and upper in seen: 
+                ans = max(ans, upper)
+                
+        return ans
