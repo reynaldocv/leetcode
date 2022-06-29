1ced28badc2e82486fb4a1359401562c06fefe10
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-06-29 18:11:10 -0300

    Create 2317. [Medium] Maximum XOR After Operations.py

---
+++ 2000 - 2999/2317. [Medium] Maximum XOR After Operations.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/maximum-xor-after-operations/
+
+class Solution:
+    def maximumXOR(self, nums: List[int]) -> int:
+        ans = 0 
+        
+        for num in nums: 
+            ans = ans | num 
+            
+        return ans 
