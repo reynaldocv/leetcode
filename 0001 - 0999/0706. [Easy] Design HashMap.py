854558db74f142e5f13eb72231027a56a4cf54bf
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-07-01 17:05:03 -0300

    Update 0706. [Easy] Design HashMap.py

--- 0001 - 0999/0706. [Easy] Design HashMap.py
+++ 0001 - 0999/0706. [Easy] Design HashMap.py
@@ -3,51 +3,17 @@
 class MyHashMap:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.array = []
+        self.data = [-1]*1000001
         
-
     def put(self, key: int, value: int) -> None:
-        """
-        value will always be non-negative.
-        """
-        for i in range(len(self.array)):
-            if self.array[i][0] == key:
-                del self.array[i]                
-                break
-        self.array.append((key, value))
-   
+        self.data[key] = value
         
-        
-
     def get(self, key: int) -> int:
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        """
-        ans = -1
-        for (x, y) in self.array:
-            if x == key:
-                ans = y
-                break
-        return ans 
-        
-
+        return self.data[key]
+    
     def remove(self, key: int) -> None:
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        """
-        ans = -1
-        for i in range(len(self.array)):
-            if self.array[i][0] == key:
-                ans = i
-                break
-        if ans != -1:
-            del self.array[ans]
-   
+        self.data[key] = -1
         
-
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
