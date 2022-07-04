88eb0c63c288230f18eb6c45a160cd3ad75cdbac
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-07-04 11:09:15 -0300

    Update 0560. [Medium] Subarray Sum Equals K.py

--- 0001 - 0999/0560. [Medium] Subarray Sum Equals K.py
+++ 0001 - 0999/0560. [Medium] Subarray Sum Equals K.py
@@ -1,21 +1,22 @@
 # https://leetcode.com/problems/subarray-sum-equals-k/
 
 class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:        
-        n = len(nums)        
-        counter = defaultdict(lambda: 0)
-        ans = 0 
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        seen = defaultdict(lambda: 0)
+        seen[0] += 1 
+        
         aSum = 0 
-        for i in range(n):
-            aSum += nums[i]
-            if aSum == k: 
-                ans += 1
-            if aSum - k in counter: 
-                ans += counter[aSum - k]
-            counter[aSum] += 1
+        
+        ans = 0 
+        
+        for num in nums: 
+            aSum += num 
+            if aSum - k in seen: 
+                ans += seen[aSum - k] 
+                
+            seen[aSum] += 1 
         
         return ans
-        
         
         
         
