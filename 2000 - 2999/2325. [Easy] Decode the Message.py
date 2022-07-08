cb1ef0d0e1ab53bedada6a884f757a84c828e753
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-07-08 15:53:27 -0300

    Create 2325. [Easy] Decode the Message.py

---
+++ 2000 - 2999/2325. [Easy] Decode the Message.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/decode-the-message/
+
+class Solution:
+    def decodeMessage(self, key: str, message: str) -> str:
+        letter = {" ": " "}
+        
+        i = 0 
+        for char in key: 
+            if char not in letter: 
+                letter[char] = chr(ord("a") + i)
+                i += 1 
+        
+        ans = ""
+        for char in message: 
+            ans += letter[char]
+            
+        return ans 
