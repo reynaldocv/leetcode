60c471f3150f7a1867aafb8341dbaad8b90023ff
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-08-08 14:57:07 -0300

    Create 2367. [Easy] Number of Arithmetic Triplets.py

---
+++ 2000 - 2999/2367. [Easy] Number of Arithmetic Triplets.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/number-of-arithmetic-triplets/
+
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        seen = set()
+        ans = 0 
+        
+        for num in nums: 
+            seen.add(num)
+            
+        for num in nums: 
+            if num + diff in seen and num + 2*diff in seen: 
+                ans += 1 
+        
+        return ans 
