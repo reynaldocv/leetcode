492c60f5603da7801feeec57a38b4445163a8adb
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-08-15 10:35:44 -0300

    Create 2347. [Easy] Best Poker Hand.py

---
+++ 2000 - 2999/2347. [Easy] Best Poker Hand.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/best-poker-hand/
+
+class Solution:
+    def bestHand(self, ranks: List[int], suits: List[str]) -> str:
+        suits = {suit for suit in suits}
+        
+        maxElem = 0
+        counter = defaultdict(lambda: 0)
+        
+        for rank in ranks: 
+            counter[rank] += 1 
+            maxElem = max(maxElem, counter[rank])
+        
+        if len(set(suits)) == 1:
+            return "Flush"
+        
+        if maxElem > 2:
+            return "Three of a Kind"
+        
+        if maxElem > 1:
+            return "Pair"
+        
+        return "High Card"
