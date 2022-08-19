dbe9cd31912191a6ac4e4fcff1195d81339c1882
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-08-19 15:44:33 -0300

    Update 1539. [Easy] Kth Missing Positive Number.py

--- 1000 - 1999/1539. [Easy] Kth Missing Positive Number.py
+++ 1000 - 1999/1539. [Easy] Kth Missing Positive Number.py
@@ -25,21 +25,3 @@ class Solution:
                 end = mid 
                 
         return end             
-
-class Solution2:
-    def findKthPositive(self, arr: List[int], k: int) -> int:
-        n = len(arr)
-        if k <= arr[n - 1] - n:
-            dic = {}
-            for i in arr: 
-                dic[i] = True
-            idx = 0
-            for i in range(1, arr[n - 1]):
-                if i not in dic:  
-                    idx += 1
-                if idx == k:
-                    return i
-        else:
-            aux = arr[n - 1] - n
-            k -= aux
-            return arr[n - 1] + k
