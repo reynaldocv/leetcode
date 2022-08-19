76574a8560f664e448da8623a62a9587f5e86b05
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-08-19 15:27:46 -0300

    Update 0744. [Easy] Find Smallest Letter Greater Than Target.py

--- 0001 - 0999/0744. [Easy] Find Smallest Letter Greater Than Target.py
+++ 0001 - 0999/0744. [Easy] Find Smallest Letter Greater Than Target.py
@@ -2,11 +2,20 @@
 
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        aux = "a"
         n = len(letters)
-        for i in range(n):
-            if i - 1 > 0:
-                aux = letters[i - 1]
-            if aux <= target < letters[i]:
-                return letters[i]
-        return letters[0]
+        
+        start = -1
+        end = n 
+        
+        while end - start > 1: 
+            mid = (end + start)//2
+            
+            if letters[mid] <= target:
+                start = mid 
+            else:
+                end = mid 
+                
+        return letters[0] if end == n else letters[end]
+                
+        
+        
