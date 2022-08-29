8dd154d35d58e4e446fa43f722373bef66c45d91
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-08-29 15:21:48 -0300

    Create 2390. [Medium] Removing Stars From a String.py

---
+++ 2000 - 2999/2390. [Medium] Removing Stars From a String.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/removing-stars-from-a-string/
+
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+        
+        for char in s: 
+            if char == "*":
+                if stack: 
+                    stack.pop() 
+            else: 
+                stack.append(char)
+                
+        return "".join(stack)
+    
+    
+        
