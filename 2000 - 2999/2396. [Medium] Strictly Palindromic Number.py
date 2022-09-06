9e7a26717f0e194ef11db0545464934d04ba4c92
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-06 09:24:39 -0300

    Create 2396. [Medium] Strictly Palindromic Number.py

---
+++ 2000 - 2999/2396. [Medium] Strictly Palindromic Number.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/strictly-palindromic-number/
+
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        def helper(num, base):
+            ans = ""
+            
+            while num > 0: 
+                ans += str(num % base)
+                num //= base 
+                
+            return ans == ans[::-1]
+        
+        for base in range(2, n - 1):
+            if not helper(n, base):
+                return False 
+            
+        return True 
