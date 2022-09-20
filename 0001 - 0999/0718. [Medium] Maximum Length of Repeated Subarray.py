d8caf44713e667217a3107c901e471fafe714654
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-20 11:26:31 -0300

    Update 0718. [Medium] Maximum Length of Repeated Subarray.py

--- 0001 - 0999/0718. [Medium] Maximum Length of Repeated Subarray.py
+++ 0001 - 0999/0718. [Medium] Maximum Length of Repeated Subarray.py
@@ -1,6 +1,28 @@
 # https://leetcode.com/problems/maximum-length-of-repeated-subarray/
 
+
 class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:        
+        m, n = len(nums1), len(nums2)
+            
+        index = defaultdict(lambda: [])
+        
+        dp = [[0 for i in range(n + 1)] for _ in range(m + 1)]
+        
+        for (i, num) in enumerate(nums1):
+            index[num].append(i)
+            
+        ans = 0 
+        
+        for (j, num) in enumerate(nums2):
+            for i in index[num]:
+                dp[i + 1][j + 1] = 1 + dp[i][j]
+            
+                ans = max(ans, dp[i + 1][j + 1])
+                
+        return ans 
+    
+class Solution2:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
         m, n = len(nums1), len(nums2)
         dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
