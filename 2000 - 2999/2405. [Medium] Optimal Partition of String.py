aa932386087d46989a044cfe551df1b885838aa8
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-20 13:17:45 -0300

    Create 2405. [Medium] Optimal Partition of String.py

---
+++ 2000 - 2999/2405. [Medium] Optimal Partition of String.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/optimal-partition-of-string/
+
+class Solution:
+    def partitionString(self, s: str) -> int:
+        seen = set()
+        ans = 1
+        
+        for char in s:
+            if char in seen:
+                ans += 1
+                seen = {char}
+            else:
+                seen.add(char)
+                
+        return ans
+        
