4bebd336bfa2bdafcf33b121c7dbc75ed1921dc1
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-21 17:55:04 -0300

    Update 0066. [Easy] Plus One.py

--- 0001 - 0999/0066. [Easy] Plus One.py
+++ 0001 - 0999/0066. [Easy] Plus One.py
@@ -2,16 +2,17 @@
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        aux, n = 1, len(digits)
-        for i in range(n -1, -1, -1):
-            dig = digits[i] + aux
-            if dig >= 10:
-                digits[i] = 0
-                aux = 1
-            else: 
-                digits[i] = dig
-                aux = 0
-        if aux == 1: 
+        n = len(digits)
+        
+        tmp = 1
+        
+        for i in range(n - 1, -1, -1):
+            aSum = digits[i] + tmp            
+            digits[i] = (aSum % 10)            
+            tmp = aSum // 10 
+            
+        if tmp: 
             digits.insert(0, 1)
+
         return digits
         
