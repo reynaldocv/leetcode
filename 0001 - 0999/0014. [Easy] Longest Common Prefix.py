82509f3330b8b6f31ead828088e082cd46ddd9c5
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-21 17:17:25 -0300

    Update 0014. [Easy] Longest Common Prefix.py

--- 0001 - 0999/0014. [Easy] Longest Common Prefix.py
+++ 0001 - 0999/0014. [Easy] Longest Common Prefix.py
@@ -2,22 +2,21 @@
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        ans = ""
-        minstr = strs[0]
-        for a in strs: 
-            if len(a) <= len(minstr): 
-                minstr = a
+        n, m = len(strs), len(strs[0])
         
-        for i in range(len(minstr)):
-            go = True
-            for j in range(len(strs)):
-                if strs[j][i] != minstr[i]:
-                    go = False
-                    break
-            if go: 
-                ans += minstr[i]
-            else: 
-                break
+        ans = ""
+        
+        for i in range(m):
+            letter = strs[0][i]
+            
+            for j in range(1, n):
+                char = strs[j][i] if i < len(strs[j]) else ""
+
+                if char != letter: 
+                    return ans 
+                
+            ans += letter
+            
         return ans
                     
                 
