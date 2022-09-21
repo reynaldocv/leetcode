d3e68215a62f0ba9113890c45fcc517ae8c74097
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-21 17:08:03 -0300

    Update 0009. [Easy] Palindrome Number.py

--- 0001 - 0999/0009. [Easy] Palindrome Number.py
+++ 0001 - 0999/0009. [Easy] Palindrome Number.py
@@ -2,9 +2,8 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0: return False
-        x = str(x)
-        y = x[::-1]
-        return x == y
-        
+        if x < 0: 
+            return False 
+        else: 
+            return str(x) == str(x)[:: -1] 
         
