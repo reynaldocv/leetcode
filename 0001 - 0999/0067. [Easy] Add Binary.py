eafa76ea2cfd33c21776a37bad65891bc0d817cf
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-21 18:00:05 -0300

    Update 0067. [Easy] Add Binary.py

--- 0001 - 0999/0067. [Easy] Add Binary.py
+++ 0001 - 0999/0067. [Easy] Add Binary.py
@@ -2,14 +2,15 @@
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        def ChangeBase(n, k, j):
-            if n <= 0: 
-                return 0
-            else: 
-                return (n % j) + k*ChangeBase(n//j, k, j) 
-            
+        aSum = int(a, 2) + int(b, 2)
         
-        auxa = ChangeBase(int(a), 2, 10)  
-        auxb = ChangeBase(int(b), 2, 10)    
+        ans = ""
         
-        return str(ChangeBase(auxa + auxb, 10, 2))
+        if aSum == 0: 
+            return "0"
+        
+        while aSum > 0: 
+            ans += str(aSum % 2)
+            aSum //= 2 
+        
+        return ans[:: -1] 
