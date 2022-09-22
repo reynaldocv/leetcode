97caaf04b17505b4c1dc0cfcc40843d4eadd3f3c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 17:17:16 -0300

    Update 0160. [Easy] Intersection of Two Linked Lists.py

--- 0001 - 0999/0160. [Easy] Intersection of Two Linked Lists.py
+++ 0001 - 0999/0160. [Easy] Intersection of Two Linked Lists.py
@@ -6,12 +6,6 @@
 #         self.val = x
 #         self.next = None
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         seen = set()
