16ffdd39248ce075a8a7226f0436ffc7880abd00
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 20:01:29 -0300

    Update 0326. [Easy] Power of Three.py

--- 0001 - 0999/0326. [Easy] Power of Three.py
+++ 0001 - 0999/0326. [Easy] Power of Three.py
@@ -2,9 +2,11 @@
 
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n < 1: return False
-        while (n % 3 == 0): 
-            n = n // 3
-        return n == 1
+        if n <= 0: 
+            return False 
+        
+        while n % 3 == 0: 
+            n //= 3 
             
+        return n == 1 
         
