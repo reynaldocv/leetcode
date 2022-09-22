361b273767fce3a9d809d05da009b611c9d70a2d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 19:15:52 -0300

    Update 0231. [Easy] Power of Two.py

--- 0001 - 0999/0231. [Easy] Power of Two.py
+++ 0001 - 0999/0231. [Easy] Power of Two.py
@@ -2,10 +2,11 @@
 
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n <= 0: return False           
-        while n % 2 == 0:
-            n = n // 2
-        return True if n == 1 else False
-            
-            
+        if n <= 0: 
+            return False 
         
+        
+        while n % 2 == 0: 
+            n //= 2 
+                
+        return n == 1 
