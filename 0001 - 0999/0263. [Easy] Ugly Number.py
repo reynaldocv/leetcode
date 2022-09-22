601a331688da5d7029f5af29c7df97484dd6121f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 19:35:42 -0300

    Update 0263. [Easy] Ugly Number.py

--- 0001 - 0999/0263. [Easy] Ugly Number.py
+++ 0001 - 0999/0263. [Easy] Ugly Number.py
@@ -2,14 +2,17 @@
 
 class Solution:
     def isUgly(self, n: int) -> bool:
-        numbers = [2, 3, 5]
-        idx = 0
-        while n > 1 and idx <= 2:
-            while n % numbers[idx] == 0:
-                n = n// numbers[idx]
-            idx += 1
+        if n == 0: 
+            return False
         
+        factors = [2, 3, 5]
+        
+        for factor in factors: 
+            while n % factor == 0: 
+                n //= factor
+                
         return n == 1
+                
         
         
         
