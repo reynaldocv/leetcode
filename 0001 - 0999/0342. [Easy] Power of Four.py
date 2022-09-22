e84878f51b5a12da6b59fd5bd9db84086f831d64
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 20:16:21 -0300

    Update 0342. [Easy] Power of Four.py

--- 0001 - 0999/0342. [Easy] Power of Four.py
+++ 0001 - 0999/0342. [Easy] Power of Four.py
@@ -2,9 +2,12 @@
 
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n < 1: return False
-        while n % 4 == 0:
-            n = n // 4
+        if n < 1: 
+            return False 
         
-        return n == 1
+        while n % 4 == 0: 
+            n //= 4 
+            
+        return n == 1 
+        
         
