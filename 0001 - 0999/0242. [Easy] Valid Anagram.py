b3c3fb8f5dfe57807a83b0d2302b7aebe704a742
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 19:18:42 -0300

    Update 0242. [Easy] Valid Anagram.py

--- 0001 - 0999/0242. [Easy] Valid Anagram.py
+++ 0001 - 0999/0242. [Easy] Valid Anagram.py
@@ -2,9 +2,5 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        auxs = [i for i in s]
-        auxt = [i for i in t]
-        auxs.sort()
-        auxt.sort()
-        return "".join(auxs) == "".join(auxt)
+        return sorted(s) == sorted(t)
         
