5cadbc616d3e0fda57c2346d6cb5ac1dce505831
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 19:40:17 -0300

    Update 0278. [Easy] First Bad Version.py

--- 0001 - 0999/0278. [Easy] First Bad Version.py
+++ 0001 - 0999/0278. [Easy] First Bad Version.py
@@ -1,24 +1,22 @@
 # https://leetcode.com/problems/first-bad-version/
 
 # The isBadVersion API is already defined for you.
-# @param version, an integer
-# @return an integer
-# def isBadVersion(version):
+# def isBadVersion(version: int) -> bool:
 
 class Solution:
-    def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        start = 0
-        end = n       
-        while (end - start > 1):
-            m = (end + start)//2
-            if isBadVersion(m) == True: 
-                end = m
-            else:
-                start = m
+    def firstBadVersion(self, n: int) -> int:
+        start = 0 
+        end = n 
         
-        return end
+        while end - start > 1: 
+            mid = (end + start)//2
+            
+            if isBadVersion(mid): 
+                end = mid 
+            else: 
+                start = mid 
+                
+        return end 
+            
+            
         
