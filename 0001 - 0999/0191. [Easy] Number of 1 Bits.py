6f282ea0ddd22f89deb3434db106bba992754a97
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 18:27:43 -0300

    Update 0191. [Easy] Number of 1 Bits.py

--- 0001 - 0999/0191. [Easy] Number of 1 Bits.py
+++ 0001 - 0999/0191. [Easy] Number of 1 Bits.py
@@ -1,11 +1,14 @@
 # https://leetcode.com/problems/number-of-1-bits/
 
 class Solution:
-    def hammingWeight(self, n: int) -> int:        
-        ans = 0
-        while n > 0: 
-            if n % 2 == 1: 
-                ans += 1
-            n = n//2
-        return ans
+    def hammingWeight(self, n: int) -> int:
+        ans = 0 
+        
+        while n: 
+            if (n % 2) == 1:
+                ans += 1 
+            
+            n //= 2 
+            
+        return ans 
         
