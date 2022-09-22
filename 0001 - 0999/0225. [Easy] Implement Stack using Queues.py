f1a7b184fc54568621da3e925b01f4d5cd7169fe
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 18:54:42 -0300

    Update 0225. [Easy] Implement Stack using Queues.py

--- 0001 - 0999/0225. [Easy] Implement Stack using Queues.py
+++ 0001 - 0999/0225. [Easy] Implement Stack using Queues.py
@@ -3,43 +3,19 @@
 class MyStack:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.arr = []
-        self.n = 0
-
-    def push(self, x: int) -> None:
-        """
-        Push element x onto stack.
-        """
-        self.arr.append(x)
-        self.n += 1
+        self.stack = []
         
-
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        
     def pop(self) -> int:
-        """
-        Removes the element on top of the stack and returns that element.
-        """
-        ans = self.arr[self.n - 1]
-        self.arr.pop()
-        self.n -= 1
-        return ans
+        return self.stack.pop()         
 
     def top(self) -> int:
-        """
-        Get the top element.
-        """
-        return self.arr[self.n - 1]
+        return self.stack[-1]
         
-
     def empty(self) -> bool:
-        """
-        Returns whether the stack is empty.
-        """
-        return self.n == 0
-        
-
+        return len(self.stack) == 0
 
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
