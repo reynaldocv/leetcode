211ba9106ea7d4d17cefa9993e7ac386ff271c24
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 18:49:47 -0300

    Update 0219. [Easy] Contains Duplicate II.py

--- 0001 - 0999/0219. [Easy] Contains Duplicate II.py
+++ 0001 - 0999/0219. [Easy] Contains Duplicate II.py
@@ -2,12 +2,16 @@
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        dic, n = {}, len(nums)
-        for i in range(n):
-            if nums[i] in dic and (i - dic[nums[i]]) <= k:
-                    return True                
-            else: 
-                dic[nums[i]] = i
+        index = {}
         
-        return False
+        for (i, num) in enumerate(nums): 
+            if num not in index: 
+                index[num] = i 
+                
+            elif (i - index[num]) <= k:
+                return True 
+            
+            index[num] = i 
+            
+        return False 
         
