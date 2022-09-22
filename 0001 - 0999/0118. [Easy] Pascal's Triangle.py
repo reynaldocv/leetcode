c5dcf3b566b20fc9497a2caf1ebed809fe284e4c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 16:41:26 -0300

    Update 0118. [Easy] Pascal's Triangle.py

--- 0001 - 0999/0118. [Easy] Pascal's Triangle.py
+++ 0001 - 0999/0118. [Easy] Pascal's Triangle.py
@@ -2,11 +2,10 @@
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        n = numRows
-        pascal = [[1]*i for i in range(1, n + 1)]
+        ans = [[1]*i for i in range(1, numRows + 1)]
         
-        for i in range(2, n):
-            for j in range(1, len(pascal[i]) - 1):
-                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
-        
-        return pascal
+        for i in range(2, numRows):
+            for j in range(1, i):
+                ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j]
+                
+        return ans 
