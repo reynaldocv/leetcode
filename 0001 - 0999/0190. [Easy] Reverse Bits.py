e53cbb8a9eb2187618c368bc6eac5d251ac1dc99
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-22 18:25:59 -0300

    Update 0190. [Easy] Reverse Bits.py

--- 0001 - 0999/0190. [Easy] Reverse Bits.py
+++ 0001 - 0999/0190. [Easy] Reverse Bits.py
@@ -1,14 +1,14 @@
 # https://leetcode.com/problems/reverse-bits/
 
 class Solution:
-    # @param n, an integer
-    # @return an integer
-    def reverseBits(self, n):
-        if n == 0: return 0
-        ans, power =0, 31
-        while n: 
-            ans += (n & 1) << power
-            n = n >> 1
-            power -= 1            
-        return ans
+    def reverseBits(self, n: int) -> int:
+        ans = 0 
+        
+        for i in range(32):
+            bit = n % 2           
+            ans = 2*ans + bit
+            
+            n //= 2
+            
+        return ans 
             
