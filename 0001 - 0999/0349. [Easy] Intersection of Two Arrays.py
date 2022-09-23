9cb31f43f83114fb87542eb1ea1195ae7c803286
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-23 14:16:07 -0300

    Update 0349. [Easy] Intersection of Two Arrays.py

--- 0001 - 0999/0349. [Easy] Intersection of Two Arrays.py
+++ 0001 - 0999/0349. [Easy] Intersection of Two Arrays.py
@@ -2,20 +2,16 @@
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        seen1 = set()
-        seen2 = set()
+        seen = set()
         
         for num in nums1: 
-            seen1.add(num)
+            seen.add(num)
+            
+        ans = set()
             
         for num in nums2: 
-            seen2.add(num)
-            
-        ans = []
-            
-        for key in seen1: 
-            if key in seen2: 
-                ans.append(key)
-                
+            if num in seen and num not in ans:
+                ans.add(num)
+        
         return ans 
         
