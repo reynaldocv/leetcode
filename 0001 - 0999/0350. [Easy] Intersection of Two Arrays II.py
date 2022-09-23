7ac7a24afa58eb75094262cf42a4bc0d59150e0a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-23 14:20:07 -0300

    Update 0350. [Easy] Intersection of Two Arrays II.py

--- 0001 - 0999/0350. [Easy] Intersection of Two Arrays II.py
+++ 0001 - 0999/0350. [Easy] Intersection of Two Arrays II.py
@@ -2,14 +2,21 @@
 
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        dic1, dic2, ans = {}, {}, []
+        counter = defaultdict(lambda: 0)
+        
         for num in nums1: 
-            dic1[num] = dic1.get(num, 0) + 1
+            counter[num] += 1 
+            
+        ans = []
+        
         for num in nums2: 
-            if num in dic1: 
-                dic2[num] = dic2.get(num, 0) + 1
-                if dic2[num] <= dic1[num]:
-                    ans.append(num)
-        return ans
+            if num in counter:
+                counter[num] -= 1 
+                ans.append(num)
+                
+                if counter[num] == 0: 
+                    counter.pop(num)
+                    
+        return ans 
         
         
