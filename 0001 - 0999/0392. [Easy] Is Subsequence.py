8e3214ef7d6ebe300cf25599865d2133769b5b32
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-25 16:46:15 -0300

    Update 0392. [Easy] Is Subsequence.py

--- 0001 - 0999/0392. [Easy] Is Subsequence.py
+++ 0001 - 0999/0392. [Easy] Is Subsequence.py
@@ -3,12 +3,15 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         m, n = len(s), len(t)
-        if m == 0: return True        
-        idx = 0        
-        for i in range(n):
-            if s[idx] == t[i]:
-                idx += 1
-            if idx == m: 
-                return True
-        return False
+        
+        i, j = 0, 0
+        
+        while i < m and j < n: 
+            if s[i] == t[j]:
+                i += 1 
+                
+            j += 1 
+            
+        return i == m 
+                
                 
