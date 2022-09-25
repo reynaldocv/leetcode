40dd7eede5d2a4f71452f36cc0a0bf7809ada6a4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-25 17:25:45 -0300

    Update 0409. [Easy] Longest Palindrome.py

--- 0001 - 0999/0409. [Easy] Longest Palindrome.py
+++ 0001 - 0999/0409. [Easy] Longest Palindrome.py
@@ -1,19 +1,21 @@
 # https://leetcode.com/problems/longest-palindrome/
 
 class Solution:
-    def longestPalindrome(self, s: str) -> int:        
-        n = len(s)
-        dic = {}
-        ans, odd = 0, 0
-        for i in s:
-            dic[i] = dic.get(i, 0) + 1 
-        for i in dic: 
-            if dic[i] % 2 == 1: 
-                ans += dic[i] - 1
-                odd = 1
+    def longestPalindrome(self, s: str) -> int:
+        seen = set()
+        ans = 0 
+        
+        for char in s: 
+            if char not in seen: 
+                seen.add(char)
             else: 
-                ans += dic[i]
-        return ans + odd
+                ans += 2 
+                seen.remove(char)
+                
+        ans += 1 if seen else 0 
+        
+        return ans 
+        
         
      
                 
