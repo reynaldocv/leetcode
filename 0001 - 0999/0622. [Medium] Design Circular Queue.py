24cd868b6f47ac4ed084effa20da07ea47804a2a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-25 16:12:42 -0300

    Update 0622. [Medium] Design Circular Queue.py

--- 0001 - 0999/0622. [Medium] Design Circular Queue.py
+++ 0001 - 0999/0622. [Medium] Design Circular Queue.py
@@ -3,46 +3,45 @@
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.n = 0 
+        self.total = k        
+        self.idx = 0
         self.arr = []
-        self.k = k
         
-
     def enQueue(self, value: int) -> bool:
-        if self.n < self.k: 
+        if self.idx < self.total:
             self.arr.append(value)
-            self.n += 1
-            return True
+            self.idx += 1 
+            
+            return True 
         
-        return False
+        return False 
 
     def deQueue(self) -> bool:
-        if self.n > 0: 
-            self.arr.pop(0)
-            self.n -= 1
-            return True
+        if self.idx > 0: 
+            self.idx -= 1 
+            self.arr.pop(0) 
+            
+            return True 
         
-        return False
-
+        return False 
+        
     def Front(self) -> int:
-        if self.n > 0: 
+        if self.idx > 0:
             return self.arr[0]
         
-        return -1
+        return -1 
 
     def Rear(self) -> int:
-        if self.n > 0: 
-            return self.arr[-1]
+        if self.idx > 0: 
+            return self.arr[self.idx - 1]
         
         return -1
 
     def isEmpty(self) -> bool:
-        return self.n == 0
-        
+        return self.idx == 0
 
     def isFull(self) -> bool:
-        return self.n == self.k
-
+        return self.idx == self.total
 
 # Your MyCircularQueue object will be instantiated and called as such:
 # obj = MyCircularQueue(k)
