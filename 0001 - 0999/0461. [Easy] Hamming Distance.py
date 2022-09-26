c29885262e06025b0584922215c64b77bd19a9d4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-26 10:55:27 -0300

    Update 0461. [Easy] Hamming Distance.py

--- 0001 - 0999/0461. [Easy] Hamming Distance.py
+++ 0001 - 0999/0461. [Easy] Hamming Distance.py
@@ -1,12 +1,15 @@
 # https://leetcode.com/problems/hamming-distance/
 
 class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
-        ans = 0
-        while (x > 0 or y > 0):
-            if x % 2 != y % 2:
-                ans += 1
-            x, y = x//2, y//2
-        return ans
+    def hammingDistance(self, x: int, y: int) -> int: 
+        ans = 0 
+        
+        while x > 0 or y > 0: 
+            if x & 1 != y & 1:
+                ans += 1 
+                
+            x >>= 1 
+            y >>= 1 
             
+        return ans 
         
