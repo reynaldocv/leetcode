50ea7f14e323ab1f4b8dc84cb3f48a13b69c654a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-26 10:16:03 -0300

    Update 0415. [Easy] Add Strings.py

--- 0001 - 0999/0415. [Easy] Add Strings.py
+++ 0001 - 0999/0415. [Easy] Add Strings.py
@@ -2,28 +2,28 @@
 
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        n = len(num1)
-        m = len(num2)
-        if n < m:
-            num1, num2 = num2, num1
-            n, m = m, n
-        q = 0
-        ans = ""
-        for i in range(0, m):
-            aux = int(num1[n - i - 1]) + int(num2[m - i - 1]) + q
-            q = aux//10
-            r = aux % 10
-            ans = str(r) + ans
-        print(ans)
-        for i in range(n - m - 1, -1, -1):
-            aux = int(num1[i]) + q
-            q = aux//10
-            r = aux % 10
-            ans = str(r) + ans
-        print(ans)        
-        if q == 1:
-            ans = "1" + ans
-        print(ans)
+        num1, num2 = num1[:: -1], num2[:: -1]
         
-        return ans            
+        aux = 0 
+        
+        m, n = len(num1), len(num2)
+        
+        limit = max(m, n)
+        
+        ans = ""
+        
+        for i in range(limit):
+            val1 = int(num1[i]) if i < m else 0 
+            val2 = int(num2[i]) if i < n else 0 
+            
+            aSum = val1 + val2 + aux
+            
+            aux = aSum // 10 
+                        
+            ans += str(aSum % 10)
+            
+        if aux: 
+            ans += str(1)
+            
+        return ans[:: -1]
             
