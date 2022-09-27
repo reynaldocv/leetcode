daf804bb6f52f37db4e992ab6d8a8952a510a167
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-27 17:01:10 -0300

    Update 0888. [Easy] Fair Candy Swap.py

--- 0001 - 0999/0888. [Easy] Fair Candy Swap.py
+++ 0001 - 0999/0888. [Easy] Fair Candy Swap.py
@@ -2,12 +2,18 @@
 
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
-        n = len(aliceSizes)
-        m = len(bobSizes)
-        dic = {bobSizes[x]:True for x in range(m)}
-        sumAlice = sum(aliceSizes)
-        candies = ((sumAlice) + sum(bobSizes))//2
-        for i in range(n):
-            aux = candies - (sumAlice - aliceSizes[i])
-            if dic.get(aux, False): 
-                return (aliceSizes[i], aux)
+        totalAlice = sum(aliceSizes)
+        totalBob = sum(bobSizes)
+        
+        mid = (totalAlice + totalBob)//2
+        
+        seen = set()
+        
+        for num in aliceSizes: 
+            seen.add(num) 
+            
+        for num in bobSizes: 
+            tmp = mid - (totalBob - num)
+            
+            if tmp in seen: 
+                return [tmp, num]
