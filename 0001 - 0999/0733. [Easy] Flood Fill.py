7eadeb0e55f8e75ae6c7dd1ffb0e1a79fec47379
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-27 17:11:47 -0300

    Update 0733. [Easy] Flood Fill.py

--- 0001 - 0999/0733. [Easy] Flood Fill.py
+++ 0001 - 0999/0733. [Easy] Flood Fill.py
@@ -1,23 +1,27 @@
 # https://leetcode.com/problems/flood-fill/
 
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         m, n = len(image), len(image[0])
         
-        color = image[sr][sc]
-        stack = [(sr, sc)]
-        seen = {(sr, sc): True}
+        oldColor = image[sr][sc]
         
-        image[sr][sc] = newColor
+        if oldColor == color: 
+            return image
+        
+        stack = [(sr, sc)]
+        
+        image[sr][sc] = color
         
         while stack: 
-            (x, y) = stack.pop()
-            for (r, s) in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            (x, y) = stack.pop() 
+            
+            for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                 p, q = x + r, y + s
+                
                 if 0 <= p < m and 0 <= q < n: 
-                    if (p, q) not in seen and image[p][q] == color: 
-                        seen[(p, q)] = True
-                        image[p][q] = newColor
+                    if image[p][q] == oldColor:
+                        image[p][q] = color 
                         stack.append((p, q))
                         
         return image
