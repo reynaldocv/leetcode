30644e959043a7981c036dbc4fd60cf38bc68995
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-28 14:34:10 -0300

    Update 0724. [Easy] Find Pivot Index.py

--- 0001 - 0999/0724. [Easy] Find Pivot Index.py
+++ 0001 - 0999/0724. [Easy] Find Pivot Index.py
@@ -1,16 +1,19 @@
 # https://leetcode.com/problems/find-pivot-index/
 
 class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:     
-        sumLeft = 0
-        sumRight = sum(nums)
-        n = len(nums)
-        for i in range(n):
-            if i - 1 >= 0:
-                sumLeft += nums[i - 1]
-            sumRight -= nums[i]
-            if sumLeft == sumRight:
-                return i
+    def pivotIndex(self, nums: List[int]) -> int:
+        prefix = 0 
+        suffix = sum(nums)
+        
+        for (i, num) in enumerate(nums):
+            suffix -= num 
+            
+            if prefix == suffix: 
+                return i 
+            
+            prefix += num 
+            
         return -1
+        
 
         
