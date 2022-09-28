a1e62c26998f2bd467152f7bb0a578a358834a6d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-28 16:10:33 -0300

    Update 0200. [Medium] Number of Islands.py

--- 0001 - 0999/0200. [Medium] Number of Islands.py
+++ 0001 - 0999/0200. [Medium] Number of Islands.py
@@ -4,30 +4,27 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def helper(x, y):
             stack = [(x, y)]
-            grid[x][y] = 0 
+            grid[x][y] = "0"
             
             while stack: 
-                (x, y) = stack.pop()
-                for (r, s) in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                (x, y) = stack.pop() 
+                
+                for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                     p, q = x + r, y + s
-                    if 0 <= p < m and 0 <= q < n: 
-                        if grid[p][q] == "1": 
-                            grid[p][q] = "0" 
-                            stack.append((p, q))
-            
+                    
+                    if 0 <= p < m and 0 <= q < n and grid[p][q] == "1":
+                        grid[p][q] = "0"
+                        stack.append((p, q))
+                            
+            return 1 
+        
         m, n = len(grid), len(grid[0])
         
-        ans = 0
+        ans = 0 
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
-                    ans += 1
-                    helper(i, j)
+                    ans += helper(i, j)
                     
         return ans 
-                    
-        
-        
-        
-        
