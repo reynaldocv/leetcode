d317fc384d055bfbd62ae31830e4534efe8b1c8d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-28 14:23:54 -0300

    Update 1480. [Easy] Running Sum of 1d Array.py

--- 1000 - 1999/1480. [Easy] Running Sum of 1d Array.py
+++ 1000 - 1999/1480. [Easy] Running Sum of 1d Array.py
@@ -2,7 +2,9 @@
 
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        l = len(nums)
-        for i in range(1, l):
+        n = len(nums)
+        
+        for i in range(1, n):
             nums[i] += nums[i - 1]
+            
         return nums
