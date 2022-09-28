7f6e7633464701bd0e05b5e62b05afc87ba37e3b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-28 16:17:32 -0300

    Update 0054. [Medium] Spiral Matrix.py

--- 0001 - 0999/0054. [Medium] Spiral Matrix.py
+++ 0001 - 0999/0054. [Medium] Spiral Matrix.py
@@ -3,31 +3,32 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         m, n = len(matrix), len(matrix[0])
+        
+        (x, y) = (0, -1)
+        
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         
-        x, y = 0, 0 
         k = 0 
         
-        seen = {(0, 0): True, (0, n): True, (m, n - 1): True, (m - 1, -1): True}
-        ans = [matrix[0][0]]
+        ans = []
         
-        i = 1 
+        seen = {(0, n), (m, n - 1), (m - 1, -1)}
         
-        while i < m*n:
+        while len(ans) < m*n: 
             x += directions[k][0]
             y += directions[k][1]
             
             if (x, y) in seen: 
                 x -= directions[k][0]
                 y -= directions[k][1]
-                k = (k + 1) % 4
+                
+                k = (k + 1) % 4 
                 
             else: 
+                seen.add((x, y))
                 ans.append(matrix[x][y])
-                seen[(x, y)] = True
-                i += 1 
                 
-        return ans
+        return ans 
                 
             
                 
