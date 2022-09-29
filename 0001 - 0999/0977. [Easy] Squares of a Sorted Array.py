a387a385af1e7c8ad1c10268a7ad6af795c42821
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-09-29 17:47:04 -0300

    Update 0977. [Easy] Squares of a Sorted Array.py

--- 0001 - 0999/0977. [Easy] Squares of a Sorted Array.py
+++ 0001 - 0999/0977. [Easy] Squares of a Sorted Array.py
@@ -1,8 +1,29 @@
 # https://leetcode.com/problems/squares-of-a-sorted-array/
 
 class Solution:
-    def sortedSquares(self, A: List[int]) -> List[int]:
-        ans = [x**2 for x in A]
-        ans.sort()
-        return ans
-        
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        end = bisect_left(nums, 0)
+        start = end - 1 
+
+        counter = 0
+
+        ans = [] 
+
+        while counter < n: 
+            val1 = nums[start]**2 if start >= 0 else inf 
+            val2 = nums[end]**2 if end < n else inf 
+
+            if val1 < val2: 
+                ans.append(val1)
+                start -= 1 
+            else:
+                ans.append(val2)
+                end += 1 
+
+            counter += 1 
+
+        return ans 
+Console
+
