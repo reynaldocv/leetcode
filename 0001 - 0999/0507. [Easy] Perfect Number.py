b3549fcd4fc86c47c9f6e3b63626f73a46a7056c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-01 15:53:14 -0300

    Update 0507. [Easy] Perfect Number.py

--- 0001 - 0999/0507. [Easy] Perfect Number.py
+++ 0001 - 0999/0507. [Easy] Perfect Number.py
@@ -2,14 +2,19 @@
 
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        if num == 1: return False
-        n = int(num**0.5)        
+        if num == 1: 
+            return False 
+        
+        limit = int(num**.5)
         divisors = set([1])
-        for i in range(2, n + 1):
-            if num % i == 0:
+        
+        for i in range(2, limit + 1):
+            if num % i == 0: 
                 divisors.add(i)
                 divisors.add(num//i)
-
-        return num == sum(divisors)
+                    
+        return sum(divisors) == num
+            
+            
         
         
