fc78c02fb847bb6abd4789cd62508771d6e09783
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-01 16:15:15 -0300

    Update 0521. [Easy] Longest Uncommon Subsequence I.py

--- 0001 - 0999/0521. [Easy] Longest Uncommon Subsequence I.py
+++ 0001 - 0999/0521. [Easy] Longest Uncommon Subsequence I.py
@@ -2,5 +2,7 @@
 
 class Solution:
     def findLUSlength(self, a: str, b: str) -> int:
-        if a == b: return -1
+        if a == b: 
+            return -1
+        
         return max(len(a), len(b))
