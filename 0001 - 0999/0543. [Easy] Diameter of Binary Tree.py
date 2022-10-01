47af575aa0b1053b18b1d701c6b51af98ca7f024
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-01 16:56:04 -0300

    Update 0543. [Easy] Diameter of Binary Tree.py

--- 0001 - 0999/0543. [Easy] Diameter of Binary Tree.py
+++ 0001 - 0999/0543. [Easy] Diameter of Binary Tree.py
@@ -11,12 +11,31 @@ class Solution:
         def hight(root):
             if root == None: 
                 return 0
+            
             else: 
                 h1 = hight(root.left)
                 h2 = hight(root.right)
+        
                 self.diameter = max(self.diameter, h1 + h2)
+                
                 return 1 + max(h1, h2)
         
         self.diameter = 0
+        
         hight(root)
+        
         return self.diameter
+
+class Solution2:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def helper(node):
+            if node: 
+                leftDiameter, leftHigh = helper(node.left)
+                rightDiameter, rightHigh = helper(node.right)
+                
+                return max(leftDiameter, rightDiameter, leftHigh + rightHigh), max(leftHigh, rightHigh) + 1
+            
+            return 0, 0 
+        
+        return helper(root)[0]
+        
