b1e253ac00d243b38c3f9ccf484eade637f6f34d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-01 16:32:19 -0300

    Update 0541. [Easy] Reverse String II.py

--- 0001 - 0999/0541. [Easy] Reverse String II.py
+++ 0001 - 0999/0541. [Easy] Reverse String II.py
@@ -2,14 +2,17 @@
 
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        arr = []
-        i = 0
-        while len(s[i*2*k: (i + 1)*2*k]) > 0:
-            arr.append(s[i*2*k: (i + 1)*2*k])
-            i += 1
+        n = len(s)
+        
         ans = ""
-        for ar in arr:     
-            ans += ar[:k][::-1]
-            ans += ar[k:2*k]
-        return ans
+        count = 0 
+        for i in range(0, n, k):
+            if count == 0: 
+                ans += s[i: i + k][:: -1]
+            else:
+                ans += s[i: i + k]
+            
+            count = 1 - count
+                
+        return ans 
         
