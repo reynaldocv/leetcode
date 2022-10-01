43732d2f4f35cbb6d7c93a4c7272ca643f38b7ac
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-01 12:17:06 -0300

    Update 0492. [Easy] Construct the Rectangle.py

--- 0001 - 0999/0492. [Easy] Construct the Rectangle.py
+++ 0001 - 0999/0492. [Easy] Construct the Rectangle.py
@@ -1,16 +1,12 @@
 # https://leetcode.com/problems/construct-the-rectangle/
 
 class Solution:
-    def constructRectangle(self, area: int) -> List[int]:
-        aux = int(area**.5) + 1
-        ans = [(area - 2, area, 1)]
-        for i in range(2, aux + 1):
-            if area % i == 0:
-                b = area//i
-                if b >= i:
-                    ans.append((b - i, b, i))
+    def constructRectangle(self, area: int) -> List[int]:     
+        start = int(area**0.5)
         
-        ans.sort()
-        return [ans[0][1], ans[0][2]]
+        for width in range(start, 0, -1):
+            if area % width == 0: 
+                return [area//width, width]
+        
         
         
