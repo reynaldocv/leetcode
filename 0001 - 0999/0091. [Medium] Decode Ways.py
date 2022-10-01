855c54d43a7d4063e21c2e11c797c2d9d16c1217
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-01 11:37:23 -0300

    Update 0091. [Medium] Decode Ways.py

--- 0001 - 0999/0091. [Medium] Decode Ways.py
+++ 0001 - 0999/0091. [Medium] Decode Ways.py
@@ -2,20 +2,22 @@
 
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dic = {str(i):True for i in range(1, 27)}
-        n = len(s)
-        arr = [0 for i in range(n)]
-        if s[0] == "0": return 0 
-        if n == 1: return 1
-        arr[0] = 1
-        arr[1] = 1 if s[1] in dic else 0
-        arr[1] += 1 if s[0] +  s[1] in dic else 0 
+        @cache
+        def helper(string):
+            n = len(string)
+            if n == 0: 
+                return 1 
+            else: 
+                ans = 0 
+                for i in range(1, min(n + 1, 3)):
+                    val = string[: i]
+                    if val in seen: 
+                        ans += helper(string[i: ])
+                        
+                return ans 
+                
+        seen = {str(i) for i in range(1, 27)}
         
-        for i in range(2, n):
-            if s[i] in dic: 
-                arr[i] += arr[i - 1]
-            if s[i - 1] + s[i] in dic: 
-                arr[i] += arr[i - 2]
-       
-        return arr[n - 1]
-        
+        return helper(s)
+                
+                
