43386e3d36ac3e480c6c248907683174a445dc0a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-04 17:47:57 -0300

    Update 0036. [Medium] Valid Sudoku.py

--- 0001 - 0999/0036. [Medium] Valid Sudoku.py
+++ 0001 - 0999/0036. [Medium] Valid Sudoku.py
@@ -2,38 +2,54 @@
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def square(i, j, board):
-            ans = []
-            for k in range(3*i, 3*i + 3):
-                for l in range(3*j, 3*j + 3):
-                    ans.append(board[k][l])
-            return ans
+        def helper():
+            for i in range(9):
+                seen = set()
+                for val in board[i]:
+                    if val != ".":
+                        if val not in seen: 
+                            seen.add(val)
+                        else: 
+                            return False 
+            
+            return True 
+        
+        def collaborator(row, col):
+            seen = set()
+            for i in range(3*row, 3*(row + 1)):
+                for j in range(3*col, 3*(col + 1)):
+                    val = board[i][j]
+                    
+                    if val != ".":
+                        if val not in seen: 
+                            seen.add(val)
+                        else: 
+                            return False 
+                        
+            return True 
+    
+        if not helper():
+            return False 
+        
+        board = [[board[j][i] for j in range(9)] for i in range(9)]
+        
+        if not helper():
+            return False 
+        
+        for i in range(3):
+            for j in range(3):        
+                if not collaborator(i, j):
+                    return False 
+                
+        return True
         
         
-        def repeated(arr):
-            dic = {}
-            for i in arr: 
-                if i != "." and i in dic: 
-                    return False
-                else: 
-                    dic[i] = True
-            return True
         
-        ans = True
-        for row in board: 
-            ans = ans and repeated(row)
         
-        if ans: 
-            board2 = [[board[i][j] for i in range(9)] for j in range(9)]
-            for row in board2: 
-                ans = ans and repeated(row)
-            if ans: 
-                for i in range(3):
-                    for j in range(3):
-                        arr = square(i, j, board)
-                        ans = ans and repeated(arr)
-                return ans
-            else: 
-                return False        
-        else: 
-            return False
+        
+        
+        
+        
+        
+        
+        
