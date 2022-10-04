82ccaf3c1e42a3c741f135525445cc4d4d2520da
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-04 17:53:32 -0300

    Update 0074. [Medium] Search a 2D Matrix.py

--- 0001 - 0999/0074. [Medium] Search a 2D Matrix.py
+++ 0001 - 0999/0074. [Medium] Search a 2D Matrix.py
@@ -2,6 +2,30 @@
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        
+        stack = [(0, 0)]
+        seen = {(0, 0)}
+        
+        while stack: 
+            (x, y) = stack.pop()
+            
+            if matrix[x][y] == target: 
+                return True
+            
+            for (r, s) in [(1, 0), (0, 1)]:
+                p, q = x + r, y + s 
+                
+                if 0 <= p < m and 0 <= q < n: 
+                    if matrix[p][q] <= target: 
+                        if (p, q) not in seen: 
+                            seen.add((p, q))
+                            stack.append((p, q))
+                            
+        return False
+
+class Solution2:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         def searchBinary(arr, target):
             start = 0 
             end = len(arr)
