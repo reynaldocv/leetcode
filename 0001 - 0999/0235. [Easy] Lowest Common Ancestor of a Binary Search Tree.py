51f231e367b94863bb454e0baabeac9b949ca99d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-05 11:42:11 -0300

    Update 0235. [Easy] Lowest Common Ancestor of a Binary Search Tree.py

--- 0001 - 0999/0235. [Easy] Lowest Common Ancestor of a Binary Search Tree.py
+++ 0001 - 0999/0235. [Easy] Lowest Common Ancestor of a Binary Search Tree.py
@@ -2,26 +2,24 @@
 
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pval = p.val
-        qval = q.val 
-        if pval > qval: 
-            pval, qval = qval, pval
-        while root: 
-            if pval < root.val < qval:
-                return root 
-            elif pval == root.val or qval == root.val:
-                return root 
-            else: 
-                if pval < root.val and qval < root.val:
-                    root = root.left
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def helper(node, lower, upper):
+            if node: 
+                if lower < node.val < upper: 
+                    left = helper(node.left, lower, node.val) 
+                    right = helper(node.right, node.val, upper)
+                    
+                    return left and right
+                
                 else: 
-                    root = root.right
-        return None
+                    return False 
+                
+            return True 
+        
+        return helper(root, -inf, inf)
         
