ad9d51295abf2837da9d63cca602addff524338e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-06 20:56:47 -0300

    Update 0213. [Medium] House Robber II.py

--- 0001 - 0999/0213. [Medium] House Robber II.py
+++ 0001 - 0999/0213. [Medium] House Robber II.py
@@ -2,20 +2,29 @@
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def helper(start, end):
-            arr = list(nums)
-            ans = nums[start] if start < len(nums) else 0
-            for i in range(start, end):
-                max1 = arr[i - 2] if i - 2 >= start else 0
-                max2 = arr[i - 3] if i - 3 >= start else 0
-                arr[i] += max(max1, max2)
-                ans = max(ans, arr[i])
-                
-            return ans
+        def helper(arr):
+            n = len(arr)
             
+            dp = [num for num in arr]
+            ans = 0 
+            
+            for i in range(n):
+                val3 = dp[i - 3] if i - 3 >= 0 else 0 
+                val2 = dp[i - 2] if i - 2 >= 0 else 0 
+                
+                dp[i] += max(val2, val3)
+                ans = max(ans, dp[i])
+                
+            return ans 
+        
         n = len(nums)
-        if n == 1:
+        
+        if n == 1: 
             return nums[0]
         
-        return max(helper(1, n), helper(0, n - 1))
+        return max(helper(nums[: -1]), helper(nums[1: ]))
+                
+            
+        
+        
         
