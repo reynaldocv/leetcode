a873ae720a4e868e87e3f588cea8d6e9727971bd
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-07 16:11:57 -0300

    Update 0045. [Medium] Jump Game II.py

--- 0001 - 0999/0045. [Medium] Jump Game II.py
+++ 0001 - 0999/0045. [Medium] Jump Game II.py
@@ -2,9 +2,29 @@
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        @cache 
+        def helper(x):
+            if x == n - 1: 
+                return 0 
+            else: 
+                ans = inf 
+                
+                for end in range(x + 1, min(x + nums[x] + 1, n)):
+                    ans = min(ans, helper(end))
+                    
+                return 1 + ans 
+            
         n = len(nums)
+        
+        return helper(0)
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        
         arr = [inf for i in range(n)]
         arr[0] = 1
+        
         for i in range(n - 1):
             if arr[i] != inf:
                 for j in range(i + 1, i + nums[i] + 1):
