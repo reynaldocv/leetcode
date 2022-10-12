fba6cb873a8915e22b5e31c97087bbe83ff848c9
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-11 21:16:07 -0300

    Update 0976. [Easy] Largest Perimeter Triangle.py

--- 0001 - 0999/0976. [Easy] Largest Perimeter Triangle.py
+++ 0001 - 0999/0976. [Easy] Largest Perimeter Triangle.py
@@ -2,12 +2,15 @@
 
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
-        nums.sort()
         n = len(nums)
-        ans = 0
-        for i in range(n - 2):
-            if nums[i + 1] - nums[i] < nums[i + 2] < nums[i] + nums[i + 1]:
-                ans = max(ans, nums[i] + nums[i + 1] + nums[i + 2])
         
-        return ans
+        nums.sort() 
+        
+        ans = 0 
+        
+        for i in range(1, n - 1):
+            if nums[i] - nums[i - 1] < nums[i + 1] < nums[i] + nums[i - 1]:
+                ans = max(ans, nums[i - 1] + nums[i] + nums[i + 1])
+                
+        return ans 
         
