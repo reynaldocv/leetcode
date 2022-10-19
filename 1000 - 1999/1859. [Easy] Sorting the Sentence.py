fdbc864842a60014f286d3b6e85b00f22928b06e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-19 15:18:50 -0300

    Update 1859. [Easy] Sorting the Sentence.py

--- 1000 - 1999/1859. [Easy] Sorting the Sentence.py
+++ 1000 - 1999/1859. [Easy] Sorting the Sentence.py
@@ -2,15 +2,11 @@
 
 class Solution:
     def sortSentence(self, s: str) -> str:
-        aux = []
-        ans = ""
         s = s.split(" ")
-        for i in s: 
-            l = len(i)
-            aux.append((i[l-1:], i[0:l-1]))
-        aux.sort()
-        for i in range(len(aux)):
-            ans += aux[i][1] + " "
-            
-        return ans[0:len(ans) - 1]
+        
+        s.sort(key = lambda item: item[-1: ])
+        
+        ans = [word[: -1] for word in s]
+        
+        return " ".join(ans)
         
