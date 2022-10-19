079096ad52448dc714e33523a4d00549727ae733
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-19 09:36:04 -0300

    Update 0692. [Medium] Top K Frequent Words.py

--- 0001 - 0999/0692. [Medium] Top K Frequent Words.py
+++ 0001 - 0999/0692. [Medium] Top K Frequent Words.py
@@ -2,10 +2,15 @@
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        counter = {word:words.count(word) for word in words}
-        arr = [*counter.keys()]
-        arr.sort(key = lambda item: (-counter[item], item))
-       
+        counter = defaultdict(lambda: 0)
         
-        return arr[:k]
-        # https://leetcode.com/problems/top-k-frequent-words/
+        for word in words: 
+            counter[word] += 1 
+            
+        ans = [key for key in counter]
+            
+        ans.sort(key = lambda item: (-counter[item], item))
+                   
+        return ans[: k]
+            
+        
