fcd30be75d0aeb7691bbe7b651399fd9b3c92101
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-24 16:20:13 -0300

    Update 0917. [Easy] Reverse Only Letters.py

--- 0001 - 0999/0917. [Easy] Reverse Only Letters.py
+++ 0001 - 0999/0917. [Easy] Reverse Only Letters.py
@@ -2,25 +2,30 @@
 
 class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
-        letters = ""
+        seen = set()
+
         for i in range(26):
-            letters += chr(ord("a") + i)
-            letters += chr(ord("A") + i)
+            seen.add(chr(ord("a") + i))
+            seen.add(chr(ord("A") + i))
+            
+        tmp = ""
         
-        aux = ""
-        for letter in s: 
-            if letter in letters: 
-                aux += letter
-        aux = aux[::-1]
+        for char in s: 
+            if char in seen:
+                tmp = char + tmp
+                
+        idx = 0 
         
         ans = ""
         
-        j = 0
-        for letter in s: 
-            if letter in letters: 
-                ans += aux[j]
-                j += 1            
+        for char in s: 
+            if char in seen:
+                ans += tmp[idx] 
+                idx += 1 
+                
             else: 
-                ans += letter
+                ans += char
+                
+        return ans 
+            
         
-        return ans
