37059cbd73ed6bbf409fa07d3e0f7819106dcef9
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-25 14:01:24 -0300

    Update 1662. [Easy] Check If Two String Arrays are Equivalent.py

--- 1000 - 1999/1662. [Easy] Check If Two String Arrays are Equivalent.py
+++ 1000 - 1999/1662. [Easy] Check If Two String Arrays are Equivalent.py
@@ -2,10 +2,5 @@
 
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        w1, w2 = "", ""
-        for i in range(len(word1)):
-            w1 += word1[i]
-        for i in range(len(word2)):
-            w2 += word2[i]
+        return "".join(word1) == "".join(word2)
         
-        return w1 == w2
