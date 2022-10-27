068b27d16a2a54dd4c2f57ddea578ef46e52cc25
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-27 13:40:06 -0300

    Update 1221. [Easy] Split a String in Balanced Strings.py

--- 1000 - 1999/1221. [Easy] Split a String in Balanced Strings.py
+++ 1000 - 1999/1221. [Easy] Split a String in Balanced Strings.py
@@ -2,11 +2,17 @@
 
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
+        counter = 0 
+        
         ans = 0
-        counter = 0
-        for i in range(len(s)):
-            counter = counter + 1 if (s[i] == "L") else counter - 1
-            ans = ans + 1 if (counter == 0) else ans
-        return ans
+        
+        for char in s: 
+            counter += 1 if char == "R" else -1 
+            
+            if counter == 0: 
+                ans += 1 
+                
+        return ans 
+                
         
         
