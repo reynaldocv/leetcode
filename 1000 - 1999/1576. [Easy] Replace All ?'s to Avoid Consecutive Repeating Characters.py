9e1b6aa1406c530cad0d7a13d468676996817038
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-27 10:38:27 -0300

    Update 1576. [Easy] Replace All ?'s to Avoid Consecutive Repeating Characters.py

--- 1000 - 1999/1576. [Easy] Replace All ?'s to Avoid Consecutive Repeating Characters.py
+++ 1000 - 1999/1576. [Easy] Replace All ?'s to Avoid Consecutive Repeating Characters.py
@@ -2,23 +2,22 @@
 
 class Solution:
     def modifyString(self, s: str) -> str:
-        letters = ""
-        for i in range(ord("a"), ord("z") + 1):
-            letters += chr(i)
-            
+        n = len(s) 
+        
         ans = ""
-        n = len(s)
-        for i in range(n):
-            if s[i] == "?":
-                arr = []
-                if i - 1 >= 0:
-                    arr.append(ans[i - 1])
-                if i + 1 < n:
-                    arr.append(s[i + 1])
-                idx = 0
-                while letters[idx] in arr: 
-                    idx += 1
-                ans += letters[idx]
+        
+        for (i, char) in enumerate(s): 
+            if char == "?":
+                seen = ans[i - 1] if i - 1 >= 0 else ""
+                seen += s[i + 1] if i + 1 < n else ""
+                
+                for newChar in "abc":
+                    if newChar not in seen: 
+                        ans += newChar
+
+                        break 
+                        
             else: 
-                ans += s[i]
-        return ans
+                ans += char
+
+        return ans 
