f99aad3c7dcf8573d48bdecfba90c4e5461cf68f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-27 10:59:43 -0300

    Update 1582. [Easy] Special Positions in a Binary Matrix.py

--- 1000 - 1999/1582. [Easy] Special Positions in a Binary Matrix.py
+++ 1000 - 1999/1582. [Easy] Special Positions in a Binary Matrix.py
@@ -2,20 +2,22 @@
 
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
-        n = len(mat)
-        m = len(mat[0])
-        mat2 = [[mat[i][j] for i in range(n)] for j in range(m)]
+        m, n = len(mat), len(mat[0])
         
-        print(mat, mat2)
+        tmp1 = [[0 for j in range(n + 1)] for i in range(m + 1)]
+        tmp2 = [[0 for j in range(n + 1)] for i in range(m + 1)]
         
-        aux = [sum(mat[i]) for i in range(n)]
-        aux2 = [sum(mat2[i]) for i in range(m)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                tmp1[i][j] = tmp1[i][j - 1] + mat[i - 1][j - 1]
+                tmp2[i][j] = tmp2[i - 1][j] + mat[i - 1][j - 1]
+                
+        ans = 0 
         
-        ans = 0
-        
-        for i in range(n):
-            for j in range(m):
-                if mat[i][j] == 1:
-                    if aux[i] == 1 and aux2[j] == 1:
-                        ans += 1
-        return ans
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1: 
+                    if tmp1[i + 1][n] == 1 and tmp2[m][j + 1] == 1: 
+                        ans += 1 
+                        
+        return ans 
