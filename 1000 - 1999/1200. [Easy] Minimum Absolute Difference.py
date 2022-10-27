b8d4fd09b14ca55c13f93506639869042c02fdd9
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-27 11:07:12 -0300

    Update 1200. [Easy] Minimum Absolute Difference.py

--- 1000 - 1999/1200. [Easy] Minimum Absolute Difference.py
+++ 1000 - 1999/1200. [Easy] Minimum Absolute Difference.py
@@ -2,19 +2,20 @@
 
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        arr.sort()
+        minDiff = inf 
+        
+        n = len(arr)
+        
+        arr.sort() 
+            
         ans = []
-        min_ = abs(arr[1] - arr[0])
-        ans = [[arr[0], arr[1]]]
-        l = len(arr)
         
-        for i in range(2, l):
-            dist = abs(arr[i] - arr[i - 1])
-            if dist < min_:
-                ans = [[arr[i - 1], arr[i]]]
-                min_ = dist
-            elif dist == min_:
-                ans.append([arr[i - 1], arr[i]])
-        
-        return ans
-        
+        for i in range(n - 1):
+            if arr[i + 1] - arr[i] < minDiff:                 
+                ans = [[arr[i], arr[i + 1]]]
+                minDiff = min(minDiff, arr[i + 1] - arr[i])       
+            
+            elif arr[i + 1] - arr[i] == minDiff:
+                ans.append([arr[i], arr[i + 1]])
+                
+        return ans 
