27b291ff67f02055ccecd35ca54e163ea2d169be
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-10-31 10:44:38 -0300

    Update 0766. [Easy] Toeplitz Matrix.py

--- 0001 - 0999/0766. [Easy] Toeplitz Matrix.py
+++ 0001 - 0999/0766. [Easy] Toeplitz Matrix.py
@@ -2,22 +2,25 @@
 
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        n = len(matrix)
-        m = len(matrix[0])
-        begin = [(0, i) for i in range(m)]
-        begin.extend([(i, 0) for i in range(1, n)])
-        min_ = min(n, m)
-        ans = True 
-        print(begin)
-        for (x, y) in begin:
-            aux = matrix[x][y]
-            for j in range(1, min_):
-                if x + j < n and y + j < m:
-                    if aux != matrix[x + j][y + j]:
-                        ans = False
-                        break
-            if not ans:
-                break 
-        return ans
-                    
+        m, n = len(matrix), len(matrix[0])
+        
+        stack = []
+        
+        for j in range(n):
+            stack.append((0, j))
+            
+        for i in range(1, m):
+            stack.append((i, 0))
+            
+        for (x, y) in stack: 
+            value = matrix[x][y]
+            
+            while x < m and y < n:         
+                if matrix[x][y] != value: 
+                    return False 
+                
+                x += 1 
+                y += 1
+                            
+        return True 
                 
