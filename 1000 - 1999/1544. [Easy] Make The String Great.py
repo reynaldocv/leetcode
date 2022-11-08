4eef80f9b1d7d138b982a22e16eb5ae881a295a7
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-08 13:51:05 -0300

    Update 1544. [Easy] Make The String Great.py

--- 1000 - 1999/1544. [Easy] Make The String Great.py
+++ 1000 - 1999/1544. [Easy] Make The String Great.py
@@ -2,29 +2,13 @@
 
 class Solution:
     def makeGood(self, s: str) -> str:
-        def isLower(l):
-            return True if abs(ord(l) - ord("a")<= 26) else False
-        def isUpper(l):
-            return True if abs(ord(l) - ord("A")<= 26) else False        
+        stack = []
         
-        FILO = []
-        n = len(s)
-        for i in range(n):
-            letter = s[i]
-            if len(FILO) > 0: 
-                if isLower(FILO[len(FILO) - 1]) and isUpper(letter):
-                    if FILO[len(FILO) - 1] == letter.lower():
-                        FILO.pop()
-                    else:
-                        FILO.append(letter)
-                elif isUpper(FILO[len(FILO) - 1]) and isLower(letter): 
-                    if FILO[len(FILO) - 1] == letter.upper():
-                        FILO.pop()
-                    else:
-                        FILO.append(letter)
-                else:
-                    FILO.append(letter)
-            else:
-                FILO.append(letter)
-        
-        return "".join(FILO)
+        for char in s: 
+            if stack and stack[-1] != char and stack[-1].lower() == char.lower():
+                    stack.pop() 
+                    
+            else: 
+                stack.append(char)
+                
+        return "".join(stack)
