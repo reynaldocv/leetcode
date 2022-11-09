0f4a89e00f33a7f2bc93f80468a251e5ac54df0a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-09 15:32:41 -0300

    Update 1047. [Easy] Remove All Adjacent Duplicates In String.py

--- 1000 - 1999/1047. [Easy] Remove All Adjacent Duplicates In String.py
+++ 1000 - 1999/1047. [Easy] Remove All Adjacent Duplicates In String.py
@@ -1,18 +1,18 @@
 # https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
 
 class Solution:
-    def removeDuplicates(self, s: str) -> str:        
-        aux, naux, n = [], 0, len(s)
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
         
-        for i in range(n):
-            if naux > 0 and s[i] == aux[naux - 1]:
-                aux.pop()
-                naux -= 1
-            else:
-                aux.append(s[i])
-                naux += 1
+        for char in s: 
+            if stack and stack[-1] == char: 
+                stack.pop() 
             
-        return "".join(aux)
+            else: 
+                stack.append(char)
+                
+        return "".join(stack)
+        
                 
                 
         
