6428d806fb96fb7508ce489014bbf40e9d54052d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-16 13:43:33 -0300

    Update 2278. [Easy] Percentage of Letter in String.py

--- 2000 - 2999/2278. [Easy] Percentage of Letter in String.py
+++ 2000 - 2999/2278. [Easy] Percentage of Letter in String.py
@@ -2,14 +2,14 @@
 
 class Solution:
     def percentageLetter(self, s: str, letter: str) -> int:
+        count = 0 
         total = 0 
-        cnt = 0 
+        
         for char in s: 
             if char == letter: 
-                cnt += 1 
+                count += 1 
             
             total += 1 
-            
-        ans = 100*cnt//total
+                
+        return int(count*100/total)
         
-        return ans 
