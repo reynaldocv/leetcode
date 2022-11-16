d9371147f25bafc95c1f70d3bf85ca764ed344d8
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-16 13:39:53 -0300

    Update 1903. [Easy] Largest Odd Number in String.py

--- 1000 - 1999/1903. [Easy] Largest Odd Number in String.py
+++ 1000 - 1999/1903. [Easy] Largest Odd Number in String.py
@@ -2,14 +2,13 @@
 
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        n = len(num)
-        j = -1
-        for i in range(n - 1, -1, -1):
-            if int(num[i]) % 2 == 1: 
-                j = i
-                break
+        limit = 0
         
-        return "" if j == -1 else num[:j + 1]
+        for (i, char) in enumerate(num): 
+            if int(char) % 2 == 1: 
+                limit = i + 1 
+                
+        return num[: limit]
         
     
         
