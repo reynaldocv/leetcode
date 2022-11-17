ff5efecefbef719746760676c78d55ff13965271
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-17 20:34:20 -0300

    Update 0223. [Medium] Rectangle Area.py

--- 0001 - 0999/0223. [Medium] Rectangle Area.py
+++ 0001 - 0999/0223. [Medium] Rectangle Area.py
@@ -2,16 +2,21 @@
 
 class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-        area1 = (ax2 - ax1)*(ay2 - ay1)
-        area2 = (bx2 - bx1)*(by2 - by1)
+class Solution:
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
+        def helper(ax, ay, bx, by):
+            return abs(bx - ax)*abs(by - ay)
         
+        ans = helper(ax1, ay1, ax2, ay2) + helper(bx1, by1, bx2, by2)
         
-        if ax2 <= bx1 or bx2 <= ax1 or by2 <= ay1 or by1 >= ay2:
-            return area1 + area2
-        else: 
-            overlapping = (min(ay2, by2) - max(ay1, by1)) * (min(ax2, bx2) - max(ax1, bx1))
-            return area1 + area2 - overlapping
-        
+        if not(ax2 <= bx1 or bx2 <= ax1 or by2 <= ay1 or ay2 <= by1): 
+            cx1 = max(ax1, bx1)
+            cy1 = max(ay1, by1)
+            cx2 = min(ax2, bx2)
+            cy2 = min(ay2, by2)
             
-        
+            ans -= helper(cx1, cy1, cx2, cy2)
+            
+        return ans 
+            
        
