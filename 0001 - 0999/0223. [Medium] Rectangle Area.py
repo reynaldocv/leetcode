c0b6e7c2159a32d35b377cb6eef29aceca55f69b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-17 20:34:32 -0300

    Update 0223. [Medium] Rectangle Area.py

--- 0001 - 0999/0223. [Medium] Rectangle Area.py
+++ 0001 - 0999/0223. [Medium] Rectangle Area.py
@@ -2,8 +2,6 @@
 
 class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-class Solution:
-    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
         def helper(ax, ay, bx, by):
             return abs(bx - ax)*abs(by - ay)
         
