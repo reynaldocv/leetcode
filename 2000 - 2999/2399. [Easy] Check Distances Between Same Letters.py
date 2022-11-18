bfd009643914502f545765fb29c406b64c67f029
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-18 11:57:54 -0300

    Update 2399. [Easy] Check Distances Between Same Letters.py

--- 2000 - 2999/2399. [Easy] Check Distances Between Same Letters.py
+++ 2000 - 2999/2399. [Easy] Check Distances Between Same Letters.py
@@ -3,14 +3,13 @@
 class Solution:
     def checkDistances(self, s: str, distance: List[int]) -> bool:
         seen = {}
-        index = {chr(ord("a") + i): i for i in range(26)}
-        
-        for (i, char) in enumerate(s):
-            if char in seen: 
-                dist = i - seen[char] - 1
-                if dist != distance[index[char]]:
-                    return False 
-            else:
+        for (i, char) in enumerate(s): 
+            if char not in seen: 
                 seen[char] = i 
                 
-        return True
+            else: 
+                idx = ord(char) - ord("a")
+                if i - seen[char] - 1 != distance[idx]: 
+                    return False 
+                
+        return True 
