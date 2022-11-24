c9b26267a5cc0d057e844be3e4d346c9cd1e737b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-24 15:14:50 -0300

    Update 1800. [Easy] Maximum Ascending Subarray Sum.py

--- 1000 - 1999/1800. [Easy] Maximum Ascending Subarray Sum.py
+++ 1000 - 1999/1800. [Easy] Maximum Ascending Subarray Sum.py
@@ -1,17 +1,28 @@
 # https://leetcode.com/problems/maximum-ascending-subarray-sum/
 
 class Solution:
-    def maxAscendingSum(self, nums: List[int]) -> int:
-        ans = aux = nums[0]
-        n = len(nums)
-        for i in range(1, n):
-            if nums[i - 1] < nums[i]:
-                aux += nums[i]
-            else: 
-                aux = nums[i]
-            ans = max(aux, ans)
+    def maxAscendingSum(self, nums: List[int]) -> int:      
+        prev = -inf
+        aSum = 0 
         
-        return ans
+        ans = 0 
+        
+        for num in nums: 
+            if prev < num: 
+                aSum += num                
+                
+            else: 
+                aSum = num 
+                
+            prev = num 
+            ans = max(ans, aSum)
+                
+        return ans 
+            
+            
+        
+
+        
             
                 
         
