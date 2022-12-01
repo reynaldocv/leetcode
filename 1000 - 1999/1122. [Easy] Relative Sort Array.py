569722b786acd95c81516eabd18195a03d504540
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-30 21:53:25 -0300

    Update 1122. [Easy] Relative Sort Array.py

--- 1000 - 1999/1122. [Easy] Relative Sort Array.py
+++ 1000 - 1999/1122. [Easy] Relative Sort Array.py
@@ -2,18 +2,11 @@
 
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        counter = {}
-        ans = []
-        for i in arr1:
-            counter[i] = counter.get(i, 0) + 1
-        for i in range(len(arr2)):
-            for j in range(counter[arr2[i]]):
-                ans.append(arr2[i])
-            del counter[arr2[i]]
+        index = defaultdict(lambda: inf)
         
-        for i in sorted(counter.keys()):
-            for j in range(counter[i]):
-                ans.append(i)
-            del counter[i]
-            
-        return ans
+        for (i, num) in enumerate(arr2):
+            index[num] = i 
+        
+        arr1.sort(key = lambda item: (index[item], item))
+        
+        return arr1
