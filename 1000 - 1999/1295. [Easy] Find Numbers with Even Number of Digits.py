3d88c63efc3f37c8c9d1309262c0026a225de25c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-30 22:16:40 -0300

    Update 1295. [Easy] Find Numbers with Even Number of Digits.py

--- 1000 - 1999/1295. [Easy] Find Numbers with Even Number of Digits.py
+++ 1000 - 1999/1295. [Easy] Find Numbers with Even Number of Digits.py
@@ -2,8 +2,11 @@
 
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
-        ans = 0
-        for num in nums:
-            ans = ans + 1 if (len(str(num)) % 2 == 0) else ans
+        ans = 0 
+        
+        for num in nums: 
+            if len(str(num)) % 2 == 0: 
+                ans += 1 
+                
         return ans
         
