7038bf2e7de5441676a254da5cf3bb1315601858
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-30 21:32:36 -0300

    Update 1351. [Easy] Count Negative Numbers in a Sorted Matrix.py

--- 1000 - 1999/1351. [Easy] Count Negative Numbers in a Sorted Matrix.py
+++ 1000 - 1999/1351. [Easy] Count Negative Numbers in a Sorted Matrix.py
@@ -3,8 +3,10 @@
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
         def helper(arr):
-            start = -1            
-            end = m 
+            n = len(arr)
+            
+            start = -1
+            end = n 
             
             while end - start > 1: 
                 mid = (end + start)//2
@@ -13,16 +15,32 @@ class Solution:
                     start = mid 
                 else: 
                     end = mid 
-            
-            return m - end
+                    
+            return n - end
         
         ans = 0 
         
-        m = len(grid[0])
-        
-        for row in grid: 
-            ans += helper(row)
+        for arr in grid: 
+            ans += helper(arr)
             
         return ans 
                     
+class Solution2:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
         
+        stack = [(m - 1, n - 1)] if grid[-1][-1] < 0 else []
+        seen = {(m - 1, n - 1)} if grid[-1][-1] < 0 else {}
+        
+        while stack: 
+            (x, y) = stack.pop() 
+            
+            for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                p, q = x + r, y + s
+                
+                if 0 <= p < m and 0 <= q < n: 
+                    if (p, q) not in seen and grid[p][q] < 0: 
+                        seen.add((p, q))
+                        stack.append((p, q))
+                        
+        return len(seen)  
