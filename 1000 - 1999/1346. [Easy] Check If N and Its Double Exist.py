323d06489a32aec2792b4891d39cd67e1cf41673
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-30 21:21:04 -0300

    Update 1346. [Easy] Check If N and Its Double Exist.py

--- 1000 - 1999/1346. [Easy] Check If N and Its Double Exist.py
+++ 1000 - 1999/1346. [Easy] Check If N and Its Double Exist.py
@@ -2,16 +2,13 @@
 
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        dic = {}
-        n = len(arr)
-        for i in range(n):
-            num = arr[i]
-            if num % 2 == 0:
-                if num//2 in dic or num*2 in dic:
-                    return True
-            else:
-                if num*2 in dic:
-                    return True
-            dic[num] = True
-        return False
+        seen = set()
         
+        for num in arr: 
+            if 2*num in seen or (num % 2 == 0 and num//2 in seen): 
+                return True 
+            
+            seen.add(num)
+            
+        return False 
+                
