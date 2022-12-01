73b5d432ddf4eb470fc0c8bbf665fd9da2348967
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-11-30 21:42:54 -0300

    Update 1128. [Easy] Number of Equivalent Domino Pairs.py

--- 1000 - 1999/1128. [Easy] Number of Equivalent Domino Pairs.py
+++ 1000 - 1999/1128. [Easy] Number of Equivalent Domino Pairs.py
@@ -2,25 +2,18 @@
 
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        def Combination(a, b):
-            ans = 1
-            c = max(a - b, b)
-            d = min(a - b, b)
-            for i in range(a, c ,  - 1):
-                ans *= i
-            for i in range(2, d + 1):
-                ans //= i
-            return ans
-            
-        dic = {}
-        for domino in dominoes: 
-            domino.sort()
-            aux = domino[0]*10  + domino[1]
-            dic[aux] = dic.get(aux, 0) + 1
-        print(dic)
-        ans = 0
-        for k in dic: 
-            if dic[k] > 1: 
-                ans += Combination(dic[k], 2)
+        counter = defaultdict(lambda: 0)
         
-        return ans
+        for (a, b) in dominoes:
+            if a > b: 
+                a, b = b, a
+            
+            counter[(a, b)] += 1 
+            
+        ans = 0 
+        
+        for key in counter: 
+            ans += counter[key]*(counter[key] - 1)//2
+                
+        return ans 
+        
