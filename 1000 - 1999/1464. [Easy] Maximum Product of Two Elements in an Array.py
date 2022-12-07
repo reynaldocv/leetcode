a48a11ba70514ac7d301774dfb539b194a88dc63
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-12-07 17:02:30 -0300

    Update 1464. [Easy] Maximum Product of Two Elements in an Array.py

--- 1000 - 1999/1464. [Easy] Maximum Product of Two Elements in an Array.py
+++ 1000 - 1999/1464. [Easy] Maximum Product of Two Elements in an Array.py
@@ -2,5 +2,14 @@
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        nums.sort(reverse = True)
-        return (nums[0] - 1)*(nums[1] - 1)
+        arr = [0, 0]
+        
+        for num in nums: 
+            insort(arr, num)
+            
+            arr = arr[1: ]
+            
+        return (arr[0] - 1)*(arr[1] - 1)
+                
+        
+        
