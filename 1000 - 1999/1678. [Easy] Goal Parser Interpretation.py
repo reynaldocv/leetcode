d77aeacfc48d76bb30d7caee99cac4f092dfe032
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-12-07 17:22:06 -0300

    Update 1678. [Easy] Goal Parser Interpretation.py

--- 1000 - 1999/1678. [Easy] Goal Parser Interpretation.py
+++ 1000 - 1999/1678. [Easy] Goal Parser Interpretation.py
@@ -6,4 +6,23 @@ class Solution:
         command = command.replace("()", "o")
         return command
         
+class Solution2:
+    def interpret(self, command: str) -> str:
+        stack = []
         
+        for char in command: 
+            if char == ")":
+                if stack[-1] == "(":
+                    stack.pop()
+                    stack.append("o")
+                else: 
+                    stack.pop()
+                    stack.pop()
+                    stack.pop()
+                    stack.append("al")
+                    
+            else: 
+                stack.append(char)
+
+        return "".join(stack)
+                
