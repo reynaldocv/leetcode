b08fad3057b110351f734a492c9a930c25ed3f5e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-12-26 03:09:25 -0300

    Update 0405. [Easy] Convert a Number to Hexadecimal.py

--- 0001 - 0999/0405. [Easy] Convert a Number to Hexadecimal.py
+++ 0001 - 0999/0405. [Easy] Convert a Number to Hexadecimal.py
@@ -2,17 +2,24 @@
 
 class Solution:
     def toHex(self, num: int) -> str:
-        if num == 0: return "0"
+        if num == 0: 
+            return "0"
         
-        val = {i: str(i) for i in range(10)}
-        for i in range(6):
-            val[i + 10] = chr(i + ord("a"))
         if num < 0: 
-            num += 2**32
-        ans = ""
-        while num > 0: 
-            r = num % 16
-            ans = val[r] + ans
-            num = num // 16
+            num += 2**32 
+            
+        letters = "0123456789abcdef"
         
+        ans = ""
+            
+        while num > 0: 
+            units = num % 16
+            
+            ans = letters[units] + ans
+            
+            num //= 16
+            
         return ans
+                
+            
+            
