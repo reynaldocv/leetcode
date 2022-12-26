550dc99e4360b5cba97b0d09e789738ca05e2516
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-12-26 03:17:05 -0300

    Update 0455. [Easy] Assign Cookies.py

--- 0001 - 0999/0455. [Easy] Assign Cookies.py
+++ 0001 - 0999/0455. [Easy] Assign Cookies.py
@@ -2,19 +2,26 @@
 
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        n , m = len(g), len(s)
-        g.sort(reverse = True)
-        s.sort(reverse = True)
-        i, j = 0, 0
-        ans = 0
-        while i < n and j < m: 
-            if g[i] > s[j]:
-                i += 1
-            else:
-                ans += 1
-                j += 1
-                i += 1
-           
-        return ans
+        g.sort() 
+        
+        s.sort() 
+        
+        m, n = len(g), len(s)
+        
+        i, j = 0, 0 
+        
+        ans = 0 
+        
+        while i < m and j < n: 
+            if g[i] <= s[j]:
+                ans += 1 
+                i += 1 
+                j += 1 
+                
+            else: 
+                j += 1 
+                
+        return ans 
+            
         
         
