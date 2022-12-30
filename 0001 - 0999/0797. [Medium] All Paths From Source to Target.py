08adc2222e7fd1bf6c5e43cc7478a80899c8f247
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2022-12-30 06:10:39 -0500

    Update 0797. [Medium] All Paths From Source to Target.py

--- 0001 - 0999/0797. [Medium] All Paths From Source to Target.py
+++ 0001 - 0999/0797. [Medium] All Paths From Source to Target.py
@@ -4,17 +4,20 @@ class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         n = len(graph)
         
-        stack = [([0])]
+        stack = [[0]]
+        
         ans = []
         
         while stack: 
-            way = stack.pop()
+            way = stack.pop(0) 
+            
             u = way[-1]
-            if u == n - 1:
+            
+            if u == n - 1: 
                 ans.append(way)
-            for v in graph[u]:
-                stack.append(way + [v])
+            else: 
+                for v in graph[u]:
+                    stack.append(way + [v])
+                    
+        return ans 
                 
-        return ans
-                
-        
