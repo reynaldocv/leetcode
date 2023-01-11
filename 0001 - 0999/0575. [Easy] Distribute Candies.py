2dc873a0d831cebe3e6b82d207938e0e460dff1e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-11 00:43:51 -0500

    Update 0575. [Easy] Distribute Candies.py

--- 0001 - 0999/0575. [Easy] Distribute Candies.py
+++ 0001 - 0999/0575. [Easy] Distribute Candies.py
@@ -1,11 +1,13 @@
 # https://leetcode.com/problems/distribute-candies/
 
 class Solution:
-    def distributeCandies(self, candies: List[int]) -> int:
-        dic = {}
-        n = len(candies)
-        for i in range(n):
-            dic[candies[i]] = dic.get(candies[i], 0) + 1
-            
-        return min(n//2, len(dic))
+    def distributeCandies(self, candyType: List[int]) -> int:
+        n = len(candyType) 
+        
+        seen = set([])
+        
+        for candy in candyType: 
+            seen.add(candy)
+                        
+        return min(n//2, len(seen))
         
