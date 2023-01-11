a85f499a70619ccea3a83e547b0da9bad283372b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-10 23:47:22 -0500

    Update 0482. [Easy] License Key Formatting.py

--- 0001 - 0999/0482. [Easy] License Key Formatting.py
+++ 0001 - 0999/0482. [Easy] License Key Formatting.py
@@ -1,20 +1,23 @@
 # https://leetcode.com/problems/license-key-formatting/
-
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
-        s = s[::-1]
-        s = s.replace("-", "")
-        n = len(s)
-        aux, ans = "", ""
-        for i in range(n):
-            aux += s[i].upper()
-            if len(aux) == k:
-                ans += aux + "-"
-                aux = ""
-        if len(aux) > 0:
-            ans += aux
-        else: 
-            ans = ans[:len(aux) - 1]
-        return ans[::-1]
+        ans = ""
+        
+        cnt = 0 
+        
+        for char in s[:: -1]:
+            if char != "-":
+                cnt = (cnt + 1) % k 
+                ans = ans + char.upper()
                 
+                if cnt == 0: 
+                    ans += "-"
+        
+        if not ans: 
+            return ""
+        
+        if ans[-1] == "-":
+            ans = ans[: -1]
+            
+        return ans[:: -1]
                 
