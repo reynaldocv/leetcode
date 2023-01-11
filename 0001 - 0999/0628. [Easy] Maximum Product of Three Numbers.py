1984ce6edc478d3f6166be9b3724a309f4880ff3
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-11 02:10:36 -0500

    Update 0628. [Easy] Maximum Product of Three Numbers.py

--- 0001 - 0999/0628. [Easy] Maximum Product of Three Numbers.py
+++ 0001 - 0999/0628. [Easy] Maximum Product of Three Numbers.py
@@ -2,23 +2,10 @@
 
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
-        arrPos = []
-        arrNeg = []
-        arrPos.sort()
-        for i in range(len(nums)):             
-            arrPos.append(nums[i])
-            if len(arrPos) >= 4:
-                arrPos.sort()
-                arrPos = arrPos[1:]
-            if nums[i] < 0:
-                arrNeg.append(nums[i])                
-                if len(arrNeg) >= 3:
-                    arrNeg.sort()
-                    arrNeg.pop()
-        ans, ans1, ans2 = 1, 0, 0
-        if len(arrNeg) == 2:
-            ans1 = arrNeg[0] * arrNeg[1] * arrPos[2]
-        ans2 = arrPos[0] * arrPos[1] * arrPos[2]
-        ans = max(ans1, ans2)
-        return ans
-            
+        nums.sort() 
+        
+        num1 = nums[-1]*nums[-2]*nums[-3]
+        
+        num2 = nums[0]*nums[1]*nums[-1]
+        
+        return max(num1, num2)
