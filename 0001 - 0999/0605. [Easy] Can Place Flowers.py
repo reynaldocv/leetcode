15090116fbe1abbf3d8cd6366eab45192334550f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-11 01:53:23 -0500

    Update 0605. [Easy] Can Place Flowers.py

--- 0001 - 0999/0605. [Easy] Can Place Flowers.py
+++ 0001 - 0999/0605. [Easy] Can Place Flowers.py
@@ -2,23 +2,29 @@
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        m = len(flowerbed)
-        ans = 0
-        if 1 not in flowerbed: 
-            ans = (m + 1)//2
-        else:
-            idx = flowerbed.index(1)
-            print(ans)
-            ans = idx//2
-            print(ans)            
-            counter = 0
-            for i in range(idx + 1, m):
-                print(i, ans, counter)
-                if flowerbed[i] == 1:
-                    ans += (counter - 1)//2
-                    counter = 0
+        first = True 
+        
+        count = 0 
+        
+        ans = 0 
+        
+        for flower in flowerbed: 
+            if flower == 0: 
+                count += 1 
+                
+            else: 
+                if first: 
+                    ans += count//2
+                    first = False 
                 else: 
-                    counter += 1
-            ans += counter//2
-            print(ans)
-        return n <= ans
+                    ans += (count - 1)//2
+                    
+                count = 0 
+        
+        if first: 
+            ans = (count + 1)//2
+        else: 
+            ans += count//2
+        
+        return ans >= n
+                
