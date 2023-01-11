d89c3267b85ce4e91ab7843df5b7d21abb60a31f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-11 00:40:19 -0500

    Update 0506. [Easy] Relative Ranks.py

--- 0001 - 0999/0506. [Easy] Relative Ranks.py
+++ 0001 - 0999/0506. [Easy] Relative Ranks.py
@@ -1,14 +1,24 @@
 # https://leetcode.com/problems/relative-ranks/
 
 class Solution:
-    def findRelativeRanks(self, score: List[int]) -> List[str]:
-        prizes = {1:"Gold Medal", 2: "Silver Medal", 3: "Bronze Medal"}
-        aux, n = [], len(score)
-        for i in range(n): 
-            aux.append((score[i], i))
-        aux.sort(reverse = True)
-        for i in range(n):
-            (sc, po) = aux[i]
-            score[po] = prizes.get(i + 1, str(i + 1))
-        return score
+    def findRelativeRanks(self, score: List[int]) -> List[str]:        
+        n = len(score) 
+        
+        labels = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+        
+        arr = [i for i in range(n)]
+            
+        arr.sort(key = lambda item: -score[item])
+        
+        ans = ["" for i in range(n)]
+        
+        for (i, ith) in enumerate(arr):
+            if i < 3: 
+                ans[ith] = labels[i]
+                 
+            else: 
+                 ans[ith] = str(i + 1)
+                 
+        return ans 
+                
         
