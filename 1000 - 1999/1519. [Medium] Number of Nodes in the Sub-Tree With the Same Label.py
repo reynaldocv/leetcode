f3d5d785343627e95676260d83a0c6f0ac55902c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-11 23:02:01 -0500

    Update 1519. [Medium] Number of Nodes in the Sub-Tree With the Same Label.py

--- 1000 - 1999/1519. [Medium] Number of Nodes in the Sub-Tree With the Same Label.py
+++ 1000 - 1999/1519. [Medium] Number of Nodes in the Sub-Tree With the Same Label.py
@@ -1,33 +1,39 @@
 # https://leetcode.com/problems/number-of-nodes-in-the-sub-tree-with-the-same-label/
 
 class Solution:
-    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
-        def helper(u):            
+    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:        
+        def helper(u):
             counter = defaultdict(lambda: 0)
-            if u in seen: 
-                return counter
             
-            else: 
-                seen[u] = True 
-                for v in graph[u]:
-                    cnt = helper(v)
-                    for key in cnt: 
-                        counter[key] += cnt[key]
-
-                counter[labels[u]] += 1 
-                ans[u] = counter[labels[u]]
-
-                return counter
+            for v in graph[u]:
+                if v not in seen: 
+                    seen.add(v)
+                    
+                    count = helper(v)
+                    
+                    for key in count: 
+                        counter[key] += count[key]
+                    
+            idx = index[labels[u]]
+            
+            counter[idx] += 1 
+            
+            ans[u] = counter[idx]
+            
+            return counter
+        
+        index = {chr(ord("a") + i): i for i in range(26)}
         
         graph = defaultdict(lambda: [])
-        for (a, b) in edges: 
-            graph[a].append(b)
-            graph[b].append(a)
-            
-        seen = {}
         
-        ans = [0 for _ in range(n)]
+        for (u, v) in edges: 
+            graph[u].append(v)
+            graph[v].append(u)
             
+        seen = {0}
+        
+        ans = [0 for i in range(n)]
+        
         helper(0)
         
         return ans 
