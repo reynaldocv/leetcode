87cfe871853b84d86a5be33c80beda4f87a783b9
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-13 02:01:19 -0500

    Update 0680. [Easy] Valid Palindrome II.py

--- 0001 - 0999/0680. [Easy] Valid Palindrome II.py
+++ 0001 - 0999/0680. [Easy] Valid Palindrome II.py
@@ -2,17 +2,28 @@
 
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def isPalindrome(s):
-            if len(s)>= 1:
-                return s == s[::-1]
-            return False
+        def helper(start, end):
+            if start > end: 
+                return True 
+            
+            elif s[start] == s[end]:
+                return helper(start + 1, end - 1)
+            
+            else: 
+                tmp = s[start: end]
+                
+                if tmp == tmp[:: -1]:
+                    return True 
+                
+                tmp = s[start + 1: end + 1]
+                
+                if tmp == tmp[:: -1]:
+                    return True 
+                
+                return False 
+            
+        n = len(s)
         
-        start, end = 0, len(s) - 1
-        while s[start] == s[end] and start < end:
-            start += 1
-            end -= 1
-        if end <= start:
-            return True
-        else: 
-            return isPalindrome(s[start + 1: end + 1]) or isPalindrome(s[start: end])
-        
+        return helper(0, n - 1)
+                
+                
