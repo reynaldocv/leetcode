d80bf2ed1f953e7b4b39bbb5205caaa52522cae8
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-13 02:07:36 -0500

    Update 0693. [Easy] Binary Number with Alternating Bits.py

--- 0001 - 0999/0693. [Easy] Binary Number with Alternating Bits.py
+++ 0001 - 0999/0693. [Easy] Binary Number with Alternating Bits.py
@@ -2,15 +2,14 @@
 
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        def DecToBin(n):
-            if n < 2: return n
-            return (n % 2) + 10*DecToBin(n//2)
+        binString = str(bin(n))[2: ]
         
-        n = str(DecToBin(n))
-        ans = True
-        for i in range(len(n) - 1):
-            if n[i] == n[i + 1]:
-                ans = False
-                break
-        return ans
+        n = len(binString)
         
+        for i in range(1, n):
+            if binString[i] == binString[i - 1]:
+                return False 
+            
+        return True 
+                   
+            
