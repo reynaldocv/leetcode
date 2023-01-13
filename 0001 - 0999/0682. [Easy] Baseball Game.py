c457bddbccbf63e6ea86d276c1370664fba18859
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-13 02:05:09 -0500

    Update 0682. [Easy] Baseball Game.py

--- 0001 - 0999/0682. [Easy] Baseball Game.py
+++ 0001 - 0999/0682. [Easy] Baseball Game.py
@@ -1,19 +1,21 @@
 # https://leetcode.com/problems/baseball-game/
 
 class Solution:
-    def calPoints(self, ops: List[str]) -> int:
-        n = len(ops)
-        arr = []
-        for i in range(n):
-            if ops[i] == "C":
-                arr.pop()            
-            elif ops[i] == "D":
-                aux = arr[len(arr) - 1]*2
-                arr.append(aux)         
-            elif ops[i] == "+":
-                aux = arr[len(arr) - 1] + arr[len(arr) - 2]
-                arr.append(aux)         
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        
+        for operation in operations: 
+            if operation == "C":
+                stack.pop() 
+                
+            elif operation == "D":
+                stack.append(2*stack[-1])
+                
+            elif operation == "+":
+                stack.append(stack[-1] + stack[-2])
+                
             else: 
-                arr.append(int(ops[i]))
-            print(arr)
-        return sum(arr)
+                stack.append(int(operation))
+                
+        return sum(stack)
+        
