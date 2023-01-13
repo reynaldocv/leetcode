8486292769f2ec4cb6b3033d5c795150da39399d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-13 01:37:24 -0500

    Update 0661. [Easy] Image Smoother.py

--- 0001 - 0999/0661. [Easy] Image Smoother.py
+++ 0001 - 0999/0661. [Easy] Image Smoother.py
@@ -2,19 +2,21 @@
 
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        n, m = len(img), len(img[0])
-        ans = [[0 for j in range(m)] for i in range(n)]
-        for i in range(n):
-            for j in range(m):
-                aux = img[i][j]
-                div = 1
-                for (a, b) in [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, -1], [-1, 1]]:
-                    x, y = i + a, j + b
-                    if 0 <= x < n and 0 <= y < m: 
-                        aux += img[x][y]
-                        div += 1
-                ans[i][j] = int(aux/div)
-        return ans
+        def helper(x, y):
+            ans = 0 
+            
+            cnt = 0 
+            
+            for i in [x - 1, x, x + 1]:
+                for j in [y - 1, y, y + 1]:
+                    if 0 <= i < m and 0 <= j < n: 
+                        ans += img[i][j]
+                        cnt += 1 
+                        
+            return ans//cnt
                     
+        m, n = len(img), len(img[0])
         
-        
+        return [[helper(i, j) for j in range(n)] for i in range(m)]
+             
+                    
