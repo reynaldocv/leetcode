75ca0e968e24ea976c8a97c73f0580793ed26181
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-18 22:27:11 -0500

    Update 0974. [Medium] Subarray Sums Divisible by K.py

--- 0001 - 0999/0974. [Medium] Subarray Sums Divisible by K.py
+++ 0001 - 0999/0974. [Medium] Subarray Sums Divisible by K.py
@@ -3,16 +3,22 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         seen = defaultdict(lambda: 0)
-        seen[0] += 1
-        ans = 0
+        
+        seen[0] += 1 
+        
         prev = 0 
-        for (i, num) in enumerate(nums):
-            prev = (prev + num) % k
+        
+        ans = 0 
+        
+        for num in nums:
+            prev = (prev + num) % k 
             
             if prev in seen: 
                 ans += seen[prev]
                 
-            seen[prev] += 1
-        
-        return ans
+            seen[prev] += 1 
             
+        return ans 
+            
+        
+        
