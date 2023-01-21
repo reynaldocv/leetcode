0fb9ed0c46ae2cd5c17e9618862a09900110dfa5
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-21 16:54:03 -0500

    Update 0700. [Easy] Search in a Binary Search Tree.py

--- 0001 - 0999/0700. [Easy] Search in a Binary Search Tree.py
+++ 0001 - 0999/0700. [Easy] Search in a Binary Search Tree.py
@@ -1,5 +1,3 @@
-# https://leetcode.com/problems/search-in-a-binary-search-tree/
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,18 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
-        if root == None:
-            return root
-        elif root.val == val:
-            return root
-        else:
-            left = self.searchBST(root.left, val)
-            if left != None:
-                return left
-            else:
-                right = self.searchBST(root.right, val)
-                return right
-            
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        def helper(node, val):
+            if node: 
+                if node.val == val: 
+                    return node
+                
+                elif val < node.val: 
+                    return helper(node.left, val)
+                
+                else: 
+                    return helper(node.right, val)
+                
+            return None
+        
+        return helper(root, val)
                 
         
