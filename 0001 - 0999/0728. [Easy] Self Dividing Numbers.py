7a0db61cb6f23464c509172fd4c19f54eb4b72b1
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-21 17:18:55 -0500

    Update 0728. [Easy] Self Dividing Numbers.py

--- 0001 - 0999/0728. [Easy] Self Dividing Numbers.py
+++ 0001 - 0999/0728. [Easy] Self Dividing Numbers.py
@@ -2,16 +2,23 @@
 
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        def helper(num):
+            tmp = num 
+            
+            while tmp > 0:
+                unit = tmp % 10
+                
+                if unit == 0 or num % unit != 0: 
+                    return False 
+                
+                tmp = tmp // 10 
+                
+            return True 
         
         ans = []
+        
         for num in range(left, right + 1):
-            DivideNumber = True
-            for digit in str(num):
-                if digit == "0" or num % int(digit) != 0:
-                    DivideNumber = False
-                    break
-            if DivideNumber == True:
+            if helper(num):
                 ans.append(num)
+                
         return ans
-            
-                    
