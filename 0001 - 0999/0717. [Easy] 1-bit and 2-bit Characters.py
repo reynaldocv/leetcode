0fdb800189f55116e6ca20b942e98b62436d014e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-21 17:14:36 -0500

    Update 0717. [Easy] 1-bit and 2-bit Characters.py

--- 0001 - 0999/0717. [Easy] 1-bit and 2-bit Characters.py
+++ 0001 - 0999/0717. [Easy] 1-bit and 2-bit Characters.py
@@ -3,21 +3,15 @@
 class Solution:
     def isOneBitCharacter(self, bits: List[int]) -> bool:
         n = len(bits)
-        if bits[n - 1] != 0: return False
         
-        bits = [str(bits[i]) for i in range(n - 1)]
-        exist = {"": True}
-        for i in range(n - 1):            
-            aux = "".join(bits[:i])
-            if aux in exist: 
-                if bits[i] == "0":
-                    exist[aux + "0"] = True
-                if i + 1 < n - 1:
-                    pair = bits[i] + bits[i + 1]
-                    if pair == "10" or pair == "11":
-                        exist[aux + pair] = True
+        i = 0 
         
-        return True if "".join(bits) in exist else False
+        while i < n - 1: 
+            if bits[i] == 1: 
+                i += 2 
+            else: 
+                i += 1
                 
+        return i == n - 1
         
         
