24a0495033642bbb092eafbaed34cb9b23fa3370
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-23 01:24:27 -0500

    Update 0997. [Easy] Find the Town Judge.py

--- 0001 - 0999/0997. [Easy] Find the Town Judge.py
+++ 0001 - 0999/0997. [Easy] Find the Town Judge.py
@@ -2,13 +2,16 @@
 
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        arr1 = [0 for i in range(n)]
-        arr2 = [0 for i in range(n)]
+        follower = [0 for i in range(n)]
+        followed = [0 for i in range(n)]
+        
         for (a, b) in trust:
-            arr1[a - 1] += 1
-            arr2[b - 1] += 1
+            follower[a - 1] += 1
+            followed[b - 1] += 1
             
         for i in range(n):
-            if arr1[i] == 0 and arr2[i] == n - 1:
+            if follower[i] == 0 and followed[i] == n - 1:
                 return i + 1
         return -1
+                
+        
