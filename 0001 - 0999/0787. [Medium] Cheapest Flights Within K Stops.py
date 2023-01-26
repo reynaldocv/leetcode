6847da4a115d8a2fd20fd3683756143a301b5c5d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-01-26 11:15:10 -0500

    Update 0787. [Medium] Cheapest Flights Within K Stops.py

--- 0001 - 0999/0787. [Medium] Cheapest Flights Within K Stops.py
+++ 0001 - 0999/0787. [Medium] Cheapest Flights Within K Stops.py
@@ -2,24 +2,29 @@
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = defaultdict(lambda: [])
-        for (u, v, price) in flights:
-            graph[u].append((v, price))
+        graph = defaultdict(lambda: {})
+        
+        for (a, b, price) in flights: 
+            graph[a][b] = price
             
-        prices = defaultdict(lambda: inf)
-        prices[src] = 0
+        distance = defaultdict(lambda: inf)
         
-        stack = [(0, 0, src)]
+        distance[src] = 0
+            
+        stack = [(src, 0, 0)]
         
-        while stack:
-            (price, flies, u) = stack.pop(0)
-            if flies <= k: 
-                for (v, p) in graph[u]:
-                    if prices[v] > price + p:
-                        prices[v] = price + p
-                        stack.append((prices[v], flies + 1, v))
-                    
-        return -1 if prices[dst] == inf else prices[dst]
+        while stack: 
+            (u, dist, steps) = stack.pop(0) 
+            
+            if steps <= k:             
+                for v in graph[u]:
+                    newDistance = dist + graph[u][v]
+                    if distance[v] > newDistance:
+                        distance[v] = newDistance
+                        
+                        stack.append((v, newDistance, steps + 1))
+                        
+        return -1 if distance[dst] == inf else distance[dst]
         
         
         
