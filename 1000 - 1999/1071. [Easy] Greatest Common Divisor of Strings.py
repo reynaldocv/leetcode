a4397f7624fa0dabfe326fb60d0707c0eb2c0621
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-02-01 08:48:55 -0500

    Update 1071. [Easy] Greatest Common Divisor of Strings.py

--- 1000 - 1999/1071. [Easy] Greatest Common Divisor of Strings.py
+++ 1000 - 1999/1071. [Easy] Greatest Common Divisor of Strings.py
@@ -2,16 +2,18 @@
 
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        n, m = len(str1), len(str2)
-        if n < m:
-            n, m, str1, str2 = m, n, str2, str1
-        for i in range(m, 0, -1):
-            if m % i == 0 and n % i == 0:
-                print(i, m, n)
-                aux = str2[:i]
-                aux2 = aux*(m//i)
-                aux3 = aux*(n//i)
-                if aux2 == str2 and aux3 == str1:
-                    return aux
+        m, n = len(str1), len(str2)
+        
+        if m < n: 
+            m, n = n, m 
+            str1, str2 = str2, str1
+        
+        for i in range(n, 0, -1):
+            if n % i == 0: 
+                ans = str2[: i]
+                
+                if str2 == ans*(n//i) and str1 == ans*(m//i):
+                    return ans 
+                
         return ""
         
