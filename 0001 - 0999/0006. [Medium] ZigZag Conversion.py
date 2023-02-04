09da4166dd4aed3eddd0c9201cb6adb2b109e1be
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-02-03 22:40:47 -0500

    Update 0006. [Medium] ZigZag Conversion.py

--- 0001 - 0999/0006. [Medium] ZigZag Conversion.py
+++ 0001 - 0999/0006. [Medium] ZigZag Conversion.py
@@ -5,15 +5,20 @@ class Solution:
         if numRows == 1: return s
         
         aux = ["" for i in range(numRows)]
+        
         n = len(s)
+        
         j = 0
         ratio = 1
+        
         for i in range(n):
             aux[j] += s[i]            
             if j == numRows - 1: 
                 ratio = -1
+                
             elif j == 0:                
                 ratio = 1
+                
             j += ratio
 
         return "".join(aux)
