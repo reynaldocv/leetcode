723ea5ed0cf5849f4ffd156cee02fddc5e86b3a6
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-02-07 07:59:55 -0500

    Update 0896. [Easy] Monotonic Array.py

--- 0001 - 0999/0896. [Easy] Monotonic Array.py
+++ 0001 - 0999/0896. [Easy] Monotonic Array.py
@@ -3,17 +3,24 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         n = len(nums)
-        if n == 1: return True
+        
+        if n == 1: 
+            return True
+        
         asc = desc = True
+        
         for i in range(0, n - 1):
             if nums[i] < nums[i + 1]:
                 asc = False
                 break
+                
+              
+                
         for i in range(0, n - 1):
             if nums[i] > nums[i + 1]:
                 desc = False
                 break
+                
         return (asc or desc)
-    
         
-        
+ 
