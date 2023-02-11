3b2021901b50a900f061c2e67f1668220e5c4a59
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-02-11 12:33:26 -0500

    Update 1129. [Medium] Shortest Path with Alternating Colors.py

--- 1000 - 1999/1129. [Medium] Shortest Path with Alternating Colors.py
+++ 1000 - 1999/1129. [Medium] Shortest Path with Alternating Colors.py
@@ -3,28 +3,32 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
         graph = defaultdict(lambda: defaultdict(lambda: []))
+        
         for (a, b) in redEdges: 
-            graph[a][True].append(b)
+            graph[True][a].append(b)
         
         for (a, b) in blueEdges: 
-            graph[a][False].append(b)
+            graph[False][a].append(b)
             
-        stack = [(-1, 0, True, True), (-1, 0, False, False)]        
+        stack = [(0, True), (0, False)]        
+        seen = {(0, True), (0, False)}
         
         ans = [-1 for _ in range(n)]
+        
         steps = 0 
-        dictSeen = {True: {}, False: {}} 
         
         while stack: 
             newStack = []
-            for (_, x, blue, seen) in stack: 
+            
+            for (x, blue) in stack: 
                 if ans[x] == -1:
                     ans[x] = steps
                 
-                for y in graph[x][not blue]:
-                    if (x, y, not blue) not in dictSeen[seen]:
-                        dictSeen[seen][(x, y, not blue)] = True
-                        newStack.append((x, y, not blue, seen))
+                for y in graph[not blue][x]:
+                    if (y, not blue) not in seen:
+                        seen.add((y, not blue))
+                        
+                        newStack.append((y, not blue))
             
             stack = newStack
             steps += 1 
