6ad894ad4f317627bd649cc71ca7c9ee5e7b3772
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-02-23 15:12:01 -0300

    Update 0762. [Easy] Prime Number of Set Bits in Binary Representation.py

--- 0001 - 0999/0762. [Easy] Prime Number of Set Bits in Binary Representation.py
+++ 0001 - 0999/0762. [Easy] Prime Number of Set Bits in Binary Representation.py
@@ -1,28 +1,26 @@
 # https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/
 
 class Solution:
-    def countPrimeSetBits(self, L: int, R: int) -> int:
-        def counter1(num):
-            ans = 0
-            while num > 0:
-                ans = ans + 1 if (num % 2 == 1) else ans
-                num = num // 2
-            return ans
-        
-        def isPrime(num):
-            if (num == 1):
-                return False
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        def helper(num):
+            ans = 0 
             
-            aux = int(num**(.5))
-            ans = True
-            for i in range(2, aux + 1):
-                if num % i == 0:
-                    ans = False
-                    break
-            return ans
+            while num: 
+                bit = num % 2
+                
+                if bit == 1: 
+                    ans += 1 
+                    
+                num = num//2
+                
+            return ans 
+               
+        primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
         
         ans = 0
-        for i in range(L, R + 1):
-            if isPrime(counter1(i)):
-                ans += 1
-        return ans
+        
+        for num in range(left, right + 1):
+            if helper(num) in primes: 
+                ans += 1 
+                
+        return ans 
