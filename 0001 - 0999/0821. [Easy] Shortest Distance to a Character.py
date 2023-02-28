ec095aedcb15289d55cb45eb9b9f5917669e1200
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-02-28 16:54:25 -0300

    Update 0821. [Easy] Shortest Distance to a Character.py

--- 0001 - 0999/0821. [Easy] Shortest Distance to a Character.py
+++ 0001 - 0999/0821. [Easy] Shortest Distance to a Character.py
@@ -1,19 +1,31 @@
 # https://leetcode.com/problems/shortest-distance-to-a-character/
 
 class Solution:
-    def shortestToChar(self, S: str, C: str) -> List[int]:
-        l = len(S)
-        ans = [l for i in range(l)]
-        aux = l
-        for i in range(l):
-            if S[i] == C:
-                ans[i] = 0
-            else:
-                ans[i] = ans[i - 1] + 1
-        for i in range(l - 2, -1, -1):
-            if S[i] != C:                
-                ans[i] = min(ans[i], ans[i + 1] + 1)
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        n = len(s)
         
+        ans = [n for _ in range(n)]
+        
+        distance = n
+        
+        for (i, char) in enumerate(s):
+            if char == c:
+                distance = 0 
+            else:
+                distance += 1
+                
+            ans[i] = distance
+            
+        distance = n
+        
+        for i in range(n - 1, -1, -1):
+            if s[i] == c:
+                distance = 0 
+            else:
+                distance += 1
+            
+            ans[i] = min(ans[i], distance)
+            
         return ans
                 
             
