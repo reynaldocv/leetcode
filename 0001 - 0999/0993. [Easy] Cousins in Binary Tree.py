5c88dfa3e24b4ace27a3c973985d4d721059dfa0
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-03-07 20:53:00 -0300

    Update 0993. [Easy] Cousins in Binary Tree.py

--- 0001 - 0999/0993. [Easy] Cousins in Binary Tree.py
+++ 0001 - 0999/0993. [Easy] Cousins in Binary Tree.py
@@ -7,17 +7,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
-        def levelFamily(root, level, father):
-            if root:
-                self.level[root.val] = (level, father)
-                levelFamily(root.left, level + 1, root.val)
-                levelFamily(root.right, level + 1, root.val)
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        def helper(node, lvl, parent):
+            if node: 
+                seen[node.val] = (lvl, parent)
                 
-        level = self.level = {}
-        levelFamily(root, 0, None)
-        if x in level and y in level:
-            return level[x][0] == level [y][0] and level[x][1] != level [y][1]
-        else:
-            return False
+                helper(node.left, lvl + 1, node.val)
+                helper(node.right, lvl + 1, node.val)
                 
+        seen = {}
+        
+        helper(root, 0, -1)
+        
+        if x not in seen and y not in seen: 
+            return False 
+        
+        return seen[x][0] == seen[y][0] and seen[x][1] != seen[y][1]
+        
+        
+                
+        
+        
+        
+        
