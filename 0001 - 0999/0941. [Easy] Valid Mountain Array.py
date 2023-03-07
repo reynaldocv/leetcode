f1245ea91013557e233200e0ef7d48e5a654fe86
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-03-07 20:33:42 -0300

    Update 0941. [Easy] Valid Mountain Array.py

--- 0001 - 0999/0941. [Easy] Valid Mountain Array.py
+++ 0001 - 0999/0941. [Easy] Valid Mountain Array.py
@@ -2,26 +2,27 @@
 
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
+        def helper(nums):
+            n = len(nums)
+            
+            ans = -1
+            
+            for i in range(1, n):
+                if nums[i - 1] < nums[i]:
+                    ans = i 
+                    
+                else: 
+                    break 
+                    
+            return ans 
+        
         n = len(arr)
-        if n <= 2: return False
-        asc = [False for i in range(n)]
-        des = [False for i in range(n)]
-        asc[0] = True
-        for i in range(1, n):
-            if arr[i] > arr[i - 1]:
-                asc[i] = True
-            else:
-                break
-        print(asc)
-        des[n - 1] = True
-        for i in range(n - 2, -1, -1):
-            if arr[i] > arr[i + 1]:
-                des[i] = True
-            else:
-                break        
-        print(des)
-        for i in range(1, n - 1):
-            if asc[i] and des[i]:
-                return True
-        return False
+        
+        left = helper(arr)
+        right = helper(arr[:: -1])
+        
+        if left == -1 or right == -1: 
+            return False 
+        
+        return left == n - right - 1
         
