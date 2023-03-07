e2df9214e36bef4ebc6e43c252584e8061fa3acf
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-03-07 20:46:08 -0300

    Update 0942. [Easy] DI String Match.py

--- 0001 - 0999/0942. [Easy] DI String Match.py
+++ 0001 - 0999/0942. [Easy] DI String Match.py
@@ -1,21 +1,25 @@
 # https://leetcode.com/problems/di-string-match/
 
 class Solution:
-    def diStringMatch(self, S: str) -> List[int]:
+    def diStringMatch(self, s: str) -> List[int]:
+        n = len(s)
+        
         ans = []
-        begin = 0
-        end = len(S) 
-        pos = 0
-        while (pos < len(S)):
-            if S[pos] == "I":
-                ans.append(begin)
-                begin += 1
-            else:
-                ans.append(end)
-                end -= 1
-            pos += 1
-        ans.append(end)
-        return ans
-    
+        
+        left = 0 
+        right = n 
+        
+        for i in range(n):
+            if s[i] == "I":
+                ans.append(left)
+                left += 1
             
+            else:
+                ans.append(right)
+                right -= 1
+            
+        ans.append(right)
+        
+        return ans
+        
             
