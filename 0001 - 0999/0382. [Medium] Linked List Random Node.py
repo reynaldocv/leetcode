9d8bdfd94ebf7a902eac8ff1473b0047402a8a9f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-03-10 15:23:20 -0300

    Update 0382. [Medium] Linked List Random Node.py

--- 0001 - 0999/0382. [Medium] Linked List Random Node.py
+++ 0001 - 0999/0382. [Medium] Linked List Random Node.py
@@ -9,15 +9,15 @@ class Solution:
 
     def __init__(self, head: Optional[ListNode]):
         self.arr = []
+        
         while head: 
             self.arr.append(head.val)
+        
             head = head.next
         
     def getRandom(self) -> int:
         return random.choice(self.arr)
         
-
-
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(head)
 # param_1 = obj.getRandom()
