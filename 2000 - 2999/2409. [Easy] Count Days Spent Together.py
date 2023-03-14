039736206a58bb03407ee14b6f0ae507f1cbcd77
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-03-14 15:58:33 -0300

    Update 2409. [Easy] Count Days Spent Together.py

--- 2000 - 2999/2409. [Easy] Count Days Spent Together.py
+++ 2000 - 2999/2409. [Easy] Count Days Spent Together.py
@@ -2,21 +2,19 @@
 
 class Solution:
     def countDaysTogether(self, arriveAlice: str, leaveAlice: str, arriveBob: str, leaveBob: str) -> int:
-        days = [0, 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30]
+        @cache 
+        def helper(word):
+            tmpM = int(word[: 2])
+            tmpD = int(word[3: ])
+            
+            return days[tmpM - 1] + tmpD
+            
+        days = [0, 31, 28, 31, 30, 31, 30, 31, 31,30, 31, 30]
         
-        for i in range(2, 13):
+        for i in range(1, 12):
             days[i] += days[i - 1]
-
-        arriveAlice = days[int(arriveAlice[: 2])] + int(arriveAlice[3:])
-        leaveAlice = days[int(leaveAlice[: 2])] + int(leaveAlice[3:])
-        
-        arriveBob = days[int(arriveBob[: 2])] + int(arriveBob[3:])
-        leaveBob = days[int(leaveBob[: 2])] + int(leaveBob[3:])
-        
-        arrive = max(arriveAlice, arriveBob)
-        leave = min(leaveAlice, leaveBob)
-        
-        if leave < arrive: 
-            return 0 
-        else: 
-            return leave - arrive + 1
+            
+        tmp = min(helper(leaveAlice), helper(leaveBob)) - max((helper(arriveAlice), helper(arriveBob))) + 1
+            
+        return tmp if tmp >= 1 else 0 
+            
