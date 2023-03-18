c0bb7669decdb156916e6d1d521e07d7d3066435
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-03-18 16:43:47 -0300

    Update 1472. [Medium] Design Browser History.py

--- 1000 - 1999/1472. [Medium] Design Browser History.py
+++ 1000 - 1999/1472. [Medium] Design Browser History.py
@@ -3,35 +3,36 @@
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.links = [homepage]
-        self.idx = 0
-        
+        self.pages = [homepage]
+        self.idx = 0 
 
     def visit(self, url: str) -> None:
-        self.links = self.links[:self.idx + 1]
-        self.links.append(url)
-        self.idx += 1
-
-    def back(self, steps: int) -> str:
-        if self.idx > steps: 
-            self.idx -= steps
-        else: 
-            self.idx = 0
-            
-        return self.links[self.idx]    
-         
-    def forward(self, steps: int) -> str:
-        if self.idx < len(self.links) - steps:
-            self.idx += steps
-        else: 
-            self.idx = len(self.links) - 1
-             
-        return self.links[self.idx]
+        self.pages = self.pages[: self.idx + 1]
         
-
+        self.pages.append(url)
+        self.idx += 1 
+        
+    def back(self, steps: int) -> str:
+        if self.idx - steps < 0:
+            self.idx = 0 
+            
+        else:
+            self.idx -= steps
+            
+        return self.pages[self.idx]
+    
+    def forward(self, steps: int) -> str:
+        if self.idx + steps >= len(self.pages):
+            self.idx = len(self.pages) - 1
+            
+        else: 
+            self.idx += steps
+            
+        return self.pages[self.idx]
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
 # obj.visit(url)
 # param_2 = obj.back(steps)
 # param_3 = obj.forward(steps)
+        
