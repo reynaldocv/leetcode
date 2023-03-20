3495163ec35ea50a7adbad13ca5283175c16cdb3
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-03-20 16:28:06 -0300

    Update 0567. [Medium] Permutation in String.py

--- 0001 - 0999/0567. [Medium] Permutation in String.py
+++ 0001 - 0999/0567. [Medium] Permutation in String.py
@@ -2,34 +2,37 @@
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        m, n = len(s1), len(s2)
+        def helper(word):
+            ans = (0, )*26
+        
+            for char in word: 
+                idx = index[char]
+
+                ans = ans[: idx] + (ans[idx] + 1,) + ans[idx + 1: ]
+
+            return ans
+            
+        n = len(s1)
         
         index = {chr(ord("a") + i): i for i in range(26)}
-        counter = (0, )*26
         
-        for char in s1: 
-            idx = index[char]
-            
-            counter = counter[: idx] + (counter[idx] + 1, ) + counter[idx + 1: ]
-            
-        counting = (0, )* 26
+        counter = helper(s1)
+                    
+        count = helper(s2[: n - 1])
         
-        for char in s2[: m - 1]:
+        for (i, char) in enumerate(s2[n - 1: ]):
             idx = index[char]
             
-            counting = counting[: idx] + (counting[idx] + 1, ) + counting[idx + 1: ]
+            count = count[: idx] + (count[idx] + 1,) + count[idx + 1: ]
             
-        for (i, char) in enumerate(s2[m - 1: ]):
-            idx = index[char]
-            
-            counting = counting[: idx] + (counting[idx] + 1, ) + counting[idx + 1: ]
-            
-            if counter == counting: 
+            if count == counter: 
                 return True 
             
             idx = index[s2[i]]
             
-            counting = counting[: idx] + (counting[idx] - 1, ) + counting[idx + 1: ]
+            count = count[: idx] + (count[idx] - 1,) + count[idx + 1: ]
             
-        return False      
+        return False 
+            
+               
         
