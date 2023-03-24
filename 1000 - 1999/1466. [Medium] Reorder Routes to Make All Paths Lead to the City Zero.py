f708ef73d17b840b45561574d9cd0b04f7f43c30
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-03-24 13:50:40 -0300

    Update 1466. [Medium] Reorder Routes to Make All Paths Lead to the City Zero.py

--- 1000 - 1999/1466. [Medium] Reorder Routes to Make All Paths Lead to the City Zero.py
+++ 1000 - 1999/1466. [Medium] Reorder Routes to Make All Paths Lead to the City Zero.py
@@ -2,25 +2,29 @@
 
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        visited = [False for _ in range(n)]
-        graph = [[] for _ in range(n)]
-        for (u, v) in connections: 
-            graph[u].append((v, 1))
-            graph[v].append((u, 0))
+        graph = defaultdict(lambda: {})
+        
+        for (a, b) in connections: 
+            graph[a][b] = 1
+            graph[b][a] = 0
             
         stack = [0]
-        visited[0] = True
-        ans = 0
+        
+        seen = {0}
+        
+        ans = 0 
         
         while stack: 
             u = stack.pop()
-            for (v, changed) in graph[u]:
-                if visited[v] == False: 
-                    visited[v] = True
-                    ans += changed
+            
+            for v in graph[u]:
+                if v not in seen: 
+                    seen.add(v)
                     stack.append(v)
                     
-        return ans
+                    ans += graph[u][v]                    
+                    
+        return ans 
                 
             
         
