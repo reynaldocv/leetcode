5647a59d7f1835c5878171920eb5f9d5787091f3
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-03-25 10:48:26 -0300

    Update 2316. [Medium] Count Unreachable Pairs of Nodes in an Undirected Graph.py

--- 2000 - 2999/2316. [Medium] Count Unreachable Pairs of Nodes in an Undirected Graph.py
+++ 2000 - 2999/2316. [Medium] Count Unreachable Pairs of Nodes in an Undirected Graph.py
@@ -3,32 +3,29 @@
 class Solution:
     def countPairs(self, n: int, edges: List[List[int]]) -> int:
         def helper(son):
-            while son != parent[son]:
+            while parent[son] != son: 
                 son = parent[son]
                 
             return son 
         
-        def collaborator(son1, son2):
-            parent1 = helper(son1)
-            parent2 = helper(son2)
-            
-            parent[parent1] = parent[parent2] = min(parent1, parent2)
-            
         parent = [i for i in range(n)]
         
-        for (a, b)  in edges: 
-            collaborator(a, b)
+        for (a, b) in edges: 
+            parentA = helper(a)
+            parentB = helper(b)
             
+            parent[parentA] = parent[parentB] = min(parentA, parentB)
+            
+        ans = n*(n + 1)//2
+        
         counter = defaultdict(lambda: 0)
         
         for i in range(n):
-            counter[helper(i)] += 1 
-        
-        ans = n*(n - 1)
-        
-        for key in counter: 
-            ans -= (counter[key] - 1)*counter[key]
+            parentI = helper(i)            
+            counter[parentI] += 1 
             
-        return ans//2
+            ans -= counter[parentI]
+            
+        return ans 
             
             
