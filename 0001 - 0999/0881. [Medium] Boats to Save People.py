20d122f6072c83495e076ac00d18090e1be2f040
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-03 10:35:01 -0300

    Update 0881. [Medium] Boats to Save People.py

--- 0001 - 0999/0881. [Medium] Boats to Save People.py
+++ 0001 - 0999/0881. [Medium] Boats to Save People.py
@@ -1,35 +1,24 @@
 # https://leetcode.com/problems/boats-to-save-people/
-
+    
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        
-        people.sort()
-        ans = 0
-        while len(people) >= 2: 
-            man1 = people.pop(~0)            
-            idx = bisect_left(people, limit - man1 + 1)
-            idx -= 1
-            if idx != -1:
-                people.pop(idx)
-            
-            ans += 1
-            
-        return ans + len(people)
-    
-class Solution2:
-    def numRescueBoats(self, people: List[int], limit: int) -> int:        
-        people.sort()
         n = len(people)
         
-        idx1, idx2 = 0, n - 1
-        ans = 0
+        people.sort() 
         
-        while idx1 <= idx2:
-            ans += 1
-            if people[idx1] + people[idx2] <= limit:
-                idx1 += 1
-            idx2 -= 1
+        start = 0 
+        end = n - 1 
         
+        ans = 0 
+        
+        while start <= end: 
+            ans += 1 
+            
+            if people[start] + people[end] <= limit: 
+                start += 1 
+                
+            end -= 1 
+                
         return ans
                 
         
