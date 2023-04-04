822f221164ff80310d27fe81db8b3780ad3cca12
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-04 14:06:19 -0300

    Update 2405. [Medium] Optimal Partition of String.py

--- 2000 - 2999/2405. [Medium] Optimal Partition of String.py
+++ 2000 - 2999/2405. [Medium] Optimal Partition of String.py
@@ -3,14 +3,16 @@
 class Solution:
     def partitionString(self, s: str) -> int:
         seen = set()
+        
         ans = 1
         
         for char in s:
             if char in seen:
                 ans += 1
-                seen = {char}
-            else:
-                seen.add(char)
+                
+                seen = set()
+            
+            seen.add(char)
                 
         return ans
         
