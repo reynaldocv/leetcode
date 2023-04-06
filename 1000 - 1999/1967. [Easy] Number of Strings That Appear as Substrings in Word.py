515d45e48330cefb7b187ac4e55442f8f0abd918
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-06 17:10:55 -0300

    Update 1967. [Easy] Number of Strings That Appear as Substrings in Word.py

--- 1000 - 1999/1967. [Easy] Number of Strings That Appear as Substrings in Word.py
+++ 1000 - 1999/1967. [Easy] Number of Strings That Appear as Substrings in Word.py
@@ -2,8 +2,11 @@
 
 class Solution:
     def numOfStrings(self, patterns: List[str], word: str) -> int:
-        ans = 0
-        for pattern in patterns:
+        ans = 0 
+        
+        for pattern in patterns: 
             if pattern in word: 
-                ans += 1
-        return ans
+                ans += 1 
+                
+        return ans 
+        
