486c491ec78fe4c5d3d18c8c54984150216f215d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-06 17:00:00 -0300

    Update 1952. [Easy] Three Divisors.py

--- 1000 - 1999/1952. [Easy] Three Divisors.py
+++ 1000 - 1999/1952. [Easy] Three Divisors.py
@@ -2,10 +2,14 @@
 
 class Solution:
     def isThree(self, n: int) -> bool:
-        if n < 4: return False
-        arr = [1 for i in range(n + 1)]
-        for i in range(2, n + 1):
-            for j in range(i, n + 1, i):
-                arr[j] += 1
-        print(arr)
-        return True if arr[n] == 3 else False
+        ans = 1
+        
+        for i in range(1, n):
+            if n % i == 0:
+                ans += 1 
+                
+        return ans == 3
+                
+                       
+
+        
