f565ad432d84c1b93f69afd990b110506a106a02
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-06 17:42:58 -0300

    Update 2011. [Easy] Final Value of Variable After Performing Operations.py

--- 2000 - 2999/2011. [Easy] Final Value of Variable After Performing Operations.py
+++ 2000 - 2999/2011. [Easy] Final Value of Variable After Performing Operations.py
@@ -3,11 +3,13 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         ans = 0 
-        for operation in operations: 
-            if operation == "X++" or operation == "++X":
-                ans += 1
-            if operation == "X--" or operation == "--X":
-                ans -= 1
         
-        return ans
+        for operation in operations: 
+            if operation == "--X" or operation == "X--":
+                ans -= 1 
+            
+            else: 
+                ans += 1 
+                
+        return ans 
         
