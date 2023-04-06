cce58ee3e33f8c5e10bf6c79b1267698fa1c0309
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-06 16:55:47 -0300

    Update 1945. [Easy] Sum of Digits of String After Convert.py

--- 1000 - 1999/1945. [Easy] Sum of Digits of String After Convert.py
+++ 1000 - 1999/1945. [Easy] Sum of Digits of String After Convert.py
@@ -1,20 +1,24 @@
 # https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
 
 class Solution:
+class Solution:
     def getLucky(self, s: str, k: int) -> int:
-        
-        aux = ""
-        n = len(s)
-        for i in range(n):
-            aux = aux + str(ord(s[i]) - ord("a") + 1)
-        e = 0
-        while e < k:
-            sum_ = 0
-            for i in aux: 
-                sum_ += int(i)
-            aux = str(sum_)
-            e += 1
+        def helper(word):
+            ans = 0
             
-        return int(aux)
+            for char in word:
+                ans += int(char)
+                
+            return str(ans)
         
+        index = {chr(ord("a") + i): str(i + 1) for i in range(26)}
         
+        ans = "" 
+        
+        for char in s: 
+            ans += index[char]
+          
+        for _ in range(k):
+            ans = helper(ans)
+            
+        return int(ans)
