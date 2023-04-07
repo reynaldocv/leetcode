0e1b82abdb03a8f3927d690d0688924f60d2c52a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-06 21:21:59 -0300

    Update 1020. [Medium] Number of Enclaves.py

--- 1000 - 1999/1020. [Medium] Number of Enclaves.py
+++ 1000 - 1999/1020. [Medium] Number of Enclaves.py
@@ -2,35 +2,49 @@
 
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        def helper(i, j, val):
-            grid[i][j] = val
-            seen = {(i, j): True}
-            stack = [(i, j)]
+        def helper(x, y):
+            stack = [(x, y)]
+            
+            grid[x][y] = 0 
+            seen = {(x, y)}
+            
             while stack: 
-                (i, j) = stack.pop()
-                for (x, y) in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-                    if 0 <= x < m and 0 <= y < n: 
-                        if grid[x][y] == 1 and (x, y) not in seen: 
-                            seen[(x, y)] = True
-                            grid[x][y] = val
-                            stack.append((x, y))
-            
+                (x, y) = stack.pop() 
+                
+                for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    p, q = x + r, y + s
+                    
+                    if 0 <= p < m and 0 <= q < n: 
+                        if grid[p][q] == 1: 
+                            grid[p][q] = 0 
+                            
+                            seen.add((p, q))
+                            stack.append((p, q))
+                            
             return len(seen)
-            
-        ans = sum(sum(row) for row in grid)
         
         m, n = len(grid), len(grid[0])
         
-        bounds= []
-        for i in range(m):
-            bounds.extend([(i, 0), (i, n - 1)])
+        ans = sum(sum(row) for row in grid)
         
-        for j in range(1, n - 1):
-            bounds.extend([(0, j), (m - 1, j)])
+        stack = set()
+        
+        for i in range(m):
+            stack.add((i, 0))
+            stack.add((i, n - 1))
             
-        for (i, j) in bounds:
-            if grid[i][j] == 1: 
-                ans -= helper(i, j, 2)
-                
-        return ans
+        for j in range(n):
+            stack.add((0, j))
+            stack.add((m - 1, j))
+            
+        for (x, y) in stack: 
+            if grid[x][y] == 1: 
+                ans -= helper(x, y)
+                    
+        return ans 
+        
+        
+        
+        
+        
                 
