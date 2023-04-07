92899476d5ef1a8750ee93fb7f1fbf80a127eb08
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-07 11:49:21 -0300

    Update 0914. [Easy] X of a Kind in a Deck of Cards.py

--- 0001 - 0999/0914. [Easy] X of a Kind in a Deck of Cards.py
+++ 0001 - 0999/0914. [Easy] X of a Kind in a Deck of Cards.py
@@ -2,23 +2,24 @@
 
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
-        def MCD (a, b):
+        def helper(a, b):
             if a % b == 0: 
-                return b
-            else: 
-                return MCD(b, a % b)
-        
-        dic = {}
-        for card in deck: 
-            dic[card] = dic.get(card, 0) + 1
+                return b 
             
-        card = deck[0]
-        ans = dic[card]
-        for k in dic: 
-            ans = MCD(ans, dic[k]) 
-                            
-        return False if ans == 1 else True
-                
+            return helper(b, a % b)
+        
+        counter = defaultdict(lambda: 0)
+        
+        for num in deck: 
+            counter[num] += 1 
+            
+        ans = counter[deck[0]]
+        
+        for key in counter: 
+            ans = helper(ans, counter[key])
+            
+        return ans >= 2
+        
         
         
         
