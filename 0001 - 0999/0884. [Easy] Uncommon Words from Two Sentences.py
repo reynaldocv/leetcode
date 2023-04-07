0f91d475fa39c232c2d91bfc581cb796fc1b33ba
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-07 10:57:27 -0300

    Update 0884. [Easy] Uncommon Words from Two Sentences.py

--- 0001 - 0999/0884. [Easy] Uncommon Words from Two Sentences.py
+++ 0001 - 0999/0884. [Easy] Uncommon Words from Two Sentences.py
@@ -2,15 +2,20 @@
 
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        words = s1 + " " + s2
-        words = words.split(" ")
-        dic = {}
-        for word in words:
-            dic[word] = dic.get(word, 0) + 1
-        ans = []
-        for word in dic: 
-            if dic[word] == 1:
-                ans.append(word)
+        counter = defaultdict(lambda: 0)
         
-        return ans
+        word = ""
+        
+        for char in s1 + " " + s2 + " ":
+            if char == " ":
+                if word != "":
+                    counter[word] += 1 
+                
+                word = ""
+                
+            else: 
+                word += char 
+                
+        return [key for key in counter if counter[key] == 1]
+                
         
