e42c04da4104e0a2c15c95800189d452cf3376d8
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-07 11:53:44 -0300

    Update 0922. [Easy] Sort Array By Parity II.py

--- 0001 - 0999/0922. [Easy] Sort Array By Parity II.py
+++ 0001 - 0999/0922. [Easy] Sort Array By Parity II.py
@@ -1,22 +1,25 @@
 # https://leetcode.com/problems/sort-array-by-parity-ii/
 
 class Solution:
-    def sortArrayByParityII(self, A: List[int]) -> List[int]:
-        auxEven = []
-        auxOdd = []        
-        for i in A:
-            if i % 2 == 0:
-                auxEven.append(i)
-            else:
-                auxOdd.append(i)
-        i = 0; 
-        while len(auxEven) != 0:
-            aux = auxEven.pop(0)
-            A[i] = aux
-            i += 1
-            aux = auxOdd.pop(0)
-            A[i] = aux
-            i += 1
-        return A
-            
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        
+        even = 0 
+        odd = 1 
+        
+        ans = [0 for _ in range(n)]
+        
+        for num in nums:
+            if num % 2 == 1: 
+                ans[odd] = num 
+                
+                odd += 2 
+                
+            else: 
+                ans[even] = num 
+                
+                even += 2 
+                
+        return ans 
+                
         
