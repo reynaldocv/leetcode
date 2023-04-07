33959ae51b9630bf3ef8e7bbdcd26eadc3816409
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-06 21:38:50 -0300

    Update 1507. [Easy] Reformat Date.py

--- 1000 - 1999/1507. [Easy] Reformat Date.py
+++ 1000 - 1999/1507. [Easy] Reformat Date.py
@@ -2,19 +2,28 @@
 
 class Solution:
     def reformatDate(self, date: str) -> str:
-        mes = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        dic = {}
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        
+        month = {}
+        
         for i in range(12):
-            if i < 9:
-                dic[mes[i]] = "0" + str(i + 1)
-            else:
-                dic[mes[i]] = str(i + 1)
+            if i < 9: 
+                month[months[i]] = "0" + str(i + 1)
             
+            else: 
+                month[months[i]] = str(i + 1)
+            
+        
         date = date.split(" ")
-        ans = date[2] + "-" + dic[date[1]] + "-"
-        day = date[0][0:len(date[0]) - 2]
-        if len(day) == 1:
-            day = "0" + day
-        ans += day
-        return ans
+        
+        if len(date[0]) == 3: 
+            date[0] = "0" + date[0][0]
+        
+        else: 
+            date[0] = date[0][0] + date[0][1]
+            
+        ans = date[2] + "-" + month[date[1]] + "-" + date[0]
+        
+        return ans 
+        
         
