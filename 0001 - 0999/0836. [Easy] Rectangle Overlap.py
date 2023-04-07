9385618cc8d80d4939190cc04358e457b8912ea7
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-07 10:42:25 -0300

    Update 0836. [Easy] Rectangle Overlap.py

--- 0001 - 0999/0836. [Easy] Rectangle Overlap.py
+++ 0001 - 0999/0836. [Easy] Rectangle Overlap.py
@@ -2,17 +2,10 @@
 
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
-        if rec1[0] >= rec2[2] or rec1[2] <= rec2[0]:
-            return False
+        (x1, y1, x2, y2) = rec1
+        (x3, y3, x4, y4) = rec2
         
-        if rec1[1] >= rec2[3] or rec1[3] <= rec2[1]:
-            return False
+        return min(x2, x4) >  max(x1, x3) and min(y2, y4) > max(y1, y3)
+           
         
-        if rec1[2] > rec2[0] or rec1[3] > rec2[1]:
-            return True
-        
-        if rec1[0] < rec2[2] or rec1[1] < rec2[3]:
-            return True
-        
-        return False
 
