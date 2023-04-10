c7b90ab0cf7aacb7aca9bb7aebf719492d90706d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-10 19:52:53 -0300

    Update 0933. [Easy] Number of Recent Calls.py

--- 0001 - 0999/0933. [Easy] Number of Recent Calls.py
+++ 0001 - 0999/0933. [Easy] Number of Recent Calls.py
@@ -1,18 +1,17 @@
 # https://leetcode.com/problems/number-of-recent-calls/
 
 class RecentCounter:
-
     def __init__(self):
-        self.arr = []
+        self.stack = []
         
-
     def ping(self, t: int) -> int:
-        self.arr.append(t)
-        while self.arr[0] < t - 3000: 
-            self.arr.pop(0)
+        self.stack.append(t)
+        
+        while self.stack and self.stack[0] < t - 3000:
+            self.stack.pop(0)
             
-        return len(self.arr)
-
+        return len(self.stack)
+        
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
 # param_1 = obj.ping(t)
