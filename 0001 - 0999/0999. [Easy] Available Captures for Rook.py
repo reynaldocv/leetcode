56a2b5fc0fa5e962e83555fe08bf794444a4da81
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-10 20:13:54 -0300

    Update 0999. [Easy] Available Captures for Rook.py

--- 0001 - 0999/0999. [Easy] Available Captures for Rook.py
+++ 0001 - 0999/0999. [Easy] Available Captures for Rook.py
@@ -2,52 +2,31 @@
 
 class Solution:
     def numRookCaptures(self, board: List[List[str]]) -> int:
-        x, y = -1, -1
+        def helper(x, y, r, s):
+            while 0 <= x < 8 and 0 <= y < 8: 
+                if board[x][y] == "p":
+                    return 1
+                
+                elif board[x][y] == "B":
+                    return 0 
+                
+                x, y = x + r, y + s
+                
+            return 0
+        
+        (x, y) = (0, 0)
+        
         for i in range(8):
             for j in range(8):
                 if board[i][j] == "R":
-                    x, y = i, j
-        ans = 0               
-        for i in range(1, 8):
-            if x + i < 8:
-                if board[x + i][y] == "p":
-                    ans += 1
-                    break
-                if board[x + i][y] == "B":
-                    break
-            else:
-                break
+                    (x, y) = (i, j)
+                    
+                    break 
+               
+        ans = 0 
         
-        for i in range(1, 8):
-            if x - i >= 0:
-                if board[x - i][y] == "p":
-                    ans += 1
-                    break
-                if board[x - i][y] == "B":
-                    break
-            else:
-                break
-        
-        for i in range(1, 8):
-            if y + i < 8:
-                if board[x][y + i] == "p":
-                    ans += 1
-                    break
-                if board[x][y + i] == "B":
-                    break
-            else:
-                break
-                
-        for i in range(1, 8):
-            if y - i >= 0:
-                if board[x][y - i] == "p":
-                    ans += 1
-                    break
-                if board[x][y - i] == "B":
-                    break
-            else:
-                break
-        
-    
-        
-        return ans
+        for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            ans += helper(x, y, r, s)
+            
+        return ans 
+                    
