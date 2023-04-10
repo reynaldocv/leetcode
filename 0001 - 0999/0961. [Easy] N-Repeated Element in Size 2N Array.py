b138e1484e73d9642d14bfb9964ed28fe6937cec
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-10 20:01:56 -0300

    Update 0961. [Easy] N-Repeated Element in Size 2N Array.py

--- 0001 - 0999/0961. [Easy] N-Repeated Element in Size 2N Array.py
+++ 0001 - 0999/0961. [Easy] N-Repeated Element in Size 2N Array.py
@@ -1,11 +1,12 @@
 # https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
 
 class Solution:
-    def repeatedNTimes(self, A: List[int]) -> int:
-        counter = {}
-        l = len(A)
-        for i in range(l):
-            counter[A[i]] = counter.get(A[i], 0) + 1
-            if counter[A[i]] > 1:
-                return A[i]
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        seen = set()
         
+        for num in nums: 
+            if num in seen: 
+                return num 
+            
+            seen.add(num)
+            
