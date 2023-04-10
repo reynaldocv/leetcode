5d49c6c34ef67b3100747f95636704c28cc5353d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-10 20:50:40 -0300

    Update 1009. [Easy] Complement of Base 10 Integer.py

--- 1000 - 1999/1009. [Easy] Complement of Base 10 Integer.py
+++ 1000 - 1999/1009. [Easy] Complement of Base 10 Integer.py
@@ -1,15 +1,23 @@
 # https://leetcode.com/problems/complement-of-base-10-integer/
 
 class Solution:
-    def bitwiseComplement(self, N: int) -> int:
-        if N == 0: return 1        
-        complement = 0
-        power = 0
-        while N > 0:
-            complement += (((N%2) + 1)% 2)*2**power
-            N = N // 2
-            power += 1
-        return complement
+    def bitwiseComplement(self, n: int) -> int:
+        if n == 0: 
+            return 1 
+        
+        ans = 0 
+        
+        ratio = 1
+        
+        while n: 
+            bit = n % 2
+            
+            ans += ratio*(1 - bit)
+            
+            n //= 2 
+            ratio *= 2
+            
+        return ans 
                           
                     
         
