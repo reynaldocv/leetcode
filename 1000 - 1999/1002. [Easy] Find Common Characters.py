343cd764a76f2e9db257a26f49793b1cf9a49679
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-10 20:28:39 -0300

    Update 1002. [Easy] Find Common Characters.py

--- 1000 - 1999/1002. [Easy] Find Common Characters.py
+++ 1000 - 1999/1002. [Easy] Find Common Characters.py
@@ -1,23 +1,29 @@
 # https://leetcode.com/problems/find-common-characters/
 
 class Solution:
-    def commonChars(self, A: List[str]) -> List[str]:
-        counterA = {}
-        for char in A[0]:
-            counterA[char] = counterA.get(char, 0) + 1
+    def commonChars(self, words: List[str]) -> List[str]:
+        counter = defaultdict(lambda: 0)
         
-        for i in range(1, len(A)):
-            counterB = {}
-            for char in A[i]:
-                if char in counterA:
-                    counterB[char] = counterB.get(char, 0) + 1                    
+        for char in words[0]:
+            counter[char] += 1 
             
-            for char in counterB:
-                counterB[char] = min(counterA[char], counterB[char])
-            counterA = counterB
+        for word in words[1: ]:
+            cnt = defaultdict(lambda: 0)
+            
+            for char in word:
+                if char in counter: 
+                    cnt[char] += 1 
+                    
+            for key in cnt: 
+                cnt[key] = min(cnt[key], counter[key])
+                
+            counter = cnt 
+            
         ans = []
-        for char in counterA:
-            for times in range(counterA[char]):
-                ans.append(char)
-        return ans
+            
+        for key in counter: 
+            for _ in range(counter[key]):
+                ans.append(key)
+                
+        return ans 
         
