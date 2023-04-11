80644300bc33fd196a4a9071b6f5124f4d9062f5
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-10 21:30:57 -0300

    Update 1037. [Easy] Valid Boomerang.py

--- 1000 - 1999/1037. [Easy] Valid Boomerang.py
+++ 1000 - 1999/1037. [Easy] Valid Boomerang.py
@@ -6,13 +6,14 @@ class Solution:
         (x1, y1) = points[1]
         (x2, y2) = points[2]
         
-        if (x0 - x1) != 0 and (x0 - x2):
+        if (x0 - x1) != 0 and (x0 - x2) != 0:
             return (y0 - y2)/(x0 - x2) != (y0 - y1)/(x0 - x1)
-        if (x1 - x2) != 0 and (x1 - x0):
+        
+        if (x1 - x2) != 0 and (x1 - x0) != 0:
             return (y1 - y2)/(x1 - x2) != (y1 - y0)/(x1 - x0)
-        if (x2 - x0) != 0 and (x2 - x1):
+        
+        if (x2 - x0) != 0 and (x2 - x1) != 0:
             return (y2 - y0)/(x2 - x0) != (y2 - y1)/(x2 - x1)
+        
         return False
-        
-        
         
