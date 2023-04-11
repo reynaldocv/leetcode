74470f71e825acbdd640fcbfdadf592a8518baeb
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-11 11:09:45 -0300

    Update 1175. [Easy] Prime Arrangements.py

--- 1000 - 1999/1175. [Easy] Prime Arrangements.py
+++ 1000 - 1999/1175. [Easy] Prime Arrangements.py
@@ -2,19 +2,29 @@
 
 class Solution:
     def numPrimeArrangements(self, n: int) -> int:
-        def factorial(n):
-            if n <= 1:
-                return 1
-            else:
-                return n*factorial(n - 1)
-            
-        arr = [1 for i in range(n + 1)]
-        primes = 0
-        for i in range(2, n + 1):
-            if arr[i] == 1:
-                primes += 1
-                for j in range(2*i, n + 1, i):
-                    arr[j] += 1
+        if n <= 2: 
+            1 
         
-        return (factorial(primes)*factorial(n - primes)) % (10**9 + 7)
+        MOD = 10**9 + 7
+        
+        dp = [1 for _ in range(n + 1)]
+        
+        primes = 0 
+        
+        for i in range(2, n + 1):
+            if dp[i] == 1: 
+                primes += 1 
+                
+            for j in range(2*i, n + 1, i):
+                dp[j] += 1 
+                
+        ans = 1 
+        
+        for i in range(2, primes + 1):
+            ans = (ans*i) % MOD
+            
+        for i in range(2, n - primes + 1):
+            ans = (ans*i) % MOD
+            
+        return ans 
         
