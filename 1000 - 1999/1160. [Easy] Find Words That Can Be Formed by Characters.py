2ea3e894d8f52eaa98ff5753a521a7d8d179a67d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-11 10:54:53 -0300

    Update 1160. [Easy] Find Words That Can Be Formed by Characters.py

--- 1000 - 1999/1160. [Easy] Find Words That Can Be Formed by Characters.py
+++ 1000 - 1999/1160. [Easy] Find Words That Can Be Formed by Characters.py
@@ -2,21 +2,30 @@
 
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
-        counterChars = {}
-        for char in chars:
-            counterChars[char] = counterChars.get(char, 0) + 1
+        counter = defaultdict(lambda: 0)
         
-        ans = 0
-        for word in words:
-            counter = {}
-            success = True
+        for char in chars: 
+            counter[char] += 1
+            
+        ans = 0 
+            
+        for word in words: 
+            n = len(word)
+            
+            cnt = defaultdict(lambda: 0)
+            
+            add = True
+            
             for char in word:
-                counter[char] = counter.get(char, 0) + 1
-            for char in counter:
-                if counter[char] > counterChars.get(char, 0):
-                    success = False
-                    break
-            if success:
-                ans += len(word)
-        return ans
-        
+                cnt[char] += 1 
+                
+                if cnt[char] > counter[char]:
+                    add = False 
+                    
+                    break 
+                
+            if add: 
+                ans += n 
+                
+        return ans 
+                    
