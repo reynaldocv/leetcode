e5fd86c1cb5235cddf9746590a15f0894cfd6667
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-10 22:00:46 -0300

    Update 1560. [Easy] Most Visited Sector in a Circular Track.py

--- 1000 - 1999/1560. [Easy] Most Visited Sector in a Circular Track.py
+++ 1000 - 1999/1560. [Easy] Most Visited Sector in a Circular Track.py
@@ -2,19 +2,18 @@
 
 class Solution:
     def mostVisited(self, n: int, rounds: List[int]) -> List[int]:
-        m = len(rounds)
-        i = rounds[0]
-        j = rounds[m - 1]
-        if i == j:
-            ans = [i] 
-        elif i < j: 
-            ans = [x for x in range(i, j + 1)]
+        start = rounds[0]
+        end = rounds[-1]
+        
+        if start == end: 
+            return [start]
+        
+        elif start <= end:
+            return [i for i in range(start, end + 1)]
+        
         else: 
-            ans = []
-            for x in range(1, n + 1):
-                if x <= j or x >= i:
-                    ans.append(x)
-                
-                
-        return ans
+            return [i for i in range(1, end + 1)] + [i for i in range(start, n + 1)]
+
+            
+        
         
