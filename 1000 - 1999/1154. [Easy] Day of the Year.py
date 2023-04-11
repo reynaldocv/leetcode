a3efce43d45694df99bb9104cf50e9e7e8c984ce
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-11 10:50:55 -0300

    Update 1154. [Easy] Day of the Year.py

--- 1000 - 1999/1154. [Easy] Day of the Year.py
+++ 1000 - 1999/1154. [Easy] Day of the Year.py
@@ -2,19 +2,29 @@
 
 class Solution:
     def dayOfYear(self, date: str) -> int:
-        daysOfMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30]
-        date = date.split("-")
+        days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         
-        daysth = 0
-        year = int(date[0])
-        month = int(date[1])
-        day = int(date[2])
+        year = int(date[: 4])
+        month = int(date[5: 7])
+        day = int(date[8: ])
         
-        for i in range(month):
-            daysth += daysOfMonth[i]
-        if month > 2:
-            if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-                daysth += 1
+        for i in range(1, 12):
+            days[i] += days[i - 1]
+            
+        ans = day
         
-        return daysth + day
+        if month >= 3:
+            if year % 4 == 0:
+                if year % 100 == 0:                    
+                    if year % 400 == 0: 
+                        ans += 1 
+                
+                else: 
+                    ans += 1 
+            
+        ans += days[month - 1] 
+        
+        return ans 
+        
+        
             
