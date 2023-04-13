959c5e1a64c66feebec45a983d23608f77203e40
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-13 15:02:38 -0300

    Update 1232. [Easay] Check If It Is a Straight Line.py

--- 1000 - 1999/1232. [Easay] Check If It Is a Straight Line.py
+++ 1000 - 1999/1232. [Easay] Check If It Is a Straight Line.py
@@ -2,19 +2,25 @@
 
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        
-        k = []
-        for i in range(1, len(coordinates)):
-            count = 1
-            dy = (coordinates[i][1]-coordinates[i-1][1])
-            dx = (coordinates[i][0]-coordinates[i-1][0])
-            if dx == 0:
-                count = inf 
-            else:
-                count = dy/dx
-                
-            k.append(count)
+        def helper(x, y, p, q):
+            if x == p: 
+                return "inf"
             
-        k = set(k)
+            else: 
+                return (y - q)/(x - p)
+            
+        counter = set()
         
-        return True if len(k) == 1 else False
+        n = len(coordinates)
+        
+        for i in range(1, n):
+            (x, y) = coordinates[0]
+            (p, q) = coordinates[i]
+            
+            counter.add(helper(x, y, p, q))
+                        
+            if len(counter) > 1: 
+                return False 
+            
+        return True 
+                     
