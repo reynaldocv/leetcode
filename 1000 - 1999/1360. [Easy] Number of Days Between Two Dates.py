d7b4dd5e262ded375f01113a52e490c1e3542427
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-12 23:37:44 -0300

    Update 1360. [Easy] Number of Days Between Two Dates.py

--- 1000 - 1999/1360. [Easy] Number of Days Between Two Dates.py
+++ 1000 - 1999/1360. [Easy] Number of Days Between Two Dates.py
@@ -2,28 +2,33 @@
 
 class Solution:
     def daysBetweenDates(self, date1: str, date2: str) -> int:
-        def NumberOfDays(day, month, year):
-            dayspermonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30]
-            days = 0
-            years = year - 1 - 1900
-            leapyears = years // 4 + 1
-            days += (years - leapyears)*365
-            days += leapyears*366
+        def helper(day, month, year):
+            ans = day
             
-            for i in range(month):
-                days += dayspermonth[i]
+            years = year - 1 - 1900
+            
+            leapYears = years // 4 + 1
+            
+            ans += (years - leapYears)*365            
+            ans += leapYears*366
+            
+            ans += days[month - 1]
             
             if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
                 if month > 2:
-                    day += 1
-                
-            days += day
-            
-            return days
+                    ans += 1
+                            
+            return ans
+        
+        days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30]
+        
+        for i in range(1, 12):
+            days[i] += days[i - 1]
         
         date1 = date1.split("-")
         date2 = date2.split("-")
-        days1 = NumberOfDays(int(date1[2]), int(date1[1]), int(date1[0]))
-        days2 = NumberOfDays(int(date2[2]), int(date2[1]), int(date2[0]))
+        
+        days1 = helper(int(date1[2]), int(date1[1]), int(date1[0]))
+        days2 = helper(int(date2[2]), int(date2[1]), int(date2[0]))
             
         return abs(days2 - days1)
