10de5e118965d9f329a8ce09b2a2302936a25d91
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-13 15:15:41 -0300

    Update 1185. [Easy] Day of the Week.py

--- 1000 - 1999/1185. [Easy] Day of the Week.py
+++ 1000 - 1999/1185. [Easy] Day of the Week.py
@@ -2,5 +2,31 @@
 
 class Solution:
     def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
-        ans = datetime.date(year, month, day)
-        return ans.strftime('%A')
+        def helper(day, month, year):
+            ans = day
+            
+            years = year - 1 - 1900
+            
+            leapYears = years // 4 + 1
+            
+            ans += (years - leapYears)*365            
+            ans += leapYears*366
+            
+            ans += days[month - 1]
+            
+            if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+                if month > 2:
+                    ans += 1
+                            
+            return ans
+        
+        labels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        
+        days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30]
+        
+        for i in range(1, 12):
+            days[i] += days[i - 1]
+        
+        days = helper(day, month, year) 
+        
+        return labels[days % 7]
