f635dc1f63e9b510664a69b51a585078e6c60a8a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-13 14:59:32 -0300

    Update 1252. [Easy] Cells with Odd Values in a Matrix.py

--- 1000 - 1999/1252. [Easy] Cells with Odd Values in a Matrix.py
+++ 1000 - 1999/1252. [Easy] Cells with Odd Values in a Matrix.py
@@ -1,17 +1,21 @@
 # https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/
 
 class Solution:
-    def oddCells(self, n: int, m: int, indices: List[List[int]]) -> int:
-        matrix = [[0 for i in range(m)] for j in range(n)]
+    def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
+        matrix = [[0 for j in range(n)] for i in range(m)]
+        
         for (row, col) in indices:
-            for c in range(m):
-                matrix[row][c] += 1
-            for r in range(n):
-                matrix[r][col] += 1
+            for j in range(n):
+                matrix[row][j] += 1
+                
+            for i in range(m):
+                matrix[i][col] += 1
+                
         ans = 0
-        for col in range(m):
-            for row in range(n):
-                if (matrix[row][col] % 2 == 1):
+        
+        for i in range(m):
+            for j in range(n):
+                if (matrix[i][j] % 2 == 1):
                     ans = ans + 1
                 
         return ans
