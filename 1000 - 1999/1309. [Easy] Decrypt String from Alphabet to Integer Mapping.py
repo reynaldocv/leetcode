fe45ad3f75477161b156fcdd75f6c71e9056902b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-12 23:59:11 -0300

    Update 1309. [Easy] Decrypt String from Alphabet to Integer Mapping.py

--- 1000 - 1999/1309. [Easy] Decrypt String from Alphabet to Integer Mapping.py
+++ 1000 - 1999/1309. [Easy] Decrypt String from Alphabet to Integer Mapping.py
@@ -2,15 +2,25 @@
 
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        codes = {}
-        for i in range(10, 27):
-            codes[str(i) + "#"] = chr(97 + i - 1)
+        letters = {str(i + 1) + "#": chr(ord("a") + i) for i in range(9, 26)}
         
-        for i in codes:
-            s = s.replace(i, codes[i])
-            
-        for i in range(1,10): 
-            s = s.replace(str(i), chr(96 + i))
-            
-        return s
+        for i in range(1, 10):
+            letters[str(i)] = chr(ord("a") + i - 1)
         
+        stack = []
+        
+        for char in s: 
+            if char == "#":
+                second = stack.pop() 
+                first = stack.pop() 
+                
+                stack.append(letters[first + second + "#"])
+                
+            else: 
+                stack.append(char)
+                
+        for (i, elem) in enumerate(stack): 
+            if elem in "123456789":
+                stack[i] = letters[elem]
+                
+        return "".join(stack)
