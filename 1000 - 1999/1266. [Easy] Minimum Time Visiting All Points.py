d9e1688757e651f7580378383b3c5643e212f69c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-13 14:54:37 -0300

    Update 1266. [Easy] Minimum Time Visiting All Points.py

--- 1000 - 1999/1266. [Easy] Minimum Time Visiting All Points.py
+++ 1000 - 1999/1266. [Easy] Minimum Time Visiting All Points.py
@@ -2,10 +2,16 @@
 
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        l = len(points)
+        n = len(points)
+        
         ans = 0
-        for i in range(1, l):
+        
+        for i in range(1, n):
             x = points[i][0] - points[i-1][0]
             y = points[i][1] - points[i-1][1]
+            
             ans += max(abs(x), abs(y))
+        
         return ans
+        
+        
