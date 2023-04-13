220fc9024e59cb6b50ed78cfd16b15d7921a9098
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-12 23:27:35 -0300

    Update 1370. [Easy] Increasing Decreasing String.py

--- 1000 - 1999/1370. [Easy] Increasing Decreasing String.py
+++ 1000 - 1999/1370. [Easy] Increasing Decreasing String.py
@@ -2,22 +2,33 @@
 
 class Solution:
     def sortString(self, s: str) -> str:
-        dic = {}
-        for i in s:
-            dic[i] = dic.get(i, 0) + 1
+        n = len(s)
         
-        ans = "";
-        while len(dic) > 0:
-            for a in sorted(dic.keys()):
-                ans += a
-                dic[a] -= 1
-                if dic[a] == 0:
-                    del dic[a]
-            for a in sorted(dic.keys(), reverse = True):
-                ans += a
-                dic[a] -= 1
-                if dic[a] == 0:
-                    del dic[a]
-        return ans
+        counter = defaultdict(lambda: 0)
+        letters = set() 
+        
+        for char in s: 
+            counter[char] += 1             
+            letters.add(char)
             
+        letters = [elem for elem in letters]
+        
+        letters.sort() 
+                
+        ans = ""
+        
+        while len(ans) < n:           
+            for letter in letters: 
+                if counter[letter] > 0: 
+                    ans += letter
+                
+                counter[letter] -= 1 
+            
+            for letter in letters[:: -1]: 
+                if counter[letter] > 0: 
+                    ans += letter
+                
+                counter[letter] -= 1 
+                
+        return ans 
             
