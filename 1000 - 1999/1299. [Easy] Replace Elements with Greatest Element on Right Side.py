2181e3aa659e473d685b80e1acb85b2b54a311ca
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-13 00:02:12 -0300

    Update 1299. [Easy] Replace Elements with Greatest Element on Right Side.py

--- 1000 - 1999/1299. [Easy] Replace Elements with Greatest Element on Right Side.py
+++ 1000 - 1999/1299. [Easy] Replace Elements with Greatest Element on Right Side.py
@@ -2,11 +2,15 @@
 
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        l = len(arr)
-        aux = arr[l - 1]
-        for i in range(l - 1, -1, -1):
-            aux = max(aux, arr[i])
-            arr[i] = aux
-        arr.pop(0)
-        arr.append(-1)
+        n = len(arr)
+        
+        maxElem = -1 
+        
+        for i in range(n - 1, -1, -1):
+            num = arr[i]
+            
+            arr[i] = maxElem 
+            
+            maxElem = max(maxElem, num)
+            
         return arr
