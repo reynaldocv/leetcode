ff1781496b934d31e44409495978cd1f50729910
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-13 14:49:57 -0300

    Update 1275. [Easy] Find Winner on a Tic Tac Toe Game.py

--- 1000 - 1999/1275. [Easy] Find Winner on a Tic Tac Toe Game.py
+++ 1000 - 1999/1275. [Easy] Find Winner on a Tic Tac Toe Game.py
@@ -2,32 +2,36 @@
 
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        def rowValue(a):
-            x = a[0]
-            for i in range(1, 3):
-                if a[i] != x:
-                    return ""
-            return "" if x == " " else x
+        def helper(player):
+            for i in range(3):                
+                if (i, 0) in seen[player] and (i, 1) in seen[player] and (i, 2) in seen[player]: 
+                    return True
+                
+                if (0, i) in seen[player] and (1, i) in seen[player] and (2, i) in seen[player]: 
+                    return True
+                
+            if (0, 0) in seen[player] and (1, 1) in seen[player] and (2, 2) in seen[player]: 
+                return True
             
-        michi = [[" " for i in range(3)] for j in range(3)]
-        m = "A"
-        for (i, j) in moves:
-            michi[i][j] = m
-            m = "A" if m == "B" else "B"
-        
-        michi2 = [[michi[i][j] for i in range(3)] for j in range(3)]
-       
-        ans = ""
-        for i in range(3):
-            ans += rowValue(michi[i])
-            ans += rowValue(michi2[i])
+            if (2, 0) in seen[player] and (1, 1) in seen[player] and (0, 2) in seen[player]: 
+                return True
             
-        ans += rowValue([michi[0][0], michi[1][1], michi[2][2]])
-        ans += rowValue([michi[2][0], michi[1][1], michi[0][2]])
+            return False
         
-        if len(ans) >= 1:
-            return ans[0] 
-        elif len(moves) == 9:
-            return "Draw"
+        seen = defaultdict(lambda: set())
+        
+        values = ["A", "B", "Draw", "Pending"]
+        
+        n = len(moves)
+        
+        for (i, (x, y)) in enumerate(moves):
+            seen[i % 2].add((x, y))
+            
+            if helper(i % 2):
+                return values[i % 2]
+            
+        if n == 9: 
+            return values[2]
+        
         else: 
-            return "Pending"
+            return values[3]
