992dfa0a9d29733586a70bee4b1d88ba68dff9e5
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-14 13:51:25 -0300

    Update 2216. [Medium] Minimum Deletions to Make Array Beautiful.py

--- 2000 - 2999/2216. [Medium] Minimum Deletions to Make Array Beautiful.py
+++ 2000 - 2999/2216. [Medium] Minimum Deletions to Make Array Beautiful.py
@@ -2,23 +2,21 @@
 
 class Solution:
     def minDeletion(self, nums: List[int]) -> int:
-        prev = inf
-        turn = False
         n = len(nums)
         
-        m = 0 
-        for num in nums:
-            if turn == False: 
-                prev = num
-                m += 1   
-                turn = True
-            else: 
-                if prev != num:
-                    m += 1 
-                    turn = False 
-                    prev = inf
+        i, j = 0, 1
         
-        if m % 2 == 1: 
-            m -= 1 
-            
-        return n - m 
+        ans = n
+        
+        while j < n: 
+            if nums[i] == nums[j]:
+                j += 1 
+                
+            else: 
+                ans -= 2 
+                
+                i = j + 1
+                j += 1 
+                
+        return ans 
+        
