b81ad46ffadad850754398098dc77e57a531e8d3
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-14 13:41:02 -0300

    Update 0456. [Medium] 132 Pattern.py

--- 0001 - 0999/0456. [Medium] 132 Pattern.py
+++ 0001 - 0999/0456. [Medium] 132 Pattern.py
@@ -1,8 +1,27 @@
 # https://leetcode.com/problems/132-pattern/
 
 class Solution:
-    def find132pattern(self, nums: List[int]) -> bool:        
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        
+        ref = -inf 
+        
+        for num in nums[:: -1]:
+            if num < ref: 
+                return True 
+            
+            while stack and stack[-1] < num: 
+                ref = stack.pop() 
+                
+            stack.append(num)
+        
+        return False 
+    
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
         n = len(nums)
+        
         left = [num for num in nums]
         
         for i in range(1, n):
@@ -11,32 +30,17 @@ class Solution:
         arr = [nums[-1]]
         
         for i in range(n - 2, 0, -1):
-            if left[i - 1] < nums[i]:
-                idx = bisect_right(arr, left[i - 1])
-                if idx < len(arr):
-                    if arr[idx] < nums[i]:
-                        return True
+            elem = left[i - 1]
             
+            if elem < nums[i]:
+                idx = bisect_left(arr, elem + 1)
+                
+                if idx < len(arr) and elem < arr[idx] < nums[i]:
+                    return True 
+                
             insort(arr, nums[i])
             
-        return False
-      
-class Solution2:
-    def find132pattern(self, nums: List[int]) -> bool: 
-        stack = []
-        ref = -inf
-        
-        for x in nums[::-1]:
-            if x < ref: 
-                return True
-            while stack and stack[-1] < x: 
-                ref = stack.pop()
-            
-            stack.append(x)
-            
-        return False
-
-        
+        return False         
         
 
       
