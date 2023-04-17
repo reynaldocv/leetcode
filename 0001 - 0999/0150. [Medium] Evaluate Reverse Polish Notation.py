59534e30ba32aa53d4030c11cf0ccd36c8f1282e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-17 14:11:22 -0300

    Update 0150. [Medium] Evaluate Reverse Polish Notation.py

--- 0001 - 0999/0150. [Medium] Evaluate Reverse Polish Notation.py
+++ 0001 - 0999/0150. [Medium] Evaluate Reverse Polish Notation.py
@@ -3,24 +3,19 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        n = len(tokens)
-        for i in range(n):
-            token = tokens[i]
-            if token in "+-/*":
-                num2 = int(stack.pop())
-                num1 = int(stack.pop())
-                if token == "+":
-                    stack.append(num1 + num2)
-                elif token == "-":
-                    stack.append(num1 - num2)
-                elif token == "*":
-                    stack.append(num1 * num2)
-                else:
-                    stack.append(num1 / num2)                
+        
+        for token in tokens: 
+            if token in "+-*/":
+                num2 = str(int(stack.pop()))
+                num1 = str(int(stack.pop()))
+                
+                stack.append(eval(num1 + token + num2))
+                
             else: 
                 stack.append(token)
-            
-        return int(stack.pop())
+                
+        return stack[-1]
+        
                 
             
             
