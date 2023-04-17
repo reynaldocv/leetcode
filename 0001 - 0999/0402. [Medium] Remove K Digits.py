04e1e1d22bae7e2baef0b275fa7d0a521c8f2c75
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-17 14:37:43 -0300

    Update 0402. [Medium] Remove K Digits.py

--- 0001 - 0999/0402. [Medium] Remove K Digits.py
+++ 0001 - 0999/0402. [Medium] Remove K Digits.py
@@ -1,20 +1,26 @@
 # https://leetcode.com/problems/remove-k-digits/
 
 class Solution:
-    def removeKdigits(self, num: str, k: int) -> str:        
-        ans = []
+    def removeKdigits(self, num: str, k: int) -> str:
         n = len(num)
         
-        for (i, char) in enumerate(num):
-            while ans and ans[-1] > char and len(ans) > i - k:
-                ans.pop()
-            
-            if len(ans) < n - k: 
-                ans.append(char)
-                
-        while ans and ans[0] == "0":
-            ans.pop(0)
-            
-        return "".join(ans) if ans else "0"
-                
+        if n == k: 
+            return "0"
         
+        stack = []
+        
+        for (i, digit) in enumerate(num):
+            while stack and stack[-1] > digit and len(stack) + n - i > n - k:
+                stack.pop() 
+             
+            if len(stack) < n - k: 
+                stack.append(digit)
+                
+        while stack and stack[0] == "0":
+            stack.pop(0)
+            
+        if stack:             
+            return "".join(stack) 
+        
+        return "0"
+               
