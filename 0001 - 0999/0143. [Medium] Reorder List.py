daf3a0a9ae733d4ec7dfc94f041bf7aaecf3a15e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-17 15:55:55 -0300

    Update 0143. [Medium] Reorder List.py

--- 0001 - 0999/0143. [Medium] Reorder List.py
+++ 0001 - 0999/0143. [Medium] Reorder List.py
@@ -9,55 +9,36 @@ class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
-        """
-        n = 0 
-        reverse = None
-        cur = head
-        while cur: 
-            reverse = ListNode(cur.val, reverse)
-            cur = cur.next
-            n += 1
+        """        
+        arr = []
         
-        ans = ListNode(0, head)
-        i = 0
-        while (i < n//2):
-            ans.next = head
-            head = head.next
-            ans = ans.next
+        while head: 
+            arr.append(head)
             
-            ans.next = reverse
-            reverse = reverse.next
-            ans = ans.next            
-            i += 1
+            head = head.next 
+            
+        ans = []
         
-        if n % 2 == 0: 
-            ans.next = None
-        else: 
-            ans.next.next = None
-    
-    
-class Solution2:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        stack = []
-        cur = head.next
-        while cur:
-            stack.append(cur)
-            cur = cur.next
+        turn = 1
+        
+        while arr: 
+            if turn: 
+                ans.append(arr.pop(0))
             
-        cur = head
-        right = True
-        while stack: 
-            if right: 
-                cur.next = stack.pop()
-            else: 
-                cur.next = stack.pop(0)
-            right = not right
-            cur = cur.next
+            else:
+                ans.append(arr.pop())
+                
+            turn = 1 - turn 
             
-        cur.next = None
+        n = len(ans)
+        
+        for i in range(n - 1):
+            ans[i].next = ans[i + 1]
             
+        ans[-1].next = None 
+        
+        
+            
+        
         
         
