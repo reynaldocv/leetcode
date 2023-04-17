5d3114be03240ffb5d35cc01ef1929c69f6d4986
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-17 16:30:04 -0300

    Update 1996. [Medium] The Number of Weak Characters in the Game.py

--- 1000 - 1999/1996. [Medium] The Number of Weak Characters in the Game.py
+++ 1000 - 1999/1996. [Medium] The Number of Weak Characters in the Game.py
@@ -2,15 +2,18 @@
 
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        properties.sort(key = lambda item: (-item[0], item[1]))
+        
+        (maxAttack, maxDefense) = properties[0]
+        
         ans = 0 
-        properties.sort(reverse = True, key = lambda item: (item[0], -item[1]))
-        maxDef = -inf
-        for (attack, defense) in properties:
-            if defense < maxDef: 
-                ans += 1
-            else: 
-                maxDef = defense
         
-        return ans
-        
+        for (attack, defense) in properties[1: ]: 
+            if attack < maxAttack and defense < maxDefense:
+                ans +=1 
+                
+            maxDefense = max(maxDefense, defense)
+            
+        return ans 
+            
         
