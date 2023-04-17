049f405c2b26503537cf625a7e5b1b1481a760fe
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-17 15:44:51 -0300

    Update 1653. [Medium] Minimum Deletions to Make String Balanced.py

--- 1000 - 1999/1653. [Medium] Minimum Deletions to Make String Balanced.py
+++ 1000 - 1999/1653. [Medium] Minimum Deletions to Make String Balanced.py
@@ -2,20 +2,27 @@
 
 class Solution:
     def minimumDeletions(self, s: str) -> int:
+        def helper(word, character):
+            ans = [0]
+            
+            counter = 0 
+            
+            for char in word: 
+                if char == character:
+                    counter += 1 
+                    
+                ans.append(counter)
+                
+            return ans 
+        
         n = len(s)
         
-        right = [0 for i in range(n)]
-        cnt = 0
-        for i in range(n - 1, -1, -1):
-            cnt += 1 if s[i] == "a" else 0 
-            right[i] = cnt
-            
-        ans = min(cnt, n - cnt)
+        left = helper(s, "a")
+        right = helper(s[:: -1], "b")[:: -1]
         
-        left = 0 
-        for i in range(n):
-            ans = min(ans, left + right[i])
-            left += 1 if s[i] == "b" else 0
-            
-        return ans
+        ans = inf 
         
+        for i in range(n + 1):
+            ans = min(ans, n - left[i] - right[i])
+            
+        return ans 
