d85db5cd4afb249270c5b43ad39d4ef1b0007d89
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-18 11:48:34 -0300

    Update 0739. [Medium] Daily Temperatures.py

--- 0001 - 0999/0739. [Medium] Daily Temperatures.py
+++ 0001 - 0999/0739. [Medium] Daily Temperatures.py
@@ -3,14 +3,18 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
-        ans = [0]*n
+        
         stack = []
-        for (i, t) in enumerate(temperatures):
-            while stack and stack[-1][0] < t: 
-                (elem, idx) = stack.pop()
-                ans[idx] = i - idx
+        
+        ans = [0 for _ in range(n)]
+        
+        for (i, temperature) in enumerate(temperatures):
+            while stack and stack[-1][0] < temperature: 
+                (_, idx) = stack.pop() 
                 
-            stack.append((t, i))
+                ans[idx] = i - idx 
+                
+            stack.append((temperature, i))
             
-        return ans
+        return ans 
             
