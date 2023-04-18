c656e0bcfe4c1bbedbbbe71f4c7d1bc89a0e2d0b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-18 15:22:23 -0300

    Update 1081. [Medium] Smallest Subsequence of Distinct Characters.py

--- 1000 - 1999/1081. [Medium] Smallest Subsequence of Distinct Characters.py
+++ 1000 - 1999/1081. [Medium] Smallest Subsequence of Distinct Characters.py
@@ -3,28 +3,29 @@
 class Solution:
     def smallestSubsequence(self, s: str) -> str:
         counter = defaultdict(lambda: 0)
-        for char in s: 
-            counter[char] += 1
-            
-        stack, seen = [], {}
         
         for char in s: 
-            if char in seen: 
+            counter[char] += 1            
+        
+        stack = []
+        seen = set()
+        
+        for char in s:
+            if char in seen:
                 counter[char] -= 1
-                continue
-            
-            while stack and stack[-1] > char: 
-                if counter[stack[-1]] > 1:
-                    counter[stack[-1]] -= 1
+                
+            else: 
+                while stack and stack[-1] > char and counter[stack[-1]] > 1:
                     elem = stack.pop()
-                    seen.pop(elem)
-                else: 
-                    break
-            
-            stack.append(char)
-            seen[char] = True
-            
+
+                    counter[elem] -= 1
+                    seen.remove(elem)
+
+                stack.append(char)
+                seen.add(char)
+
         return "".join(stack)
+        
                 
 
         
