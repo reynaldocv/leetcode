0c702b39ac27bc1dfedc81043b27b2524b670199
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-18 15:21:07 -0300

    Update 0316. [Medium] Remove Duplicate Letters.py

--- 0001 - 0999/0316. [Medium] Remove Duplicate Letters.py
+++ 0001 - 0999/0316. [Medium] Remove Duplicate Letters.py
@@ -3,26 +3,26 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         counter = defaultdict(lambda: 0)
+        
         for char in s: 
-            counter[char] += 1
-            
+            counter[char] += 1            
         
         stack = []
-        seen = {}
+        seen = set()
         
         for char in s:
             if char in seen:
                 counter[char] -= 1
-                continue
-            while stack and stack[-1] > char:
-                if counter[stack[-1]] > 1:
+                
+            else: 
+                while stack and stack[-1] > char and counter[stack[-1]] > 1:
                     elem = stack.pop()
+
                     counter[elem] -= 1
-                    seen.pop(elem)
-                else:
-                    break
-                    
-            stack.append(char)
-            seen[char] = True
-            
+                    seen.remove(elem)
+
+                stack.append(char)
+                seen.add(char)
+
         return "".join(stack)
+        
