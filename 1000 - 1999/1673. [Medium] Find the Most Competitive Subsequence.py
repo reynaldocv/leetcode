10e97756570ce28b0f9650cb53bb4fe30a6ee853
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-19 18:40:19 -0300

    Update 1673. [Medium] Find the Most Competitive Subsequence.py

--- 1000 - 1999/1673. [Medium] Find the Most Competitive Subsequence.py
+++ 1000 - 1999/1673. [Medium] Find the Most Competitive Subsequence.py
@@ -2,15 +2,16 @@
 
 class Solution:
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)        
-        stack = [] 
+        n = len(nums)
         
-        for (i, num) in enumerate(nums):             
+        stack = []
+        
+        for (i, num) in enumerate(nums): 
             while stack and stack[-1] > num and len(stack) + n - i > k: 
-                stack.pop()
+                stack.pop() 
                 
             if len(stack) < k: 
                 stack.append(num)
-        
-        return stack 
+                
+        return stack
         
