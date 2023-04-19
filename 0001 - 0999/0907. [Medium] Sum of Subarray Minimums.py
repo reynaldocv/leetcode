dbac0e02a2189f6c36b7193e0f1cb8afab2eb71e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-19 18:24:45 -0300

    Update 0907. [Medium] Sum of Subarray Minimums.py

--- 0001 - 0999/0907. [Medium] Sum of Subarray Minimums.py
+++ 0001 - 0999/0907. [Medium] Sum of Subarray Minimums.py
@@ -3,17 +3,23 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         MOD = 10**9 + 7
-        arr.append(0)
         
-        stack = [-1]
-        ans = 0
+        stack = [(-1, -1)]
         
-        for (i, num) in enumerate(arr):
-            while len(stack) > 1 and arr[stack[-1]] > num: 
-                last = stack.pop()
-                ans += (last - stack[-1])*(i - last)*arr[last] % MOD
+        ans = 0 
+        
+        for (i, num) in enumerate(arr + [0]):
+            while stack[-1][0] > num: 
+                (number, middle) = stack.pop() 
                 
-            stack.append(i)
+                left = middle - stack[-1][1] 
+                right = i - middle 
+                
+                ans = (ans + left*right*number) % MOD 
+                
+            stack.append((num, i))
             
-        return ans % MOD
+        return ans 
+    
+    
         
