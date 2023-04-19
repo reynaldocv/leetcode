e74569bdfa691f627b1dbe866a70eba7f881ce5a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-19 15:28:49 -0300

    Update 1124. [Medium] Longest Well-Performing Interval.py

--- 1000 - 1999/1124. [Medium] Longest Well-Performing Interval.py
+++ 1000 - 1999/1124. [Medium] Longest Well-Performing Interval.py
@@ -2,24 +2,24 @@
 
 class Solution:
     def longestWPI(self, hours: List[int]) -> int:
-        arr = [1 if hour > 8 else - 1 for hour in hours]
+        arr = [1 if hour > 8 else -1 for hour in hours]
         
+        seen = {0: -1}        
         prev = 0 
-        seen = {prev: -1}
         
-        ans = 0 
+        ans = 0
         
-        for (i, num) in enumerate(arr): 
-            prev += num
-            if prev - 1 in seen:
-                ans = max(ans, i - seen[prev - 1])
-                
+        for (i, hour) in enumerate(arr): 
+            prev += hour 
             if prev > 0: 
                 ans = max(ans, i + 1)
+            
+            if prev - 1 in seen:             
+                ans = max(ans, i - seen[prev - 1])
                 
             if prev not in seen: 
-                seen[prev] = i
-            
-        return ans
+                seen[prev] = i 
+                
+        return ans 
             
         
