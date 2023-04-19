9455361d4cd7342f94b557eab9550ef6e9ac430a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-19 20:58:38 -0300

    Update 1025. [Easy] Divisor Game.py

--- 1000 - 1999/1025. [Easy] Divisor Game.py
+++ 1000 - 1999/1025. [Easy] Divisor Game.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/divisor-game/
 
 class Solution:
-    def divisorGame(self, N: int) -> bool:
-        return N % 2 == 0
+    def divisorGame(self, n: int) -> bool:
+        return n % 2 == 0
         
