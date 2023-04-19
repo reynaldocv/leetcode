bd255a0700cb259707a3f9d0bb844bcb363d7ed8
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-19 20:48:28 -0300

    Update 1030. [Easy] Matrix Cells in Distance Order.py

--- 1000 - 1999/1030. [Easy] Matrix Cells in Distance Order.py
+++ 1000 - 1999/1030. [Easy] Matrix Cells in Distance Order.py
@@ -1,16 +1,15 @@
 # https://leetcode.com/problems/matrix-cells-in-distance-order/
 
 class Solution:
-    def allCellsDistOrder(self, R: int, C: int, r0: int, c0: int) -> List[List[int]]:
-        aux = []
-        ans = []
-        for r in range(R):
-            for c in range(C):
-                dist = abs(r0 - r) + abs(c - c0)
-                aux.append((dist, (r, c)))
-        aux.sort()
-        for (dis, pair) in aux:
-            ans.append(pair)
-        return ans
+    def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
+        def helper(x, y):
+            p = abs(x - rCenter)
+            q = abs(y - cCenter)
+            
+            return p + q
         
-                
+        ans = [(i, j) for i in range(rows) for j in range(cols)]
+        
+        ans.sort(key = lambda item: (helper(item[0], item[1])))
+        
+        return ans 
