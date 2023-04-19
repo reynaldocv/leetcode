bb72e20e06e3dfd5b4f8feed11715a3ff964ea90
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-18 23:08:27 -0300

    Update 0856. [Medium] Score of Parentheses.py

--- 0001 - 0999/0856. [Medium] Score of Parentheses.py
+++ 0001 - 0999/0856. [Medium] Score of Parentheses.py
@@ -3,21 +3,25 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
         stack = []
+        
         for char in s: 
-            if char == ")":
-                aSum = 0
+            if char == ")":                
+                aSum = 0 
+
                 while stack and stack[-1] != "(":
-                    aSum += stack.pop()
-                
-                stack.pop()        
-                
+                    aSum += stack.pop() 
+
+                stack.pop()
+
                 if aSum == 0: 
                     stack.append(1)
-                else: 
+
+                else:                         
                     stack.append(2*aSum)
+
             else: 
                 stack.append("(")
-        
-        return sum(stack)
-        
+                
+        return sum(stack)        
+                
         
