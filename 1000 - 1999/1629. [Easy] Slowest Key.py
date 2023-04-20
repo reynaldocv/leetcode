679ff1c17f9986edb48d45fc195c0d9b32bb8fa8
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-19 22:18:20 -0300

    Update 1629. [Easy] Slowest Key.py

--- 1000 - 1999/1629. [Easy] Slowest Key.py
+++ 1000 - 1999/1629. [Easy] Slowest Key.py
@@ -2,20 +2,12 @@
 
 class Solution:
     def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
-        n = len(keysPressed)
-        maxTime = releaseTimes[0]
-        arrTime = [keysPressed[0]]
-        ans = releaseTimes[0]
+        n = len(releaseTimes) 
+        
+        ans = (releaseTimes[0], keysPressed[0])
         
         for i in range(1, n):
-            auxTime = releaseTimes[i] - releaseTimes[i - 1]
-            if auxTime >= maxTime:                 
-                if auxTime == maxTime: 
-                    arrTime.append(keysPressed[i])
-                else: 
-                    arrTime = [keysPressed[i]]
-                maxTime = auxTime
-        
-        arrTime.sort(reverse = True)
-        return arrTime[0]
+            ans = max(ans, (releaseTimes[i] - releaseTimes[i - 1], keysPressed[i]))
+            
+        return ans[1]
         
