215203178961504a151c2109c23edfd13a2b46c4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-19 21:04:50 -0300

    Update 1005. [Easy] Maximize Sum Of Array After K Negations.py

--- 1000 - 1999/1005. [Easy] Maximize Sum Of Array After K Negations.py
+++ 1000 - 1999/1005. [Easy] Maximize Sum Of Array After K Negations.py
@@ -2,32 +2,23 @@
 
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
-        neg, pos = [], []
+        heap = []
+        
         for num in nums: 
-            if num < 0: 
-                neg.append(num)
-            else: 
-                pos.append(num)
-        n = len(pos)
-        m = len(neg)
-        pos.sort()
-        neg.sort()
-     
-        if m == 0: 
-            if k % 2 == 1 and 0 not in pos: 
-                pos[0] *= -1
-        elif k <= m: 
-            for i in range(k):
-                neg[i] *= -1
-        else: 
-            for i in range(m):
-                neg[i] *= -1
-            k -= m
-            pos.extend(neg)
-            pos.sort()
-            neg = []
-            if 0 not in pos and k % 2 == 1: 
-                pos[0] *= -1 
-                
-        print(pos, neg)
-        return sum(pos) + sum(neg)
+            heappush(heap, num)
+            
+        for _ in range(k):
+            num = heappop(heap)
+            
+            if num == 0: 
+                break 
+            
+            heappush(heap, -num)
+            
+        ans = 0 
+        
+        while heap: 
+            ans += heappop(heap)
+            
+        return ans 
+        
