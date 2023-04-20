9ba36c722c22fef5388d198bb2377e1d9a4e0336
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-19 21:52:49 -0300

    Update 1566. [Easy] Detect Pattern of Length M Repeated K or More Times.py

--- 1000 - 1999/1566. [Easy] Detect Pattern of Length M Repeated K or More Times.py
+++ 1000 - 1999/1566. [Easy] Detect Pattern of Length M Repeated K or More Times.py
@@ -3,14 +3,15 @@
 class Solution:
     def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
         n = len(arr)
-        arrStr = [str(i) for i in arr]
-        strArr = "-".join(arrStr)
-        for i in range(n - m):
-            aux = [str(i) for i in arr[i: i + m]]*k
-            aux = "-".join(aux)
-            print(aux, strArr)
-            if strArr.count(aux) > 0:
-                return True 
-        return False
-            
         
+        arr = [str(num) for num in arr]
+        
+        word = "-".join(arr)
+        
+        for i in range(n - m):
+            sequence = "-".join(arr[i: i + m]*k)
+            
+            if sequence in word:
+                return True 
+            
+        return False
