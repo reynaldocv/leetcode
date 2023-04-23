4250a59eaa52af45a8c3575927e2e9655a538f69
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-23 11:54:34 -0300

    Update 1416. [Hard] Restore The Array.py

--- 1000 - 1999/1416. [Hard] Restore The Array.py
+++ 1000 - 1999/1416. [Hard] Restore The Array.py
@@ -2,6 +2,36 @@
 
 class Solution:
     def numberOfArrays(self, s: str, k: int) -> int:
+        MOD = 10**9 + 7
+        
+        m, n = len(s), len(str(k))
+        
+        dp = [1] + [0 for _ in range(m)]
+        
+        for i in range(1, m + 1):
+            tmp = ""
+            j = i 
+            
+            ans = 0 
+            
+            while j >= 0 and len(tmp) <= n: 
+                tmp = s[j - 1] + tmp 
+                
+                if s[j - 1] != "0":
+                    if int(tmp) <= k: 
+                        ans += (dp[j - 1] % MOD)
+                        
+                    else: 
+                        break 
+                        
+                j -= 1 
+        
+            dp[i] = ans % MOD
+        
+        return dp[-1] % MOD
+    
+class Solution2:
+    def numberOfArrays(self, s: str, k: int) -> int:
         @cache 
         def helper(i):
             if i == n: 
