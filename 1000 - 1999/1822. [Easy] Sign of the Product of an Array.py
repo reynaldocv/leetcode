0c6a4b5ae10bc09db001c9c8cea4bf71e6c462ff
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-24 13:34:52 -0300

    Update 1822. [Easy] Sign of the Product of an Array.py

--- 1000 - 1999/1822. [Easy] Sign of the Product of an Array.py
+++ 1000 - 1999/1822. [Easy] Sign of the Product of an Array.py
@@ -3,12 +3,17 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
         ans = 1
-        for num in nums:
-            if num == 0: 
-                ans = 0
-                break
-            elif num < 0:
-                ans *= -1
-        return ans
+        
+        for num in nums: 
+            if num > 0: 
+                ans *= 1 
                 
+            elif num < 0: 
+                ans *= -1
+                
+            else: 
+                return 0 
+            
+        return ans 
+        
         
