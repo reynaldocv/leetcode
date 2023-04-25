7396083bb2003b00c63ae7dabd64ca85ff5d3aa7
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-25 11:13:31 -0300

    Update 2336. [Medium] Smallest Number in Infinite Set.py

--- 2000 - 2999/2336. [Medium] Smallest Number in Infinite Set.py
+++ 2000 - 2999/2336. [Medium] Smallest Number in Infinite Set.py
@@ -3,22 +3,27 @@
 class SmallestInfiniteSet:
 
     def __init__(self):
-        self.seen = {i: True for i in range(1, 1001)}
-        self.heap = [i for i in range(1, 1001)]
-        
-        heapify(self.heap)
-        
-    def popSmallest(self) -> int:
-        ans = heappop(self.heap)
-        self.seen.pop(ans)
-        
-        return ans
-    
-    def addBack(self, num: int) -> None:
-        if num not in self.seen:
-            heappush(self.heap, num)
-            self.seen[num] = True
+        self.heap = []
+        self.next = 1
+        self.seen = set()
 
+    def popSmallest(self) -> int:
+        if self.heap: 
+            elem = heappop(self.heap)
+            self.seen.remove(elem)
+
+            return elem        
+            
+        else: 
+            self.next += 1 
+            
+            return self.next - 1             
+
+    def addBack(self, num: int) -> None:
+        if num < self.next and num not in self.seen:
+            heappush(self.heap, num)
+            self.seen.add(num)
+        
 # Your SmallestInfiniteSet object will be instantiated and called as such:
 # obj = SmallestInfiniteSet()
 # param_1 = obj.popSmallest()
