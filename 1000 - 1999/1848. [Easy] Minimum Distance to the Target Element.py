5c6ea47c662e964144e4291581ee8999dd26c81d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-25 14:43:06 -0300

    Update 1848. [Easy] Minimum Distance to the Target Element.py

--- 1000 - 1999/1848. [Easy] Minimum Distance to the Target Element.py
+++ 1000 - 1999/1848. [Easy] Minimum Distance to the Target Element.py
@@ -2,11 +2,11 @@
 
 class Solution:
     def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
-        n = len(nums)
         ans = inf
-        for i in range(n):
-            if nums[i] == target:
-                ans = min(ans, abs(i - start, ))
         
+        for (i, num) in enumerate(nums):
+            if num == target: 
+                ans = min(ans, abs(i - start))
+                
         return ans
         
