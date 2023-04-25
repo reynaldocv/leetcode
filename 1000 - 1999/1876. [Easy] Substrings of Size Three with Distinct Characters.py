eb96a4c6c01cebb612160abf4e8e8bad1b8d7978
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-25 13:52:37 -0300

    Update 1876. [Easy] Substrings of Size Three with Distinct Characters.py

--- 1000 - 1999/1876. [Easy] Substrings of Size Three with Distinct Characters.py
+++ 1000 - 1999/1876. [Easy] Substrings of Size Three with Distinct Characters.py
@@ -3,19 +3,13 @@
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
         n = len(s)
-        ans = 0
+        
+        ans = 0 
+        
         for i in range(n - 2):
-            dic = {s[i] : True}
-            add = True
-            for j in [i + 1, i + 2]:
-                if dic.get(s[j], False) == True: 
-                    add = False
-                    break
-                else: 
-                    dic[s[j]] = True
-                    
-            if add: 
-                ans += 1
-                    
-        return ans
+            if s[i] != s[i + 1] and s[i] != s[i + 2] and s[i + 1] != s[i + 2]:
+                ans += 1 
+                
+        return ans         
+        
         
