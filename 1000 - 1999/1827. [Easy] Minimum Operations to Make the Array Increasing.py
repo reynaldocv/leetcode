633f17d73d04b775414c0a487870c41aca6c8c44
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-25 15:11:31 -0300

    Update 1827. [Easy] Minimum Operations to Make the Array Increasing.py

--- 1000 - 1999/1827. [Easy] Minimum Operations to Make the Array Increasing.py
+++ 1000 - 1999/1827. [Easy] Minimum Operations to Make the Array Increasing.py
@@ -2,13 +2,20 @@
 
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        start, ans = nums[0], 0
+        n = len(nums) 
         
-        for i in range(1, len(nums)):
-            if start >= nums[i]:
-                ans += start - nums[i] + 1
-                start += 1
-            else:
-                start = nums[i]
+        last = nums[0] + 1
+        
+        ans = 0 
+        
+        for i in range(1, n):
+            if last > nums[i]:
+                ans += last - nums[i]
+                
+                last += 1
+                
+            else: 
+                last = nums[i] + 1
+                
         return ans
-        
+                
