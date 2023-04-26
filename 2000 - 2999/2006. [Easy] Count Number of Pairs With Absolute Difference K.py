c8f173d2722a86f4b31605d05fecb596ea23c0e7
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-26 11:40:12 -0300

    Update 2006. [Easy] Count Number of Pairs With Absolute Difference K.py

--- 2000 - 2999/2006. [Easy] Count Number of Pairs With Absolute Difference K.py
+++ 2000 - 2999/2006. [Easy] Count Number of Pairs With Absolute Difference K.py
@@ -2,13 +2,33 @@
 
 class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
-        counter = {}
+        counter = defaultdict(lambda: 0)
+                              
         for num in nums: 
-            counter[num] = counter.get(num, 0) + 1
+            counter[num] += 1
         
         ans = 0 
+                              
         for key in counter: 
-            ans += counter[key]*counter.get(key - k, 0)
+            if key - k in counter: 
+                ans += counter[key]*counter[key - k]
             
         return ans
+
+class Solution2:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        counter = defaultdict(lambda: 0)
+        
+        nums.sort() 
+        
+        ans = 0 
+        
+        for num in nums: 
+            ans += counter[num - k]
+            
+            counter[num] += 1 
+            
+        return ans 
+            
+        
         
