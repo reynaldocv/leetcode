23b6b7ba01874f451aa9e790a70b6db4ecc21bbf
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-26 11:34:58 -0300

    Update 1995. [Easy] Count Special Quadruplets.py

--- 1000 - 1999/1995. [Easy] Count Special Quadruplets.py
+++ 1000 - 1999/1995. [Easy] Count Special Quadruplets.py
@@ -2,14 +2,33 @@
 
 class Solution:
     def countQuadruplets(self, nums: List[int]) -> int:
-        ans = 0         
-        n = len(nums)
-        for i, j, k , l in combinations(range(n), 4):
-            i, j, k, l = sorted([i, j, k, l])
-            if nums[i] + nums[j] + nums[k] == nums[l]:
-                ans += 1
+        n = len(nums) 
         
-        return ans
+        ans = 0 
+        
+        for i in range(n - 3):
+            for j in range(i + 1, n - 2):
+                for k in range(j + 1, n - 1):
+                    for l in range(k + 1, n):
+                        if nums[i] + nums[j] + nums[k] == nums[l]:
+                            ans += 1 
+                            
+        return ans 
+
+class Solution2:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        n = len(nums) 
+        
+        ans = 0 
+        
+        for (i, j, k, l) in combinations([i for i in range(n)], 4):
+            (i, j, k, l) = sorted([i, j, k, l])
+            
+            if nums[i] + nums[j] + nums[k] == nums[l]:
+                ans += 1 
+                            
+        return ans 
+        
                 
         
         
