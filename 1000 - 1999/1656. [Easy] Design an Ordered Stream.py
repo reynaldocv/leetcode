64b5f764432bf6a8a0f255077cc949bc1936640b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-26 11:12:42 -0300

    Update 1656. [Easy] Design an Ordered Stream.py

--- 1000 - 1999/1656. [Easy] Design an Ordered Stream.py
+++ 1000 - 1999/1656. [Easy] Design an Ordered Stream.py
@@ -3,22 +3,21 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.array = [None]*n
-        self.pointer = 0
-        self.n = n
+        self.arr = ["" for _ in range(n)]
+        self.idx = 0
+        self.n = n 
 
     def insert(self, idKey: int, value: str) -> List[str]:
-        self.array[idKey - 1] = value
+        self.arr[idKey - 1] = value
+        
         ans = []
-        while self.pointer < self.n and self.array[self.pointer] != None:
-            ans.append(self.array[self.pointer])
-            self.pointer += 1
         
+        while self.idx < self.n and self.arr[self.idx] != "":
+            ans.append(self.arr[self.idx])
+            
+            self.idx += 1 
+            
         return ans
-        
-        
-        
-
 
 # Your OrderedStream object will be instantiated and called as such:
 # obj = OrderedStream(n)
