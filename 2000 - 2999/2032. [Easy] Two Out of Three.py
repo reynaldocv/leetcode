ef4f91b218fb3ced3ecb7abe193cde31587fc5ba
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-27 20:12:37 -0300

    Update 2032. [Easy] Two Out of Three.py

--- 2000 - 2999/2032. [Easy] Two Out of Three.py
+++ 2000 - 2999/2032. [Easy] Two Out of Three.py
@@ -4,18 +4,14 @@ class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
         counter = defaultdict(lambda: [0, 0, 0])
         
-        for num in nums1: 
-            counter[num][0] = 1
-        
-        for num in nums2:
-            counter[num][1] = 1
-        
-        for num in nums3:
-            counter[num][2] = 1
+        for (i, nums) in enumerate([nums1, nums2, nums3]):
+            for num in nums: 
+                counter[num][i] = 1 
             
         ans = []
+        
         for num in counter: 
-            if sum(counter[num]) >= 2: 
+            if sum(counter[num]) > 1: 
                 ans.append(num)
                 
         return ans
