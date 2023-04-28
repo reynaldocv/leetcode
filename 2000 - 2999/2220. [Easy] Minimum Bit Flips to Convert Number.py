4db30adba36ccb2adf23447471a8097918f6dabc
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-28 00:27:46 -0300

    Update 2220. [Easy] Minimum Bit Flips to Convert Number.py

--- 2000 - 2999/2220. [Easy] Minimum Bit Flips to Convert Number.py
+++ 2000 - 2999/2220. [Easy] Minimum Bit Flips to Convert Number.py
@@ -3,13 +3,13 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
         ans = 0 
-        while goal or start: 
-            bit1 = goal & 1 
-            bit2 = start & 1
-            if bit1 != bit2: 
-                ans += 1
-            
-            goal //= 2 
+        
+        while start or goal: 
+            if start % 2 != goal % 2: 
+                ans += 1 
+                
             start //= 2 
+            goal //= 2 
             
         return ans 
+        
