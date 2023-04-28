242f726708f4715be8b0a53f38da7bf41fe22805
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-27 22:19:17 -0300

    Update 2094. [Easy] Finding 3-Digit Even Numbers.py

--- 2000 - 2999/2094. [Easy] Finding 3-Digit Even Numbers.py
+++ 2000 - 2999/2094. [Easy] Finding 3-Digit Even Numbers.py
@@ -2,19 +2,19 @@
 
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
-        counter = defaultdict(lambda: 0)
-        arr = []
-        for digit in digits: 
-            if counter[digit] < 3: 
-                counter[digit] += 1
-                arr.append(digit)
-            
-        ans = set()
-        for p in permutations(arr, 3):
-            if p[0] != 0 and p[2] % 2 == 0:
-                num = p[0]*100 + p[1]*10 + p[2]
-                ans.add(num)
+        n = len(digits)
+        
+        seen = set()
+        
+        for (a, b, c) in permutations(digits, 3):
+            if a != 0 and c % 2 == 0: 
+                num = 100*a + 10*b + c
                 
-        return sorted(ans)
+                seen.add(num)
+                
+        return sorted(seen)
+        
+            
+            
                 
                 
