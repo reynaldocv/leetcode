5b0d85d2771c4b1eab65c74784bb18728d787813
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-27 23:10:12 -0300

    Update 2138. [Easy] Divide a String Into Groups of Size k.py

--- 2000 - 2999/2138. [Easy] Divide a String Into Groups of Size k.py
+++ 2000 - 2999/2138. [Easy] Divide a String Into Groups of Size k.py
@@ -3,15 +3,22 @@
 class Solution:
     def divideString(self, s: str, k: int, fill: str) -> List[str]:
         n = len(s)
-        ans = []
-        for i in range(0, n , k):
-            ans.append(s[i: i + k])
         
-        if ans:
+        limit = n//k 
+        
+        if n % k != 0: 
+            limit += 1 
+            
+        ans = []
+        
+        for i in range(limit):
+            ans.append(s[i*k: (i + 1)*k])
+            
+        while len(ans[-1]) < k: 
             ans[-1] += fill*(k - len(ans[-1]))
             
-        return ans
-                             
+        return ans 
+                    
                              
                        
     
