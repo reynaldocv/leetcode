2076c3fca8bb3f56cd212a405595f467eab8730e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-27 23:04:36 -0300

    Update 2133. [Easy] Check if Every Row and Column Contains All Numbers.py

--- 2000 - 2999/2133. [Easy] Check if Every Row and Column Contains All Numbers.py
+++ 2000 - 2999/2133. [Easy] Check if Every Row and Column Contains All Numbers.py
@@ -1,19 +1,28 @@
 # https://leetcode.com/problems/check-if-every-row-and-column-contains-all-numbers/
 
 class Solution:
-    def checkValid(self, matrix: List[List[int]]) -> bool:               
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        def helper(row):
+            seen = set()
+            
+            for num in row: 
+                if num <= n: 
+                    seen.add(num)
+                    
+            return len(seen) == n
+        
         n = len(matrix)
-        p = [i for i in range(1, n + 1)]
         
         for row in matrix: 
-            if sorted(row) != p:
+            if not helper(row):
+                return False 
+            
+        matrix = [[matrix[i][j] for i in range(n)] for j in range(n)]
+        
+        for row in matrix: 
+            if not helper(row):
                 return False
             
-        tMatrix = [[matrix[i][j] for i in range(n)] for j in range(n)]
-        
-        for row in tMatrix: 
-            if sorted(row) != p:
-                return False
-        
         return True
+        
         
