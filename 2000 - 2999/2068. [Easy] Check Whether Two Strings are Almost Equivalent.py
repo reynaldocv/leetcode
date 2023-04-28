8327e1ae3ad9a4052e7730721a38589445f70b36
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-27 21:04:14 -0300

    Update 2068. [Easy] Check Whether Two Strings are Almost Equivalent.py

--- 2000 - 2999/2068. [Easy] Check Whether Two Strings are Almost Equivalent.py
+++ 2000 - 2999/2068. [Easy] Check Whether Two Strings are Almost Equivalent.py
@@ -2,22 +2,19 @@
 
 class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
-        counter1 = defaultdict(lambda: 0)
-        counter2 = defaultdict(lambda: 0)
-        keys = {}
+        counter = defaultdict(lambda: 0)
         
         for char in word1: 
-            keys[char] = True
-            counter1[char] += 1
+            counter[char] += 1 
             
         for char in word2: 
-            keys[char] = True
-            counter2[char] += 1
+            counter[char] -= 1 
             
-        for key in keys: 
-            if abs(counter1[key] - counter2[key]) > 3: 
-                return False
+        for key in counter: 
+            if abs(counter[key]) > 3: 
+                return False 
             
-        return True
+        return True 
+        
                 
         
