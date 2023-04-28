441d86d08ec89ca98b00332c1ccd936eb33e81ad
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-28 01:04:53 -0300

    Update 2273. [Easy] Find Resultant Array After Removing Anagrams.py

--- 2000 - 2999/2273. [Easy] Find Resultant Array After Removing Anagrams.py
+++ 2000 - 2999/2273. [Easy] Find Resultant Array After Removing Anagrams.py
@@ -2,22 +2,24 @@
 
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        n = len(words) 
-        
-        start = 0 
-        ans = []
-        
-        while start < n:
-            k = 1
-            key = sorted(words[start])
-            while start + k < n and sorted(words[start + k]) == key:
-                k += 1
+        @cache 
+        def helper(word):
+            ans = (0, )*26
+            
+            for char in word: 
+                idx = index[char]
                 
-            k -= 1 
-            
-            ans.append(words[start])
-            
-            start += k + 1
-            
-        return ans
+                ans = ans[: idx] +(ans[idx] + 1, ) + ans[idx + 1: ]
+                
+            return ans 
+        
+        index = {chr(ord("a") + i): i for i in range(26)}
+        
+        stack = [words[0]]
+        
+        for word in words[1: ]:
+            if helper(stack[-1]) != helper(word):
+                stack.append(word)
+                
+        return stack 
         
