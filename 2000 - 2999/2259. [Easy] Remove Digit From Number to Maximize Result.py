f01643ea9eaacc5af5a81fe139e146c8142b8f7d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-28 00:49:31 -0300

    Update 2259. [Easy] Remove Digit From Number to Maximize Result.py

--- 2000 - 2999/2259. [Easy] Remove Digit From Number to Maximize Result.py
+++ 2000 - 2999/2259. [Easy] Remove Digit From Number to Maximize Result.py
@@ -2,10 +2,13 @@
 
 class Solution:
     def removeDigit(self, number: str, digit: str) -> str:
-        ans = 0 
-        for (i, char) in enumerate(number):
-            if char == digit: 
-                ans = max(ans, int(number[: i] + number[i + 1: ]))
-                
-        return str(ans)
+        n = len(number)
         
+        ans = "0"
+        
+        for i in range(n):
+            if number[i] == digit:
+                ans = max(ans, number[: i] + number[i + 1: ])
+            
+        return ans 
+            
