ea415306599f04d96c7cab3d855195509112cab4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-27 22:53:54 -0300

    Update 2119. [Easy] A Number After a Double Reversal.py

--- 2000 - 2999/2119. [Easy] A Number After a Double Reversal.py
+++ 2000 - 2999/2119. [Easy] A Number After a Double Reversal.py
@@ -3,6 +3,7 @@
 class Solution:
     def isSameAfterReversals(self, num: int) -> bool:
         def helper(num):
-            return int(str(num)[::-1])
+            return int(str(num)[:: -1])
         
         return num == helper(helper(num))
+        
