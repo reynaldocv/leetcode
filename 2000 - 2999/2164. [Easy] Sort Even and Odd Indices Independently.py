fc2ded66b498b083f086ce80a23eb06b836a0615
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-27 23:40:08 -0300

    Update 2164. [Easy] Sort Even and Odd Indices Independently.py

--- 2000 - 2999/2164. [Easy] Sort Even and Odd Indices Independently.py
+++ 2000 - 2999/2164. [Easy] Sort Even and Odd Indices Independently.py
@@ -2,19 +2,30 @@
 
 class Solution:
     def sortEvenOdd(self, nums: List[int]) -> List[int]:
-        even = [num for (i, num) in enumerate(nums) if i % 2 == 0]
-        odd = [num for (i, num) in enumerate(nums) if i % 2 == 1]
+        n = len(nums)
         
-        even.sort()
-        odd.sort(reverse = True)
+        even = []
+        odd = []
+        
+        for (i, num) in enumerate(nums):
+            if i % 2 == 0: 
+                even.append(num)
+                
+            else: 
+                odd.append(num)
+                
+        even.sort() 
+        odd.sort(key = lambda item: -item)
         
         ans = []
-        for i in range(min(len(even), len(odd))):
+        
+        for i in range(n//2):
             ans.append(even[i])
             ans.append(odd[i])
             
-        if len(even) != len(odd):
+        if n % 2 == 1: 
             ans.append(even[-1])
             
-        return ans
+        return ans 
+        
         
