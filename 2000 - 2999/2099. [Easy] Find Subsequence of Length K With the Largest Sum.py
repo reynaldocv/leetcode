828ebc7f9252526586c4fdea006af682be294a9c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-27 22:43:37 -0300

    Update 2099. [Easy] Find Subsequence of Length K With the Largest Sum.py

--- 2000 - 2999/2099. [Easy] Find Subsequence of Length K With the Largest Sum.py
+++ 2000 - 2999/2099. [Easy] Find Subsequence of Length K With the Largest Sum.py
@@ -1,14 +1,29 @@
 # https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
 
 class Solution:
-    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:        
-        nums = [(num, i) for (i, num) in enumerate(nums)]
-        nums.sort(reverse = True)
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        heap = []
         
-        nums = nums[:k]
-        nums.sort(key = lambda item: item[1])
+        for (i, num) in enumerate(nums):
+            heappush(heap, (-num, i))
+            
+        arr = []
         
-        return [num for (num, _) in nums]
+        for _ in range(k):
+            (num, i) = heappop(heap)
+            
+            arr.append((i, -num))
+            
+        arr.sort() 
+        
+        ans = []
+        
+        for (_, num) in arr: 
+            ans.append(num)
+            
+        return ans 
+            
+        
         
         
   
