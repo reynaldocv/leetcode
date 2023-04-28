456ef91889c7e9e4063c6ac7ecfa4ec3985396d1
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-28 00:35:32 -0300

    Update 2224. [Easy] Minimum Number of Operations to Convert Time.py

--- 2000 - 2999/2224. [Easy] Minimum Number of Operations to Convert Time.py
+++ 2000 - 2999/2224. [Easy] Minimum Number of Operations to Convert Time.py
@@ -2,21 +2,17 @@
 
 class Solution:
     def convertTime(self, current: str, correct: str) -> int:
-        def helper(string):
-            return int(string[:2])*60 + int(string[3:])
+        start = int(current[: 2])*60 + int(current[3: ])
+        end = int(correct[: 2])*60 + int(correct[3: ])
         
-        start = helper(current)
-        end = helper(correct)
-        
-        diff = end - start 
+        diff = end - start
         
         ans = 0 
         
-        for value in [60, 15, 5, 1]:
-            while diff >= value: 
-                ans += 1 
-                diff -= value
-                
-        return ans 
+        for k in [60, 15, 5, 1]:
+            ans += (diff//k)
             
+            diff = diff % k 
+            
+        return ans 
 
