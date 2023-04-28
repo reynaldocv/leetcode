d956849bda035f7997c365286eeb31e4311f5110
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-04-28 18:55:43 -0300

    Update 0002. [Medium] Add Two Numbers.py

--- 0001 - 0999/0002. [Medium] Add Two Numbers.py
+++ 0001 - 0999/0002. [Medium] Add Two Numbers.py
@@ -6,26 +6,25 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        aux = 0 
         
-        ans = l3 = ListNode(0)
-        aux = 0
-        while  l1 or l2: 
-            aux1 = l1.val if l1 else 0
-            aux2 = l2.val if l2 else 0
-            aux3 = aux1 + aux2 + aux
-            if aux3 >= 10: 
-                aux = 1
-                aux3 = aux3 % 10
-            else: 
-                aux = 0
-            l3.next = ListNode(aux3)
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-            l3 = l3.next
-        if aux == 1: 
-            l3.next = ListNode(1)
-        return ans.next
+        ans = cur = ListNode(0)
+        
+        while l1 or l2 or aux: 
+            val1 = l1.val if l1 else 0 
+            val2 = l2.val if l2 else 0 
             
+            value = (val1 + val2 + aux) % 10 
+            aux = (val1 + val2 + aux)//10
+            
+            cur.next = ListNode(value)
+            
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None 
+            
+            cur = cur.next 
+            
+        return ans.next
             
         
