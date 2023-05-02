d234c3a2ff35166f66b52b3a3b91df15d3b2ae5f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-02 16:20:40 -0300

    Update 2303. [Easy] Calculate Amount Paid in Taxes.py

--- 2000 - 2999/2303. [Easy] Calculate Amount Paid in Taxes.py
+++ 2000 - 2999/2303. [Easy] Calculate Amount Paid in Taxes.py
@@ -2,17 +2,21 @@
 
 class Solution:
     def calculateTax(self, brackets: List[List[int]], income: int) -> float:
-        prev = 0 
         ans = 0 
         
+        prev = 0 
+        
         for (upper, percent) in brackets: 
-            if income > 0:
-                val = min(upper - prev, income)
-                ans += val*percent/100                
-                income -= val 
+            if income > 0: 
+                tmp = min(upper - prev, income)
+
+                ans += percent*tmp/100
+
+                income -= tmp
+                
+                prev = upper
+                
             else: 
                 break 
-                
-            prev = upper
                 
         return ans 
