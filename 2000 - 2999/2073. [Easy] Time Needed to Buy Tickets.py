4e7bf6672a5ba5465e2f30fccb2bc905c67c4a31
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-02 12:50:37 -0300

    Update 2073. [Easy] Time Needed to Buy Tickets.py

--- 2000 - 2999/2073. [Easy] Time Needed to Buy Tickets.py
+++ 2000 - 2999/2073. [Easy] Time Needed to Buy Tickets.py
@@ -2,14 +2,16 @@
 
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        ans = 0 
+        n = len(tickets)
         
-        for (i, val) in enumerate(tickets):
-            if i <= k:
-                ans += min(tickets[i], tickets[k])
-            else: 
-                ans += min(tickets[i], tickets[k] - 1)
+        ans = 0
         
-        return ans
-                
+        for i in range(k + 1):
+            ans += min(tickets[i], tickets[k])
+            
+        for i in range(k + 1, n):
+            ans += min(tickets[i], tickets[k] - 1)
+            
+        return ans 
+            
             
