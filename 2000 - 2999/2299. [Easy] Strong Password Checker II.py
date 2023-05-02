50ddaf4762ad0ad386c2091f7881b0ad29cfbd74
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-02 15:51:06 -0300

    Update 2299. [Easy] Strong Password Checker II.py

--- 2000 - 2999/2299. [Easy] Strong Password Checker II.py
+++ 2000 - 2999/2299. [Easy] Strong Password Checker II.py
@@ -2,33 +2,30 @@
 
 class Solution:
     def strongPasswordCheckerII(self, password: str) -> bool:
-        upper = {chr(ord("A") + i) for i in range(26)}
-        lower = {chr(ord("a") + i) for i in range(26)}
-        
-        digits = {str(i) for i in range(10)}
-        
-        specials = "!@#$%^&*()-+"
-        
-        counter = (0, 0, 0, 0)
-        
-        for char in password: 
-            if char in upper: 
-                counter = (1, ) + counter[1: ]
-            elif char in lower: 
-                counter = counter[: 1] + (1, ) + counter[2: ]
-            elif char in digits: 
-                counter = counter[: 2] + (1, ) + counter[3: ]
-            elif char in specials: 
-                counter = counter[: 3] + (1, ) + counter[4: ]
-            
         n = len(password)
-
-        if sum(counter) >= 4 and n >= 8: 
-            
-            for i in range(n - 1):
-                if password[i] == password[i + 1]:
-                    return False 
-
-            return True 
         
-        return False 
+        index = {}
+        
+        for i in range(26):
+            index[chr(ord("a") + i)] = 0 
+            index[chr(ord("A") + i)] = 1
+            
+        for i in range(10):
+            index[str(i)] = 2 
+            
+        for char in "!@#$%^&*()-+":
+            index[char] = 3 
+            
+        counter = (0, )*4
+        
+        for (i, char) in enumerate(password):
+            idx = index[char]
+            
+            counter = counter[: idx] + (1, ) + counter[idx + 1: ]
+            
+            if i < n - 1 and char == password[i + 1]:
+                return False 
+            
+        return sum(counter) == 4 and n > 7
+                
+          
