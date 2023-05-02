2f0aee8575296364d294a527bc9e620a036918bb
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-02 11:49:08 -0300

    Update 2062. [Easy] Count Vowel Substrings of a String.py

--- 2000 - 2999/2062. [Easy] Count Vowel Substrings of a String.py
+++ 2000 - 2999/2062. [Easy] Count Vowel Substrings of a String.py
@@ -4,18 +4,22 @@ class Solution:
     def countVowelSubstrings(self, word: str) -> int:
         def helper(sentence):
             counter = [0]*5
+            
             for char in sentence: 
                 if char not in "aeiou":
                     return False
+                
                 else: 
                     counter[index[char]] = 1
             
             return sum(counter) == 5
                 
         n = len(word)
+        
         index = {char: i for (i, char) in enumerate("aeiou")}
         
         ans = 0
+        
         for i in range(n - 5 + 1):
             for j in range(i + 5, n + 1):
                 if helper(word[i: j]):
