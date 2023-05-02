843582ad9ef0ff8a25f9b6bdde04ddc003fde646
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-02 16:39:33 -0300

    Update 2243. [Easy] Calculate Digit Sum of a String.py

--- 2000 - 2999/2243. [Easy] Calculate Digit Sum of a String.py
+++ 2000 - 2999/2243. [Easy] Calculate Digit Sum of a String.py
@@ -4,12 +4,16 @@ class Solution:
     def digitSum(self, s: str, k: int) -> str:
         while len(s) > k: 
             aux = ""
+            
             for i in range(len(s)//k + 1):
                 value = s[k*i: k*(i + 1)]
+                
                 aSum = 0
                 go = False
+                
                 for val in value: 
                     go = True
+                
                     aSum += int(val)
                     
                 if go: 
