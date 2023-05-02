98c6dd5f0d9df8f84d3779151924fa734ff59177
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-02 16:42:09 -0300

    Update 2047. [Easy] Number of Valid Words in a Sentence.py

--- 2000 - 2999/2047. [Easy] Number of Valid Words in a Sentence.py
+++ 2000 - 2999/2047. [Easy] Number of Valid Words in a Sentence.py
@@ -4,22 +4,31 @@ class Solution:
     def countValidWords(self, sentence: str) -> int:
         def helper(word):
             m = len(word)
+        
             hyphens = 0
+        
             for (i, char) in enumerate(word): 
                 if char in "0123456789":
                     return False
+                
                 if char in ".,!" and i != m - 1:
                     return False
+                
                 if char == "-":
                     hyphens += 1
                     
             if hyphens == 0: 
                 return True
+            
             elif hyphens >= 2:
                 return False
+            
             else: 
                 idx = word.index("-")
-                m = m - 1 if word[-1] in ".,!" else m 
+                
+                if word[-1] in ".,!":
+                    m = m - 1 
+                    
                 if 0 < idx and idx < m - 1:
                     return True
                 
@@ -34,3 +43,5 @@ class Solution:
               
         return ans
                     
+            
+        
