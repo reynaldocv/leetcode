288e60464e7c66f1c34a14c07282775796bbffc6
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-03 14:41:36 -0300

    Update 0042. [Hard] Trapping Rain Water.py

--- 0001 - 0999/0042. [Hard] Trapping Rain Water.py
+++ 0001 - 0999/0042. [Hard] Trapping Rain Water.py
@@ -2,24 +2,25 @@
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        def helper(arr):
-            ans = [num for num in arr]
+        def helper(array):
+            ans = [num for num in array]
             
             for i in range(1, n):
                 ans[i] = max(ans[i], ans[i - 1])
-            
+                
             return ans 
-            
+        
         n = len(height)
         
         left = helper(height)
         right = helper(height[:: -1])[:: -1]
         
-        ans = 0 
+        ans = 0
         
         for i in range(n):
             ans += min(left[i], right[i]) - height[i]
             
         return ans 
+       
 
         
