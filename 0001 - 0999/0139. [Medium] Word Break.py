020712ccc7961d24c5ece79575b02df94a9f643f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-03 15:07:43 -0300

    Update 0139. [Medium] Word Break.py

--- 0001 - 0999/0139. [Medium] Word Break.py
+++ 0001 - 0999/0139. [Medium] Word Break.py
@@ -24,3 +24,23 @@ class Solution:
         
         return helper(0)
                 
+class Solution2:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n, m = len(s), max(len(word) for word in wordDict)
+        
+        dp = [True] + [False for _ in range(n)]
+        
+        seen = [word for word in wordDict]
+        
+        for i in range(n + 1):
+            if dp[i] == True: 
+                tmp = ""
+                
+                for j in range(i, min(i + m, n)):
+                    tmp += s[j]
+                    
+                    if tmp in seen: 
+                        dp[j + 1] = True 
+
+        return dp[-1]
+        
