7c5d799faa5d26095ab9ee95e34d5f1aa0fd4bdb
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-03 15:27:41 -0300

    Update 0091. [Medium] Decode Ways.py

--- 0001 - 0999/0091. [Medium] Decode Ways.py
+++ 0001 - 0999/0091. [Medium] Decode Ways.py
@@ -2,22 +2,50 @@
 
 class Solution:
     def numDecodings(self, s: str) -> int:
-        @cache
-        def helper(string):
-            n = len(string)
-            if n == 0: 
+        @cache        
+        def helper(i):
+            if i == n: 
                 return 1 
+            
+            elif i > n: 
+                return 0 
+            
             else: 
-                ans = 0 
-                for i in range(1, min(n + 1, 3)):
-                    val = string[: i]
-                    if val in seen: 
-                        ans += helper(string[i: ])
+                ans = 0                 
+                tmp = ""
+                
+                for j in range(i, min(i + 2, n)):
+                    tmp += s[j]
+                    
+                    if tmp in seen: 
+                       ans += helper(j + 1)
                         
                 return ans 
-                
-        seen = {str(i) for i in range(1, 27)}
+            
+        n = len(s)
         
-        return helper(s)
+        seen = [word for word in wordDict]
+        
+        return helper(0)
                 
+class Solution2:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n, m = len(s), max(len(word) for word in wordDict)
+        
+        dp = [True] + [False for _ in range(n)]
+        
+        seen = [word for word in wordDict]
+        
+        for i in range(n + 1):
+            if dp[i] == True: 
+                tmp = ""
                 
+                for j in range(i, min(i + m, n)):
+                    tmp += s[j]
+                    
+                    if tmp in seen: 
+                        dp[j + 1] = True 
+
+        return dp[-1]
+        
+         
