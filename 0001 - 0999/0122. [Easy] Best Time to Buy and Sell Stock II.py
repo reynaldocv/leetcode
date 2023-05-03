b9bfc841573a29e84eac5fd7b25b88a151736679
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-03 13:42:26 -0300

    Update 0122. [Easy] Best Time to Buy and Sell Stock II.py

--- 0001 - 0999/0122. [Easy] Best Time to Buy and Sell Stock II.py
+++ 0001 - 0999/0122. [Easy] Best Time to Buy and Sell Stock II.py
@@ -3,10 +3,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        if n <= 1: return 0
-        ans = 0
-        for i in range(1, n):
-            if prices[i] > prices[i - 1]:               
-                ans += prices[i] - prices[i - 1]
+        
+        ans = 0 
+        
+        for i in range(n - 1):
+            if prices[i + 1] > prices[i]:
+                ans += prices[i + 1] - prices[i]
                 
-        return ans
+        return ans 
+    
+        
+        
