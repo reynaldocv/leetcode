092f4082d136e29eb9d5e80eee272eaf93b9cdd6
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-03 13:34:27 -0300

    Update 1014. [Medium] Best Sightseeing Pair.py

--- 1000 - 1999/1014. [Medium] Best Sightseeing Pair.py
+++ 1000 - 1999/1014. [Medium] Best Sightseeing Pair.py
@@ -2,21 +2,25 @@
 
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        def helper(array):
+            for i in range(1, n):
+                array[i] = max(array[i], array[i- 1])
+                
+            return array
+        
         n = len(values)
         
         left = [num + i for (i, num) in enumerate(values)]
         right = [num - i for (i, num) in enumerate(values)]
         
-        for i in range(1, n):
-            left[i] = max(left[i - 1], left[i])
-            
-        for i in range(n - 2, -1, -1):
-            right[i] = max(right[i + 1], right[i])
-            
+        left = helper(left)
+        right = helper(right[:: -1])[:: -1]
+        
         ans = -inf
         
         for i in range(n - 1):
             ans = max(ans, left[i] + right[i + 1])
             
         return ans 
+        
         
