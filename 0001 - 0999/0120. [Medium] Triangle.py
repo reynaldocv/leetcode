fdbc675baee402412fb7e1a7e1200abd910d9b05
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-04 13:46:43 -0300

    Update 0120. [Medium] Triangle.py

--- 0001 - 0999/0120. [Medium] Triangle.py
+++ 0001 - 0999/0120. [Medium] Triangle.py
@@ -3,9 +3,14 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-    
-        for lvl in range(n - 2, -1, -1):
-            for j in range(lvl + 1):
-                triangle[lvl][j] = min(triangle[lvl + 1][j], triangle[lvl + 1][j + 1]) + triangle[lvl][j]
+        
+        for i in range(1, n):
+            for j in range(i + 1):
+                left = triangle[i - 1][j - 1] if j - 1 >= 0 else inf 
+                right = triangle[i - 1][j] if j < i else inf 
                 
-        return triangle[0][0]
+                triangle[i][j] += min(left, right)
+                
+        return min(triangle[-1])
+
+        
