abe9f1450e97f872d759b0f03b7d797372f1a65f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-06 10:01:32 -0300

    Update 1498. [Medium] Number of Subsequences That Satisfy the Given Sum Condition.py

--- 1000 - 1999/1498. [Medium] Number of Subsequences That Satisfy the Given Sum Condition.py
+++ 1000 - 1999/1498. [Medium] Number of Subsequences That Satisfy the Given Sum Condition.py
@@ -1,17 +1,25 @@
 # https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/
 
 class Solution:
+class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
         MOD = 10**9 + 7
-        nums.sort()
+        
+        nums.sort() 
+        
         ans = 0 
+        
         for (i, num) in enumerate(nums):
-            if num <= target // 2: 
-                idx = bisect_right(nums, target - num)
-                if idx > i: 
-                    ans += 1 + 2**(idx - i - 1) - 1      
-                    ans %= MOD
+            if num <= target//2: 
+                idx = bisect_left(nums, target - num + 1)   
+                idx -= 1 
+
+                ans += 2**(idx - i)
+                ans %= MOD
+                
             else: 
                 break
-            
-        return ans % MOD
+                    
+        return ans 
+    
+        
