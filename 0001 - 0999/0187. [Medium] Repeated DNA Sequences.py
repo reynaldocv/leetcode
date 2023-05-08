7a6dced5231a98a1bfb0b84b280ef73928ab5005
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-08 14:54:35 -0300

    Update 0187. [Medium] Repeated DNA Sequences.py

--- 0001 - 0999/0187. [Medium] Repeated DNA Sequences.py
+++ 0001 - 0999/0187. [Medium] Repeated DNA Sequences.py
@@ -2,23 +2,20 @@
 
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        n = len(s)
-        if n <= 10: 
-            return []
+        n = len(s) 
         
-        key = s[:10]
-        seen = {key: 1}
-        maxRepeated = 1
+        counter = defaultdict(lambda: 0)
         
-        for i in range(10, n):
-            key = key[1:] + s[i]
-            seen[key] = seen.get(key, 0) + 1
-            maxRepeated = max(seen[key], maxRepeated)
+        ans = []
         
-        if maxRepeated > 1: 
-            ans = [key for key in seen if seen[key] == maxRepeated]
-        else: 
-            ans = []
+        for i in range(n - 10 + 1):
+            string = s[i: i + 10]
+            
+            counter[string] += 1 
+            
+            if counter[string] == 2: 
+                ans.append(string)
+                
+        return ans 
         
-        return ans
         
