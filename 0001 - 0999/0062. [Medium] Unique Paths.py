7bd4138b9d5e4bf49b643ba7c245ac1a8c740d19
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-08 13:08:14 -0300

    Update 0062. [Medium] Unique Paths.py

--- 0001 - 0999/0062. [Medium] Unique Paths.py
+++ 0001 - 0999/0062. [Medium] Unique Paths.py
@@ -2,10 +2,14 @@
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        arr = [1 for i in range(n)]
-        for j in range(1, m):
-            for i in range(1, n):
-                arr[i] = arr[i - 1] + arr[i]
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
         
-        return arr[n - 1]
+        dp[0][1] = 1 
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+                
+        return dp[-1][-1]
+        
         
