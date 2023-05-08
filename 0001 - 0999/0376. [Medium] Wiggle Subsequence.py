5a006e71bd9375f43e30429bdccb1a4fc728b00e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-08 14:40:34 -0300

    Update 0376. [Medium] Wiggle Subsequence.py

--- 0001 - 0999/0376. [Medium] Wiggle Subsequence.py
+++ 0001 - 0999/0376. [Medium] Wiggle Subsequence.py
@@ -2,22 +2,18 @@
 
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [[0, 0] for _ in range(n)]
-        dp[0] = [1, 1]
-        ans = 1
+        n = len(nums) 
+         
+        negative = 1
+        positive = 1
         
         for i in range(1, n):
-            B, A = 0, 0
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    A = max(A, dp[j][1])
-                if nums[j] > nums[i]:
-                    B = max(B, dp[j][0])
-                    
-            dp[i] = [A + 1, B + 1]
-            ans = max(ans, max(A + 1, B + 1))
-        
-        return ans
+            if nums[i] - nums[i - 1] > 0: 
+                positive = negative + 1
+                
+            elif nums[i] - nums[i - 1] < 0:
+                negative = positive + 1
+                
+        return max(negative, positive)
         
         
