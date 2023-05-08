d420bad235f1ac03c6b7fa1fab82763d3a670870
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-08 13:11:53 -0300

    Update 0064. [Medium] Minimum Path Sum.py

--- 0001 - 0999/0064. [Medium] Minimum Path Sum.py
+++ 0001 - 0999/0064. [Medium] Minimum Path Sum.py
@@ -2,18 +2,16 @@
 
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0])
-        for i in range(1, m):
-            grid[0][i] += grid[0][i - 1]
+        m, n = len(grid), len(grid[0])
         
-        for i in range(1, n):
-            grid[i][0] += grid[i - 1][0]
-            
-        for i in range(1, n):
-            for j in range(1, m):
-                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+        dp = [[inf for j in range(n + 1)] for i in range(m + 1)]
         
-        return grid[n - 1][m - 1]
-            
+        dp[0][1] = 0 
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = grid[i - 1][j - 1] + min(dp[i - 1][j], dp[i][j - 1])
+                
+        return dp[-1][-1]
+        
         
