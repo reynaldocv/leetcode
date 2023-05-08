acae9511c27d88bb7f22247d9854694f38ca0d77
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-08 14:26:44 -0300

    Update 0516. [Medium] Longest Palindromic Subsequence.py

--- 0001 - 0999/0516. [Medium] Longest Palindromic Subsequence.py
+++ 0001 - 0999/0516. [Medium] Longest Palindromic Subsequence.py
@@ -4,19 +4,20 @@ class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         n = len(s)
         
-        t = s[:: -1]
+        word1, word2 = s, s[:: -1]
         
         dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-                
+        
         for i in range(1, n + 1):
             for j in range(1, n + 1):
-                if s[i - 1] == t[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
-                    
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                
                 else: 
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
                     
         return dp[-1][-1]
+        
     
 class Solution2:
     def longestPalindromeSubseq(self, s: str) -> int:
