9fa7b23523daf511c71c3e1528fc980f06f4defd
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-08 14:33:41 -0300

    Update 0300. [Medium] Longest Increasing Subsequence.py

--- 0001 - 0999/0300. [Medium] Longest Increasing Subsequence.py
+++ 0001 - 0999/0300. [Medium] Longest Increasing Subsequence.py
@@ -2,30 +2,20 @@
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        arr = []        
+        arr = []
+        
         for num in nums: 
-            if len(arr) == 0 or arr[-1] < num: 
+            idx = bisect_left(arr, num)
+            
+            if idx == len(arr):
                 arr.append(num)
+                
             else: 
-                idx = bisect_left(arr, num)
-                arr[idx] = num
-             
-        return len(arr)
+                arr[idx] = num 
+                
+        return len(arr)        
         
-class Solution2:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        arr = [1]*n
-        ans = 1
-        for i in range(1, n):
-            maxElem = 0
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    maxElem = max(maxElem, arr[j])                    
-            arr[i] = maxElem + 1
-            ans = max(ans, arr[i])
-        
-        return ans
+
     
 
         
