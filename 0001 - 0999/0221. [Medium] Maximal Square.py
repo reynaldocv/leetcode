e1cbec4183c1fd790536fb68fe8011a59efad00a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-08 13:23:31 -0300

    Update 0221. [Medium] Maximal Square.py

--- 0001 - 0999/0221. [Medium] Maximal Square.py
+++ 0001 - 0999/0221. [Medium] Maximal Square.py
@@ -2,20 +2,21 @@
 
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        n = len(matrix)
-        m = len(matrix[0])
+        m, n = len(matrix), len(matrix[0])
         
-        ans = 0        
-        for i in range(n):
-            for j in range(m):
-                matrix[i][j] = int(matrix[i][j])       
-                if matrix[i][j] == 1: 
-                    ans = 1
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
         
-        for i in range(1, n):
-            for j in range(1, m):
-                if matrix[i][j] == 1:  
-                    matrix[i][j] = min(matrix[i - 1][j],matrix[i][j - 1],matrix[i - 1][j - 1]) + 1
-                    ans = max(ans, matrix[i][j]) 
-            
-        return ans**2
+        ans = 0 
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if matrix[i - 1][j - 1] == "1": 
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])
+                    
+                    ans = max(ans, dp[i][j]**2)
+                    
+        return ans 
+                    
+        
+        
+        
