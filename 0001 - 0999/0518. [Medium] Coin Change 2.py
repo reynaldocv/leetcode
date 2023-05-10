75a9bd77b43276a27d2a0f87fd822505b8c4e974
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-09 22:59:20 -0300

    Update 0518. [Medium] Coin Change 2.py

--- 0001 - 0999/0518. [Medium] Coin Change 2.py
+++ 0001 - 0999/0518. [Medium] Coin Change 2.py
@@ -1,32 +1,14 @@
 # https://leetcode.com/problems/coin-change-2/
-
+    
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        def ways(amount, maxCoin):
-            if amount == 0: 
-                return 1
-            elif (amount, maxCoin) in seen: 
-                return seen[(amount, maxCoin)]
-            else: 
-                ans = 0 
-                for coin in coins: 
-                    if coin <= maxCoin and amount - coin >= 0: 
-                        ans += ways(amount - coin, coin)
-                seen[(amount, maxCoin)] = ans
-                return ans
-            
-        seen = {(0, 0): 1}
-        return ways(amount, max(coins))
-    
-class Solution2:
-    def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0]*(amount + 1)
-        dp[0] = 1
-        for coin in coins:
-            for x in range(coin, amount + 1):
-                dp[x] += dp[x - coin]
+        dp = [1] + [0 for _ in range(amount)]
         
-        return dp[-1]
+        for coin in coins: 
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
                 
+        return dp[-1]
+        
         
         
