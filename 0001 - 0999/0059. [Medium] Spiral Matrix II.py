6ba7c6653866e931b482b91c8532cec38475fd5b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-09 22:32:48 -0300

    Update 0059. [Medium] Spiral Matrix II.py

--- 0001 - 0999/0059. [Medium] Spiral Matrix II.py
+++ 0001 - 0999/0059. [Medium] Spiral Matrix II.py
@@ -2,23 +2,35 @@
 
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        matrix = [[False for i in range(n)] for j in range(n)]
-        direction = [(0,1), (1, 0), (0, -1), (-1, 0)]
-        k = 0 
-        x, y = 0, 0 
-        value = 1 
-        while value <= n*n: 
-            if x < n and y < n and matrix[x][y] == False: 
-                matrix[x][y] = value
-                value += 1
-            else:
-                x -= direction[k][0]
-                y -= direction[k][1]
-                k = (k + 1) % 4
-            x += direction[k][0]
-            y += direction[k][1]
+        ans = [[0 for _ in range(n)] for _ in range(n)]
         
-        return matrix
+        val = 1 
+        
+        (x, y) = (0, -1)
+        
+        seen = {(0, n), (n, n - 1), (n - 1, -1)}
+        
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        k = 0 
+        
+        while val <= n**2:
+            x += directions[k][0]
+            y += directions[k][1]
+            
+            if (x, y) in seen: 
+                x -= directions[k][0]
+                y -= directions[k][1]
+                
+                k = (k + 1) % 4 
+            
+            else: 
+                ans[x][y] = val
+                val += 1 
+                
+                seen.add((x, y))
+                
+        return ans 
                 
                 
         
