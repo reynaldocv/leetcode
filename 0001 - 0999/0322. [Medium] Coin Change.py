6b6c0ba8ced7dc645e18152b1dc3621fed5d0590
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-09 22:52:12 -0300

    Update 0322. [Medium] Coin Change.py

--- 0001 - 0999/0322. [Medium] Coin Change.py
+++ 0001 - 0999/0322. [Medium] Coin Change.py
@@ -2,13 +2,13 @@
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:        
-        numCoins = [inf]*(amount + 1)     
-        numCoins[0] = 0
-        for i in range(amount + 1):
-            for coin in coins: 
-                if i + coin <= amount: 
-                    numCoins[i + coin] = min(numCoins[i + coin], numCoins[i] + 1)
+        dp = [0] + [inf for _ in range(amount)]
         
-        return -1 if numCoins[amount] == inf else numCoins[amount]
-       
-            
+        for coin in coins: 
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], 1 + dp[i - coin])
+          
+        return dp[-1] if dp[-1] != inf else -1
+        
+        
+        
