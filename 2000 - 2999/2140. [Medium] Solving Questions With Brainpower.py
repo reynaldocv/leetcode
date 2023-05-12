ecbda6c05dd78795b5d0f07c12a9a848ee1ddd29
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-12 14:05:30 -0300

    Update 2140. [Medium] Solving Questions With Brainpower.py

--- 2000 - 2999/2140. [Medium] Solving Questions With Brainpower.py
+++ 2000 - 2999/2140. [Medium] Solving Questions With Brainpower.py
@@ -13,6 +13,46 @@ class Solution:
         n = len(questions)
         
         return helper(0)
+    
+class Solution2:
+    def mostPoints(self, questions: List[List[int]]) -> int:        
+        n = len(questions)
+        
+        dp = [0 for _ in range(n)]
+        
+        for i in range(n - 1, -1, -1):
+            end = i + questions[i][1] + 1
+            
+            dp[i] = questions[i][0]
+            
+            if end < n:                 
+                dp[i] += dp[end]
+                
+            if i + 1 < n: 
+                dp[i] = max(dp[i], dp[i + 1])
+                
+        return dp[0]
+    
+ class Solution3:
+    def mostPoints(self, questions: List[List[int]]) -> int:        
+        n = len(questions)
+        
+        dp = defaultdict(lambda: 0)
+        
+        for i in range(n - 1, -1, -1):
+            end = i + questions[i][1] + 1
+            
+            dp[i] = questions[i][0] + dp[end]
+            
+            dp[i] = max(dp[i], dp[i + 1])
+                
+        return dp[0]
+            
+            
+                           
+                
+            
+            
                            
                           
         
