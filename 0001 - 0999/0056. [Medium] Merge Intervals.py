268bb4de4fb2937f32be94aa835e6620c80eed0d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-12 22:24:29 -0300

    Update 0056. [Medium] Merge Intervals.py

--- 0001 - 0999/0056. [Medium] Merge Intervals.py
+++ 0001 - 0999/0056. [Medium] Merge Intervals.py
@@ -2,21 +2,34 @@
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        dic = {}
-        for (start, end) in intervals: 
-            dic[start] = dic.get(start, 0) + 1
-            dic[end] = dic.get(end, 0) - 1
-        idx = [*dic]
-        idx.sort()
-        n = len(idx)
-        aux, ans = 0, []
-        for i in range(n):            
-            if aux == 0: 
-                start = idx[i]
-            aux += dic[idx[i]]
-            if aux <= 0: 
-                ans.append([start, idx[i]])
+        counter = defaultdict(lambda: 0)
         
-        return ans
+        for (a, b) in intervals: 
+            counter[a] += 1
+            counter[b] -= 1 
+            
+        coords = [key for key in counter]
+        coords.sort()
+        
+        ans = []
+        
+        prev = 0 
+        
+        start = None 
+        
+        for x in coords: 
+            prev += counter[x]
+            
+            if start == None: 
+                start = x 
+            
+            if prev == 0: 
+                ans.append((start, x))
                 
+                start = None 
+                
+        return ans 
+            
+        
+        
             
