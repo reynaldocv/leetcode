970b25fdb14504a2a32f04ba5b8e2f40275c6412
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-15 15:05:37 -0300

    Update 0028. [Easy] Implement strStr().py

--- 0001 - 0999/0028. [Easy] Implement strStr().py
+++ 0001 - 0999/0028. [Easy] Implement strStr().py
@@ -2,7 +2,7 @@
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle in haystack:
+        if needle in haystack: 
             return haystack.index(needle)
-        return -1
         
+        return -1
