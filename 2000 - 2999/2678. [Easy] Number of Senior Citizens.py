65033f02d1fab0699104a491e78ab545fb441379
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-17 15:02:31 -0300

    Create 2678. [Easy] Number of Senior Citizens.py

---
+++ 2000 - 2999/2678. [Easy] Number of Senior Citizens.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/number-of-senior-citizens/
+
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        ans = 0 
+        
+        for string in details: 
+            age = int(string[11: 13])
+            
+            if age > 60: 
+                ans += 1 
+                
+        return ans 
+                
+        
