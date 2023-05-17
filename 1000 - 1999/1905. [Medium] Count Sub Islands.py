d5151451df2057aca7d566e26ee3cfca4989a8ab
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-17 14:16:51 -0300

    Update 1905. [Medium] Count Sub Islands.py

--- 1000 - 1999/1905. [Medium] Count Sub Islands.py
+++ 1000 - 1999/1905. [Medium] Count Sub Islands.py
@@ -2,21 +2,26 @@
 
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        def helper(x, y):            
+        def helper(x, y):
             stack = [(x, y)]
-            grid2[x][y] = 0
-            islands = {grid1[x][y]: True}
-                
+            grid2[x][y] = 0 
+            
+            seen = {grid1[x][y]}
+            
             while stack: 
-                (x, y) = stack.pop()
-                for (r, s) in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-                    if 0 <= r < m and 0 <= s < n: 
-                        if grid2[r][s] == 1: 
-                            grid2[r][s] = 0
-                            stack.append((r, s))
-                            islands[grid1[r][s]] = True
+                (x, y) = stack.pop() 
+                
+                for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    p, q = x + r, y + s
+                    
+                    if 0 <= p < m and 0 <= q < n: 
+                        if grid2[p][q] == 1: 
+                            grid2[p][q] = 0 
                             
-            return not 0 in islands
+                            seen.add(grid1[p][q])
+                            stack.append((p, q))
+                            
+            return len(seen) == 1 and 1 in seen
         
         m, n = len(grid1), len(grid1[0])
         
@@ -24,10 +29,10 @@ class Solution:
         
         for i in range(m):
             for j in range(n):
-                if grid2[i][j] == 1: 
-                    if helper(i, j): 
+                if grid2[i][j] == 1:
+                    if helper(i, j):
                         ans += 1 
                         
-        return ans 
+        return ans
                     
         
