4fcba902b3df9202911ea97e78392a31e639e406
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-17 14:26:09 -0300

    Update 0153. [Medium] Find Minimum in Rotated Sorted Array.py

--- 0001 - 0999/0153. [Medium] Find Minimum in Rotated Sorted Array.py
+++ 0001 - 0999/0153. [Medium] Find Minimum in Rotated Sorted Array.py
@@ -3,23 +3,27 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 1: 
-            return nums[0]
         
         start = 0 
         end = n 
-        ans = nums[start]
-        while end - start > 1: 
-            m = (end + start)//2
-            if nums[start] < nums[m]:
-                ans = min(ans, nums[start])
-                start = m
-            else: 
-                ans = min(ans, nums[m])
-                end = m 
         
-        return ans
+        while end - start > 1: 
+            middle = (end + start)//2
+            
+            if nums[0] <= nums[middle]:
+                start = middle 
                 
+            else: 
+                end = middle 
+                
+        if end == n: 
+            return nums[0]
+        
+        else: 
+            return nums[end]
+        
+        
+        
             
         
         
