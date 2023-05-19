fd6f8e654df2c929416145754eb32e3a27129bec
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-18 21:37:28 -0300

    Update 0151. [Medium] Reverse Words in a String.py

--- 0001 - 0999/0151. [Medium] Reverse Words in a String.py
+++ 0001 - 0999/0151. [Medium] Reverse Words in a String.py
@@ -1,20 +1,19 @@
 # https://leetcode.com/problems/reverse-words-in-a-string/
 
 class Solution:
-    def reverseWords(self, s: str) -> str:        
-        words = []
+    def reverseWords(self, s: str) -> str:
+        arr = []
+        
         word = ""
-        n = len(s)
-        for i in range(n):
-            if s[i] != " ":
-                word += s[i]
-            elif word != "":
-                words.append(word)
-                word = ""
         
-        if word != "":
-            words.append(word)
-            
-        return " ".join(words[::-1])
+        for char in s + " ":
+            if char == " ":
+                if word != "":
+                    arr.insert(0, word)
+                    
+                    word = ""
+                    
+            else: 
+                word += char 
                 
-        
+        return " ".join(arr)
