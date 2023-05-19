54a59af70176e771bc3054482d8b7d978ef82bde
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-18 21:45:19 -0300

    Update 0011. [Medium] Container With Most Water.py

--- 0001 - 0999/0011. [Medium] Container With Most Water.py
+++ 0001 - 0999/0011. [Medium] Container With Most Water.py
@@ -3,14 +3,19 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         n = len(height)
-        left = 0
-        right = n - 1
-        ans = 0
-        while right - left > 0:
-            ans = max(ans, min(height[left], height[right])*(right - left))
-            if height[left] < height[right]:
-                left += 1
-            else: 
-                right -= 1
         
-        return ans
+        start = 0 
+        end = n - 1
+        
+        ans = 0 
+        
+        while end - start > 0: 
+            ans = max(ans, min(height[start], height[end])*(end - start))
+            
+            if height[start] <= height[end]:
+                start += 1 
+        
+            else: 
+                end -= 1 
+                
+        return ans 
