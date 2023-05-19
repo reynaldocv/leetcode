202580c8d726955bb1ce48c17bb1577fef2bfbc0
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-18 22:06:50 -0300

    Update 1493. [Medium] Longest Subarray of 1's After Deleting One Element.py

--- 1000 - 1999/1493. [Medium] Longest Subarray of 1's After Deleting One Element.py
+++ 1000 - 1999/1493. [Medium] Longest Subarray of 1's After Deleting One Element.py
@@ -2,23 +2,40 @@
 
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
+        def helper(arr):
+            ans = [0 for _ in range(n)]
+            
+            cnt = 0 
+            
+            for (i, num) in enumerate(arr): 
+                if num == 1:
+                    cnt += 1 
+                    
+                else: 
+                    cnt = 0 
+                    
+                ans[i] = cnt
+                    
+            return ans 
+        
         n = len(nums)
         
-        right = [num for num in nums]
+        prefix = helper(nums)
+        suffix = helper(nums[:: -1])[:: -1]
         
-        for i in range(n - 1, -1, -1):
-            if nums[i] == 1:
-                right[i] += right[i + 1] if i + 1 < n else 0 
-        
-        ans = 0 
-        prev = 0 
+        ans = 0
         
         for i in range(n):
-            valR = right[i + 1] if i + 1 < n else 0                 
-            ans = max(ans, prev + valR)
+            left = prefix[i - 1] if i - 1 >= 0 else 0 
+            right = suffix[i + 1] if i + 1 < n else 0 
             
-            prev = prev + 1 if nums[i] == 1 else 0 
+            ans = max(ans, left + right)
+            
+        return ans 
+            
+            
+            
                 
-        return ans
+        
                 
                
