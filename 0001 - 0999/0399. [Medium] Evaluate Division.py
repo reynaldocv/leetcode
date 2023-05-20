98a21ac14049bc648335e0307bfefb289ab12898
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-20 10:38:33 -0300

    Update 0399. [Medium] Evaluate Division.py

--- 0001 - 0999/0399. [Medium] Evaluate Division.py
+++ 0001 - 0999/0399. [Medium] Evaluate Division.py
@@ -1,32 +1,39 @@
 # https://leetcode.com/problems/evaluate-division/
 
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        def search(u, end, path, seen):
-            for (v, val) in graph[u]:
-                if v == end:
-                    return path * val
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:    
+        def helper(num, den):
+            stack = [(num, 1)]
+            
+            seen = set()
+            
+            while stack: 
+                (x, ans) = stack.pop() 
                 
-                elif v not in seen:
-                    seen[v] = True
-                    ans = search(v, end, path*val, seen)
-                    if ans:
-                        return ans
-            return None
+                if x == den: 
+                    return ans 
+                
+                for y in graph[x]:
+                    if y not in seen: 
+                        seen.add(y)
+                        
+                        stack.append((y, ans*graph[x][y]))
+                                     
+            return -1 
         
-        graph = defaultdict(lambda: [])
+        graph = defaultdict(lambda: {})
         
         for (i, (a, b)) in enumerate(equations):                       
-            graph[a].append((b, values[i]))
-            graph[b].append((a, values[i]**-1))
+            graph[a][b] = values[i]
+            graph[b][a] = 1/values[i]
              
         ans = []
              
         for (num, den) in queries:
-            divisor = search(den, num, 1, {})
-            if divisor:
-                ans.append(1/divisor)
-            else:
+            if num not in graph or den not in graph: 
                 ans.append(-1)
-                
-        return ans
+            
+            else: 
+                ans.append(helper(num, den))            
+            
+        return ans 
