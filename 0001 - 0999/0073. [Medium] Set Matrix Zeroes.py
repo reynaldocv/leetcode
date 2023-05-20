7f43492d88ca1deaf1b88edfc8d41a1da6c0c9b2
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-20 11:12:55 -0300

    Update 0073. [Medium] Set Matrix Zeroes.py

--- 0001 - 0999/0073. [Medium] Set Matrix Zeroes.py
+++ 0001 - 0999/0073. [Medium] Set Matrix Zeroes.py
@@ -5,24 +5,22 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rowsZero = {}
-        colsZero = {}
-        n = len(matrix)
-        m = len(matrix[0])
-        for i in range(n):
-            for j in range(m):
-                if matrix[i][j] == 0: 
-                    rowsZero[i] = True
-                    colsZero[j] = True
-                    
-        for i in range(n):
-            if i in rowsZero: 
-                for j in range(m):
-                    matrix[i][j] = 0
+        m, n = len(matrix), len(matrix[0]) 
         
-        for j in range(m):
-            if j in colsZero: 
-                for i in range(n):
-                    matrix[i][j] = 0
+        columns = set()
+        rows = set()
+        
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0: 
+                    columns.add(j)
+                    rows.add(i)
+                    
+        for i in range(m):
+            for j in range(n):
+                if i in rows or j in columns: 
+                    matrix[i][j] = 0 
+                    
+                    
                     
                     
