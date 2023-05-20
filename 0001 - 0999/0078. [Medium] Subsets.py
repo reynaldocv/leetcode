e42773947a11152a1b5774ca75c72bdc992bc5f6
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-20 11:24:42 -0300

    Update 0078. [Medium] Subsets.py

--- 0001 - 0999/0078. [Medium] Subsets.py
+++ 0001 - 0999/0078. [Medium] Subsets.py
@@ -3,29 +3,40 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = [[]]
+        
         for num in nums: 
-            aux = []
-            for subset in ans: 
-                aux.append(subset + [num])
-                
-            ans.extend(aux)
+            tmp = []
             
-        return ans
+            for arr in ans: 
+                tmp.append(arr + [num])
+                
+            ans.extend(tmp)
+            
+        return ans 
 
 class Solution2:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def helper(arr, i, n,  elems):
-            if i <= n: 
-                self.ans.append(list(arr))
+        def helper(arr, i):
+            if i == n: 
+                ans.append(arr[: -1])
+                
+            else: 
                 for j in range(i, n):
-                    arr.append(elems[j])
-                    helper(arr, j + 1, n, elems)
+                    arr.append(nums[j])
+                    
+                    helper(arr, j + 1)
+                    
                     arr.pop()
+             
+        nums.append(inf)
         
-        self.ans = []
-        helper([], 0, len(nums), nums)
+        n = len(nums)
         
-        return self.ans
+        ans = []
+        
+        helper([], 0)
+        
+        return ans 
                 
         
         
