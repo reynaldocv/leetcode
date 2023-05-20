0ac3f537b3eca910c6f82dfe6ba089b2b6115976
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-19 21:35:47 -0300

    Update 0547. [Medium] Number of Provinces.py

--- 0001 - 0999/0547. [Medium] Number of Provinces.py
+++ 0001 - 0999/0547. [Medium] Number of Provinces.py
@@ -2,22 +2,56 @@
 
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def helper(son):
+            while son != parent[son]:
+                son = parent[son]
+                
+            return son 
+        
+        n = len(isConnected)
+        
+        parent = [i for i in range(n)]
+        
+        for i in range(n):
+            for j in range(n):
+                if i != j and isConnected[i][j] == 1: 
+                    parentI = helper(i)
+                    parentJ = helper(j)
+                    
+                    parent[parentI] = parent[parentJ] = min(parentI, parentJ)
+                    
+        seen = set()
+        
+        for i in range(n):
+            seen.add(helper(i))
+            
+        return len(seen)
+        
+class Solution2:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
         def helper(v0):
             stack = [v0]
             seen[v0] = True
+            
             while stack: 
                 v0 = stack.pop()                
+                
                 for (v1, edge) in enumerate(isConnected[v0]):
                     if edge == 1 and v1 not in seen: 
                         seen[v1] = True
+                        
                         stack.append(v1)
                         
         seen = {}
+        
         n = len(isConnected)
+        
         ans = 0
+        
         for v0 in range(n):
             if v0 not in seen: 
                 helper(v0)
+        
                 ans += 1
                 
         return ans
