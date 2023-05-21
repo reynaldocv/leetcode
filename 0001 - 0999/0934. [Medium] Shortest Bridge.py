10797ef290639a7095abadecdee951df70dc3107
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-21 15:59:57 -0300

    Update 0934. [Medium] Shortest Bridge.py

--- 0001 - 0999/0934. [Medium] Shortest Bridge.py
+++ 0001 - 0999/0934. [Medium] Shortest Bridge.py
@@ -4,33 +4,46 @@ class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         n = len(grid)
         
-        (x, y) = next((i, j) for i in range(n) for j in range(n) if grid[i][j] == 1)
+        (i, j) = next((i, j) for i in range(n) for j in range(n) if grid[i][j] == 1)
         
-        stack = [(x, y)]
-        seen = {(x, y): True}
+        stack = [(i, j)]
+        seen = {(i, j)}
+        
+        grid[i][j] = 0 
+        
         while stack: 
-            (x, y) = stack.pop()
-            for r, s in [(x - 1, y), (x, y - 1), (x, y + 1), (x + 1, y)]: 
-                if 0 <= r < n and 0 <= s < n:
-                    if grid[r][s] == 1 and (r, s) not in seen: 
-                        seen[(r, s)] = True
-                        stack.append((r, s))
-                        
-        ans = 0
+            (x, y) = stack.pop() 
+            
+            for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                p, q = x + r, y + s
+                
+                if 0 <= p < n and 0 <= q < n and grid[p][q] == 1: 
+                    grid[p][q] = 0 
+                    
+                    seen.add((p, q))                    
+                    stack.append((p, q))
         
-        stack = list(seen)
+        stack = [(x, y) for (x, y) in seen]
         
-        while stack:
+        ans = -1
+        
+        while stack: 
             newStack = []
-            for (x, y) in stack: 
-                for (r, s) in [(x - 1, y), (x + 1, y), (x, y + 1), (x, y - 1)]: 
-                    if 0 <= r < n and 0 <= s < n:
-                        if (r, s) not in seen: 
-                            if grid[r][s] == 1: 
-                                return ans 
-                               
-                            newStack.append((r, s))
-                            seen[(r, s)] = True
+            
+            for (x, y) in stack:                 
+                if grid[x][y] == 1: 
+                    return ans 
+                
+                for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    p, q = x + r, y + s
+
+                    if 0 <= p < n and 0 <= q < n and (p, q) not in seen: 
+                        seen.add((p, q))
+
+                        newStack.append((p, q))
+
             stack = newStack
-            ans += 1
-                 
+            
+            ans += 1 
+       
+                  
