11e843b82fe386a6b0f396c9c603e7406f38b399
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-21 16:02:26 -0300

    Update 0934. [Medium] Shortest Bridge.py

--- 0001 - 0999/0934. [Medium] Shortest Bridge.py
+++ 0001 - 0999/0934. [Medium] Shortest Bridge.py
@@ -8,36 +8,32 @@ class Solution:
         
         stack = [(i, j)]
         seen = {(i, j)}
-        
-        grid[i][j] = 0 
-        
+       
         while stack: 
             (x, y) = stack.pop() 
             
             for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                 p, q = x + r, y + s
                 
-                if 0 <= p < n and 0 <= q < n and grid[p][q] == 1: 
-                    grid[p][q] = 0 
-                    
+                if 0 <= p < n and 0 <= q < n and grid[p][q] == 1 and (p, q) not in seen: 
                     seen.add((p, q))                    
                     stack.append((p, q))
         
         stack = [(x, y) for (x, y) in seen]
         
-        ans = -1
+        ans = 0
         
         while stack: 
             newStack = []
             
-            for (x, y) in stack:                 
-                if grid[x][y] == 1: 
-                    return ans 
-                
+            for (x, y) in stack: 
                 for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                     p, q = x + r, y + s
 
                     if 0 <= p < n and 0 <= q < n and (p, q) not in seen: 
+                        if grid[p][q] == 1: 
+                            return ans 
+                        
                         seen.add((p, q))
 
                         newStack.append((p, q))
@@ -45,5 +41,7 @@ class Solution:
             stack = newStack
             
             ans += 1 
-       
-                  
+        
+                    
+
+            
