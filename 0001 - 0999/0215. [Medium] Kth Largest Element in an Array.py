53ebc5890d745964a4e8567b3e2944456b254aa3
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-21 17:31:31 -0300

    Update 0215. [Medium] Kth Largest Element in an Array.py

--- 0001 - 0999/0215. [Medium] Kth Largest Element in an Array.py
+++ 0001 - 0999/0215. [Medium] Kth Largest Element in an Array.py
@@ -2,6 +2,27 @@
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort(reverse = True)
-        return nums[k - 1]
+        nums.sort()
+        
+        return nums[-k]
+        
+class Solution2:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        counter = defaultdict(lambda: 0)
+        
+        maxElem = -inf 
+        for num in nums:
+            maxElem = max(maxElem, num)
+            
+            counter[num] += 1 
+            
+        prev = 0 
+            
+        for num in range(maxElem, -10**4 - 1, -1):
+            if num in counter: 
+                prev += counter[num]
+                
+            if prev >= k: 
+                return num 
+            
         
