25bbf240a54a0d2b55b0d799d218107b62bfebac
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-22 13:16:10 -0300

    Update 0347. [Medium] Top K Frequent Elements.py

--- 0001 - 0999/0347. [Medium] Top K Frequent Elements.py
+++ 0001 - 0999/0347. [Medium] Top K Frequent Elements.py
@@ -2,14 +2,20 @@
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = {}
-        for num in nums: 
-            counter[num] = counter.get(num, 0) + 1
-            
-        arr  = [*counter]       
-        arr.sort(reverse = True, key = lambda item: counter[item])
+        counter = defaultdict(lambda: 0)
+
+        arr = []
         
-        return arr[:k]
-    
+        for num in nums:
+            if num not in counter: 
+                arr.append(num)
+                
+            counter[num] += 1 
+            
+        arr.sort(key = lambda item: -counter[item])
+        
+        return arr[: k]
+                
+        
             
         
