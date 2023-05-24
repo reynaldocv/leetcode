072dd94796ef9feb16bf127447a7cd0164be1d0c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-24 14:30:26 -0300

    Update 0137. [Medium] Single Number II.py

--- 0001 - 0999/0137. [Medium] Single Number II.py
+++ 0001 - 0999/0137. [Medium] Single Number II.py
@@ -3,21 +3,39 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         n = 32       
-        counter = (0,)*(n + 1)
-        pos = 0
-        for num in nums: 
-            k = 0
-            pos = (pos + 1) % 3 if num >= 0 else pos
-            num = abs(num)
-            while num > 0:
-                if (num & 1 == 1):                    
-                    counter = counter[:n - k] + ((counter[n - k] + 1) % 3,) + counter[n - k + 1:]                
-                num = num >> 1
-                k += 1
-        ans = 0
-        for i in range(n, -1, -1):
-            ans += counter[i] << n - i
         
-        return -ans if pos == 0 else ans
+        counter = (0,)*(n + 1)
+        
+        sign = 0
+        
+        for num in nums: 
+            if num > 0:             
+                sign = (sign + 1) % 3
                 
+            elif num < 0: 
+                sign = (sign - 1) % 3
+                
+            num = abs(num)
+        
+            position = 0 
+            
+            while num: 
+                unit = num % 2 
+                
+                if unit == 1: 
+                    counter = counter[: position] + ((counter[position] + 1) % 3, ) + counter[position + 1: ]
+                
+                num //= 2 
+                
+                position += 1 
+                
+        ans = 0
+        
+        for i in range(n + 1):
+            ans += counter[i] << i
+        
+        if sign == 2: 
+            ans *= -1
+            
+        return ans 
         
