97e0e5adcab628a18d0154996c9deb11c68e4fc3
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-05-24 23:43:27 -0300

    Update 0837. [Medium] New 21 Game.py

--- 0001 - 0999/0837. [Medium] New 21 Game.py
+++ 0001 - 0999/0837. [Medium] New 21 Game.py
@@ -1,21 +1,21 @@
 # https://leetcode.com/problems/new-21-game/
 
 class Solution:
-    def new21Game(self, n: int, k: int, maxPts: int) -> float:        
-        if k == 0 or n >= k + maxPts: 
-            return 1.0
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        dp = [1] + [0 for _ in range(n)]
         
-        dp = [1.0] + [0.0 for _ in range(n)]
+        s = 0 
         
-        Wsum = 1.0
-        
+        if k > 0:
+            s = 1 
+            
         for i in range(1, n + 1):
-            dp[i] = Wsum / maxPts
-            if i < k: 
-                Wsum += dp[i]                
-            if i - maxPts >= 0: 
-                Wsum -= dp[i - maxPts]
+            dp[i] = s / maxPts
+            if i < k:
+                s += dp[i]
+                
+            if i - maxPts >= 0 and i - maxPts < k:
+                s -= dp[i - maxPts]
                 
         return sum(dp[k:])
-    
    
