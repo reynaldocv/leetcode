998b8bd7b6e99154cddf006ba97122c779f2b9bd
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-01 15:32:20 -0300

    Update 1091. [Medium] Shortest Path in Binary Matrix.py

--- 1000 - 1999/1091. [Medium] Shortest Path in Binary Matrix.py
+++ 1000 - 1999/1091. [Medium] Shortest Path in Binary Matrix.py
@@ -3,22 +3,63 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        distances = [[inf for _ in range(n)] for _ in range(n)]
-        distances[0][0] = 1 if grid[0][0] == 0 else inf
-        stack = [(0, 0)] if grid[0][0] == 0 else []
-        seen = {(0, 0): True}
+        
+        if grid[0][0] != 0: 
+            return -1 
+        
+        stack = [(0, 0)]
+        seen = {(0, 0)}
+        
+        ans = 1
         
         while stack: 
-            (x, y) = stack.pop(0)
-            for (i, j) in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
-                r, s = x + i, y + j
-                if 0 <= r < n and 0 <= s < n: 
-                    if (r, s) not in seen and grid[r][s] == 0: 
-                        distances[r][s] = min(distances[r][s], 1 + distances[x][y])
-                        seen[(r, s)] = True
-                        stack.append((r, s))
-      
-        return - 1 if distances[-1][-1] == inf else distances[-1][-1]
+            newStack = []
+            
+            for (x, y) in stack: 
+                if x == n - 1 and y == n - 1: 
+                    return ans 
+                
+                for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (-1, 1), (1, -1)]:
+                    p, q = x + r, y + s
+                    
+                    if 0 <= p < n and 0 <= q < n and grid[p][q] == 0 and (p, q) not in seen:
+                        seen.add((p, q))
+                        
+                        newStack.append((p, q))
+                        
+            stack = newStack            
+            ans += 1 
+            
+        return -1 
         
+class Solution2:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        
+        distance = defaultdict(lambda: defaultdict(lambda: inf))
+        
+        if grid[0][0] == 1: 
+            return -1 
+        
+        distance[0][0] = 1 
+        
+        stack = [(0, 0)]
+
+        while stack: 
+            (x, y) = stack.pop(0) 
+            
+            for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (-1, 1), (1, -1)]:
+                    p, q = x + r, y + s
+                    
+                    if 0 <= p < n and 0 <= q < n and grid[p][q] == 0:
+                        if distance[p][q] > distance[x][y] + 1:
+                            distance[p][q] = distance[x][y] + 1
+                            
+                            stack.append((p, q))
+                            
+        if distance[n - 1][n - 1] == inf: 
+            return -1 
+        
+        return distance[n - 1][n - 1]
         
         
