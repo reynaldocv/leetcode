b7db36c003ddbf943db5922cf58c9f6f1e704e92
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-05 16:53:02 -0300

    Update 0097. [Medium] Interleaving String.py

--- 0001 - 0999/0097. [Medium] Interleaving String.py
+++ 0001 - 0999/0097. [Medium] Interleaving String.py
@@ -2,24 +2,45 @@
 
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        idxS1, idxS2 = 0, 0
-        n, m, l = len(s1), len(s2), len(s3)
-        
-        if l != m + n:
-            return False
-        
-        board = [[False for _ in range(m + 1)] for _ in range(n + 1)]
-        board[0][0] = True
-        for j in range(1, m + 1):
-            board[0][j] = board[0][j -1] and s2[j - 1] == s3[j - 1]
-        for i in range(1, n + 1):
-            board[i][0] = board[i -1][0] and s1[i - 1] == s3[i - 1]
-        
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
-                if board[i - 1][j] and s1[i - 1] == s3[i + j - 1]:
-                    board[i][j] = board[i - 1][j]
-                if board[i][j - 1] and s2[j - 1] == s3[i + j - 1]:
-                    board[i][j] = board[i][j - 1]
+        @cache 
+        def helper(s1, s2, s3):
+            if s1 == s2 and s2 == s3 and s1 == "":
+                return True 
+            
+            else: 
+                ans = False 
                 
-        return board[n][m]
+                if s1 and s3 and s1[0] == s3[0]:
+                    ans = ans or helper(s1[1: ], s2, s3[1: ])
+                    
+                if s2 and s3 and s2[0] == s3[0]:
+                    ans = ans or helper(s1, s2[1: ], s3[1: ])
+                    
+                return ans 
+            
+        return helper(s1, s2, s3)
+    
+class Solution2:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        m, n, l = len(s1), len(s2), len(s3)
+        
+        if m + n != l: 
+            return False 
+        
+        dp = [[False for _ in range(n + 1)] for _ in range(m + 1)]
+        
+        dp[0][0] = True 
+        
+        for i in range(m + 1):
+            for j in range(n + 1):
+                idx = i + j - 1
+                
+                if i >= 1 and s1[i - 1] == s3[idx] and dp[i - 1][j]:
+                    dp[i][j] = True 
+                
+                if j >= 1 and s2[j - 1] == s3[idx] and dp[i][j - 1]:
+                    dp[i][j] = True 
+     
+        return dp[-1][-1]
+     
+        
