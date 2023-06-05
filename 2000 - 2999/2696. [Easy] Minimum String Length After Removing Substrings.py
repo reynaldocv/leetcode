95e4890af48eaceee332bd4b7d05e888a0688344
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-05 10:11:20 -0300

    Create 2696. [Easy] Minimum String Length After Removing Substrings.py

---
+++ 2000 - 2999/2696. [Easy] Minimum String Length After Removing Substrings.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/minimum-string-length-after-removing-substrings/
+
+class Solution:
+    def minLength(self, s: str) -> int:
+        stack = []
+        
+        for char in s: 
+            if char == "B" and stack and stack[-1] == "A":
+                stack.pop()
+                
+            elif char == "D" and stack and stack[-1] == "C":
+                stack.pop()
+                
+            else: 
+                stack.append(char)
+                
+        return len(stack)
+                
+            
