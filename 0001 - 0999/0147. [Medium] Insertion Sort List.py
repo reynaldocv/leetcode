2a9c82f7705984ada680e3ca825fdbb359291a36
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-07 16:30:45 -0300

    Update 0147. [Medium] Insertion Sort List.py

--- 0001 - 0999/0147. [Medium] Insertion Sort List.py
+++ 0001 - 0999/0147. [Medium] Insertion Sort List.py
@@ -7,20 +7,20 @@
 #         self.next = next
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        nodes = []
         arr = []
-        n = 0 
-        while head:
-            idx = bisect_left(arr, head.val)
-            arr.insert(idx, head.val)
-            nodes.insert(idx, head)
-            head = head.next
-            n += 1
-            
-        for i in range(1, n):
-            nodes[i - 1].next = nodes[i]
-            
-        nodes[-1].next = None
         
-        return nodes[0]
+        while head: 
+            insort(arr, head, key = lambda item: item.val)
+            
+            head = head.next
+            
+        n = len(arr)
+        
+        for i in range(n - 1):
+            arr[i].next = arr[i + 1]
+            
+        arr[-1].next = None 
+        
+        return arr[0]
+        
             
