1c8d9d3d4edd491d9dd407588c905bf975ce1004
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-06 23:21:48 -0300

    Update 2680. [Medium] Maximum OR.py

--- 2000 - 2999/2680. [Medium] Maximum OR.py
+++ 2000 - 2999/2680. [Medium] Maximum OR.py
@@ -3,9 +3,9 @@
 class Solution:
     def maximumOr(self, nums: List[int], k: int) -> int:
         def helper(arr):
-            ans = [0]
+            ans = [arr[0]]
             
-            for num in arr: 
+            for num in arr[1: ]: 
                 ans.append(ans[-1] | num)
                 
             return ans 
@@ -18,8 +18,8 @@ class Solution:
         ans = 0 
         
         for i in range(n):
-            l = left[i]
-            r = right[i + 1]
+            l = left[i - 1] if i - 1 >= 0 else 0 
+            r = right[i + 1] if i + 1 < n else 0 
             
             ans = max(ans, l | r | nums[i] << k)
             
