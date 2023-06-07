e8dfcb52526dc6b3cc6f4e3d3a244713004cbc02
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-06 22:49:12 -0300

    Update 0018. [Medium] 4Sum.py

--- 0001 - 0999/0018. [Medium] 4Sum.py
+++ 0001 - 0999/0018. [Medium] 4Sum.py
@@ -2,26 +2,31 @@
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        n = len(nums)
-        if n < 4: 
-            return []
-        else: 
-            nums.sort()
-            ans = []
-            exist = {}
-            for i in range(n):
-                for j in range(i + 1, n):
-                    k = j + 1
-                    l = n - 1
-                    while l - k > 0:
-                        aux = nums[i] + nums[j] + nums[k] + nums[l] 
-                        if aux == target: 
-                            key = str(nums[i]) + "-" + str(nums[j]) + "-" + str(nums[k]) + "-" + str(nums[l])
-                            if key not in exist:  
-                                ans.append([nums[i], nums[j], nums[k], nums[l]])
-                                exist[key] = True                                
-                        if aux > target: 
-                            l -= 1
-                        else: 
-                            k += 1
-            return ans
+        n = len(nums) 
+        
+        nums.sort() 
+        
+        ans = set()
+        
+        for i in range(n - 3):
+            for j in range(i + 1, n - 2):
+                k = j + 1
+                l = n - 1
+                
+                while k < l:
+                    aSum = nums[i] + nums[j] + nums[k] + nums[l]
+                    
+                    if aSum == target: 
+                        ans.add((nums[i], nums[j], nums[k], nums[l]))  
+                        
+                        k += 1 
+                        l -= 1 
+
+                    elif aSum < target: 
+                        k += 1 
+                        
+                    else: 
+                        l -= 1 
+                        
+        return ans 
+        
