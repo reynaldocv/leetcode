efe8a9b1a4d1a9b4d40ae362aeea685770b109d4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-09 15:47:58 -0300

    Update 0007. [Easy] Reverse Integer.py

--- 0001 - 0999/0007. [Easy] Reverse Integer.py
+++ 0001 - 0999/0007. [Easy] Reverse Integer.py
@@ -2,13 +2,16 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
-        aux = 1
-        if x < 0:             
-            aux = -1
-        strx = str(x*aux)
-        ans = aux*int(strx[::-1])
-        if (-2**31 - 1 < ans < 2**31 - 1):
-            return ans
-        else:
-            return 0
+        sign = 1 
         
+        if x < 0: 
+            x *= -1 
+            sign = -1 
+            
+        ans = sign * int(str(x)[::-1]) 
+        
+        if -(2**31) <= ans < 2**31:        
+            return ans 
+        
+        else: 
+            return 0 
