117e56d6339315acde05a33280a21fa8c5eb90cf
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-12 11:19:26 -0300

    Update 0228. [Easy] Summary Ranges.py

--- 0001 - 0999/0228. [Easy] Summary Ranges.py
+++ 0001 - 0999/0228. [Easy] Summary Ranges.py
@@ -2,22 +2,24 @@
 
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        prev = -inf
-        start = -inf 
+        start = None 
+        prev = -inf 
+        
         ans = []
         
         for num in nums + [inf]: 
-            if prev + 1 != num: 
-                if prev != -inf:
-                    if start != prev: 
-                        ans.append(str(start) + "->" + str(prev))
-                    else:
+            if prev + 1 == num:
+                prev = num 
+                
+            else:
+                if start != None: 
+                    if start == prev: 
                         ans.append(str(start))
+                    
+                    else:                         
+                        ans.append(str(start) + "->" + str(prev))
                 
                 start = num 
-                prev = num 
-            
-            else: 
                 prev = num 
                 
         return ans 
