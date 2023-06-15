ec1ce58c1da96947d60a4a1285cd72d4f7c27195
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-14 22:56:43 -0300

    Update 0204. [Easy] Count Primes.py

--- 0001 - 0999/0204. [Easy] Count Primes.py
+++ 0001 - 0999/0204. [Easy] Count Primes.py
@@ -2,14 +2,16 @@
 
 class Solution:
     def countPrimes(self, n: int) -> int:
-        primes = [1 for i in range(n)]
+        dp = [1 for i in range(n)]
         
-        ans = 0
+        ans = 0 
+        
         for i in range(2, n):
-            if primes[i] == 1:
-                ans += 1
-                for j in range(2*i, n, i):                    
-                    primes[j] += 1
-           
+            if dp[i] == 1: 
+                ans += 1 
+            
+                for j in range(2*i, n, i):
+                    dp[j] = 0 
+                    
+        return ans 
         
-        return ans
