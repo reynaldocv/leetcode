83fc823b0860e9122d190d06aff0139afe14b992
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-14 22:52:22 -0300

    Update 0201. [Medium] Bitwise AND of Numbers Range.py

--- 0001 - 0999/0201. [Medium] Bitwise AND of Numbers Range.py
+++ 0001 - 0999/0201. [Medium] Bitwise AND of Numbers Range.py
@@ -2,30 +2,14 @@
 
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        if left == 0: 
-            return 0 
-        else:
-            ans = 0
-            while left > 0: 
-                i = int(log(left, 2))
-                commonBit = 0
-                if 2**i <= right <= 2**(i + 1) - 1:
-                    commonBit = 1 << i
-                else: 
-                    break
-                ans += commonBit
-                left -= commonBit
-                right -= commonBit
+        count = 0 
+        
+        while left != right: 
+            left //= 2 
+            right //= 2 
             
-            return ans
-        
-class Solution2:
-    def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        count = 0
-        while left != right:
-            left = left >> 1
-            right = right >> 1
-            count += 1
-        
+            count += 1 
+            
         return left << count
+            
         
