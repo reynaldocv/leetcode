5cb0df6b5c0dbf3acec0416d4554dfe600ae42d3
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-16 23:19:53 -0300

    Update 0240. [Medium] Search a 2D Matrix II.py

--- 0001 - 0999/0240. [Medium] Search a 2D Matrix II.py
+++ 0001 - 0999/0240. [Medium] Search a 2D Matrix II.py
@@ -2,22 +2,29 @@
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        n, m = len(matrix), len(matrix[0])        
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        x, y = 0, 0
-        points = [[0, 0]]
-        visited = [[False for j in range(m)] for i in range(n)]
-        while len(points) > 0: 
-            (x, y) = points.pop()
-            visited[x][y] = True
+        m, n = len(matrix), len(matrix[0])
+        
+        if matrix[0][0] > target: 
+            return False 
+
+        stack = [(0, 0)]
+        seen = {(0, 0)}
+
+        while stack: 
+            (x, y) = stack.pop()
+            
             if matrix[x][y] == target: 
-                return True
-            else: 
-                for (i, j) in directions: 
-                    r, s = x + i, y + j
-                    if 0 <= r < n and 0 <= s < m: 
-                        if visited[r][s] == False and visited[r][s] <= target: 
-                            points.append((r,s))
-        return False
-                    
+                return True 
+            
+            for (r, s) in [(1, 0), (0, 1)]:
+                p, q = x + r, y + s
+                
+                if 0 <= p < m and 0 <= q < n:                 
+                    if (p, q) not in seen and matrix[p][q] <= target: 
+                        seen.add((p, q))
+
+                        stack.append((p, q))
+
+        return False 
+            
             
