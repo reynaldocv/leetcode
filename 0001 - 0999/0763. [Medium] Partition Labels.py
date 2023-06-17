36010f23f11121b87c6a41f73279c1a4f69795e0
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-16 23:11:41 -0300

    Update 0763. [Medium] Partition Labels.py

--- 0001 - 0999/0763. [Medium] Partition Labels.py
+++ 0001 - 0999/0763. [Medium] Partition Labels.py
@@ -2,20 +2,23 @@
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        ending = {}
+        last = defaultdict(lambda: -1)
         
         for (i, char) in enumerate(s):
-            ending[char] = i
+            last[char] = i 
+            
+        start = -1 
+        end = -1
         
-        start = 0 
-        end = 0 
         ans = []
         
         for (i, char) in enumerate(s):
-            end = max(end, ending[char])
-            if end == i: 
-                ans.append(end - start + 1)
-                start = i + 1
-        
-        return ans
+            end = max(end, last[char])
+            
+            if i == end: 
+                ans.append(i - start)
+                
+                start = i
+                
+        return ans 
         
