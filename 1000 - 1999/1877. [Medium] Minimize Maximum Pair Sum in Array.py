743229ed2f7db0ada7eca9ab6862f9fbad6004b4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-23 16:25:59 -0300

    Update 1877. [Medium] Minimize Maximum Pair Sum in Array.py

--- 1000 - 1999/1877. [Medium] Minimize Maximum Pair Sum in Array.py
+++ 1000 - 1999/1877. [Medium] Minimize Maximum Pair Sum in Array.py
@@ -3,9 +3,13 @@
 class Solution:
     def minPairSum(self, nums: List[int]) -> int:
         n = len(nums)
-        nums.sort()
-        ans = 0
-        for i in range(n):
+        
+        nums.sort() 
+        
+        ans = 0 
+        
+        for i in range(n//2):
             ans = max(ans, nums[i] + nums[n - 1 - i])
             
-        return ans
+        return ans 
+        
