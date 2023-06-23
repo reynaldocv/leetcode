6ff4b7407bd4fade302edb00473424df7e7a94ce
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-23 00:48:08 -0300

    Update 1637. [Medium] Widest Vertical Area Between Two Points Containing No Points.py

--- 1000 - 1999/1637. [Medium] Widest Vertical Area Between Two Points Containing No Points.py
+++ 1000 - 1999/1637. [Medium] Widest Vertical Area Between Two Points Containing No Points.py
@@ -2,10 +2,14 @@
 
 class Solution:
     def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
-        points = sorted(points)
-        ans = 0
-        for i in range(len(points) - 1):
-            ans = max(points[i + 1][0] - points[i][0], ans)
+        n = len(points) 
         
-        return ans
+        points.sort(key = lambda item: item[0])
+        
+        ans = 0 
+        
+        for i in range(n - 1):
+            ans = max(ans, points[i + 1][0] - points[i][0])
+            
+        return ans 
         
