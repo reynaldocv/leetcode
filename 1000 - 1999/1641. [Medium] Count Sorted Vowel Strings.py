1734543a2357803f1cfa252591f32a4c7a50f03d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-23 18:32:31 -0300

    Update 1641. [Medium] Count Sorted Vowel Strings.py

--- 1000 - 1999/1641. [Medium] Count Sorted Vowel Strings.py
+++ 1000 - 1999/1641. [Medium] Count Sorted Vowel Strings.py
@@ -3,11 +3,30 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
         arr = [1]* 5
+        
         for i in range(1, n):
             aux = 0
+            
             for j in range(0, 5):
                 aux += arr[j]
+                
                 arr[j] = aux
         
         return sum(arr)
                 
+class Solution2:
+    def countVowelStrings(self, n: int) -> int:
+        @cache 
+        def helper(i, cnt):
+            if cnt == 1: 
+                return i
+            
+            else: 
+                ans = 0 
+                
+                for j in range(i, 0, -1):
+                    ans += helper(j, cnt - 1)
+                    
+                return ans 
+            
+        return helper(5, n)
