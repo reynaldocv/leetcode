4e9597976ad99a17e3a1aeee7c6f1c9137a26d76
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-23 10:23:19 -0300

    Update 2120. [Medium] Execution of All Suffix Instructions Staying in a Grid.py

--- 2000 - 2999/2120. [Medium] Execution of All Suffix Instructions Staying in a Grid.py
+++ 2000 - 2999/2120. [Medium] Execution of All Suffix Instructions Staying in a Grid.py
@@ -4,19 +4,24 @@ class Solution:
     def executeInstructions(self, n: int, startPos: List[int], s: str) -> List[int]:
         m = len(s)
         
-        moves = {"U": (-1, 0), "D": (1, 0), "R": (0, 1), "L": (0, -1)}
+        moves = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
         
-        ans = [-1 for _ in range(m)]
+        ans = []
         
-        for i in range(m):
-            x, y = startPos[0], startPos[1]
-            idx = i
-            dist = 0
-            while idx < m and 0 <= x + moves[s[idx]][0] < n and 0 <= y + moves[s[idx]][1] < n: 
-                x, y = x + moves[s[idx]][0], y + moves[s[idx]][1]                                
-                dist += 1
-                idx += 1
-                
-            ans[i] = dist
+        for i in range(m):            
+            (x, y) = startPos
             
-        return ans
+            counter = 0 
+                
+            for move in s[i: ]:
+                x, y = x + moves[move][0], y + moves[move][1]
+
+                if 0 <= x < n and 0 <= y < n: 
+                    counter += 1 
+
+                else: 
+                    break 
+
+            ans.append(counter)
+
+        return ans 
