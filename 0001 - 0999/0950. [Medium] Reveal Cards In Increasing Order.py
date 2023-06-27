251c2eef4ed67b85cbdfe48e2a6ea9e6481bde92
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-27 16:22:53 -0300

    Update 0950. [Medium] Reveal Cards In Increasing Order.py

--- 0001 - 0999/0950. [Medium] Reveal Cards In Increasing Order.py
+++ 0001 - 0999/0950. [Medium] Reveal Cards In Increasing Order.py
@@ -1,24 +1,31 @@
 # https://leetcode.com/problems/reveal-cards-in-increasing-order/
 
 class Solution:
-    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:        
-        n = len(deck)        
-        arr = [" " for _ in range(n)]
-        idx = 0
-        flip = True
-        i = 0
-        while idx < n:
-            if arr[i] == " ":
-                if flip == True: 
-                    arr[i] = idx
-                    idx += 1                
-                flip = not flip
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        n = len(deck)
+        
+        ans = [0 for _ in range(n)]
+        
+        put = True 
+        idx = 0 
+        
+        i = 0 
+        
+        deck.sort() 
+        
+        while i < n:
+            if ans[idx] == 0:
+                if put: 
+                    ans[idx] = deck[i]
+                    i += 1 
+                    
+                    put = False 
+                
+                else:
+                    put = True 
+                    
+            idx = (idx + 1) % n
             
-            i = (i + 1) % n
+        return ans 
             
-        deck.sort()
-        for i in range(n): 
-            arr[i] = deck[arr[i]]
-            
-        return arr
                 
