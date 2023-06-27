83a7e7e9523f3dd802a868371105aefa757eeb5b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-27 16:58:00 -0300

    Update 2326. [Medium] Spiral Matrix IV.py

--- 2000 - 2999/2326. [Medium] Spiral Matrix IV.py
+++ 2000 - 2999/2326. [Medium] Spiral Matrix IV.py
@@ -9,31 +9,31 @@ class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
         ans = [[-1 for _ in range(n)] for _ in range(m)]
         
-        seen = {(0, n), (m, n - 1), (m - 1, -1)}        
+        seen = {(0, n), (m, n - 1), (m - 1, -1)}
+        
+        x, y = 0, -1
         
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         
         k = 0 
         
-        i = 0 
+        ith = 0 
         
-        x, y = 0, -1
-        
-        while head:
+        while head: 
             x += directions[k][0]
             y += directions[k][1]
             
-            if (x, y) in seen: 
+            if (x, y) not in seen:
+                ans[x][y] = head.val 
+            
+                head = head.next
+                seen.add((x, y))
+                
+            else: 
                 x -= directions[k][0]
                 y -= directions[k][1]
                 
                 k = (k + 1) % 4
-                
-            else: 
-                ans[x][y] = head.val
-                head = head.next
-                i += 1 
-                seen.add((x, y))
                 
         return ans 
             
