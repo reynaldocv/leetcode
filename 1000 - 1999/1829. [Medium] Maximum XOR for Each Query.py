81df83731f89e760ad2a559bbff5e7c691a79e7f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-27 16:28:56 -0300

    Update 1829. [Medium] Maximum XOR for Each Query.py

--- 1000 - 1999/1829. [Medium] Maximum XOR for Each Query.py
+++ 1000 - 1999/1829. [Medium] Maximum XOR for Each Query.py
@@ -2,16 +2,19 @@
 
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
-        n = len(nums)
-        ans = [0 for i in range(n)]
+        tmp = 2**maximumBit - 1
         
-        val = 2**maximumBit - 1
-        aux = 0
-        for i in range(n):
-            aux ^= nums[i]            
-            ans[n - 1 - i] = aux ^ val
+        prev = 0 
+        
+        ans = []
+        
+        for num in nums: 
+            prev ^= num 
             
-        return ans
+            ans.append(prev^tmp)
             
+        return ans[:: -1]
+            
+        
             
         
