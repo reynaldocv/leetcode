a004d49da42034d14d7b8587140e55f7da410bbc
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-06-29 17:43:06 -0300

    Update 0593. [Medium] Valid Square.py

--- 0001 - 0999/0593. [Medium] Valid Square.py
+++ 0001 - 0999/0593. [Medium] Valid Square.py
@@ -2,20 +2,19 @@
 
 class Solution:
     def validSquare(self, p1: List[int], p2: List[int], p3: List[int], p4: List[int]) -> bool:
-        def distance(x0, y0, x1, y1):
-            return ((x0 - x1)**2 + (y1 - y0)**2)**.5
-        
         points = [p1, p2, p3, p4]
-        distances = {}
+        
+        seen = set()
+        
         for i in range(4):
             for j in range(i + 1, 4):
-                dist = distance(points[i][0], points[i][1], points[j][0], points[j][1])
-                distances[dist] = distances.get(dist, 0) + 1
+                distX = abs(points[i][0] - points[j][0])**2
+                distY = abs(points[i][1] - points[j][1])**2
+                
+                seen.add(distX + distY)
+                
+        return 0 not in seen and len(seen) == 2
+                
         
-        if len(distances) == 2: 
-            arr = [*distances]
-            arr.sort()           
-            return True if (distances[arr[0]] == 4 and distances[arr[1]] == 2) else False
         
-        return False
                 
