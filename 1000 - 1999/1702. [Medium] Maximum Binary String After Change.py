1e813863e369a206368d5009954dbfe1d4f5ab69
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-07-03 14:53:13 -0300

    Create 1702. [Medium] Maximum Binary String After Change.py

---
+++ 1000 - 1999/1702. [Medium] Maximum Binary String After Change.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/maximum-binary-string-after-change/
+
+class Solution:
+    def maximumBinaryString(self, binary: str) -> str:
+        count = [0, 0]
+        
+        n = len(binary)
+        
+        start = 0 
+        
+        while start < n and binary[start] == "1":
+            start += 1 
+            
+        for char in binary[start: ]: 
+            count[int(char)] += 1 
+            
+        if count[0] <= 1: 
+            return binary
+        
+        else: 
+            return "1"*start + "1"*(count[0] - 1) + "0" + "1"*count[1]
+        
