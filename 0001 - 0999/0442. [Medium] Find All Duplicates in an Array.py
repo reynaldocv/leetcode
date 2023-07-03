b108591b80d9a3e9916a538f98f65e42f9257a43
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-07-03 11:29:43 -0300

    Update 0442. [Medium] Find All Duplicates in an Array.py

--- 0001 - 0999/0442. [Medium] Find All Duplicates in an Array.py
+++ 0001 - 0999/0442. [Medium] Find All Duplicates in an Array.py
@@ -3,10 +3,13 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
         ans = []
+        
         for num in nums: 
             n = abs(num)
+            
             if nums[n - 1] < 0:
                 ans.append(n)
+                
             else: 
                 nums[n - 1] *= -1
         
