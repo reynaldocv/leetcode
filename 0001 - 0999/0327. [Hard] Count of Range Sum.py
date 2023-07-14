69c95ac9ff8aedf16554b8d4b7f385295e180e4f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-07-14 00:21:42 -0300

    Update 0327. [Hard] Count of Range Sum.py

--- 0001 - 0999/0327. [Hard] Count of Range Sum.py
+++ 0001 - 0999/0327. [Hard] Count of Range Sum.py
@@ -3,6 +3,7 @@
 class Solution:
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
         ans = prefix = 0 
+        
         arr = [0]
         for num in nums: 
             prefix += num
@@ -11,6 +12,31 @@ class Solution:
             end = bisect_right(arr, prefix - lower)           
             
             ans += end - start 
+            
             insort(arr, prefix)
             
         return ans 
+
+class Solution2:
+    def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
+        def helper(arr, value):            
+            ans = 0 
+            
+            tmp = []
+            
+            for num in arr[:: -1]:
+                idx = bisect_left(tmp, num + value)
+
+                ans += len(tmp) - idx
+
+                insort(tmp, num)
+
+            return ans 
+
+        prefix = [0]
+        
+        for num in nums: 
+            prefix.append(prefix[-1] + num)
+            
+        return helper(prefix, lower) - helper(prefix, upper + 1)
+        
