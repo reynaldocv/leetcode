fed43a4d64dd3ff551f57a034b937a9e1a32ee3d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-07-13 23:27:39 -0300

    Update 0817. [Medium] Linked List Components.py

--- 0001 - 0999/0817. [Medium] Linked List Components.py
+++ 0001 - 0999/0817. [Medium] Linked List Components.py
@@ -5,15 +5,19 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:        
         ans = 0 
+        
         prev = False
+
+        seen = {num for num in nums}
+        
         while head: 
-            if head.val in nums: 
+            if head.val in seen: 
                 if prev == False: 
                     ans += 1
+                    
                     prev = True
             else: 
                 prev = False
@@ -21,5 +25,4 @@ class Solution:
             head = head.next
             
         return ans
-            
  
