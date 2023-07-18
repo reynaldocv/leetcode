ebad01cb15b18ef4a30c15d54562c4ad3b7a4e48
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-07-18 15:02:41 -0300

    Update 0039. [Medium] Combination Sum.py

--- 0001 - 0999/0039. [Medium] Combination Sum.py
+++ 0001 - 0999/0039. [Medium] Combination Sum.py
@@ -2,20 +2,21 @@
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def helper(arr, idx):
+            if sum(arr) == target: 
+                ans.append(arr[: ])
+                
+            else: 
+                for i in range(idx, n):
+                    if sum(arr) + candidates[i] <= target: 
+                        helper(arr + [candidates[i]], i)
+                        
+        n = len(candidates)
         
-        def recursive(arr, i, aSum):
-            if aSum == self.target: 
-                self.ans.append(arr)
-            elif aSum < self.target: 
-                for j in range(i, len(self.candidates)):
-                    arr2 = arr.copy()
-                    arr2.append(self.candidates[j])
-                    recursive(arr2, j, aSum  + self.candidates[j])
+        ans = []
         
-        self.target = target 
-        self.candidates = candidates
-        self.ans = []
+        helper([], 0)
         
-        recursive([], 0, 0)
+        return ans 
+            
         
-        return self.ans
