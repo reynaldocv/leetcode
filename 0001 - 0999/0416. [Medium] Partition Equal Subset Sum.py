3fd9f406d3c6a90d76f5227649990a2d4e3f7935
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-07-18 20:44:29 -0300

    Update 0416. [Medium] Partition Equal Subset Sum.py

--- 0001 - 0999/0416. [Medium] Partition Equal Subset Sum.py
+++ 0001 - 0999/0416. [Medium] Partition Equal Subset Sum.py
@@ -2,24 +2,21 @@ https://leetcode.com/problems/partition-equal-subset-sum/
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        _sum = sum(nums)
+        aSum = sum(nums)
         
-        if _sum % 2 == 1: 
-            return False
+        if aSum % 2 == 1: 
+            return False 
         
-        target = _sum//2
-        n = len(nums)
-        dp = [False for i in range(target + 1)]
-        dp[0] = True
+        limit = aSum//2
         
-        for i in range(n):
-            newDp = list(dp)                
-            for t in range(nums[i], target + 1):
-                if 0 <= t - nums[i] <= target: 
-                    newDp[t] = dp[t] or dp[t - nums[i]]
-                    
-            dp = newDp
+        dp = [True] + [False for _ in range(limit)]
+        
+        for num in nums: 
+            for end in range(limit, num - 1, -1):
+                if dp[end - num] == True:
+                    dp[end] = dp[end - num]
             
         return dp[-1]
+        
         
     
