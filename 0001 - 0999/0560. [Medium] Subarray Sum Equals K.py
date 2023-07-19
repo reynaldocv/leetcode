bc28e6da0d03fb707bc46c35bc90a697a4d1b11d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-07-18 21:03:07 -0300

    Update 0560. [Medium] Subarray Sum Equals K.py

--- 0001 - 0999/0560. [Medium] Subarray Sum Equals K.py
+++ 0001 - 0999/0560. [Medium] Subarray Sum Equals K.py
@@ -5,19 +5,18 @@ class Solution:
         seen = defaultdict(lambda: 0)
         seen[0] += 1 
         
-        aSum = 0 
-        
-        ans = 0 
+        ans = prev = 0 
         
         for num in nums: 
-            aSum += num 
-            if aSum - k in seen: 
-                ans += seen[aSum - k] 
-                
-            seen[aSum] += 1 
-        
-        return ans
-        
+            prev += num 
+            
+            ans += seen[prev - k]
+            
+            seen[prev] += 1 
+            
+        return ans 
+            
+            
         
         
     
