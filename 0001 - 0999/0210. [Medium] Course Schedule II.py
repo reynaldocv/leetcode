0055039dc7ead080576c2bfd1a4b027219c67c88
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-07-20 17:01:29 -0300

    Update 0210. [Medium] Course Schedule II.py

--- 0001 - 0999/0210. [Medium] Course Schedule II.py
+++ 0001 - 0999/0210. [Medium] Course Schedule II.py
@@ -3,28 +3,37 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(lambda: [])
-        counter = defaultdict(lambda: 0)
+        cntReq = defaultdict(lambda: 0)
         
         for (a, b) in prerequisites: 
             graph[b].append(a)
-            counter[a] += 1
             
-        req = defaultdict(lambda: 0)
-        ans = [u for u in range(numCourses) if counter[u] == 0]
-        stack = [u for u in range(numCourses) if counter[u] == 0]
-        seen = {u: True for u in range(numCourses) if counter[u] == 0}
+            cntReq[a] += 1 
+            
+        ans = [i for i in range(numCourses) if cntReq[i] == 0]
+        stack = [i for i in range(numCourses) if cntReq[i] == 0]
+        
+        seen = {i for i in range(numCourses) if cntReq[i] == 0}
         
         while stack: 
             u = stack.pop(0)
+            
             for v in graph[u]:
-                if v not in seen: 
-                    req[v] += 1
-                    if req[v] == counter[v]:
-                        seen[v] = True
-                        stack.append(v)
-                        ans.append(v)
-                        
-        return ans if numCourses == len(seen) else []
+                cntReq[v] -= 1 
+                
+                if cntReq[v] == 0 and v not in seen:
+                    seen.add(v)
+                    
+                    ans.append(v)                    
+                    stack.append(v)
+                    
+        m = len(ans)
+        
+        if m == numCourses: 
+            return ans 
+        
+        else: 
+            return []
         
             
         
