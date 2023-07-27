583220b2bf16c24e89f324e3542f7bf63b11d17d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-07-26 21:59:49 -0300

    Update 2141. [Hard] Maximum Running Time of N Computers.py

--- 2000 - 2999/2141. [Hard] Maximum Running Time of N Computers.py
+++ 2000 - 2999/2141. [Hard] Maximum Running Time of N Computers.py
@@ -2,6 +2,22 @@
 
 class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        batteries.sort() 
+        
+        extra = sum(batteries[: -n])
+        
+        live = batteries[-n: ]
+                
+        for i in range(n - 1):
+            if extra // (i + 1) < live[i + 1] - live[i]:
+                return live[i] + extra//(i + 1)
+            
+            extra -= (i + 1) * (live[i + 1] - live[i])
+            
+        return live[-1] + extra // n
+            
+class Solution2:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
         def helper(val):
             ans = 0 
             
