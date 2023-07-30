1169c4b41a5e691b5c5b012b8f2755cde0fcab4b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-07-30 16:56:48 -0300

    Create 2798. [Easy] Number of Employees Who Met the Target.py

---
+++ 2000 - 2999/2798. [Easy] Number of Employees Who Met the Target.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/number-of-employees-who-met-the-target/
+
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        ans = 0 
+        
+        for hour in hours: 
+            if hour >= target: 
+                ans += 1 
+                
+        return ans 
+        
