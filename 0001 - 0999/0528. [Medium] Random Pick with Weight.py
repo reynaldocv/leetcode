97f5f356a555269d47ea43ab391081d249593b50
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-08-08 11:00:28 -0300

    Update 0528. [Medium] Random Pick with Weight.py

--- 0001 - 0999/0528. [Medium] Random Pick with Weight.py
+++ 0001 - 0999/0528. [Medium] Random Pick with Weight.py
@@ -3,23 +3,22 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        total = sum(w)
+        aSum = sum(w)
         
-        self.probability = []
-        prev = 0
-        for num in w:             
-            self.probability.append((num + prev)/total)
-            prev += num
+        self.probability = [0]
+        
+        for num in w: 
+            self.probability.append(self.probability[-1] + num/aSum)
             
+        self.probability.pop(0)        
+        
     def pickIndex(self) -> int:
-        rand = uniform(0, 1)
-        idx = bisect_left(self.probability, rand)
+        rand = random.uniform(0, 1)
         
-        return idx
+        ans = bisect_left(self.probability, rand) 
         
+        return ans 
         
-
-
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
