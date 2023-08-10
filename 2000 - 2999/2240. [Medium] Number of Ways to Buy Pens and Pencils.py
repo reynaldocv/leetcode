a15e75528da80776cc07f4448dbbf7478df63b20
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-08-10 14:53:40 -0300

    Update 2240. [Medium] Number of Ways to Buy Pens and Pencils.py

--- 2000 - 2999/2240. [Medium] Number of Ways to Buy Pens and Pencils.py
+++ 2000 - 2999/2240. [Medium] Number of Ways to Buy Pens and Pencils.py
@@ -2,9 +2,21 @@
 
 class Solution:
     def waysToBuyPensPencils(self, total: int, cost1: int, cost2: int) -> int:
+        dp = [0 for _ in range(total + 1)]
+        
+        for cost in [cost1, cost2]:
+            for i in range(cost, total + 1):
+                dp[i] += dp[i - cost] + 1
+                
+        return max(dp) + 1
+        
+class Solution2:
+    def waysToBuyPensPencils(self, total: int, cost1: int, cost2: int) -> int:
         ans = 0 
+        
         for i in range(total//cost1 + 1):
             aux = total - cost1*i
+            
             ans += aux//cost2 + 1
             
         return ans 
