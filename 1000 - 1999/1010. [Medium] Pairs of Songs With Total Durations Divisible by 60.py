827b3b8689020ab1b7fca5af7ae303bbce5e9fcc
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-08-27 00:27:29 -0300

    Update 1010. [Medium] Pairs of Songs With Total Durations Divisible by 60.py

--- 1000 - 1999/1010. [Medium] Pairs of Songs With Total Durations Divisible by 60.py
+++ 1000 - 1999/1010. [Medium] Pairs of Songs With Total Durations Divisible by 60.py
@@ -2,18 +2,21 @@
 
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        counter = {}
+        counter = defaultdict(lambda: 0)
+        
         for t in time: 
-            counter[t % 60] = counter.get(t % 60, 0) + 1
+            counter[t % 60] += 1 
+            
+        ans = 0 
+            
+        for key in counter: 
+            if key == 0 or key == 30: 
+                ans += (counter[key] - 1)*counter[key]//2
+                
+            elif key < 30 and (60 - key) in counter: 
+                ans += counter[key]*counter[60 - key]
+                
+        return ans 
         
-        ans = 0
-        for t in counter:
-            if t <= 30:
-                if t == 0 or t == 30: 
-                    ans += counter[t]*(counter[t] - 1)//2
-                elif 60 - t in counter: 
-                    ans += counter[t] * counter[60 - t]
-        
-        return ans
             
         
