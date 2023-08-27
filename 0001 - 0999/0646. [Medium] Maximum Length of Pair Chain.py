f2ceba6fa2c962006dc00e1e056c5d62284f4f91
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-08-26 21:06:04 -0300

    Update 0646. [Medium] Maximum Length of Pair Chain.py

--- 0001 - 0999/0646. [Medium] Maximum Length of Pair Chain.py
+++ 0001 - 0999/0646. [Medium] Maximum Length of Pair Chain.py
@@ -2,35 +2,18 @@
 
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
-        n = len(pairs)
-        dp = [1 for _ in range(n)]
-        
-        pairs.sort()
-        
-        ans = 1
-        
-        for i in range(n):
-            for j in range(i):
-                if pairs[j][1] < pairs[i][0]: 
-                    dp[i] = max(dp[i], dp[j] + 1)
-                    
-            ans = max(ans, dp[i])
-            
-        return ans
-      
-class Solution2:
-    def findLongestChain(self, pairs: List[List[int]]) -> int:                
         pairs.sort(key = lambda item: item[1])
         
-        cur = -inf        
-        ans = 0
+        last = -inf
+        ans = 0 
         
-        for (x, y) in pairs:
-            if cur < x:
-                cur = y
-                ans += 1
+        for (start, end) in pairs: 
+            if last < start: 
+                ans += 1 
+                
+                last = end 
         
-        return ans
+        return ans 
             
             
                
