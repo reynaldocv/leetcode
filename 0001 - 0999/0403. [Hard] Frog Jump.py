8c06abf6743a85d3e56c28120cd0e219c6f9513c
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-08-26 21:43:41 -0300

    Update 0403. [Hard] Frog Jump.py

--- 0001 - 0999/0403. [Hard] Frog Jump.py
+++ 0001 - 0999/0403. [Hard] Frog Jump.py
@@ -2,25 +2,24 @@
 
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
-        @cache
-        def helper(cur, jump):
-            if cur == end: 
-                return True
-            if cur > end: 
-                return False
+        @cache 
+        def helper(start, k):
+            if start == last: 
+                return True 
             
-            for nextStep in [jump - 1, jump, jump + 1]:
-                if nextStep > 0: 
-                    if cur + nextStep in stones: 
-                        if helper(cur + nextStep, nextStep):
-                            return True
-
-            return False
+            else: 
+                for middle in range(start + k - 1, start + k + 2):
+                    if middle in seen and middle > start: 
+                        if helper(middle, middle - start):
+                            return True 
+                        
+                return False 
+            
+        seen = {stone for stone in stones}
         
-        end = stones[-1]        
-        stones = {stone: True for stone in stones}
+        begin = stones[0]
+        last = stones[-1]
         
-        if 1 not in stones: 
-            return False
+        return helper(begin, 0)
         
-        return helper(1, 1)
+        
