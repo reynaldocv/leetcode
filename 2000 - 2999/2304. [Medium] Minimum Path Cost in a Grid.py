0decb3ac20632a4f81ad678c673f63dfa82787ec
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-09-06 14:08:25 -0300

    Update 2304. [Medium] Minimum Path Cost in a Grid.py

--- 2000 - 2999/2304. [Medium] Minimum Path Cost in a Grid.py
+++ 2000 - 2999/2304. [Medium] Minimum Path Cost in a Grid.py
@@ -1,19 +1,34 @@
 # https://leetcode.com/problems/minimum-path-cost-in-a-grid/
 
 class Solution:
+    def minPathCost(self, grid: List[List[int]], moveCost: List[List[int]]) -> int:        
+        m, n = len(grid), len(grid[0])
+        
+        dp = [grid[0]] + [[inf for _ in range(n)] for _ in range(m - 1)]
+              
+        for i in range(1, m):
+            for j in range(n):
+                for k in range(n):
+                    dp[i][j] = min(dp[i][j], dp[i - 1][k] + moveCost[grid[i - 1][k]][j] + grid[i][j])
+        
+        return min(dp[-1])
+        
+class Solution2:
     def minPathCost(self, grid: List[List[int]], moveCost: List[List[int]]) -> int:
-        @cache
-        def helper(i, j):
-            if i == m-1: 
-                return grid[i][j]
+        @cache 
+        def helper(x, y):
+            if x == m - 1: 
+                return grid[x][y]
             
-            ans = inf 
-            for k in range(n): 
-                ans = min(ans, grid[i][k] + helper(i + 1, k) + moveCost[grid[i][j]][k])
-            
-            return ans 
+            else: 
+                ans = inf 
+                
+                for q in range(n):
+                    ans = min(ans, grid[x][y] + moveCost[grid[x][y]][q] + helper(x + 1, q))
+                    
+                return ans 
+                
+        m, n = len(grid), len(grid[0])
         
-        m, n = len(grid), len(grid[0])        
-        
-        return min(helper(0, j) for j in range(n))
+        return min(helper(0, y) for y in range(n))
         
