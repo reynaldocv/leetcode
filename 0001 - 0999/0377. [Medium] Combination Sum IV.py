6271ce8c3a4344a1c923cfddda5a0bb352077acf
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-09-09 16:03:03 -0300

    Update 0377. [Medium] Combination Sum IV.py

--- 0001 - 0999/0377. [Medium] Combination Sum IV.py
+++ 0001 - 0999/0377. [Medium] Combination Sum IV.py
@@ -2,8 +2,28 @@
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [0 for _ in range(target + 1)]
-        dp[0] = 1
+        @cache
+        def helper(number):
+            if number == 0: 
+                return 1 
+            
+            elif number > 0: 
+                ans = 0
+                
+                for num in nums: 
+                    ans += helper(number - num)
+                    
+                return ans 
+            
+            return 0
+        
+        nums = {num for num in nums}
+        
+        return helper(target)        
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [1] + [0 for _ in range(target)]
         
         for i in range(1, target + 1):
             for num in nums: 
