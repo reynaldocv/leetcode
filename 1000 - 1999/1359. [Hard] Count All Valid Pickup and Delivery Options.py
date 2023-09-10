725a2b479cba4897d455e285204f04d71a2a50e1
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-09-09 23:33:40 -0300

    Update 1359. [Hard] Count All Valid Pickup and Delivery Options.py

--- 1000 - 1999/1359. [Hard] Count All Valid Pickup and Delivery Options.py
+++ 1000 - 1999/1359. [Hard] Count All Valid Pickup and Delivery Options.py
@@ -3,33 +3,13 @@
 class Solution:
     def countOrders(self, n: int) -> int:
         MOD = 10**9 + 7
-        ans = 1
         
-        for i in range(1, n + 1):
-            ans *= i
-            ans *= 2*i - 1
-            
-            ans %= MOD
-            
-        return ans 
-    
-class Solution2:
-    def countOrders(self, n: int) -> int:
-        @cache
-        def helper(unpicked, undelivered):
-            if not unpicked and not undelivered: 
-                return 1 
-            
-            if (unpicked < 0 or undelivered < 0 or undelivered < unpicked):
-                return 0 
-            
-            ans = unpicked*helper(unpicked - 1, undelivered)
-            ans %= MOD
-            
-            ans += (undelivered - unpicked)*helper(unpicked, undelivered - 1)
-            ans %= MOD
-            
-            return ans 
+        ans = 1 
         
-        MOD = 10**9 + 7
-        return helper(n, n)
+        for num in range(2, 2*n + 1):
+            if num % 2 == 0: 
+                num //= 2 
+            
+            ans = (ans * num) % MOD
+            
+        return ans
