e8810d8f697dffb1d35414daff5b33d65ffe7599
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-09-11 09:52:13 -0300

    Update 2232. [Medium] Minimize Result by Adding Parentheses to Expression.py

--- 2000 - 2999/2232. [Medium] Minimize Result by Adding Parentheses to Expression.py
+++ 2000 - 2999/2232. [Medium] Minimize Result by Adding Parentheses to Expression.py
@@ -2,31 +2,22 @@
 
 class Solution:
     def minimizeResult(self, expression: str) -> str:
-        def helper(string):
-            ans = 0 
-            num = ""
-            for char in string + "+": 
-                if char == "+":
-                    ans += int(num) 
-                    num = ""
-                else: 
-                    num += char
-                    
-            return ans
-               
         n = len(expression)
         
-        idx = next(i for (i, char) in enumerate(expression) if char == "+")
+        ans = (inf, "")
         
-        ans = (helper(expression), "(" + expression + ")")
+        idx = expression.index("+")
         
-        for i in range(idx ):
-            for j in range(idx + 1, n):
-                left = 1 if i == 0 else int(expression[: i])
-                right = 1 if j == n - 1 else int(expression[j + 1:])
-                value = helper(expression[i: j + 1])
-                newExp = expression[: i] + "(" + expression[i: j + 1] + ")" + expression[j + 1:]
-                ans = min(ans, (left*value*right, newExp ))
+        for i in range(0, idx):
+            for j in range(idx + 2, n + 1):
+                left = expression[: i]
+                center = expression[i: j]                
+                right = expression[j: ]
+                
+                leftVal = int(left) if left else 1 
+                rightVal = int(right) if right else 1 
+                
+                ans = min(ans, (leftVal*eval(center)*rightVal, left + "(" + center + ")" + right))
                 
         return ans[1]
                 
