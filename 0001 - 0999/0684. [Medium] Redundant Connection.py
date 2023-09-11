5f36ba182533d33428a3fed7d35afc1403ed3459
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-09-10 21:57:18 -0300

    Update 0684. [Medium] Redundant Connection.py

--- 0001 - 0999/0684. [Medium] Redundant Connection.py
+++ 0001 - 0999/0684. [Medium] Redundant Connection.py
@@ -2,15 +2,38 @@
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        def route(start, end):
+        def helper(son):
+            while parent[son] != son: 
+                son = parent[son]
+                
+            return son 
+        
+        n = len(edges)
+        
+        parent = [i for i in range(n + 1)]
+        
+        for (u, v) in edges:             
+            parentU = helper(u)
+            parentV = helper(v)
+            
+            if parentU == parentV: 
+                return (u, v)
+            
+            parent[parentU] = parent[parentV] = min(parentU, parentV)
+            
+class Solution2:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        def helper(start, end):
             seen = {start: True}
             stack = [start]
+            
             while stack: 
                 u = stack.pop()
                 for v in graph[u]:
                     if v not in seen: 
-                        if v == end: 
+                        if v == end:                             
                             return True
+                            
                         seen[v] = True
                         stack.append(v)
             
@@ -18,8 +41,9 @@ class Solution:
         
         graph = defaultdict(lambda: [])
         for (u, v) in edges:
-            if route(u, v):
+            if helper(u, v):
                 return [u, v]
+                
             graph[u].append(v)
             graph[v].append(u)
             
