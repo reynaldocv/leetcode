7ac15708d1ef7f9b206860e264b347b9873492d0
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-09-10 22:59:18 -0300

    Update 0686. [Medium] Repeated String Match.py

--- 0001 - 0999/0686. [Medium] Repeated String Match.py
+++ 0001 - 0999/0686. [Medium] Repeated String Match.py
@@ -1,20 +1,34 @@
 # https://leetcode.com/problems/repeated-string-match/
 
 class Solution:
-    def repeatedStringMatch(self, a: str, b: str) -> int:        
-        n, m = len(a), len(b)
-        ans = 1
-        
-        sentence = a
-        
-        while len(sentence) < m + 2*n:
-            idx =  sentence.find(b)
-            if idx == -1:
-                sentence += a
-                ans += 1
-            else: 
-                return ans
-                
-        return -1
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        def helper(i):
+            ans = 1 
             
+            j = 0 
+            
+            while j < n: 
+                if a[i] != b[j]:
+                    return -1 
+                
+                i += 1 
+                j += 1 
+                
+                if i == m: 
+                    if j != n: 
+                        ans += 1 
+                        
+                    i = 0 
+                        
+            return ans             
+            
+        m, n = len(a), len(b)
+        
+        for i in range(m):
+            tmp = helper(i)
+            
+            if tmp > -1:
+                return tmp 
+            
+        return -1
         
