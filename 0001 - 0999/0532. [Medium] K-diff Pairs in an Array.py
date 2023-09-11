633293af6147d82ecb771d2899c12c70523b3e15
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-09-10 23:25:35 -0300

    Update 0532. [Medium] K-diff Pairs in an Array.py

--- 0001 - 0999/0532. [Medium] K-diff Pairs in an Array.py
+++ 0001 - 0999/0532. [Medium] K-diff Pairs in an Array.py
@@ -2,33 +2,31 @@
 
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        ans = set()
-        seen = {}
-        for num in nums: 
-            if num + k in seen:
-                ans.add(num)
-            if num - k in seen: 
-                ans.add(num - k)
-            
-            seen[num] = True
-        
-        return len(ans)
-    
-class Solution:
-    def findPairs(self, nums: List[int], k: int) -> int:
-        counter = defaultdict(lambda: 0)
-
-        nums.sort()
         ans = 0 
         
-        for num in nums: 
-            if num not in counter:
-                if num - k in counter: 
-                    ans += 1 
-                    
-            counter[num] += 1  
-            
         if k == 0: 
-            return sum(1 for key in counter if counter[key] > 1)
+            counter = defaultdict(lambda: 0)
+            
+            for num in nums: 
+                counter[num] += 1 
+                
+            for key in counter: 
+                if counter[key] > 1: 
+                    ans += 1 
+        
         else: 
-            return ans 
+            nums.sort() 
+        
+            seen = set()
+
+            for num in nums: 
+                prev = num - k
+
+                if num not in seen and prev in seen: 
+                    ans += 1  
+
+                seen.add(num)
+
+        return ans 
+                
+            
