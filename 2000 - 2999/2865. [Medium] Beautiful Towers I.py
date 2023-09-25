a0a4b1bc85ac50be45b4bfc369ee826729557b49
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-09-24 23:16:29 -0300

    Update 2865. [Medium] Beautiful Towers I.py

--- 2000 - 2999/2865. [Medium] Beautiful Towers I.py
+++ 2000 - 2999/2865. [Medium] Beautiful Towers I.py
@@ -1,1 +1,31 @@
+# https://leetcode.com/problems/beautiful-towers-i/
 
+class Solution:
+    def maximumSumOfHeights(self, maxHeights: List[int]) -> int:
+        def helper(i):
+            ans = 0 
+            
+            minValue = maxHeights[i]
+            
+            for j in range(i, -1 ,-1):
+                minValue = min(minValue,maxHeights[j])
+                
+                ans += minValue
+            
+            minValue = maxHeights[i]
+            
+            for j in range(i + 1, n):
+                minValue = min(minValue, maxHeights[j])
+                
+                ans += minValue
+            
+            return ans 
+            
+        n = len(maxHeights)
+        
+        ans = 0 
+        
+        for i in range(n):
+            ans = max(ans, helper(i))
+            
+        return ans 
