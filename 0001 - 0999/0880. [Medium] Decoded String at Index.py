de0f59368df965a7f7e0c3f5d541eb09dc38f5e9
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-09-27 12:22:53 -0300

    Update 0880. [Medium] Decoded String at Index.py

--- 0001 - 0999/0880. [Medium] Decoded String at Index.py
+++ 0001 - 0999/0880. [Medium] Decoded String at Index.py
@@ -7,6 +7,7 @@ class Solution:
         for char in s: 
             if char.isdigit():
                 size *= int(char)
+                
             else: 
                 size += 1 
                 
@@ -18,7 +19,7 @@ class Solution:
             
             if char.isdigit():
                 size /= int(char)
+                
             else: 
                 size -= 1 
-                
         
