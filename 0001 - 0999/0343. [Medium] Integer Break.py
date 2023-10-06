d2d24a5d0aad64f8ddd1ee22201544ab6e3a4519
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-10-05 21:21:27 -0300

    Update 0343. [Medium] Integer Break.py

--- 0001 - 0999/0343. [Medium] Integer Break.py
+++ 0001 - 0999/0343. [Medium] Integer Break.py
@@ -2,13 +2,27 @@
 
 class Solution:
     def integerBreak(self, n: int) -> int:
-        dp=[0 for _ in range(n + 1)]
-        dp[0] = 0
-        dp[1] = 1
+        def helper(n, m):
+            base = n//m
+            res = n % m 
+            
+            return (base + 1)**res*base**(m - res) 
+            
+        ans = 1
+        
+        for i in range(2, n):
+            ans = max(ans, helper(n, i))
+            
+        return ans 
+            
+class Solution2:
+    def integerBreak(self, n: int) -> int:
+        dp = [0, 1] + [0 for _ in range(n - 1)]
             
         for i in range(n + 1):
             for j in range(1, i):
                 dp[i] = max(dp[i], (i - j)*j, (i - j)*dp[j])
+                
         return dp[n]
         
         
