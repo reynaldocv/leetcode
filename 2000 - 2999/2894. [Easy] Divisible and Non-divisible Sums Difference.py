93e60f97be80d83059ba99459fbf91ed6ea2b13d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-10-09 01:30:54 -0300

    Create 2894. [Easy] Divisible and Non-divisible Sums Difference.py

---
+++ 2000 - 2999/2894. [Easy] Divisible and Non-divisible Sums Difference.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
+
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        ans = 0 
+        
+        for i in range(1, n + 1):
+            if i % m == 0: 
+                ans -= i 
+                
+            else: 
+                ans += i 
+                
+        return ans 
+                
+        
