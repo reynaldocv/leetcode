a6f2eb7eaca59f6f0f0a59681053c85bd2b9fb75
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-10-10 14:32:32 -0300

    Update 0134. [Medium] Gas Station.py

--- 0001 - 0999/0134. [Medium] Gas Station.py
+++ 0001 - 0999/0134. [Medium] Gas Station.py
@@ -3,13 +3,20 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         n = len(gas)
-        aSum = tank = 0
-        start = 0
+        
+        aSum = 0 
+        tank = 0
+        
+        ans = 0
+        
         for i in range(n):
             tank += gas[i] - cost[i]
             aSum += gas[i] - cost[i]
+            
             if tank < 0: 
                 tank = 0
-                start = i + 1
+                
+                ans = i + 1
         
-        return -1 if aSum < 0 else start
+        return -1 if aSum < 0 else ans
+        
