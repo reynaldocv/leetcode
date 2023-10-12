fdf07f3279b023e3043dbacd40bce42ec21f903d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-10-11 23:11:06 -0300

    Update 2251. [Hard] Number of Flowers in Full Bloom.py

--- 2000 - 2999/2251. [Hard] Number of Flowers in Full Bloom.py
+++ 2000 - 2999/2251. [Hard] Number of Flowers in Full Bloom.py
@@ -1,39 +1,33 @@
 # https://leetcode.com/problems/number-of-flowers-in-full-bloom/
 
 class Solution:
-    def fullBloomFlowers(self, flowers: List[List[int]], persons: List[int]) -> List[int]:        
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
         counter = defaultdict(lambda: 0)
         
         for (start, end) in flowers: 
             counter[start] += 1 
             counter[end + 1] -= 1 
             
-        coord = [key for key in counter]
-        coord.sort()
+        nums = [key for key in counter]
+        nums.sort() 
         
-        start = None
-        tmp = 0 
-        arr = []
-        for x in coord:             
-            if start != None: 
-                arr.append((start, x - 1, tmp))
+        arr = [(0, 0)]
+        
+        prev = 0 
+        
+        for num in nums: 
+            prev += counter[num]
             
-            start = x 
-            tmp += counter[x]
+            arr.append((num, prev))
             
         ans = []
-        for person in persons: 
-            idx = bisect_left(arr, (person, inf, inf))
-            idx -= 1     
-            if idx == -1: 
-                ans.append(0)
-            elif arr[idx][0] <= person <= arr[idx][1]:
-                ans.append(arr[idx][2])
-            else: 
-                ans.append(0)
-                
+   
+        for num in people: 
+            idx = bisect_right(arr, (num, inf)) - 1
+            
+            ans.append(arr[idx][1])
+            
         return ans 
-                
             
             
                 
