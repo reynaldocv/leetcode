01f44d6b68b2467b5d512120a216f1f21b71762a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-10-11 23:55:15 -0300

    Update 0299. [Medium] Bulls and Cows.py

--- 0001 - 0999/0299. [Medium] Bulls and Cows.py
+++ 0001 - 0999/0299. [Medium] Bulls and Cows.py
@@ -2,26 +2,29 @@
 
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        correct = 0 
+        n = len(secret) 
         
-        counterSecret = defaultdict(lambda: 0)
-        counterGuess = defaultdict(lambda: 0)
+        correct = 0         
+        counter1 = defaultdict(lambda: 0)
+        counter2 = defaultdict(lambda: 0)
         
-        n = len(secret)
         for i in range(n):
-            if secret[i] == guess[i]: 
-                correct += 1
-            counterSecret[secret[i]] += 1
-            counterGuess[guess[i]] += 1
-            
-        correct2 = 0
-        for key in counterGuess: 
-            if key in counterSecret: 
-                correct2 += min(counterGuess[key], counterSecret[key])
+            if secret[i] == guess[i]:
+                correct += 1 
                 
-        return str(correct) + "A" + str(correct2 - correct) + "B"
-    
+            else: 
+                counter1[secret[i]] += 1 
+                counter2[guess[i]] += 1 
                 
-            
+        guessing = 0 
+        
+        for key in counter2: 
+            if key in counter1: 
+                guessing += min(counter1[key], counter2[key])
+                
+        return str(correct) + "A" + str(guessing) + "B"
+                
+        
+        
         
         
