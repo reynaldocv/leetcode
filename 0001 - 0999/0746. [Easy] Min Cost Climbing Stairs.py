7439ecf138e477128019958fd0b69216ec13269d
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-10-13 02:50:43 -0300

    Update 0746. [Easy] Min Cost Climbing Stairs.py

--- 0001 - 0999/0746. [Easy] Min Cost Climbing Stairs.py
+++ 0001 - 0999/0746. [Easy] Min Cost Climbing Stairs.py
@@ -2,12 +2,13 @@
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost) 
+        n = len(cost)
         
-        dp = defaultdict(lambda: 0)
+        dp = [num for num in cost]
         
-        for i in range(n):
-            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+        for i in range(2, n):
+            dp[i] += min(dp[i - 1], dp[i - 2])
             
-        return min(dp[n - 1], dp[n - 2])
-   
+        return min(dp[-1], dp[-2])
+            
+        
