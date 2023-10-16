84fe0774cdc92c9801a683d815ccca037592b3d7
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-10-16 17:13:58 -0300

    Update 1079. [Medium] Letter Tile Possibilities.py

--- 1000 - 1999/1079. [Medium] Letter Tile Possibilities.py
+++ 1000 - 1999/1079. [Medium] Letter Tile Possibilities.py
@@ -2,6 +2,31 @@
 
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
+        def helper(arr):
+            ans.add("".join(arr))
+            
+            for i in range(n):
+                if visited[i] == False: 
+                    arr.append(tiles[i])
+                    visited[i] = True
+
+                    helper(arr)
+
+                    visited[i] = False
+                    arr.pop()
+
+        n = len(tiles)
+        
+        visited = [False for _ in range(n)]
+        
+        ans = set()
+        
+        helper([])
+        
+        return len(ans) - 1
+
+class Solution2:
+    def numTilePossibilities(self, tiles: str) -> int:
         ans = 0
         elems = [tile for tile in tiles]
         
