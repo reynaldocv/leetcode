d97af26c86084fe2a928c3f61554c75bb67c8860
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-10-24 15:47:21 -0300

    Update 0365. [Medium] Water and Jug Problem.py

--- 0001 - 0999/0365. [Medium] Water and Jug Problem.py
+++ 0001 - 0999/0365. [Medium] Water and Jug Problem.py
@@ -2,10 +2,7 @@
 
 class Solution:
     def canMeasureWater(self, jug1Capacity: int, jug2Capacity: int, targetCapacity: int) -> bool:
-        if targetCapacity == 0: 
-            return True
-                
-        if targetCapacity <= jug1Capacity + jug2Capacity: 
-            return targetCapacity % gcd(jug1Capacity, jug2Capacity) == 0
-    
-        return False
+        mcd = gcd(jug1Capacity, jug2Capacity)
+        
+        return jug1Capacity + jug2Capacity >= targetCapacity and targetCapacity % mcd == 0
+        
