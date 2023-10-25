f29c7d3944ba894eb3804994331c3e83a3d844a5
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-10-24 21:48:56 -0300

    Update 0973. [Medium] K Closest Points to Origin.py

--- 0001 - 0999/0973. [Medium] K Closest Points to Origin.py
+++ 0001 - 0999/0973. [Medium] K Closest Points to Origin.py
@@ -4,7 +4,7 @@ class Solution2:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         points.sort(key = lambda item: item[0]**2 + item[1]**2)
         
-        return points[:k]
+        return points[: k]
         
         
             
