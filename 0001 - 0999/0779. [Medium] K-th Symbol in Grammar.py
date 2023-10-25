cc206074a3c1a38087824f959434e53e78fcde83
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-10-24 21:21:21 -0300

    Update 0779. [Medium] K-th Symbol in Grammar.py

--- 0001 - 0999/0779. [Medium] K-th Symbol in Grammar.py
+++ 0001 - 0999/0779. [Medium] K-th Symbol in Grammar.py
@@ -1,17 +1,18 @@
 # https://leetcode.com/problems/k-th-symbol-in-grammar/
 
 class Solution:
-    def kthGrammar(self, n: int, k: int) -> int:        
-        value = 1
+    def kthGrammar(self, n: int, k: int) -> int:
+        ans = 0 
+        
+        k = k - 1
+        
         while n > 0: 
-            if 2**(n - 2) < k <= 2**(n - 1):
-                k -= 2**(n - 2)
-                value = 1 - value 
-            n -= 1
-        
-        return value if value == 0 else 1
-            
+            if k >= 2**(n - 1):
+                k %= 2**(n - 1)
                 
+                ans = 1 - ans
+                
+            n -= 1 
+            
+        return ans
         
-        
-        https://leetcode.com/problems/k-th-symbol-in-grammar/
