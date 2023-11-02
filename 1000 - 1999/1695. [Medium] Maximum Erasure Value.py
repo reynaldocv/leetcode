0ff2a8d2c67a06486317c202242c2a9ec72a6a7a
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-11-01 21:41:31 -0300

    Update 1695. [Medium] Maximum Erasure Value.py

--- 1000 - 1999/1695. [Medium] Maximum Erasure Value.py
+++ 1000 - 1999/1695. [Medium] Maximum Erasure Value.py
@@ -3,19 +3,22 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         prefix = [0]
+        
         for num in nums:
             prefix.append(prefix[-1] + num)
             
-        index = defaultdict(lambda: -1)
+        last = 0
+        seen = defaultdict(lambda: 0)
         
-        start = -1
         ans = 0 
         
-        for (i, num) in enumerate(nums): 
-            start = max(start, index[num])
-            ans = max(ans, prefix[i + 1] - prefix[start + 1])
+        for (i, num) in enumerate(nums):
+            last = max(last, seen[num])
             
-            index[num] = i 
+            ans = max(ans, prefix[i + 1] - prefix[last])
             
-        return ans
+            seen[num] = i + 1 
+            
+        return ans 
+        
         
