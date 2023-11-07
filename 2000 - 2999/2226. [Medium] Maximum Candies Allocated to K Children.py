b8ed91baf17dc56117674a1d7839d7d43fb464d4
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-11-07 10:52:56 -0300

    Update 2226. [Medium] Maximum Candies Allocated to K Children.py

--- 2000 - 2999/2226. [Medium] Maximum Candies Allocated to K Children.py
+++ 2000 - 2999/2226. [Medium] Maximum Candies Allocated to K Children.py
@@ -4,27 +4,28 @@ class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
         def helper(value):
             ans = 0 
+            
             for candie in candies: 
                 ans += candie//value
                 
-                if ans >= k: 
-                    return True
-                    
-            return False
+            return ans >= k 
             
         aSum = sum(candies)
         
         if aSum < k: 
             return 0 
         
-        start = 1
+        start = 0
         end = aSum + 1
         
         while end - start > 1: 
-            mid = (end + start)//2
-            if helper(mid): 
-                start = mid
+            middle = (end + start)//2
+            
+            if helper(middle):
+                start = middle 
+                
             else: 
-                end = mid
+                end = middle 
                 
         return start
+        
