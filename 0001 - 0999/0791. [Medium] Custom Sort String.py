0bf387885a0cf031111335ae2a630491b1b653a2
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-11-11 11:21:24 -0300

    Update 0791. [Medium] Custom Sort String.py

--- 0001 - 0999/0791. [Medium] Custom Sort String.py
+++ 0001 - 0999/0791. [Medium] Custom Sort String.py
@@ -2,10 +2,15 @@ https://leetcode.com/problems/custom-sort-string/
 
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        n = len(order)
-        priority = {order[i]: n - i for i in range(n)}
-        arr = [i for i in s]        
-        arr.sort(reverse = True, key = lambda item: priority.get(item, 0))
-      
+        index = defaultdict(lambda: inf)
+        
+        for (i, char) in enumerate(order):
+            index[char] = i
+        
+        arr = [char for char in s]
+        
+        arr.sort(key = lambda item: index[item])
+        
         return "".join(arr)
-      
+        
+        
