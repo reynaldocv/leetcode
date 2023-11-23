7bbe33927a8a89afe9fd21a43b0e2f4784eb31ae
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-11-23 15:42:15 -0300

    Create 2932. [Easy] Maximum Strong Pair XOR I.py

---
+++ 2000 - 2999/2932. [Easy] Maximum Strong Pair XOR I.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/maximum-strong-pair-xor-i/
+
+class Solution:
+    def maximumStrongPairXor(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        ans = 0 
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                if abs(nums[i] - nums[j]) <= min(nums[i], nums[j]):
+                    ans = max(ans, nums[i]^nums[j])
+                    
+        return ans 
