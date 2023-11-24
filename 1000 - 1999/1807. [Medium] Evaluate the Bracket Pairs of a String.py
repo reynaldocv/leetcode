d4d37e6609459aa9585f058774687f09863b1935
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-11-23 21:03:34 -0300

    Update 1807. [Medium] Evaluate the Bracket Pairs of a String.py

--- 1000 - 1999/1807. [Medium] Evaluate the Bracket Pairs of a String.py
+++ 1000 - 1999/1807. [Medium] Evaluate the Bracket Pairs of a String.py
@@ -1,34 +1,29 @@
 # https://leetcode.com/problems/evaluate-the-bracket-pairs-of-a-string/
-
 class Solution:
     def evaluate(self, s: str, knowledge: List[List[str]]) -> str:
-        replace = {}
-        for (a, b) in knowledge: 
-            replace["(" + a + ")"] = b
-            
-        key = ""
-        ans = ""
-        save = False
-        for char in s: 
-            if char not in "()":
-                if key: 
-                    key += char
-                else: 
-                    ans += char
-            elif char == "(": 
-                save = True
-                key = char
-            else: 
-                key += ")"
-                if key in replace: 
-                    ans += replace[key]
-                else:
-                    ans += "?"
-                save = False
-                key = ""
+        replace = defaultdict(lambda: "?")
         
-        return ans
-            
+        for (a, b) in knowledge: 
+            replace[a] = b 
+        
+        stack = [""]
+        
+        for char in s: 
+            if char == "(":
+                stack.append("")
+                
+            elif char == ")":
+                word = stack.pop()
+                
+                stack.append(replace[word]) 
+                
+                stack.append("")
+                
+            else: 
+                stack[-1] += char 
+        
+        return "".join(stack)
+        
             
         
         
