234e1978a3e331401b5c4d3d024f28c60b675049
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-12-03 21:34:37 -0500

    Create 2951. [Easy] Find the Peaks.py

---
+++ 2000 - 2999/2951. [Easy] Find the Peaks.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/find-the-peaks/
+
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        n = len(mountain)
+        
+        ans = []
+        
+        for i in range(1, n - 1):
+            if mountain[i - 1] < mountain[i] and mountain[i] > mountain[i + 1]:
+                ans.append(i)
+                
+        return ans
+        
