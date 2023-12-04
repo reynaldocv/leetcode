da061c5ddd8ff6a6af4fb232e87556ab556b19f7
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-12-03 21:46:57 -0500

    Create 2942. [Easy] Find Words Containing Character.py

---
+++ 2000 - 2999/2942. [Easy] Find Words Containing Character.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/find-words-containing-character/
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        ans = []
+        
+        for (i, word) in enumerate(words):
+            if x in word: 
+                ans.append(i)
+                
+        return ans 
+        
