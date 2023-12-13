83bb44d1d3977790b9c0f96f7f931519e54d6426
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-12-12 23:15:49 -0500

    Update 1375. [Medium] Number of Times Binary String Is Prefix-Aligned.py

--- 1000 - 1999/1375. [Medium] Number of Times Binary String Is Prefix-Aligned.py
+++ 1000 - 1999/1375. [Medium] Number of Times Binary String Is Prefix-Aligned.py
@@ -1,32 +1,18 @@
 # https://leetcode.com/problems/number-of-times-binary-string-is-prefix-aligned/
 
 class Solution:
-    def numTimesAllBlue(self, light: List[int]) -> int:
-        maxElem = 0 
-        aSum = 0 
+    def numTimesAllBlue(self, flips: List[int]) -> int:
+        maxElem = 0         
+        cnt = 0
+        
         ans = 0 
         
-        for num in light:
-            aSum += num
+        for num in flips: 
+            cnt += 1 
             maxElem = max(maxElem, num)
-            if maxElem*(maxElem + 1)//2 == aSum:
+            
+            if cnt == maxElem: 
                 ans += 1 
                 
-        return ans    
-
-class Solution2:
-    def numTimesAllBlue(self, light: List[int]) -> int:
-        n = len(light)
-        seen = {i + 1: True for i in range(n)}
-        heap = [i + 1 for i in range(n)]
-        
-        ans = 0 
-        for (i, idx) in enumerate(light):
-            seen.pop(idx)
-            while heap and heap[0] not in seen:
-                heappop(heap)
-                
-            if heap and heap[0] > i + 1: 
-                ans += 1 
-                 
-        return ans + 1
+        return ans 
+  
