699da73dfaaeef8b7492e74eec86df7e287b0bb6
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-12-14 21:58:18 -0500

    Update 1818. [Medium] Minimum Absolute Sum Difference.py

--- 1000 - 1999/1818. [Medium] Minimum Absolute Sum Difference.py
+++ 1000 - 1999/1818. [Medium] Minimum Absolute Sum Difference.py
@@ -5,26 +5,33 @@ class Solution:
         MOD = 10**9 + 7
         
         n = len(nums1)
+        
+        arr = []
+        
         aSum = 0 
-        for i in range(n):
-            aSum += abs(nums1[i] - nums2[i]) 
-            
-        ans = aSum
-        sort1 = sorted(nums1)
         
         for i in range(n):
-            idx = bisect_left(sort1, nums2[i])
-            if idx <= n - 1 and sort1[idx] == nums2[i]:
-                ans = min(ans, aSum - abs(nums1[i] - nums2[i]))
+            aSum  += abs(nums1[i] - nums2[i])
             
-            else: 
-                right = sort1[idx] if idx != n else inf
-                left = sort1[idx - 1] if idx != 0 else -inf
-                
-                aux = min(abs(left - nums2[i]), abs(right - nums2[i]))
-                
-                ans = min(ans, aSum - abs(nums1[i] - nums2[i]) + aux)
+            arr.append(nums1[i])
             
+        arr.sort() 
+        
+        ans = inf 
+        
+        for i in range(n):
+            tmp = abs(nums1[i] - nums2[i])
+            
+            num = nums2[i]
+            
+            idx = bisect_left(arr, num)
+            
+            if idx < n: 
+                ans = min(ans, aSum - tmp + abs(arr[idx] - num))
+                
+            if idx - 1 >= 0:
+                ans = min(ans, aSum - tmp + abs(arr[idx - 1] - num))
+                
         return ans % MOD
             
         
