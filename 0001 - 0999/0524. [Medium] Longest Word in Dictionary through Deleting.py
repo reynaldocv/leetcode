d1f1ae9a83f90822d60b9b34007eabca8030b464
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2023-12-16 01:05:12 -0500

    Update 0524. [Medium] Longest Word in Dictionary through Deleting.py

--- 0001 - 0999/0524. [Medium] Longest Word in Dictionary through Deleting.py
+++ 0001 - 0999/0524. [Medium] Longest Word in Dictionary through Deleting.py
@@ -3,14 +3,18 @@
 class Solution:
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
         ans = (0, "")
+        
         n = len(s)
         
         for word in dictionary:
             i, j = 0, 0
+            
             m = len(word)
+            
             while i < n and j < m:
                 if word[j] == s[i]:
                     j += 1
+                    
                 i += 1
                 
             if j == m:
