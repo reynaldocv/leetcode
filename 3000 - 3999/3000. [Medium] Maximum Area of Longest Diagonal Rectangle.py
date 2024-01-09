71735ed1c051d9ceebd40e09a936673678c34250
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-01-08 22:55:52 -0500

    Update 3000. [Medium] Maximum Area of Longest Diagonal Rectangle.py

--- 3000 - 3999/3000. [Medium] Maximum Area of Longest Diagonal Rectangle.py
+++ 3000 - 3999/3000. [Medium] Maximum Area of Longest Diagonal Rectangle.py
@@ -2,7 +2,9 @@
 
 class Solution:
     def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
-        dimensions.sort(key = lambda item: (item[0]**2 + item[1]**2, item[0]*item[1]))
+        ans = (0, 0)
         
-        return dimensions[-1][0]*dimensions[-1][1]
-        
+        for (length, width) in dimensions: 
+            ans = max(ans, (length**2 + width**2, length*width))
+            
+        return ans[1]
