c58c806af70b4778d4d51af0ffdd2a895a69a09f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-01-13 01:17:45 -0500

    Update 0318. [Medium] Maximum Product of Word Lengths.py

--- 0001 - 0999/0318. [Medium] Maximum Product of Word Lengths.py
+++ 0001 - 0999/0318. [Medium] Maximum Product of Word Lengths.py
@@ -2,6 +2,46 @@
 
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
+        uniqueLetters = []
+        
+        for word in words: 
+            seen = set([])
+            
+            tmp = ""
+            
+            for char in word: 
+                if char not in seen: 
+                    tmp += char 
+                    
+                seen.add(char)
+                
+            uniqueLetters.append(tmp)
+            
+        n = len(words)
+        
+        index = defaultdict(lambda: set())
+        
+        for (i, word) in enumerate(words):
+            for char in uniqueLetters[i]: 
+                index[char].add(i)
+                
+        ans = 0         
+        
+        for (i, word) in enumerate(words): 
+            index2 = {i for i in range(n)}
+            
+            for char in uniqueLetters[i]: 
+                for idx in index[char]:
+                    if idx in index2:
+                        index2.remove(idx)
+                        
+            for num in index2: 
+                ans = max(ans, len(words[num])*len(word))
+            
+        return ans 
+        
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
         def helper(word1, word2):
             for char in word1: 
                 if char in word2: 
