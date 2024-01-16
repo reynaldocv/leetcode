4c1e160e99bdf54f77c49000fc54176eef0f50d0
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-01-15 23:17:53 -0500

    Update 0357. Count Numbers with Unique Digits.py

--- 0001 - 0999/0357. Count Numbers with Unique Digits.py
+++ 0001 - 0999/0357. Count Numbers with Unique Digits.py
@@ -2,19 +2,18 @@
 
 class Solution:
     def countNumbersWithUniqueDigits(self, n: int) -> int:
-        
-        if n == 0: 
-            return 1
-        
-        aux = [0]*8
-        aux[0] = 10
-        aux[1] = 81
-        ans = aux[0] if n == 1 else aux[0] + aux[1]
-        for i in range(2, n):
-            aux[i] = aux[i - 1]*(10 - i)
-            ans += aux[i]
+        def helper(num, cnt):
+            if cnt == 0: 
+                return 1
             
-        return ans
+            else: 
+                return num*helper(num - 1, cnt - 1)
+            
+        arr = [1]
         
-        
+        for i in range(1, n + 1):
+            arr.append(9*helper(9, i - 1))
+            
+        return sum(arr)
+            
             
