68442c26c33657f918e39ca7d6f63824eac06856
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-01-16 00:03:31 -0500

    Update 0421. [Medium] Maximum XOR of Two Numbers in an Array.py

--- 0001 - 0999/0421. [Medium] Maximum XOR of Two Numbers in an Array.py
+++ 0001 - 0999/0421. [Medium] Maximum XOR of Two Numbers in an Array.py
@@ -1,39 +1,43 @@
 # https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/
 
-class Trie:
-    def __init__(self ):
-        self.root = {}
-        self.num = 0 
-        
-    def addNum(self, num):
-        node = self.get()
-        for i in range(31, -1, -1):
-            val = 1 if num & (1 << i) else 0 
-            if val not in node.root: 
-                node.root[val] = Trie()
-            node = node.root[val]        
-        node.num = num
-        
-    def get(self):
-        return self
-
 class Solution:
-    def findMaximumXOR(self, nums: List[int]) -> int:               
-        trie = Trie()
-        
-        for num in nums: 
-            trie.addNum(num)
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        def helper(num):
+            node1 = trie
             
-        ans = 0
-        for num in nums:  
-            node = trie.get()
-            for i in range(31, -1, -1):
-                val = 1 if num & (1 << i) else 0 
-                if 1 - val in node.root: 
-                    node = node.root[1 - val]
+            for i in range(32, -1, -1):
+                bit = (num >> i) & 1
+                
+                if bit not in node: 
+                    node[bit] = {}
+                    
+                node = node[bit]
+                
+            node["$"] = num
+                
+        def collaborator(num):
+            node = trie
+            
+            for i in range(32, -1, -1):
+                bit = (num >> i) & 1
+                
+                if 1 - bit in node: 
+                    node = node[1 - bit] 
+                    
                 else: 
-                    node = node.root[val]      
+                    node = node[bit]
+                    
+            return node["$"]
+        
+        trie = {}
+        
+        ans = 0 
+        
+        for num1 in nums: 
+            helper(num1)
             
-            ans = max(ans, num ^ node.num)
+            num2 = collaborator(num1)
             
-        return ans
+            ans = max(ans, num1^num2)
+            
+        return ans 
