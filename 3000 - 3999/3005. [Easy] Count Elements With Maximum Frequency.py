179825227189c13595833bacdc1395072f136678
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-01-16 21:52:09 -0500

    Create 3005. [Easy] Count Elements With Maximum Frequency.py

---
+++ 3000 - 3999/3005. [Easy] Count Elements With Maximum Frequency.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/count-elements-with-maximum-frequency/
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        counter = defaultdict(lambda: 0)
+        
+        maxFreq = 0 
+        
+        for num in nums: 
+            counter[num] += 1 
+            
+            maxFreq = max(maxFreq, counter[num])
+            
+        ans = 0 
+        
+        for num in nums: 
+            if counter[num] == maxFreq: 
+                ans += 1 
+                
+        return ans 
