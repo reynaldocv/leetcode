cc6cd55201f21b8660531c36362fdac6345956ab
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-01-25 17:17:06 -0500

    Update 0368. [Medium] Largest Divisible Subset.py

--- 0001 - 0999/0368. [Medium] Largest Divisible Subset.py
+++ 0001 - 0999/0368. [Medium] Largest Divisible Subset.py
@@ -3,15 +3,19 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         dp = [[]]
-        nums.sort()
-        for num in nums:
+
+        nums.sort() 
+        
+        for num in nums: 
             aux = []
-            for s in dp: 
-                if s and num % s[-1] == 0: 
-                    aux.append(s + [num])
+            
+            for arr in dp: 
+                if arr and num % arr[-1] == 0: 
+                    aux.append(arr + [num])
+                    
                 else: 
                     aux.append([num])
+                    
+            dp.append(max(aux, key = lambda item: len(item)))
             
-            dp.append(max(aux, key = len))
-
-        return max(dp, key=len)
+        return max(dp, key = lambda item: len(item))
