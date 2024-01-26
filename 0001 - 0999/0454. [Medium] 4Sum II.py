aba457da3cd368c6d82e60191c31142fe29a2dc8
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-01-26 00:15:19 -0500

    Update 0454. [Medium] 4Sum II.py

--- 0001 - 0999/0454. [Medium] 4Sum II.py
+++ 0001 - 0999/0454. [Medium] 4Sum II.py
@@ -2,17 +2,25 @@
 
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        dic = {}
-        for num1 in nums1: 
-            for num2 in nums2: 
-                dic[num1 + num2] = dic.get(num1 + num2, 0) + 1
+        counter1 = defaultdict(lambda: 0)
+        counter2 = defaultdict(lambda: 0)
         
-        ans = 0
+        for num1 in nums1: 
+            for num2 in nums2:
+                counter1[num1 + num2] += 1 
+    
         for num3 in nums3: 
-            for num4 in nums4: 
-                aux = -1*(num3 + num4)
-                if aux in dic: 
-                    ans += dic[aux]
-                    
-        return ans
+            for num4 in nums4:
+                counter2[num3 + num4] += 1 
+                
+        ans = 0 
+        
+        for key in counter1: 
+            if -key in counter2: 
+                ans += counter1[key]*counter2[-key]
+                
+        return ans 
+    
+    
+        
      
