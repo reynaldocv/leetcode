3b54610018c6b119e24fcc20c2fa69ba42378336
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-01-26 00:28:28 -0500

    Update 0565. [Medium] Array Nesting.py

--- 0001 - 0999/0565. [Medium] Array Nesting.py
+++ 0001 - 0999/0565. [Medium] Array Nesting.py
@@ -2,16 +2,45 @@
 
 class Solution:
     def arrayNesting(self, nums: List[int]) -> int:
+        @cache
+        def helper(x):
+            if x in seen: 
+                return 0
+            
+            else: 
+                seen.add(x)
+                
+                return 1 + helper(nums[x])
+            
         n = len(nums)
+                
+        seen = set()
+        
+        ans = 0
+        
+        for i in range(n):
+            ans = max(ans, helper(i))
+            
+        return ans 
+        
+class Solution2:
+    def arrayNesting(self, nums: List[int]) -> int:
+        n = len(nums)
+        
         visited = [False for _ in range(n)]
+        
         ans = 1
+        
         for i in range(n):
             if visited[i] == False: 
                 aux = 0
+                
                 while visited[i] == False:
                     visited[i] = True
+                    
                     i = nums[i]
                     aux += 1
+                    
                 ans = max(ans, aux)
         
         return ans
