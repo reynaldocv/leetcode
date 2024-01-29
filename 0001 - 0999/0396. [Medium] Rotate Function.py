2bad037b3602bf4db46be184bbf8c9e189712b83
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-01-29 11:30:31 -0500

    Update 0396. [Medium] Rotate Function.py

--- 0001 - 0999/0396. [Medium] Rotate Function.py
+++ 0001 - 0999/0396. [Medium] Rotate Function.py
@@ -2,18 +2,27 @@ https://leetcode.com/problems/rotate-function/
 
 class Solution:
     def maxRotateFunction(self, nums: List[int]) -> int:
-        
-        value = 0 
-        for (i, num) in enumerate(nums):
-            value += i*num
-        
-        ans = value
-        aSum = sum(nums)
         n = len(nums)
         
-        for i in range(n - 1, 0, -1):
-            value = value + aSum - n*nums[i]
-            ans = max(ans, value)
+        totalSum = 0 
+        aSum = 0 
+        
+        for (i, num) in enumerate(nums):
+            totalSum += (i + 1)*num 
             
-        return ans
+            aSum += num 
+            
+        ans = -inf
+        
+        for num in nums: 
+            tmp = totalSum - aSum            
+            
+            ans = max(ans, tmp)
+            
+            totalSum = tmp + num*n
+            
+        return ans 
+            
+            
+        
         
