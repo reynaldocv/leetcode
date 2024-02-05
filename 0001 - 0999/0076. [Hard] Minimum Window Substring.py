c3b478625f601d84df8d51e00dcef225cbbd7b41
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-02-05 14:23:18 -0500

    Update 0076. [Hard] Minimum Window Substring.py

--- 0001 - 0999/0076. [Hard] Minimum Window Substring.py
+++ 0001 - 0999/0076. [Hard] Minimum Window Substring.py
@@ -2,38 +2,49 @@
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        def isGreater(a, b):     
-            for i in range(n):
-                if a[i] < b[i]:
-                    return False
-                
-            return True
+        if t == "" and s == "":
+            return ""
         
         counter = defaultdict(lambda: 0)
-        for char in t: 
-            counter[char] += 1
         
-        index, counter3 = {}, ()
-        for (i, key) in enumerate(counter):
-            index[key] = i
-            counter3 = counter3 + (counter[key], )
-    
-        n = len(counter)
-        start = 0
-        counter2 = (0, )*n
-        ans = (inf, "")
-        for (i, char) in enumerate(s): 
-            if char in index: 
-                idx = index[char]
-                counter2 = counter2[:idx] + (counter2[idx] + 1, ) + counter2[idx + 1:]
-                if isGreater(counter2, counter3):
-                    while s[start] not in index or counter2[index[s[start]]] > counter3[index[s[start]]]:
-                        if s[start] in index:
-                            idx = index[s[start]]
-                            counter2 = counter2[:idx] + (counter2[idx] - 1, ) + counter2[idx + 1:]
-                        start += 1               
-                    ans = min(ans, (i - start, s[start: i + 1]))
+        for char in t: 
+            counter[char] += 1 
+            
+        total = len(counter)
+        qnt = 0
+        
+        cnt = defaultdict(lambda: 0)
+        start = 0 
+        
+        ans = (inf, 0, 0)
+        
+        for (i, char) in enumerate(s):
+            if char in counter: 
+                cnt[char] += 1 
+                
+                if cnt[char] == counter[char]:
+                    qnt += 1 
+                
+            while qnt == total: 
+                ans = min(ans, (i - start, start, i))
+                
+                oldChar = s[start]
+                
+                if oldChar in cnt: 
+                    cnt[oldChar] -= 1 
+            
+                    if cnt[oldChar] < counter[oldChar]:
+                        qnt -= 1 
+                
+                start += 1
+            
+        return "" if ans[0] == inf else s[ans[1]: ans[2] + 1]
+            
+            
+                
+                
                 
             
-        return ans[1]
+                
+            
                         
