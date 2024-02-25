a18364e515f84fc7c2329048416482d6abe11d01
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-02-25 15:11:48 -0500

    Create 3026. [Medium] Maximum Good Subarray Sum.py

---
+++ 3000 - 3999/3026. [Medium] Maximum Good Subarray Sum.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/maximum-good-subarray-sum/
+
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        aSum = 0 
+        
+        ans = -inf
+        
+        index = defaultdict(lambda: inf)
+        
+        for (i, num) in enumerate(nums):
+            aSum += num 
+            
+            greater = num + k
+            
+            if greater in index: 
+                ans = max(ans, aSum - index[greater])            
+            
+            lower = num - k 
+            
+            if lower in index: 
+                ans = max(ans, aSum - index[lower])            
+           
+            index[num] = min(index[num], aSum - num)
+            
+        return 0 if ans == -inf else ans 
+        
+        
+        
