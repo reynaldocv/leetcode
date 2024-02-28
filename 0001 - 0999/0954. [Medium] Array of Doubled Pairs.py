a33a566fb466b23cbbb9767e48dfe1918ca7a1ab
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-02-28 15:42:39 -0500

    Update 0954. [Medium] Array of Doubled Pairs.py

--- 0001 - 0999/0954. [Medium] Array of Doubled Pairs.py
+++ 0001 - 0999/0954. [Medium] Array of Doubled Pairs.py
@@ -2,17 +2,34 @@
 
 class Solution:
     def canReorderDoubled(self, arr: List[int]) -> bool:
-        counter = {}
+        def helper(nums):
+            counter = defaultdict(lambda: 0)
+            
+            for num in nums:
+                counter[num] += 1 
+                
+            for num in nums: 
+                if counter[num] > 0:
+                    if counter[2*num] > 0: 
+                        counter[num] -= 1 
+                        counter[2*num] -= 1 
+                        
+                    else: 
+                        return False
+                    
+            return True 
+        
+        positive = []
+        negative = []
+        
         for num in arr: 
-            counter[num] = counter.get(num, 0) + 1
-        arr.sort(key = abs)
-        print(arr)
-        n = len(arr)
-        for i in range(n):
-            if counter[arr[i]] > 0:                                    
-                counter[arr[i]] -= 1
-                if 2*arr[i] in counter and counter[2*arr[i]] > 0:
-                    counter[2*arr[i]] -= 1
-                else:
-                    return False 
-        return True 
+            if num >= 0: 
+                positive.append(num)
+                
+            else: 
+                negative.append(num)
+                
+        positive.sort()
+        negative.sort(key = lambda item: -item)
+        
+        return helper(positive) and helper(negative)
