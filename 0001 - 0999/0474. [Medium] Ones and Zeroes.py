99c19186ee0e4d9843f35dff2598280c0e27bc0e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-02-29 15:46:15 -0500

    Update 0474. [Medium] Ones and Zeroes.py

--- 0001 - 0999/0474. [Medium] Ones and Zeroes.py
+++ 0001 - 0999/0474. [Medium] Ones and Zeroes.py
@@ -3,9 +3,11 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         ans = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        
         for word in strs:             
-            count1 = len([1 for w in word if w == "1"])
-            count0 = len(word) - count1           
+            count1 = sum(int(char) for char in word)
+            count0 = len(word) - count1     
+            
             for i in range(m, -1, -1):
                 for j in range(n, -1, -1):
                     if i - count0 >= 0 and j - count1 >= 0: 
