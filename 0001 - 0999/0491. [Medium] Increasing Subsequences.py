d775710c33672285fe364514073a07eda1244790
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-02-29 16:05:49 -0500

    Update 0491. [Medium] Increasing Subsequences.py

--- 0001 - 0999/0491. [Medium] Increasing Subsequences.py
+++ 0001 - 0999/0491. [Medium] Increasing Subsequences.py
@@ -2,22 +2,23 @@
 
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)        
-        ans = set()
-        for (i, num) in enumerate(nums): 
-            aux = set()
-            
-            for seq in ans: 
-                if seq[-1] <= num: 
-                    aux.add(seq + (num,))
-            
-            for j in range(i):                
-                if nums[j] <= num: 
-                    aux.add((nums[j], num))
-            
-            for seq in aux: 
-                ans.add(seq)
+        def helper(seq, start):
+            for i in range(start, n):
+                if seq[-1] <= nums[i]:                    
+                    tmp = seq + (nums[i], )
                     
+                    if tmp not in ans:                     
+                        ans.add(tmp)
+
+                        helper(tmp, i + 1)
+
+        n = len(nums)
+        
+        ans = set([])
+        
+        for (i, num) in enumerate(nums):
+            helper((num, ), i + 1)
+        
         return ans
                 
                     
