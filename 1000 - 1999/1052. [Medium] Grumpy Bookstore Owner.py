f6ef9a0d941bacbe0f646e5edf8f4510e16b9c5f
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-01 02:55:14 -0500

    Update 1052. [Medium] Grumpy Bookstore Owner.py

--- 1000 - 1999/1052. [Medium] Grumpy Bookstore Owner.py
+++ 1000 - 1999/1052. [Medium] Grumpy Bookstore Owner.py
@@ -2,24 +2,30 @@
 
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
-        n = len(customers)
+        n = len(grumpy) 
         
-        right = [0]
-        prev = 0 
-        for i in range(n - 1, -1, -1):
-            if grumpy[i] == 0: 
-                prev += customers[i]
+        total = [0]
+        satisfied = [0]
+        
+        for (i, mood) in enumerate(grumpy):
+            if mood == 0: 
+                satisfied.append(satisfied[-1] + customers[i])
+                
+            else:
+                satisfied.append(satisfied[-1])
+                
+            total.append(total[-1] + customers[i])
             
-            right.insert(0, prev)
+        ans = satisfied[-1]
         
-        ans = 0         
-        left = 0 
-        aSum = sum(customers[:minutes])
+        for i in range(1, n + 1):
+            if i >= minutes: 
+                left = satisfied[i - minutes]
+                center = total[i] - total[i - minutes]
+                right = satisfied[-1] - satisfied[i]
+                
+                ans = max(ans, left + center + right )
+                
+        return ans 
+            
         
-        for (i, customer) in enumerate(customers[minutes: ] + [0]):
-            ans = max(ans, left + aSum + right[minutes + i])
-            aSum += customer - customers[i]
-            if grumpy[i] == 0: 
-                left += customers[i]
-        
-        return ans
