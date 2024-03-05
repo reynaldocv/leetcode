d7e5b9ef8e60bab21c7c1e63999abd6983645b1e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-05 09:40:29 -0500

    Update 1750. [Medium] Minimum Length of String After Deleting Similar Ends.py

--- 1000 - 1999/1750. [Medium] Minimum Length of String After Deleting Similar Ends.py
+++ 1000 - 1999/1750. [Medium] Minimum Length of String After Deleting Similar Ends.py
@@ -3,18 +3,22 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
         n = len(s)
+        
         start = 0 
         end = n - 1
-        while start < end and s[start] == s[end]:
-            while start + 1 < n and s[start + 1] == s[start]:
-                start += 1
-            while end - 1 >= 0 and s[end - 1] == s[end]:
-                end -= 1
                 
-            start +=  1
-            end -= 1
-        
-        return end - start + 1 if end - start + 1 > 0 else 0 
+        while start < end and s[start] == s[end]:
+            char = s[start]
+            
+            while start <= end and s[start] == char: 
+                start += 1 
+            
+            while start <= end and s[end] == char: 
+                end -= 1 
+          
+        return end - start + 1
+            
+            
             
         
         
