c956c15fe7c60f0877871f37159e95999ae20929
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-05 17:00:11 -0500

    Update 0756. [Medium] Pyramid Transition Matrix.py

--- 0001 - 0999/0756. [Medium] Pyramid Transition Matrix.py
+++ 0001 - 0999/0756. [Medium] Pyramid Transition Matrix.py
@@ -2,19 +2,18 @@
 
 class Solution:
     def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
-        def recursive(bottom, new, i):            
-            if len(bottom) == 1: 
-                return True
+        @cache
+        def helper(word, new, i):
+            if len(word) == 1: 
+                return True 
             
-            elif i == len(bottom) - 1:
-                return recursive(new, "", 0)
+            elif i == len(word) - 1:
+                return helper(new, "", 0)
             
             else: 
-                colours = triple[bottom[i: i + 2]]
-                
-                for color in colours: 
-                    if recursive(bottom, new + color, i + 1):
-                        return True
+                for color in triple[word[i: i + 2]]:
+                    if helper(word, new + color, i + 1):
+                        return True 
                     
                 return False
                
@@ -23,5 +22,5 @@ class Solution:
         for word in allowed: 
             triple[word[0: 2]].append(word[2: ])
 
-        return recursive(bottom, "", 0)
+        return helper(bottom, "", 0)
                     
