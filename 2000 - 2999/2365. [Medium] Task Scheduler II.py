ad28ade7767c3f555d2c3d626faa07ffbe3edc18
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-07 02:48:55 -0500

    Update 2365. [Medium] Task Scheduler II.py

--- 2000 - 2999/2365. [Medium] Task Scheduler II.py
+++ 2000 - 2999/2365. [Medium] Task Scheduler II.py
@@ -2,14 +2,20 @@
 
 class Solution:
     def taskSchedulerII(self, tasks: List[int], space: int) -> int:
-        start = defaultdict(lambda: 0)
+        last = defaultdict(lambda: -1)
         
-        ans = 0
+        ans = 0 
         
         for task in tasks:
-            ans = max(ans + 1, start[task])
-            start[task] = ans + space + 1
+            if last[task] < ans: 
+                ans += 1 
+                
+            else: 
+                ans = last[task] + 1
+                
+            last[task] = ans + space 
             
-        return ans
+        return ans 
+                
                 
         
