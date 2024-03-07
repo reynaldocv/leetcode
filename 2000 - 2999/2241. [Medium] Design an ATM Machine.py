ea5fe32b565acc97f7f79b4e88830ae47e0460cd
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-07 01:16:29 -0500

    Update 2241. [Medium] Design an ATM Machine.py

--- 2000 - 2999/2241. [Medium] Design an ATM Machine.py
+++ 2000 - 2999/2241. [Medium] Design an ATM Machine.py
@@ -1,36 +1,39 @@
 # https://leetcode.com/problems/design-an-atm-machine/
 
 class ATM:
+
     def __init__(self):
-        self.values = [20, 50, 100, 200, 500]
         self.counter = [0, 0, 0, 0, 0]
-        
+        self.values = [20, 50, 100, 200, 500]
+
     def deposit(self, banknotesCount: List[int]) -> None:
-        for i in range(5):
-            self.counter[i] += banknotesCount[i]
-            
+        for (i, values) in enumerate(banknotesCount): 
+            self.counter[i] += values
+    
     def withdraw(self, amount: int) -> List[int]:
-        tmp = amount
         idx = 4
-        ans = [0 for _ in range(5)]
-        aSum = 0 
-        while tmp > 0 and idx >= 0:
-           
-            while tmp >= self.values[idx] and self.counter[idx] > 0:
-                bills = min(tmp//self.values[idx], self.counter[idx])
+        
+        tmp = [0 for _ in range(5)]
+        
+        while amount > 0 and idx >= 0:            
+            if amount >= self.values[idx]:
+                bills = amount//self.values[idx]
                 
-                tmp -= self.values[idx]*bills
-                aSum += self.values[idx]*bills
+                minBills = min(bills, self.counter[idx])
                 
-                self.counter[idx] -= bills
-                ans[idx] += bills
+                tmp[idx] = minBills
+                
+                amount -= minBills*self.values[idx]
                 
             idx -= 1 
-        
-        if aSum == amount:
-            return ans 
+       
+        if amount == 0: 
+            for i in range(5):
+                self.counter[i] -= tmp[i]
+                
+            return tmp
+            
         else: 
-            self.deposit(ans)
             return [-1]
 
 # Your ATM object will be instantiated and called as such:
