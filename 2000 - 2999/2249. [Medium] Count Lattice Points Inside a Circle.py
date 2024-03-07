35af4721053a42747aebd4d4f287d356f3fee2f3
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-07 02:25:39 -0500

    Update 2249. [Medium] Count Lattice Points Inside a Circle.py

--- 2000 - 2999/2249. [Medium] Count Lattice Points Inside a Circle.py
+++ 2000 - 2999/2249. [Medium] Count Lattice Points Inside a Circle.py
@@ -3,14 +3,17 @@
 class Solution:
     def countLatticePoints(self, circles: List[List[int]]) -> int:
         ans = 0 
-        seen = set()
+        
+        seen = set([])
         
         for (p, q, r) in circles: 
             for x in range(p - r, p + r + 1):
                 for y in range(q - r, q + r + 1):
                     if (x, y) not in seen: 
                         if (x - p)**2 + (y - q)**2 <= r**2:
-                            ans += 1 
                             seen.add((x, y))
                             
+                            ans += 1 
+                            
         return ans 
+        
