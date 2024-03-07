2625173961964a1a245214a1bf92ecf0c7db7cb8
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-07 02:16:51 -0500

    Update 2245. [Medium] Maximum Trailing Zeros in a Cornered Path.py

--- 2000 - 2999/2245. [Medium] Maximum Trailing Zeros in a Cornered Path.py
+++ 2000 - 2999/2245. [Medium] Maximum Trailing Zeros in a Cornered Path.py
@@ -1,42 +1,64 @@
 # https://leetcode.com/problems/maximum-trailing-zeros-in-a-cornered-path/
+
 class Solution:
-    def maxTrailingZeros(self, grid: List[List[int]]) -> int:
+    def maxTrailingZeros(self, grid: List[List[int]]) -> int:        
+        def helper(number):
+            cnt2 = 0 
+            cnt5 = 0 
+            
+            while number % 2 == 0: 
+                cnt2 += 1 
+                
+                number //= 2 
+                
+            while number % 5 == 0: 
+                cnt5 += 1 
+                
+                number //= 5
+                
+            return [cnt2, cnt5]
+        
         m, n = len(grid), len(grid[0])
         
-        f2 = [[0]*n for _ in range(m)]
-        f5 = [[0]*n for _ in range(m)]
+        dp = [[helper(grid[i][j]) for j in range(n)] for i in range(m)]
         
-        for i in range(m): 
-            for j in range(n): 
-                x = grid[i][j]
-                while x % 2 == 0: 
-                    f2[i][j] += 1
-                    x //= 2 
+        horizontal = [[[num for num in dp[i][j]] for j in range(n)] for i in range(m)]
+        vertical = [[[num for num in dp[i][j]] for j in range(n)] for i in range(m)]
         
-                x = grid[i][j]
-                while x % 5 == 0: 
-                    f5[i][j] += 1
-                    x //= 5 
-        
-        h = [[[0, 0] for j in range(n + 1)] for i in range(m + 1)]
-        v = [[[0, 0] for j in range(n + 1)] for i in range(m + 1)]
-
-        for i in range(m): 
-            for j in range(n): 
-                h[i][j + 1][0] = h[i][j][0] + f2[i][j]
-                h[i][j + 1][1] = h[i][j][1] + f5[i][j]
-                v[i + 1][j][0] = v[i][j][0] + f2[i][j]
-                v[i + 1][j][1] = v[i][j][1] + f5[i][j]
-        
+        for i in range(m):
+            for j in range(1, n):
+                horizontal[i][j][0] += horizontal[i][j - 1][0]
+                horizontal[i][j][1] += horizontal[i][j - 1][1]
+                
+        for i in range(1, m):
+            for j in range(n):
+                vertical[i][j][0] += vertical[i - 1][j][0]
+                vertical[i][j][1] += vertical[i - 1][j][1]
+                
         ans = 0 
-        for i in range(m): 
-            for j in range(n): 
-                hh = [h[i][n][0] - h[i][j][0], h[i][n][1] - h[i][j][1]]
-                vv = [v[m][j][0] - v[i][j][0], v[m][j][1] - v[i][j][1]]
+                   
+        for i in range(m):
+            for j in range(n):
+                up2 = vertical[i][j][0]
+                up5 = vertical[i][j][1]
                 
-                ans = max(ans, min(h[i][j][0] + v[i][j][0] + f2[i][j], h[i][j][1] + v[i][j][1] + f5[i][j]))
-                ans = max(ans, min(h[i][j][0] + vv[0], h[i][j][1] + vv[1]))
-                ans = max(ans, min(hh[0] + v[i][j][0], hh[1] + v[i][j][1]))
-                ans = max(ans, min(hh[0] + vv[0] - f2[i][j], hh[1] + vv[1] - f5[i][j]))
+                down2 = vertical[-1][j][0] - up2 + dp[i][j][0]
+                down5 = vertical[-1][j][1] - up5 + dp[i][j][1]
                 
-        return ans
+                left2 = horizontal[i][j][0] - dp[i][j][0]
+                left5 = horizontal[i][j][1] - dp[i][j][1]
+                
+                right2 = horizontal[i][-1][0] - horizontal[i][j][0]
+                right5 = horizontal[i][-1][1] - horizontal[i][j][1]
+                
+                ans = max(ans, min(up2 + left2, up5 + left5))
+                ans = max(ans, min(up2 + right2, up5 + right5))
+                ans = max(ans, min(down2 + left2, down5 + left5))
+                ans = max(ans, min(down2 + right2, down5 + right5))    
+                
+        return ans 
+                
+                
+        
+        
+        
