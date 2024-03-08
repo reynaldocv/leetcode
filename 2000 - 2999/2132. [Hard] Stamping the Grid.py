e9f62688f2db0b267184847487b65beb41d53b2e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-08 12:32:01 -0500

    Update 2132. [Hard] Stamping the Grid.py

--- 2000 - 2999/2132. [Hard] Stamping the Grid.py
+++ 2000 - 2999/2132. [Hard] Stamping the Grid.py
@@ -1,45 +1,53 @@
 # https://leetcode.com/problems/stamping-the-grid/
 
 class Solution:
-    def possibleToStamp(self, grid: List[List[int]], h: int, w: int) -> bool:
+    def possibleToStamp(self, grid: List[List[int]], stampHeight: int, stampWidth: int) -> bool:
         m, n = len(grid), len(grid[0])
-
-        aSum = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
         
-        for i in range(m):
-            for j in range(n):
-                aSum[i + 1][j + 1] = aSum[i][j + 1] + aSum[i + 1][j] - aSum[i][j] + grid[i][j]
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
 
-        stamp = [[False for _ in range(n)] for _ in range(m)]
-        
-        for i in range(m - h + 1):
-            for j in range(n - w + 1):
-                stamp[i][j] = aSum[i][j] + aSum[i + h][j + w] - aSum[i][j + w] - aSum[i + h][j] == 0
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 0:
-                    flag = False
-                    
-                    for ii in range(i, max(-1, i - h), -1):
-                        if flag: 
-                            break
-                            
-                        for jj in range(j, max(-1, j - w), -1):
-                            if stamp[ii][jj]:
-                                for s in range(ii, ii + h):
-                                    for t in range(jj, jj + w):
-                                        grid[s][t] = 1  # skip them later
-                                flag = True
-                                break
-                    if not flag: 
-                        return False
-        
-        return True
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + grid[i - 1][j - 1]
                 
+        aSum = [[False for _ in range(n)] for _ in range(m)]
         
+        for i in range(m - stampHeight + 1):
+            for j in range(n - stampWidth  + 1):
+                tmp = dp[i + stampHeight][j  + stampWidth]
+                tmp -= dp[i + stampHeight][j]
+                tmp -= dp[i][j + stampWidth]                
+                tmp += dp[i][j]
+                
+                aSum[i][j] = tmp == 0
         
-            
-     
-        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0: 
+                    covered = False 
+                    
+                    for ii in range(i, max(i - stampHeight, -1), -1):
+                        if covered: 
+                            break 
+                        
+                        for jj in range(j, max(j - stampWidth, -1),  -1):                            
+                            if aSum[ii][jj]: 
+                                for r in range(ii, ii  + stampHeight):
+                                    for s in range(jj, jj + stampWidth):
+                                        grid[r][s] = 1
+                                        
+                                covered = True 
+                                
+                                break 
+                                
+                    if not covered: 
+                        return False 
+                    
+        return True 
+                                
+                                
+                                
+                                
+                                
+                
         
