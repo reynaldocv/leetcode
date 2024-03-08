453498eb3fa003c8a69c4822207cebe58ac7c0ea
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-07 22:27:24 -0500

    Update 1914. [Medium] Cyclically Rotating a Grid.py

--- 1000 - 1999/1914. [Medium] Cyclically Rotating a Grid.py
+++ 1000 - 1999/1914. [Medium] Cyclically Rotating a Grid.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/cyclically-rotating-a-grid/
 
-class Solution:
+class Solution1:
     def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         n, m = len(grid), len(grid[0])
         r1, r2, c1, c2 = 0, n - 1, 0, m - 1
@@ -42,6 +42,54 @@ class Solution:
             r1, r2, c1, c2 = r1 + 1, r2 - 1, c1 + 1, c2 - 1
             
         return grid
+
+class Solution2:
+    def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
+        
+        positions = defaultdict(lambda: [])
+        values = defaultdict(lambda: [])
+        
+        total = 0 
+        time = 0         
+        
+        (x, y) = (0, -1)
+        
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        l = 0 
+        
+        seen = {(0, n), (m, n - 1), (m - 1, - 1)}
+        
+        while total < m*n: 
+            x += directions[l][0]
+            y += directions[l][1]
             
+            if (x, y) in seen: 
+                x -= directions[l][0]
+                y -= directions[l][1]
+                
+                l = (l + 1) % 4
+                
+                if l == 0: 
+                    time += 1 
+                
+            else: 
+                total += 1 
+                
+                seen.add((x, y))
+                
+                positions[time].append((x, y))
+                values[time].append(grid[x][y])
+
+        for key in positions: 
+            m = len(positions[key])
+            
+            for (i, (x, y)) in enumerate(positions[key]):
+                grid[x][y] = values[key][(i + k) % m]
+                
+        return grid
+                
+                
             
         
