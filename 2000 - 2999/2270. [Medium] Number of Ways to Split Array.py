1923a13976fbab93be302b74d5fc4a4f0b6d1903
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-10 12:58:23 -0500

    Update 2270. [Medium] Number of Ways to Split Array.py

--- 2000 - 2999/2270. [Medium] Number of Ways to Split Array.py
+++ 2000 - 2999/2270. [Medium] Number of Ways to Split Array.py
@@ -2,18 +2,20 @@ https://leetcode.com/problems/number-of-ways-to-split-array/
 
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        n = len(nums)
+        n = len(nums) 
         
-        ans = 0
-        
-        left = 0
+        left = 0         
         right = sum(nums)
         
-        for num in nums[: -1]:
-            left += num
-            right -= num
+        ans = 0 
+        
+        for i in range(n - 1): 
+            left += nums[i] 
+            right -= nums[i]
             
-            if left >= right:
+            if left >= right: 
                 ans += 1 
                 
         return ans 
+            
+            
