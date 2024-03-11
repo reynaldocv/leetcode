5914de5fb1f58cd34ea7e8a005251955fc1fdc1e
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-11 12:05:48 -0500

    Update 0381. [Hard] Insert Delete GetRandom O(1) - Duplicates allowed.py

--- 0001 - 0999/0381. [Hard] Insert Delete GetRandom O(1) - Duplicates allowed.py
+++ 0001 - 0999/0381. [Hard] Insert Delete GetRandom O(1) - Duplicates allowed.py
@@ -8,25 +8,29 @@ class RandomizedCollection:
         
     def insert(self, val: int) -> bool:
         self.idx[val].add(len(self.arr))
+        
         self.arr.append(val)
+        
         return len(self.idx[val]) == 1
 
     def remove(self, val: int) -> bool:
         if not self.idx[val]:
             return False
-        remove, last = self.idx[val].pop(), self.arr[-1]
+        
+        remove = self.idx[val].pop()
+        last = self.arr[-1]
+        
         self.arr[remove] = last
+        
         self.idx[last].add(remove)
-        self.idx[last].discard(len(self.arr) - 1)
+        self.idx[last].remove(len(self.arr) - 1)
         
         self.arr.pop()
         return True
 
-    def getRandom(self) -> int:
-        
+    def getRandom(self) -> int:        
         return random.choice(self.arr)
-
-
+        
 # Your RandomizedCollection object will be instantiated and called as such:
 # obj = RandomizedCollection()
 # param_1 = obj.insert(val)
