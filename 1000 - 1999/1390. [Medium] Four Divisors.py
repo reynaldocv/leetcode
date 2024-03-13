88a3dd0390bd3e2de8eeba2ccb593a69aba30e98
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-12 23:15:53 -0500

    Update 1390. [Medium] Four Divisors.py

--- 1000 - 1999/1390. [Medium] Four Divisors.py
+++ 1000 - 1999/1390. [Medium] Four Divisors.py
@@ -2,19 +2,22 @@
 
 class Solution:
     def sumFourDivisors(self, nums: List[int]) -> int:
-        n = max(nums) + 1
-        numDiv = [1]*n    
-        sumDiv = [1]*n    
+        limit = max(nums) + 1
         
-        for i in range(2, n):
-            for j in range(i, n, i):
-                numDiv[j] += 1
-                sumDiv[j] += i
+        numDiv = [0 for _ in range(limit)]
+        sumDiv = [0 for _ in range(limit)]
         
-        ans = 0
+        for num in range(1, limit):
+            for multiple in range(num, limit, num):
+                numDiv[multiple] += 1 
+                sumDiv[multiple] += num 
+                
+        ans = 0     
+            
         for num in nums: 
             if numDiv[num] == 4: 
                 ans += sumDiv[num]
-        
-        return ans
+                
+        return ans 
+                
         
