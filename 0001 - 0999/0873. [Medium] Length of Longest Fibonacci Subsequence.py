dbaa0d87a64c8d7a3142481f88af1dacca3650e5
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-19 03:40:22 -0500

    Update 0873. [Medium] Length of Longest Fibonacci Subsequence.py

--- 0001 - 0999/0873. [Medium] Length of Longest Fibonacci Subsequence.py
+++ 0001 - 0999/0873. [Medium] Length of Longest Fibonacci Subsequence.py
@@ -3,20 +3,21 @@
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
         dp = defaultdict(lambda: 2)
-        seen = {num: i for (i, num) in enumerate(arr)}
+        
+        seen = {num for num in arr}
         
         ans = 0 
         
         for (i, numA) in enumerate(arr):
-            for (j, numB) in enumerate(arr[:i]):
+            for numB in arr[: i]:
                 numC = numA - numB
+                
                 if numC in seen and numC < numB: 
-                    aux = dp[(numB, numA)] = dp[(numC, numB)] + 1                
-                    ans = max(ans, aux)
+                    dp[(numB, numA)] = dp[(numC, numB)] + 1  
+                    
+                    ans = max(ans, dp[(numB, numA)])
                     
         return ans
                 
-        
-                    
-                    
+                
             
