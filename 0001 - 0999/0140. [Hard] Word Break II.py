992f11597dd7a56de9f6dd33886696cabe869455
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-18 22:38:16 -0500

    Update 0140. [Hard] Word Break II.py

--- 0001 - 0999/0140. [Hard] Word Break II.py
+++ 0001 - 0999/0140. [Hard] Word Break II.py
@@ -2,26 +2,30 @@
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        @cache 
-        def helper(word):
-            ans = []
-            if word in seen:
-                ans.append(word)            
-            
-            for i in range(1, min(len(word), 11)):
-
-                newWord = word[:i] 
-                if newWord in seen: 
-                    arr = [newWord + " " + sentence for sentence in helper(word[i:])]
-                    ans.extend(arr)
-
-            return ans
-
-        seen = {}
-        for word in wordDict: 
-            seen[word] = True
-            
-        ans = helper(s)
+        def helper(idx, arr):
+            if idx == n: 
+                ans.append(" ".join(arr))
+                
+            else: 
+                tmp = ""
+                
+                for i in range(idx, n):
+                    tmp += s[i]
+                    
+                    if tmp in seen: 
+                        arr.append(tmp)
+                        
+                        helper(i + 1, arr)
+                        
+                        arr.pop()
+        
+        ans = []
+        
+        seen = {word for word in wordDict}
+        
+        n = len(s)
+        
+        helper(0, [])
         
         return ans
             
