15ac42cf98502405e2529cdd8c8aaaf89be31654
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2024-03-19 02:50:20 -0500

    Update 0845. [Medium] Longest Mountain in Array.py

--- 0001 - 0999/0845. [Medium] Longest Mountain in Array.py
+++ 0001 - 0999/0845. [Medium] Longest Mountain in Array.py
@@ -2,43 +2,26 @@
 
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
-        n = len(arr)
-        left, right = [0]*n, [0]*n
-        left[0],  right[-1] = 0, 0
+        n = len(arr) 
+        
+        left = [0 for _ in range(n)]
+        right = [0 for _ in range(n)]
         
         for i in range(1, n):
-            left[i] = left[i - 1] + 1 if arr[i - 1] < arr[i] else 0
-        
+            if arr[i] > arr[i - 1]:
+                left[i] = left[i - 1] + 1
+            
         for i in range(n - 2, -1, -1):
-            right[i] = right[i + 1] + 1 if arr[i] > arr[i + 1] else 0
-            
-        ans = 0
-        
-        for i in range(n):
-            aux = left[i] + right[i] + 1 if left[i] and right[i] else 0
-            ans = max(ans, aux)
-            
-        return ans
-        
-class Solution2:
-    def longestMountain(self, arr: List[int]) -> int:
-        n = len(arr)
-        ans = start = 0 
-        
-        while start < n: 
-            end = start
-            
-            if end + 1 < n and arr[end] < arr[end + 1]:
-                while end + 1 < n and arr[end] < arr[end + 1]:
-                    end += 1
+            if arr[i] > arr[i + 1]:
+                right[i] = right[i + 1] + 1
                 
-                if end + 1 < n and arr[end] > arr[end + 1]:
-                    while end + 1 < n and arr[end] > arr[end + 1]:
-                        end += 1
-                    
-                    ans = max(ans, end - start + 1)
-                    
-            start = max(start + 1, end)
+        ans = 0 
+                
+        for i in range(1, n - 1):
+            if left[i] >= 1 and right[i] >= 1: 
+                ans = max(ans, left[i] + right[i] + 1)
             
-        return ans
+        return ans 
         
+        
+
