fd3a3ffbf5fa11fc84f7a7a4a1e03da80daa3c91
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-03-28 08:13:26 -0500

    Update 0462. [Medium] Minimum Moves to Equal Array Elements II.py

--- 0001 - 0999/0462. [Medium] Minimum Moves to Equal Array Elements II.py
+++ 0001 - 0999/0462. [Medium] Minimum Moves to Equal Array Elements II.py
@@ -2,10 +2,36 @@
 
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
-        nums.sort()
+        n = len(nums) 
+        
+        nums.sort() 
+        
+        prev = 0 
+        aSum = sum(nums)
+        
+        ans = inf
+        
+        for (i, num) in enumerate(nums):
+            left = i*num - prev
+            right = aSum - (n - i)*num
+            
+            ans = min(ans, left + right)
+            
+            prev += num 
+            aSum -=  num 
+            
+        return ans
+        
+class Solution2:
+    def minMoves2(self, nums: List[int]) -> int:
         n = len(nums)
+        
+        nums.sort()
+        
         median = nums[n//2]
+        
         ans = 0 
+        
         for num in nums: 
             ans += abs(num - median)
             
