d26ae6794136ef1041de8d21317c8c414fd48583
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-05 11:02:26 -0300

    Update 1544. [Easy] Make The String Great.py

--- 1000 - 1999/1544. [Easy] Make The String Great.py
+++ 1000 - 1999/1544. [Easy] Make The String Great.py
@@ -2,13 +2,19 @@
 
 class Solution:
     def makeGood(self, s: str) -> str:
+        others = {}
+        for i in range(26):
+            others[chr(ord("a") + i)] = chr(ord("A") + i)
+            others[chr(ord("A") + i)] = chr(ord("a") + i)
+            
         stack = []
         
         for char in s: 
-            if stack and stack[-1] != char and stack[-1].lower() == char.lower():
-                    stack.pop() 
-                    
+            if stack and stack[-1] == others[char]:
+                stack.pop() 
+            
             else: 
                 stack.append(char)
                 
         return "".join(stack)
+        
