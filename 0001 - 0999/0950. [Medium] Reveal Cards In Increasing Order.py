2cdcb80d0fee64b28e89b3b73d3b4c7efc0f25cd
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-10 16:15:41 -0300

    Update 0950. [Medium] Reveal Cards In Increasing Order.py

--- 0001 - 0999/0950. [Medium] Reveal Cards In Increasing Order.py
+++ 0001 - 0999/0950. [Medium] Reveal Cards In Increasing Order.py
@@ -4,28 +4,26 @@ class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
         n = len(deck)
         
-        ans = [0 for _ in range(n)]
-        
-        put = True 
-        idx = 0 
-        
-        i = 0 
-        
         deck.sort() 
         
-        while i < n:
-            if ans[idx] == 0:
-                if put: 
-                    ans[idx] = deck[i]
-                    i += 1 
-                    
-                    put = False 
+        arr = [i for i in range(n)]
+        
+        index = []
+        
+        while arr: 
+            idx = arr.pop(0)
+            
+            index.append(idx)
+            
+            if arr: 
+                arr.append(arr.pop(0))
                 
-                else:
-                    put = True 
-                    
-            idx = (idx + 1) % n
+        ans = [0 for i in range(n)]        
+        
+        for i in range(n):
+            ans[index[i]] = deck[i]
             
         return ans 
-            
+        
+        
                 
