a0caa2b6a41e42d4ef27c2be7655f68984adde84
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-12 11:02:04 -0300

    Update 1946. [Medium] Largest Number After Mutating Substring.py

--- 1000 - 1999/1946. [Medium] Largest Number After Mutating Substring.py
+++ 1000 - 1999/1946. [Medium] Largest Number After Mutating Substring.py
@@ -2,25 +2,27 @@
 
 class Solution:
     def maximumNumber(self, num: str, change: List[int]) -> str:
-        change = change[:10]
-        n = len(num)
-        go = {1:0}
+        n = len(num) 
+        
+        newNum = [str(num) for num in change]
+        
+        start = 0         
         ans = ""
-        for i in range(n):
-            if int(num[i]) < change[int(num[i])]:
-                if go[1] == 0: 
-                    go[1] = 1
-                if go[1] == 1:
-                    ans += str(change[int(num[i])])
-                else: 
-                    ans += num[i]    
-            elif int(num[i]) == change[int(num[i])]:
-                ans += str(change[int(num[i])])
-                    
-            elif go[1] == 0: 
-                ans += num[i]
-            elif go[1] == 1:
-                go[1] = 2
-                ans += num[i]
-            else:
-                ans += num[i]
+        
+        while start < n and num[start] >= newNum[int(num[start])]:
+            ans += num[start]
+            
+            start += 1 
+        
+        while start < n and num[start] <= newNum[int(num[start])]:
+            ans += newNum[int(num[start])]
+            
+            start += 1 
+            
+        while start < n:
+            ans += num[start]
+            
+            start += 1 
+            
+        return ans 
+        
