bc49e86e4b17e7b66a2b76aa52607260017e0627
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-13 12:33:56 -0300

    Update 0915. [Medium] Partition Array into Disjoint Intervals.py

--- 0001 - 0999/0915. [Medium] Partition Array into Disjoint Intervals.py
+++ 0001 - 0999/0915. [Medium] Partition Array into Disjoint Intervals.py
@@ -2,16 +2,19 @@
 
 class Solution:
     def partitionDisjoint(self, nums: List[int]) -> int:
-        n = len(nums)
+        n = len(nums) 
+        
+        left = [num for num in nums]
         right = [num for num in nums]
         
+        for i in range(1, n):
+            left[i] = max(left[i], left[i - 1])
+            
         for i in range(n - 2, -1, -1):
             right[i] = min(right[i], right[i + 1])
             
-        left = nums[0]
-        for i in range(n -1):
-            if left <= right[i + 1]:
+        for i in range(n - 1):
+            if left[i] <= right[i + 1]:
                 return i + 1
             
-            left = max(left, nums[i])
-            
+        return -1
