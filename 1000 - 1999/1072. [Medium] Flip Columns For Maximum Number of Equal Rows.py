b901a97a580a09c16e6c779932ec6177db54f702
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-15 20:46:15 -0300

    Update 1072. [Medium] Flip Columns For Maximum Number of Equal Rows.py

--- 1000 - 1999/1072. [Medium] Flip Columns For Maximum Number of Equal Rows.py
+++ 1000 - 1999/1072. [Medium] Flip Columns For Maximum Number of Equal Rows.py
@@ -2,23 +2,22 @@
 
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
-        def helper(row):
-            ans = 0 
-            for (i, bit) in enumerate(row[::-1]):
-                ans += bit*2**i
-                
-            return ans
+        m, n = len(matrix), len(matrix[0])
         
-        m = len(matrix[0])
-        counter = defaultdict(lambda: 0)        
-        ans = 0
+        counter = defaultdict(lambda: 0)
         
+        ans = 0 
+              
         for row in matrix: 
-            val1 = helper(row)
-            val2 = 2**m - 1 - val1
-            counter[val1] += 1 
-            counter[val2] += 1 
+            tmp1 = tuple([elem for elem in row])
+            tmp2 = tuple([1 - elem for elem in row])
             
-            ans = max(ans, counter[val1] + counter[val2])
+            counter[tmp1] += 1 
+            counter[tmp2] += 1 
             
-        return ans//2
+            ans = max(ans, counter[tmp1], counter[tmp2])
+            
+        return ans 
+            
+        
+            
