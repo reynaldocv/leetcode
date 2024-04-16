2eb195008d4f05144a0400e5b4f00cfc989ffac9
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-16 10:15:59 -0300

    Update 1041. [Medium] Robot Bounded In Circle.py

--- 1000 - 1999/1041. [Medium] Robot Bounded In Circle.py
+++ 1000 - 1999/1041. [Medium] Robot Bounded In Circle.py
@@ -2,17 +2,23 @@
 
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        x, y, z = 0, 0, 0 
-    
-        for char in instructions: 
-            if char == "G":
-                x, y = x + directions[z][0], y + directions[z][1]
-            elif char == "L":
-                z = (z + 1) % 4
-            else: 
-                z = (z - 1) % 4
-                
-        return True if (x == 0 and y == 0) or z != 0 else False
-            
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        
+        k = 0 
+        
+        (x, y) = (0, 0)
+        
+        for _ in range(4):
+            for char in instructions: 
+                if char == "G":
+                    x, y = x + directions[k][0],  y + directions[k][1]
+                    
+                elif char == "L":
+                    k = (k - 1) % 4
+                    
+                else: 
+                    k = (k + 1) % 4
+                    
+        return x == 0 and y == 0
+        
         
