3dae19abecf8b4138a97463cec462c6e76328076
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-17 14:50:38 -0300

    Update 2563. [Medium] Count the Number of Fair Pairs.py

--- 2000 - 2999/2563. [Medium] Count the Number of Fair Pairs.py
+++ 2000 - 2999/2563. [Medium] Count the Number of Fair Pairs.py
@@ -1,6 +1,20 @@
 # https://leetcode.com/problems/count-the-number-of-fair-pairs/
 
 class Solution:
-    def coloredCells(self, n: int) -> int:
-        return 2*n - 1 + 2*(n - 1)**2
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        arr = []
         
+        ans = 0 
+        
+        for num in nums: 
+            end = bisect_right(arr, upper - num)
+            start = bisect_left(arr, lower - num)
+            
+            ans += end - start
+            
+            insort(arr, num)
+            
+        return ans 
+            
+            
+            
