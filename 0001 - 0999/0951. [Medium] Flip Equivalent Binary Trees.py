df30b895b0eaeca0e98bbfc2132bc9ec587a9497
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-18 16:35:33 -0300

    Update 0951. [Medium] Flip Equivalent Binary Trees.py

--- 0001 - 0999/0951. [Medium] Flip Equivalent Binary Trees.py
+++ 0001 - 0999/0951. [Medium] Flip Equivalent Binary Trees.py
@@ -1,6 +1,5 @@
 # https://leetcode.com/problems/flip-equivalent-binary-trees/
 
-# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
@@ -8,23 +7,24 @@
 #         self.right = right
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def helper(node1, node2):            
+        def helper(node1, node2):
             if node1 and node2: 
-                if node1.val == node2.val: 
-                    if helper(node1.left, node2.left) and helper(node1.right, node2.right):
-                        return True
-                    return helper(node1.left, node2.right) and helper(node1.right, node2.left)
+                if node1.val == node2.val:
+                    first = helper(node1.left, node2.left) and helper(node1.right, node2.right)
+                    second = helper(node1.right, node2.left) and helper(node1.left, node2.right)
                     
-                else: 
-                    return False
+                    return (first or second) 
+                
+                else:                 
+                    return False 
                 
             elif not node1 and not node2: 
-                return True
+                return True 
+            
             else: 
-                return False
-        
+                return False 
+            
         return helper(root1, root2)
-                
             
 
             
