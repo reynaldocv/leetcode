d935c9e02c1767c7c6b0543a2ee0e1e12b75eb77
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-18 18:02:34 -0300

    Update 1034. [medium] Coloring A Border.py

--- 1000 - 1999/1034. [medium] Coloring A Border.py
+++ 1000 - 1999/1034. [medium] Coloring A Border.py
@@ -2,23 +2,49 @@
 
 class Solution:
     def colorBorder(self, grid: List[List[int]], row: int, col: int, color: int) -> List[List[int]]:
-        
-        m, n = len(grid), len(grid[0])
-        oldColor = grid[row][col]
-        
-        stack = [(row, col)]    
-        seen = {(row, col): True}
-        
-        while stack: 
-            (x, y) = stack.pop()
-            for (r, s) in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:                
-                if (r, s) not in seen: 
-                    if 0 <= r < m and 0 <= s < n and grid[r][s] == oldColor:
-                        seen[(r, s)] = True
-                        stack.append((r, s))
-                        
-                    else: 
-                        grid[x][y] = color
-                        
-        return grid
+        def helper(x, y):
+            value = grid[x][y]
+            
+            stack = [(x, y)]
+            seen = {(x, y)}
+            
+            while stack: 
+                (x, y) = stack.pop() 
+                
+                for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    p, q = x + r, y + s
                     
+                    if 0 <= p < m and 0 <= q < n: 
+                        if (p, q) not in seen and grid[p][q] == value: 
+                            seen.add((p, q))
+                            
+                            stack.append((p, q))
+                            
+            return seen 
+                        
+        m, n = len(grid), len(grid[0])
+        
+        ans = [[grid[i][j] for j in range(n)] for i in range(m)]
+        
+        cells = helper(row, col)
+        
+        for (x, y) in cells: 
+            colors = {grid[x][y]}
+            
+            for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                p, q = x + r, y + s
+                
+                if 0 <= p < m and 0 <= q < n: 
+                    colors.add(grid[p][q])
+                else: 
+                    colors.add(-1)
+                    
+            if len(colors) > 1: 
+                ans[x][y] = color
+                
+        return ans
+            
+            
+            
+            
+        
