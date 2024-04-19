215eb32207dd4bfdd18596e7712ed49c3877cae5
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-19 16:42:45 -0300

    Update 1253. [Medium] Reconstruct a 2-Row Binary Matrix.py

--- 1000 - 1999/1253. [Medium] Reconstruct a 2-Row Binary Matrix.py
+++ 1000 - 1999/1253. [Medium] Reconstruct a 2-Row Binary Matrix.py
@@ -3,23 +3,29 @@
 class Solution:
     def reconstructMatrix(self, upper: int, lower: int, colsum: List[int]) -> List[List[int]]:        
         n = len(colsum)
+        
         ans = [[0 for _ in range(n)] for _ in range(2)]
         
         for (i, num) in enumerate(colsum): 
             if num == 2: 
                 upper -= 1
                 lower -= 1
+
                 ans[0][i] = ans[1][i] = 1
+                
             elif num == 1: 
                 if upper > lower: 
                     ans[0][i] = 1
+                    
                     upper -= 1
                 else:
                     ans[1][i] = 1
+                    
                     lower -= 1
                     
         if upper == 0 and lower == 0: 
             return ans
+            
         else:
             return []
                     
