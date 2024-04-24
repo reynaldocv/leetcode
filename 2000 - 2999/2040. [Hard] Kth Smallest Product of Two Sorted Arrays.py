f6f2441890f771b33bfff1c69b0568ec13bba99b
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-24 17:27:42 -0300

    Update 2040. [Hard] Kth Smallest Product of Two Sorted Arrays.py

--- 2000 - 2999/2040. [Hard] Kth Smallest Product of Two Sorted Arrays.py
+++ 2000 - 2999/2040. [Hard] Kth Smallest Product of Two Sorted Arrays.py
@@ -2,37 +2,56 @@
 
 class Solution:
     def kthSmallestProduct(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        def helper(val):
-            ans = 0
+        def helper(value):
+            cnt = 0 
             
-            start, end = 0, len(nums2) - 1
-            arr = neg[::-1] + pos if val >= 0 else neg + pos[::-1]
-            
-            for x in arr: 
-                if x < 0: 
-                    while start < len(nums2) and x*nums2[start] > val: 
-                        start += 1
-                    ans += len(nums2) - start
-                elif x == 0: 
-                    if 0 <= val: 
-                        ans += len(nums2)
+            for num in nums1: 
+                if num == 0: 
+                    if value >= 0: 
+                        cnt += n
+                    
                 else: 
-                    while 0 <= end and x*nums2[end] > val: 
-                        end -= 1
-                    ans += end + 1
+                    tmp = abs(num)
+
+                    if num < 0: 
+                        cnt += bisect_right(negative, value//tmp)
+
+                    else: 
+                        cnt += bisect_right(nums2, value//tmp)
+
+            return cnt 
+        
+        n = len(nums2)
+        
+        idx = bisect_left(nums2, 0)
+        
+        negative = [-num for num in nums2][:: -1]
+        
+        start = -(10**5**2)
+        end = (10**5)**2
+        
+        while end - start > 1: 
+            mid = (end + start)//2
             
-            return ans 
-        
-        neg = [x for x in nums1 if x < 0]
-        pos = [x for x in nums1 if x >= 0]
-        
-        start, end = -10**10, 10**10 + 1
-        while end - start > 0: 
-            mid = (start + end) >> 1 
             if helper(mid) < k: 
-                start = mid + 1
+                start = mid 
+                
             else: 
-                end = mid
+                end = mid 
         
-        return start
-    
+        return end 
+                                        
+                    
+                                        
+                                        
+        
+                
+                
+                
+        
+        
+        
+        
+        
+        
+       
