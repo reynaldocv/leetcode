fa3b6b04c7349f8e186df81973a005f9378c6099
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-25 18:55:38 -0300

    Update 0745. [Hard] Prefix and Suffix Search.py

--- 0001 - 0999/0745. [Hard] Prefix and Suffix Search.py
+++ 0001 - 0999/0745. [Hard] Prefix and Suffix Search.py
@@ -3,20 +3,27 @@
 class WordFilter:
 
     def __init__(self, words: List[str]):
-        self.seen = defaultdict(lambda: -1)
+        self.value = {}
         
-        for (idx, word) in enumerate(words): 
-            m = len(word)
-            for i in range(1, m + 1):
-                for j in range(m):
-                    prefix, suffix = word[:i], word[j:]
-                    self.seen[prefix + "#" + suffix] = idx
-                        
-    def f(self, prefix: str, suffix: str) -> int:
-        return self.seen[prefix + "#" + suffix]
+        for (ith, word) in enumerate(words): 
+            prefix = ""
+            
+            for char in word: 
+                prefix += char 
+                
+                suffix = ""
+                
+                for char in word[:: -1]:
+                    suffix = char + suffix
+                    
+                    self.value[(prefix, suffix)] = ith
+                
+    def f(self, pref: str, suff: str) -> int:
+        if (pref, suff) not in self.value:
+            return -1 
         
-
-
+        return self.value[(pref, suff)]
+        
 # Your WordFilter object will be instantiated and called as such:
 # obj = WordFilter(words)
-# param_1 = obj.f(prefix,suffix)
+# param_1 = obj.f(pref,suff)
