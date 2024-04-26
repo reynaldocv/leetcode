5bbc4e260c4c41cf87a4425b0c840388544bdeaa
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-04-26 10:44:53 -0300

    Update 1289. [Hard] Minimum Falling Path Sum II.py

--- 1000 - 1999/1289. [Hard] Minimum Falling Path Sum II.py
+++ 1000 - 1999/1289. [Hard] Minimum Falling Path Sum II.py
@@ -2,28 +2,26 @@
 
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
-        def helper(arr, idx):
-            ans = (inf, inf)
-            for (i, num) in enumerate(arr):
-                if i != idx: 
-                    ans = min(ans, (num, i))
+        n = len(grid) 
+        
+        minVal = [(0, i) for i in range(2)]
+        
+        for i in range(n):
+            newMinVal = []
             
-            return ans
-        
-        n = len(grid)
-        
-        if n == 1: 
-            return grid[0][0]
-        
-        for i in range(n - 2, -1, -1):
-            (minElem1, idx1) = helper(grid[i + 1], -1)
-            (minElem2, idx2) = helper(grid[i + 1], idx1)
             for j in range(n):
-                if j != idx1: 
-                    grid[i][j] += minElem1
-                else: 
-                    grid[i][j] += minElem2
+                if j == minVal[0][1]:
+                    grid[i][j] += minVal[1][0]
                     
-        return min(grid[0])
+                else: 
+                    grid[i][j] += minVal[0][0]
+                    
+                insort(newMinVal, (grid[i][j], j))
+                
+                newMinVal = newMinVal[: 2]
+                
+            minVal = newMinVal
+                        
+        return min(grid[-1])
         
                 
