81d2fc83ac9ef3b5920eb7468bab6aa46a71e51d
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-03 16:53:20 -0300

    Update 1537. [Hard] Get the Maximum Score.py

--- 1000 - 1999/1537. [Hard] Get the Maximum Score.py
+++ 1000 - 1999/1537. [Hard] Get the Maximum Score.py
@@ -4,30 +4,33 @@ class Solution:
     def maxSum(self, nums1: List[int], nums2: List[int]) -> int:
         MOD = 10**9 + 7
         
-        seen1 = {num: True for num in nums1}
-        seen2 = {num: True for num in nums2}
+        seen = set() 
         
-        seen = {num: True for num in seen1 if num in seen2}
-                
-        n, m = len(nums1) ,len(nums2)
-        sum1, sum2 = 0, 0
+        stack = [0]
         
-        i, j = 0, 0 
-        
-        for _ in range(len(seen) + 1):
-            while i < n and nums1[i] not in seen: 
-                sum1 += nums1[i]
-                i += 1 
+        for num in nums1: 
+            seen.add(num)
             
-            while j < m and nums2[j] not in seen:
-                sum2 += nums2[j]
-                j += 1 
+        for num in nums2: 
+            if num in seen: 
+                stack.append(num)
             
-            if i < n and j < m: 
-                sum1 = sum2 = (nums1[i] + max(sum1, sum2)) % MOD
-                i, j = i + 1, j + 1
+        left = 0 
+        right = 0 
+            
+        while stack and (nums1 or nums2):             
+            while nums1 and nums1[-1] != stack[-1]:
+                left += nums1.pop()
                 
-        return max(sum1, sum2) % MOD
+            while nums2 and nums2[-1] != stack[-1]:
+                right += nums2.pop() % MOD 
+                
+            stack.pop() 
+                
+            left = right = max(left, right) % MOD 
+                        
+        return left
+            
             
         
             
