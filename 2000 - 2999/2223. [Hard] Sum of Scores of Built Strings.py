5a0635cf5e569431113356b830dcaa6b1feb4d92
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-03 13:18:47 -0300

    Update 2223. [Hard] Sum of Scores of Built Strings.py

--- 2000 - 2999/2223. [Hard] Sum of Scores of Built Strings.py
+++ 2000 - 2999/2223. [Hard] Sum of Scores of Built Strings.py
@@ -1,37 +1,39 @@
 # https://leetcode.com/problems/sum-of-scores-of-built-strings/
 
 class Solution:
-    def sumScores(self, s: str) -> int:
-        def helper(s):
-            dp = [0 for _ in range(n)]
-            left, right = 0, 0
-            
-            for i in range (1, n):
-                if i > right:
-                    left = right = i
-                    
-                    while right < n and s[right - left] == s[right]:
-                        right += 1
-                    right -= 1
-                    
-                    dp[i] = right - left + 1
-                else:
-                    val = i - left
-                    
-                    if dp[val] + i <= right:
-                        dp[i] = dp[val]
-                        
-                    else:
-                        left = i
-                        
-                        while right < n and s[right - left] == s[right]:
-                            right += 1
-                        right -= 1
-                        
-                        dp[i] = right - left + 1
-                        
-            return sum(dp)
+    def sumScores(self, s: str) -> int:        
+        MOD = 119_218_851_371
         
         n = len(s)
         
-        return helper(s) + n
+        prefix = [0]
+        power = [1]
+        
+        value = {chr(ord("a") + i): i for i in range(26)}
+        
+        for char in s: 
+            prefix.append((prefix[-1]*26 + value[char]) % MOD)
+            power.append((power[-1]*26) % MOD) 
+            
+        ans = 0 
+        
+        for (ith, char) in enumerate(s):
+            if s[0] == char: 
+                start = ith 
+                end = n 
+                
+                while end - start > 1: 
+                    mid = (end + start)//2
+                    
+                    tmp = (prefix[mid + 1] - prefix[ith]*power[mid - ith + 1]) % MOD 
+                    
+                    if tmp == prefix[mid - ith + 1]:
+                        start = mid 
+                        
+                    else: 
+                        end = mid 
+                        
+                ans += end - ith 
+                
+        return ans 
+    
