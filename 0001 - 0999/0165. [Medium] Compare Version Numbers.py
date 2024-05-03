74b720bf7d72d0d0d145f4199cd92a11f87c5184
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-03 15:41:02 -0300

    Update 0165. [Medium] Compare Version Numbers.py

--- 0001 - 0999/0165. [Medium] Compare Version Numbers.py
+++ 0001 - 0999/0165. [Medium] Compare Version Numbers.py
@@ -2,22 +2,19 @@
 
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        version1 = version1.split(".")
-        version2 = version2.split(".")
+        arr1 = version1.split(".")
+        arr2 = version2.split(".")
         
-        m, n = len(version1), len(version2)
+        m, n = len(arr1), len(arr2)
         
-        for i in range(max(n, m)):
-            val1 = int(version1[i]) if i < m else 0
-            val2 = int(version2[i]) if i < n else 0
+        for i in range(max(m, n)):
+            val1 = int(arr1[i]) if i < m else 0 
+            val2 = int(arr2[i]) if i < n else 0 
             
-            if val1 < val2: 
-                return -1
-            
-            elif val1 > val2: 
-                return 1 
+            if val1 != val2:
+                return -1 if val1 < val2 else 1
             
         return 0 
-        
+            
         
 
