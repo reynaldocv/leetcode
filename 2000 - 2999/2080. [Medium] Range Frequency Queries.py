af7c229fe5c431c540e8cfc9a1201605b18c3bf2
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-07 18:54:45 -0300

    Update 2080. [Medium] Range Frequency Queries.py

--- 2000 - 2999/2080. [Medium] Range Frequency Queries.py
+++ 2000 - 2999/2080. [Medium] Range Frequency Queries.py
@@ -3,23 +3,20 @@
 class RangeFreqQuery:
 
     def __init__(self, arr: List[int]):
-        self.seen = defaultdict(lambda: [])
-        for (i, num) in enumerate(arr): 
-            self.seen[num].append(i)
+        self.index = defaultdict(lambda: [])
+        
+        for (ith, num) in enumerate(arr):
+            self.index[num].append(ith)
 
     def query(self, left: int, right: int, value: int) -> int:
-        if value not in self.seen: 
-            return 0
+        if value not in self.index: 
+            return 0 
+        
         else: 
-            arr = self.seen[value]
-            start = bisect_left(arr, left)            
-            end = bisect_right(arr, right)
+            start = bisect_left(self.index[value], left)
+            end = bisect_right(self.index[value], right)
             
             return end - start
-        
-                
-                
-
 
 # Your RangeFreqQuery object will be instantiated and called as such:
 # obj = RangeFreqQuery(arr)
