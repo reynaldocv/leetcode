7494baa5e42843d7c08dd501289bea619026b283
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-09 22:09:06 -0300

    Update 0786. [Hard] K-th Smallest Prime Fraction.py

--- 0001 - 0999/0786. [Hard] K-th Smallest Prime Fraction.py
+++ 0001 - 0999/0786. [Hard] K-th Smallest Prime Fraction.py
@@ -2,46 +2,37 @@
 
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        aux = []
-        
-        arr.sort()        
-        n = len(arr)
-        
-        for (i, num) in enumerate(arr):
-            for j in range(i + 1, n):
-                aux.append((num, arr[j]))
-                
-        aux.sort(key = lambda item: item[0]/item[1])
-        
-        return aux[k - 1]
-        
-class Solution2:
-    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        def helper(x):
-            j, p, q = 0, 0, 1
-            counter = 0
-            for i in range(n):
-                while j < n and arr[i]/arr[j] > x: 
-                    j += 1
-                
-                if j < n and p/q < arr[i]/arr[j]: 
-                    p, q = arr[i], arr[j]
-                    
-                counter += n - j
-                
-            return counter, p, q
+        def helper(value):
+            (counter, p, q) = (0, 0, 1) 
             
-        start, end = 0, 1
-        
+            j = 0 
+            
+            for i in range(n):
+                while j < n and arr[i]/arr[j] > value: 
+                    j += 1 
+                    
+                if j < n and p/q < arr[i]/arr[j]:
+                    (p, q) = (arr[i], arr[j])
+                    
+                counter += n - j 
+                
+            return (counter, p, q)
+            
         n = len(arr)
+        
+        start = 0
+        end = 1 
         
         while end > start: 
-            middle = (end + start)/2
-            counter, p, q = helper(middle)
-            if counter > k: 
-                end = middle
-            elif counter < k: 
-                start = middle
-            else: 
-                return [p, q]
+            mid = (end + start)/2
         
+            (counter, p, q) = helper(mid)
+            
+            if counter > k: 
+                end = mid
+            
+            elif counter < k:             
+                start = mid
+                
+            else:                
+                return [p, q]
