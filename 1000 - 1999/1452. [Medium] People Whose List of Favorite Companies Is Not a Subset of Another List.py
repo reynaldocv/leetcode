ae68fdde1d666ff3dac187c1b5df04376b21e970
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-10 10:40:36 -0300

    Update 1452. [Medium] People Whose List of Favorite Companies Is Not a Subset of Another List.py

--- 1000 - 1999/1452. [Medium] People Whose List of Favorite Companies Is Not a Subset of Another List.py
+++ 1000 - 1999/1452. [Medium] People Whose List of Favorite Companies Is Not a Subset of Another List.py
@@ -4,23 +4,28 @@ class Solution:
     def peopleIndexes(self, favoriteCompanies: List[List[str]]) -> List[int]:
         index = {}        
         masks = []
+        
         for companies in favoriteCompanies: 
             mask = 0
+            
             for companie in companies: 
                 if companie not in index: 
                     index[companie] = len(index) + 1
+                    
                 mask ^= 1 << index[companie]
             
             masks.append(mask)
             
         ans = []
-        for (i, maskA) in enumerate(masks):
-            
+        
+        for (i, maskA) in enumerate(masks):            
             add = True
+        
             for (j, maskB) in enumerate(masks): 
                 if i != j: 
                     if maskA & maskB == maskA: 
                         add = False
+            
                         break
                     
             if add: 
