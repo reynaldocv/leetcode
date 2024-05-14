a623db85f1a4b874a3616708ce74e6f2278ab1ec
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-14 11:25:57 -0300

    Update 1219. [Medium] Path with Maximum Gold.py

--- 1000 - 1999/1219. [Medium] Path with Maximum Gold.py
+++ 1000 - 1999/1219. [Medium] Path with Maximum Gold.py
@@ -2,28 +2,33 @@
 
 class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
-        def helper(x, y):
-            ans = 0 
-            val = grid[x][y]
-            for (r, s) in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                p, q = x + r, y + s
-                if 0 <= p < m and 0 <= q < n: 
-                    if (p, q) not in seen and grid[p][q] > 0: 
-                        seen[(p, q)] = True
-                        ans = max(ans, helper(p, q))
-                        seen.pop((p, q))
-                        
-            return val + ans
+        def helper(aSum, x, y):
+            nonlocal ans 
             
+            ans = max(ans, aSum)
+            
+            for (r, s) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                p, q = x + r, y + s
+                
+                if 0 <= p < m and 0 <= q < n: 
+                    if grid[p][q] > 0: 
+                        tmp = grid[p][q]
+                        
+                        grid[p][q] = 0 
+                        
+                        helper(aSum + tmp, p, q)
+                        
+                        grid[p][q] = tmp 
+                        
         m, n = len(grid), len(grid[0])
         
         ans = 0 
+        
         for i in range(m):
             for j in range(n):
-                if grid[i][j] != 0: 
-                    seen = {(i, j): True}
-                    ans = max(ans, helper(i, j))
+                helper(0, i, j)
                 
-        return ans
+        return ans 
         
+    
         
