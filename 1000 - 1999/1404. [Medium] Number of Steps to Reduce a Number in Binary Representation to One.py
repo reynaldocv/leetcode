94206a14817abe7018aa63cdba1d62addd71c6fd
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-17 17:59:55 -0300

    Update 1404. [Medium] Number of Steps to Reduce a Number in Binary Representation to One.py

--- 1000 - 1999/1404. [Medium] Number of Steps to Reduce a Number in Binary Representation to One.py
+++ 1000 - 1999/1404. [Medium] Number of Steps to Reduce a Number in Binary Representation to One.py
@@ -2,27 +2,20 @@
 
 class Solution:
     def numSteps(self, s: str) -> int:
-        arr = list(s)
-        n = len(arr)
+        num = int(s, 2)
         
         ans = 0 
         
-        for i in range(n - 1, -1, -1):
-            if arr[i] == "0":
-                ans += 1 
-            elif i != 0 :                 
-                idx = i 
-                while idx >= 0 and arr[idx] == "1":
-                    arr[idx] = "0"
-                    idx -= 1 
-                    
-                if idx >= 0: 
-                    arr[idx] = "1"
-                    
-                ans += 2 
+        while num > 1: 
+            if num % 2 == 0: 
+                num //= 2
                 
+            else: 
+                num += 1 
+                
+            ans += 1 
+            
         return ans 
-                    
             
         
         
