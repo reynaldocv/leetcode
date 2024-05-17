7614bc3a96c2748ffbf809117d44adaea2e72669
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-17 17:23:47 -0300

    Update 1105. [Medium] Filling Bookcase Shelves.py

--- 1000 - 1999/1105. [Medium] Filling Bookcase Shelves.py
+++ 1000 - 1999/1105. [Medium] Filling Bookcase Shelves.py
@@ -1,24 +1,29 @@
 # https://leetcode.com/problems/filling-bookcase-shelves/
 
 class Solution:
-    def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:        
+    def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
         @cache
         def helper(idx):
-            if idx == n:
+            if idx == n: 
                 return 0 
-            else: 
-                w = h = 0 
-                ans = inf
-                for j in range(idx, n):
-                    w += books[j][0]
-                    if w > shelfWidth: 
-                        return ans
-                    h = max(h, books[j][1])
-                    ans = min(ans, h + helper(j + 1))
-                    
-                return ans
             
+            else: 
+                ans = inf 
+                
+                witdh = 0
+                height = 0
+                
+                for i in range(idx, n):
+                    witdh += books[i][0]                    
+                    height = max(height, books[i][1])
+                    
+                    if witdh <= shelfWidth:                     
+                        ans = min(ans, height + helper(i + 1))
+                        
+                return ans
+        
         n = len(books)
         
         return helper(0)
+        
             
