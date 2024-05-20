5077a56281e3d2eecad25b2acc3518b350670165
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-20 11:21:18 -0300

    Create 3151. [Easy] Special Array I.py

---
+++ 3000 - 3999/3151. [Easy] Special Array I.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/special-array-i/
+
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        n = len(nums)
+        
+        for i in range(n - 1):
+            if nums[i] % 2 == nums[i + 1] % 2: 
+                return False 
+            
+        return True 
+        
