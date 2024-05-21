e12015e1c9dc7dedfb782f64b73fe2088c0c1ab0
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-21 11:08:02 -0300

    Update 0932. [Medium] Beautiful Array.py

--- 0001 - 0999/0932. [Medium] Beautiful Array.py
+++ 0001 - 0999/0932. [Medium] Beautiful Array.py
@@ -1,19 +1,16 @@
 
 
 class Solution:
-    def beautifulArray(self, n: int) -> List[int]:  
-        @cache
+    def beautifulArray(self, n: int) -> List[int]:
         def helper(num):
-            if num == 1: 
+            if num == 1:
                 return [1]
             
             odds = helper((num + 1)//2)
             evens = helper(num//2)
-            ans = [2*x - 1 for x in odds] + [2*x for x in evens]
             
-            return ans
-            
+            return [2*elem - 1 for elem in odds] + [2*elem for elem in evens]
         
         return helper(n)
-     
+        
         
