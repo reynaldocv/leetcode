9e306b196e63b5f41727764747a5885577d9ae00
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-21 13:27:12 -0300

    Update 1292. [Medium] Maximum Side Length of a Square with Sum Less than or Equal to Threshold.py

--- 1000 - 1999/1292. [Medium] Maximum Side Length of a Square with Sum Less than or Equal to Threshold.py
+++ 1000 - 1999/1292. [Medium] Maximum Side Length of a Square with Sum Less than or Equal to Threshold.py
@@ -2,23 +2,32 @@
 
 class Solution:
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
-        def helper(i, j, r, s):
-            return tmp[i][j] - tmp[r - 1][j] - tmp[i][s - 1] + tmp[r - 1][s - 1]
-            
         m, n = len(mat), len(mat[0])
         
-        tmp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
-        for i in range(m):
-            for j in range(n):
-                tmp[i + 1][j + 1] = tmp[i][j + 1] + tmp[i + 1][j] - tmp[i][j] + mat[i][j]
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        
+        for i in range(1, m + 1):
+            for j in range(n + 1):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + mat[i - 1][j - 1]
                 
-        ans = 0 
+        ans = 1
+        
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                for k in range(min(i, j)):
-                    if helper(i, j, i - k, j - k) <= threshold: 
-                        ans = max(ans, k + 1)
+                for k in range(ans, min(i, j) + 1):
+                    x0, y0 = i - k, j - k 
+                    
+                    tmp = dp[i][j] - dp[x0][j] - dp[i][y0] + dp[x0][y0]
+                    
+                    if tmp <= threshold:
+                        ans += 1 
+                        
                     else:
                         break
                         
-        return ans
+        return ans - 1
+                    
+                
+        
+                
+        
