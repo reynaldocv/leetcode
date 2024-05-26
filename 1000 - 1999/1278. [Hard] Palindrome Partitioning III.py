c9871eb0b77c9c1529ec9ebbec69cafc9f75d54c
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-25 22:25:14 -0300

    Update 1278. [Hard] Palindrome Partitioning III.py

--- 1000 - 1999/1278. [Hard] Palindrome Partitioning III.py
+++ 1000 - 1999/1278. [Hard] Palindrome Partitioning III.py
@@ -20,3 +20,41 @@ class Solution:
                     ans[i] = min(ans[i], cost(i, l) + ans[l + 1])
                 
         return ans[0]
+
+class Solution2:
+    def palindromePartition(self, s: str, k: int) -> int:
+        def collaborator(word):
+            start = 0 
+            end = len(word) - 1
+            
+            ans = 0 
+            
+            while start < end:
+                if word[start] != word[end]:
+                    ans += 1 
+                
+                start += 1 
+                end -= 1 
+                
+            return ans 
+        
+        @cache
+        def helper(idx, k):
+            if k == 1: 
+                return collaborator(s[idx: ])
+            
+            else: 
+                tmp = ""
+                ans = inf
+                
+                for i in range(idx, n - k + 1):
+                    tmp += s[i]
+                    
+                    ans = min(ans, collaborator(tmp) + helper(i + 1, k - 1))
+                    
+                return ans
+            
+        n = len(s)
+            
+        return helper(0, k)
+         
