4c36fd18d6794b0f4795013a6a75b613e0ca2451
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-27 17:26:00 -0300

    Create 2059. [Medium] Minimum Operations to Convert Number.py

---
+++ 2000 - 2999/2059. [Medium] Minimum Operations to Convert Number.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/minimum-operations-to-convert-number/
+
+class Solution:
+    def minimumOperations(self, nums: List[int], start: int, goal: int) -> int:
+        stack = [start]
+        seen = {start}
+        
+        ans = 0 
+        
+        while stack: 
+            newStack = []
+            
+            for x in stack:                 
+                if x == goal: 
+                    return ans 
+                
+                if 0 <= x <= 1000: 
+                
+                    for num in nums: 
+                        for value in [x + num, x - num, x^num]:
+                            if value not in seen: 
+                                seen.add(value)
+                            
+                                newStack.append(value)
+            
+                            
+            ans += 1             
+            stack = newStack
+                           
+        return -1 
+                            
+
+                
+            
+        
+        
