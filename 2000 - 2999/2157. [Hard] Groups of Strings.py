277961b9587e895918bb925970bb3a4ad69ebf60
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-29 16:12:14 -0300

    Update 2157. [Hard] Groups of Strings.py

--- 2000 - 2999/2157. [Hard] Groups of Strings.py
+++ 2000 - 2999/2157. [Hard] Groups of Strings.py
@@ -1,44 +1,57 @@
 # https://leetcode.com/problems/groups-of-strings/
 
+# https://leetcode.com/problems/groups-of-strings/
+
 class Solution:
     def groupStrings(self, words: List[str]) -> List[int]:
-        def helper(node):
-            while node != parent[node]:
-                node = parent[node]
+        def helper(u):
+            while u != parent[u]:
+                u = parent[u]
             
-            return node
+            return u
         
-        def collaborator(node1, node2):
-            parent1, parent2 = helper(node1), helper(node2)
-            parent[parent1] = parent[parent2] = min(parent1, parent2)
+        def collaborator(u, v):
+            parentU = helper(u)
+            parentV = helper(v)
+            
+            parent[parentU] = parent[parentV] = min(parentU, parentV)
             
         n = len(words)
         
         parent = [i for i in range(n)]
         
         seen = {}
+        
         values = {chr(ord("a") + i): i for i in range(26)}
         
         for (i, word) in enumerate(words):   
-            m = sum([1 << values[char] for char in word])
-            if m in seen: 
-                collaborator(i, seen[m])
+            mask = sum([1 << values[char] for char in word])
+            
+            if mask in seen: 
+                collaborator(i, seen[mask])
                 
             for k in range(26): 
-                if m ^ (1 << k) in seen: 
-                    collaborator(i, seen[m ^ 1 << k])
+                if mask ^ (1 << k) in seen: 
+                    collaborator(i, seen[mask ^ (1 << k)])
                     
-                if m & (1 << k): 
-                    mm = m ^ (1 << k) ^ (1 << 26)
-                    if mm in seen: 
-                        collaborator(i, seen[mm])
+                if mask & (1 << k): 
+                    newMask = mask ^ (1 << k) ^ (1 << 26)
+                    if newMask in seen: 
+                        collaborator(i, seen[newMask])
                     
-                    seen[mm] = i
+                    seen[newMask] = i
                     
-            seen[m] = i 
+            seen[mask] = i 
             
         counter = defaultdict(lambda: 0)
+        
+        maxElems = 0 
+        
         for i in range(n):
             counter[helper(i)] += 1 
             
-        return [len(counter), max(counter.values())]
+            maxElems = max(maxElems, counter[helper(i)])
+            
+        return (len(counter), maxElems)
+        
+        
