82ce0bb2ad10ef2bfb25a6f1f9ba71e089488ae0
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-29 12:48:50 -0300

    Update 2065. [Hard} Maximum Path Quality of a Graph.py

--- 2000 - 2999/2065. [Hard} Maximum Path Quality of a Graph.py
+++ 2000 - 2999/2065. [Hard} Maximum Path Quality of a Graph.py
@@ -3,6 +3,7 @@
 class Solution:
     def maximalPathQuality(self, values: List[int], edges: List[List[int]], maxTime: int) -> int:
         n = len(values)
+        
         graph = [[] for _ in range(n)]
         
         for (u, v, t) in edges:
@@ -10,18 +11,24 @@ class Solution:
             graph[v].append((u, t))
             
         stack = [(0, 0, values[0], 1)]
+        
         ans = 0 
+        
         while stack: 
             (u, time, val, mask) = stack.pop()
+            
             if u == 0: 
                 ans = max(ans, val)
+                
             for (v, t) in graph[u]:
                 if time + t <= maxTime: 
-                    if not mask & 1 << v: 
+                    if not mask & (1 << v): 
                         stack.append((v, time + t, val + values[v], mask ^ 1 << v))
+                        
                     else: 
                         stack.append((v, time + t, val, mask))
                         
         return ans
-                        
+        
+           
             
