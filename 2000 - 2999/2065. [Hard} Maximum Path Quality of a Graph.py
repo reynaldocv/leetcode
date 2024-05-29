146ab59e694b517c7e1769a958655ff65c89d215
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-29 12:58:52 -0300

    Update 2065. [Hard} Maximum Path Quality of a Graph.py

--- 2000 - 2999/2065. [Hard} Maximum Path Quality of a Graph.py
+++ 2000 - 2999/2065. [Hard} Maximum Path Quality of a Graph.py
@@ -4,31 +4,32 @@ class Solution:
     def maximalPathQuality(self, values: List[int], edges: List[List[int]], maxTime: int) -> int:
         n = len(values)
         
+        values = {ith: val for (ith, val) in enumerate(values)}
+        
         graph = [[] for _ in range(n)]
         
-        for (u, v, t) in edges:
-            graph[u].append((v, t))
-            graph[v].append((u, t))
+        for (u, v, time) in edges: 
+            graph[u].append((v, time))
+            graph[v].append((u, time))
             
         stack = [(0, 0, values[0], 1)]
         
         ans = 0 
         
         while stack: 
-            (u, time, val, mask) = stack.pop()
+            (u, totalTime, totalValue, mask) = stack.pop() 
             
             if u == 0: 
-                ans = max(ans, val)
-                
-            for (v, t) in graph[u]:
-                if time + t <= maxTime: 
-                    if not mask & (1 << v): 
-                        stack.append((v, time + t, val + values[v], mask ^ 1 << v))
+                ans = max(ans, totalValue)
+            
+            for (v, time) in graph[u]:
+                if totalTime + time <= maxTime: 
+                    if mask & (1 << v): 
+                        stack.append((v, totalTime + time, totalValue, mask))
                         
                     else: 
-                        stack.append((v, time + t, val, mask))
+                        stack.append((v, totalTime + time, totalValue + values[v], mask ^ (1 << v)))
                         
-        return ans
-        
-           
-            
+        return ans                
+                        
+                                     
