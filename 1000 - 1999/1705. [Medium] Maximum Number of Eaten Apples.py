9cdfcdf8a939c209180aae7e9a83409b779fec7f
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-31 17:40:31 -0300

    Update 1705. [Medium] Maximum Number of Eaten Apples.py

--- 1000 - 1999/1705. [Medium] Maximum Number of Eaten Apples.py
+++ 1000 - 1999/1705. [Medium] Maximum Number of Eaten Apples.py
@@ -3,9 +3,13 @@
 class Solution:
     def eatenApples(self, apples: List[int], days: List[int]) -> int:
         n = len(apples)
+        
         limit = max(i + days[i] for i in range(n))
+        
         heap = []
+        
         ans = 0 
+        
         for i in range(limit + 1):
             if i < n and apples[i] > 0:                 
                 heappush(heap, (i + days[i] - 1, apples[i]))
@@ -15,12 +19,12 @@ class Solution:
                 
             if heap: 
                 ans += 1
+                
                 (lastDay, numberApples) = heappop(heap)
+                
                 if numberApples > 1: 
                     heappush(heap, (lastDay, numberApples - 1))
                 
         return ans 
-            
-                     
             
             
