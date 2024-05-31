b242a25c85cd102f78a05a64d894b505b5dac5be
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-31 17:00:24 -0300

    Update 0978. [Medium] Longest Turbulent Subarray.py

--- 0001 - 0999/0978. [Medium] Longest Turbulent Subarray.py
+++ 0001 - 0999/0978. [Medium] Longest Turbulent Subarray.py
@@ -1,26 +1,43 @@
 # https://leetcode.com/problems/longest-turbulent-subarray/
 
 class Solution:
-    def maxTurbulenceSize(self, arr: List[int]) -> int:
-        n = len(arr)
-        if n == 1: 
-            return 1
+    def maxTurbulenceSize(self, arr: List[int]) -> int:        
+        prev = arr[0]
+        greater = "="
         
-        ans = lenght = 1
-        asc = 1
+        ans = 1 
         
-        for i in range(1, n):
-            if arr[i - 1] == arr[i]:                
-                lenght = 1    
-            elif (arr[i - 1] > arr[i] and asc) or (arr[i - 1] < arr[i] and asc == 0):
-                asc = 1 - asc    
-                lenght += 1
-            else:
-                lenght = 2
+        for num in arr: 
+            if prev == num: 
+                cnt = 1 
                 
-            ans = max(ans, lenght)
+                greater = "="
+                
+            elif prev < num:
+                if greater == ">":
+                    greater = "<"
+                    
+                    cnt += 1 
+                    
+                else:         
+                    greater = "<"
+                    
+                    cnt = 2 
+                
+            else: 
+                if greater == "<":
+                    greater = ">"
+                    
+                    cnt += 1 
+                    
+                else: 
+                    greater = ">"
+                    cnt = 2
+                    
+            prev = num     
             
-        return ans
-                    
-                    
+            ans = max(ans, cnt)
+            
+        return ans 
+                 
             
