32cac812fc6d574b1c3907e1b62dd36ed85cd3dc
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-31 19:18:47 -0300

    Update 1594. [Medium] Maximum Non Negative Product in a Matrix.py

--- 1000 - 1999/1594. [Medium] Maximum Non Negative Product in a Matrix.py
+++ 1000 - 1999/1594. [Medium] Maximum Non Negative Product in a Matrix.py
@@ -6,25 +6,33 @@ class Solution:
         
         m, n = len(grid), len(grid[0])
         
-        grid[0][0] = [grid[0][0], grid[0][0]]
+        dp = [[[grid[i][j], grid[i][j]] for j in range(n)] for i in range(m)]
         
         for j in range(1, n):
-            grid[0][j] = [grid[0][j - 1][0]*grid[0][j], grid[0][j - 1][1]*grid[0][j]]
-            
+            dp[0][j][0] *= dp[0][j - 1][0]
+            dp[0][j][1] *= dp[0][j - 1][1]
+              
         for i in range(1, m):
-            grid[i][0] = [grid[i - 1][0][0]*grid[i][0], grid[i - 1][0][1]*grid[i][0]]
+            dp[i][0][0] *= dp[i - 1][0][0]
+            dp[i][0][1] *= dp[i - 1][0][1]
             
         for i in range(1, m):
             for j in range(1, n):
-                nums = [grid[i - 1][j][0]*grid[i][j], grid[i][j - 1][0]*grid[i][j]]
-                nums.extend([grid[i - 1][j][1]*grid[i][j], grid[i][j - 1][1]*grid[i][j]])
+                tmp = []
                 
-                small, large = min(nums), max(nums)
+                for num in dp[i - 1][j]:
+                    tmp.append(num*grid[i][j])
+                    
+                for num in dp[i][j - 1]:
+                    tmp.append(num*grid[i][j])
+                    
+                tmp.sort() 
                 
-                grid[i][j] = [small, large]
+                minElem = min(tmp)
+                maxElem = max(tmp)
                 
-        return (grid[-1][-1][1] % MOD) if grid[-1][-1][1] >= 0 else -1
-        
-        
+                dp[i][j] = [minElem, maxElem]
+                
+        return (dp[-1][-1][1] % MOD) if dp[-1][-1][1] >= 0 else -1
         
         
