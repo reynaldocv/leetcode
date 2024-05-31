e5e2403bcd19c06b0cf78981548139e6b137428b
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-05-31 18:56:53 -0300

    Update 1600. [Medium] Throne Inheritance.py

--- 1000 - 1999/1600. [Medium] Throne Inheritance.py
+++ 1000 - 1999/1600. [Medium] Throne Inheritance.py
@@ -3,43 +3,44 @@
 class person():
     def __init__(self, name: str): 
         self.name = name
-        self.alive = 1
+        self.alive = 1        
+        
         self.children = []
 
 class ThroneInheritance:
 
     def __init__(self, kingName: str):
         king = person(kingName)
-        self.king = king
-        self.seen = {king.name: king}
         
+        self.king = king
+        self.seen = {king.name: king}        
 
     def birth(self, parentName: str, childName: str) -> None:
         if parentName in self.seen: 
             son = person(childName)
+            
             self.seen[parentName].children.append(son)
             self.seen[son.name] = son
 
     def death(self, name: str) -> None:
         if name in self.seen: 
             self.seen[name].alive = 0 
-        
 
     def getInheritanceOrder(self) -> List[str]:
         def helper(king):
             if king: 
                 if king.alive == 1: 
                     ans.append(king.name)
+                    
                 for child in king.children: 
                     helper(child)
         
         ans = []
+        
         helper(self.king)
         
         return ans
         
-
-
 # Your ThroneInheritance object will be instantiated and called as such:
 # obj = ThroneInheritance(kingName)
 # obj.birth(parentName,childName)
