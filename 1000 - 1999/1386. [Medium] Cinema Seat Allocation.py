f61ea8042442636ed9f14332d35a3e4608649c64
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-06-01 12:55:10 -0300

    Update 1386. [Medium] Cinema Seat Allocation.py

--- 1000 - 1999/1386. [Medium] Cinema Seat Allocation.py
+++ 1000 - 1999/1386. [Medium] Cinema Seat Allocation.py
@@ -2,30 +2,46 @@
 
 class Solution:
     def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
-        def helper(arr):
-            val1, val2, val3 = 0, 0, 0
-            for x in arr: 
-                if 2 <= x <= 5:
-                    val1 += 1 
-                if 6 <= x <= 9:
-                    val2 += 1
-                if 4 <= x <= 7:
-                    val3 += 1 
-                    
-            if val1 == 0 and val2 == 0:
-                return 2
-            elif val1 == 0 or val2 == 0 or val3 == 0:
+        def helper(occupied):
+            if all(i not in occupied for i in range(2, 10)) == True:
+                return 2 
+            
+            elif all(i not in occupied for i in range(2, 6)) == True:
                 return 1
+        
+            elif all(i not in occupied for i in range(6, 10)) == True:
+                return 1
+            
+            elif all(i not in occupied for i in range(4, 8)) == True:
+                return 1
+            
             else: 
-                return 0
+                return 0 
             
-        reserved = defaultdict(lambda: [])
-        for (row, col) in reservedSeats: 
-            reserved[row].append(col)
+        reservedSeats.sort() 
         
-        ans = (n - len(reserved))*2
+        reserved = set()
+        prev = 0
         
-        for key in reserved:
-            ans += helper(reserved[key])
-            
+        rows = set()
+        
+        ans = 0 
+        
+        for (row, seat) in reservedSeats: 
+            if row != prev: 
+                if len(reserved) > 0: 
+                    ans += helper(reserved)
+                    
+                reserved = {seat}
+                
+            else: 
+                reserved.add(seat)
+                
+            prev = row
+            rows.add(row)
+        
+        ans += helper(reserved) + 2*(n - len(rows))
+                
         return ans 
+            
+            
