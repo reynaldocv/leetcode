40dc2a5cbd431092fd6557b104d954f949846fb2
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-06-01 10:42:22 -0300

    Update 0969. [Medium] Pancake Sorting.py

--- 0001 - 0999/0969. [Medium] Pancake Sorting.py
+++ 0001 - 0999/0969. [Medium] Pancake Sorting.py
@@ -3,18 +3,26 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
         sort = sorted(arr)
+        
         ans = []
+        
         n = len(arr)
+        
         while sort: 
             elem = sort.pop()
+        
             idx = arr.index(elem)
-            arr[:idx + 1] = arr[:idx + 1][::-1]
-            arr[:n] = arr[:n][::-1] 
+            
+            arr[: idx + 1] = arr[:idx + 1][::-1]
+            arr[: n] = arr[: n][::-1] 
+            
             ans.extend([idx + 1, n])        
+            
             n -= 1
             
         return ans
-            
+        
+        
         
         
         
