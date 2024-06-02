8291545515df24911104119d811b099aec412b62
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-06-02 13:53:23 -0300

    Update 2155. [Medium] All Divisions With the Highest Score of a Binary Array.py

--- 2000 - 2999/2155. [Medium] All Divisions With the Highest Score of a Binary Array.py
+++ 2000 - 2999/2155. [Medium] All Divisions With the Highest Score of a Binary Array.py
@@ -2,32 +2,39 @@
 
 class Solution:
     def maxScoreIndices(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        cnt = 0 
-        left = [0 for i in range(n)]
-        for (i, num) in enumerate(nums): 
+        ones = sum(nums)
+        zeros = 0 
+        
+        maxElem = zeros + ones
+        
+        for num in nums:
             if num == 0: 
-                cnt += 1 
-            left[i] = cnt
+                zeros += 1 
+                
+            else: 
+                ones -=1 
+                
+            maxElem = max(maxElem, zeros + ones)
+            
+        ans = []
         
-        right = [0 for i in range(n)]
-        cnt = 0 
-        for (i, num) in enumerate(nums[::-1]): 
-            if num == 1: 
-                cnt += 1 
-            right[~i] = cnt
+        ones = sum(nums)
+        zeros = 0 
         
-        arr = []
-        maxElem = 0
-        for i in range(n + 1):
-            l = left[i - 1] if i > 0 else 0
-            r = right[i] if i < n else 0 
-            arr.append((l + r, i))
-            maxElem = max(maxElem, l + r)
-        
-        ans = [i for (s, i) in arr if s == maxElem]
-        
-        return ans
+        if ones + zeros == maxElem: 
+            ans.append(0)
+            
+        for (ith, num) in enumerate(nums, 1): 
+            if num == 0: 
+                zeros += 1 
+                
+            else: 
+                ones -=1 
+                
+            if zeros + ones == maxElem: 
+                ans.append(ith)
+                                  
+        return ans             
         
     
         
