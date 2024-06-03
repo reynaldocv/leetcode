639753c438afb74c96e867e3d41c3e84443c55e4
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-06-03 14:36:02 -0300

    Update 2486. [Medium] Append Characters to String to Make Subsequence.py

--- 2000 - 2999/2486. [Medium] Append Characters to String to Make Subsequence.py
+++ 2000 - 2999/2486. [Medium] Append Characters to String to Make Subsequence.py
@@ -4,12 +4,13 @@ class Solution:
     def appendCharacters(self, s: str, t: str) -> int:
         m, n = len(s), len(t)
         
-        i, j = 0, 0
+        i, j = 0, 0 
         
         while i < m and j < n: 
-            if s[i] == t[j]:
+            if t[j] == s[i]:
                 j += 1 
                 
             i += 1 
             
         return n - j
+        
