62548be31ac0005e3c7890fcb961dab53e04af5c
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-06-16 22:38:37 -0300

    Create 3185. [Medium] Count Pairs That Form a Complete Day II.py

---
+++ 3000 - 3999/3185. [Medium] Count Pairs That Form a Complete Day II.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/count-pairs-that-form-a-complete-day-ii/
+
+class Solution:
+    def countCompleteDayPairs(self, hours: List[int]) -> int:
+        counter = defaultdict(lambda: 0)
+        
+        for hour in hours: 
+            counter[hour % 24] += 1 
+            
+        ans = 0 
+        
+        for key in counter: 
+            if key == 0 or key == 12: 
+                ans += counter[key]*(counter[key] - 1)
+                
+            elif key in counter and (24 - key) in counter: 
+                ans += counter[key]*counter[24 - key]
+                
+        return ans//2
+            
