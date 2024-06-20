e31112a7be75a1ce3bc5bacc91a22d96d34964d6
Reynaldo C. V <37712690+reynaldocv@users.noreply.github.com>
2024-06-20 13:16:34 -0300

    Update 1552. [Medium] Magnetic Force Between Two Balls.py

--- 1000 - 1999/1552. [Medium] Magnetic Force Between Two Balls.py
+++ 1000 - 1999/1552. [Medium] Magnetic Force Between Two Balls.py
@@ -5,21 +5,26 @@ class Solution:
         def helper(value):
             ans = 0 
             prev = -inf
+            
             for x in position: 
                 if x - prev >= value:
                     ans += 1                     
+                    
                     prev = x
                     
             return ans
         
         position.sort()
+        
         start = 1 
         end = position[-1] - position[0] + 1
         
         while end - start > 1: 
             mid = (end + start)//2
+        
             if helper(mid) < m: 
                 end = mid
+                
             else:
                 start = mid
                 
