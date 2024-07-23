45e64a9da439738d9dd0653aa77b8c82b2c28794
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-07-23 15:47:46 -0300

    Update 1461. [Medium] Check If a String Contains All Binary Codes of Size K.py

--- 1000 - 1999/1461. [Medium] Check If a String Contains All Binary Codes of Size K.py
+++ 1000 - 1999/1461. [Medium] Check If a String Contains All Binary Codes of Size K.py
@@ -2,16 +2,18 @@
 
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        aux = s[:k]
-        seen = {aux: 1}
-        for char in s[k:]:
-            aux = aux[1:] + char
-            seen[aux] = 1
+        prev = s[: k - 1]
+        
+        seen = set()
+        
+        for char in s[k - 1: ]:
+            prev += char 
+            
+            seen.add(prev)
+            
+            prev = prev[1: ]
             
         return len(seen) == 2**k
         
-      
-            
-
         
         
