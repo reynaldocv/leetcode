4ba54fdc7e1b211b593914c2f7ddf03b32324665
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-08-01 17:24:26 -0300

    Update 3223. [Medium] Minimum Length of String After Operations.py

--- 3000 - 3999/3223. [Medium] Minimum Length of String After Operations.py
+++ 3000 - 3999/3223. [Medium] Minimum Length of String After Operations.py
@@ -10,15 +10,13 @@ class Solution:
         ans = 0 
         
         for key in counter: 
-            if counter[key] < 3: 
-                ans += counter[key]
-                
-            elif counter[key] % 2 == 1: 
+            if counter[key] % 2 == 1: 
                 ans += 1 
                 
             else: 
                 ans += 2
             
         return ans 
+        
         
         
