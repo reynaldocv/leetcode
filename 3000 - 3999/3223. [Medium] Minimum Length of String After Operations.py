e69701ba925c69f82049b8dfd4a1cf6e1bfcae4c
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-08-01 17:22:55 -0300

    Create 3223. [Medium] Minimum Length of String After Operations.py

---
+++ 3000 - 3999/3223. [Medium] Minimum Length of String After Operations.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/minimum-length-of-string-after-operations/
+
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        counter = defaultdict(lambda: 0)
+        
+        for char in s: 
+            counter[char] += 1 
+            
+        ans = 0 
+        
+        for key in counter: 
+            if counter[key] < 3: 
+                ans += counter[key]
+                
+            elif counter[key] % 2 == 1: 
+                ans += 1 
+                
+            else: 
+                ans += 2
+            
+        return ans 
+        
+        
