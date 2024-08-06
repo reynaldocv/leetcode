1353d14e816eaed0bcbcece0dcca4b8f22c18fea
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-08-05 21:03:38 -0300

    Update 0748. [Easy] Shortest Completing Word.py

--- 0001 - 0999/0748. [Easy] Shortest Completing Word.py
+++ 0001 - 0999/0748. [Easy] Shortest Completing Word.py
@@ -1,7 +1,4 @@
 # https://leetcode.com/problems/shortest-completing-word/
-
-class Solution:
-    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
 class Solution:
     def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
         chars = [chr(ord("a") + i) for i in range(26)]
