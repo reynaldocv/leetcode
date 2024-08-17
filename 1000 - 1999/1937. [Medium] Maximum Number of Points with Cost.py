acd73f44636e41cefcf4055050365260c4a89d4e
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-08-17 15:48:52 -0300

    Update 1937. [Medium] Maximum Number of Points with Cost.py

--- 1000 - 1999/1937. [Medium] Maximum Number of Points with Cost.py
+++ 1000 - 1999/1937. [Medium] Maximum Number of Points with Cost.py
@@ -2,20 +2,24 @@
 
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        def helper(arr):            
+        def helper(arr):
             ans = [num for num in arr]
+            
             for i in range(1, n):
-                ans[i] = max(ans[i], ans[i - 1] - 1)
+                ans[i] = max(arr[i], ans[i - 1] - 1)
                 
-            return ans
+            return ans 
         
         m, n = len(points), len(points[0])
         
-        ans = [0 for i in range(n)]
+        ans = [0 for _ in range(n)]
+        
         for row in points: 
             left = helper(ans)
-            right = helper(ans[::-1])[::-1]            
+            right = helper(ans[:: -1])[:: -1]
+            
             for i in range(n):
                 ans[i] = row[i] + max(left[i], right[i])
             
         return max(ans)
+                
