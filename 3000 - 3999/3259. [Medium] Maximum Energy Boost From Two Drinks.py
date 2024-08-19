5be9ffa0bd7f03c61a6c5b280f85bd8ee7ac7bce
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-08-19 09:45:53 -0300

    Update 3259. [Medium] Maximum Energy Boost From Two Drinks.py

--- 3000 - 3999/3259. [Medium] Maximum Energy Boost From Two Drinks.py
+++ 3000 - 3999/3259. [Medium] Maximum Energy Boost From Two Drinks.py
@@ -6,11 +6,17 @@ class Solution:
         
         n = len(energyDrinkA)
         
+        ans = 0 
+        
         arr[0][1] += arr[0][0]
         arr[1][1] += arr[1][0]
+        
+        ans = max(arr[0][1], arr[1][1])
         
         for i in range(2, n):
             arr[0][i] += max(arr[0][i - 1], arr[1][i - 2])
             arr[1][i] += max(arr[1][i - 1], arr[0][i - 2])
+            
+            ans = max(ans, arr[0][i], arr[1][i])
           
-        return max(max(arr[0]), max(arr[1]))
+        return ans 
