b0694c742a2c004c8633360beccb5ac73392da63
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-08-19 09:49:20 -0300

    Update 3259. [Medium] Maximum Energy Boost From Two Drinks.py

--- 3000 - 3999/3259. [Medium] Maximum Energy Boost From Two Drinks.py
+++ 3000 - 3999/3259. [Medium] Maximum Energy Boost From Two Drinks.py
@@ -1,15 +1,15 @@
 # https://leetcode.com/problems/maximum-energy-boost-from-two-drinks/
 
 class Solution:
-    def maxEnergyBoost(self, energyDrinkA: List[int], energyDrinkB: List[int]) -> int:
-        n = len(energyDrinkA)
+    def maxEnergyBoost(self, energyDrinkA: List[int], energyDrinkB: List[int]) -> int:  
+        energyDrinkA = [0] + energyDrinkA
+        energyDrinkB = [0] + energyDrinkB
         
-        arr = [energyDrinkA, energyDrinkB]
+        arr = [energyDrinkA,  energyDrinkB]
         
-        arr[0][1] += arr[0][0]
-        arr[1][1] += arr[1][0]
+        n = len(energyDrinkA) 
         
-        ans = max(arr[0][1], arr[1][1])
+        ans = 0         
         
         for i in range(2, n):
             arr[0][i] += max(arr[0][i - 1], arr[1][i - 2])
