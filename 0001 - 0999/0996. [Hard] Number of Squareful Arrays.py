aa06418ce9946d371b066ade01de7f4ef3c06aa5
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-08-21 19:23:20 -0300

    Update 0996. [Hard] Number of Squareful Arrays.py

--- 0001 - 0999/0996. [Hard] Number of Squareful Arrays.py
+++ 0001 - 0999/0996. [Hard] Number of Squareful Arrays.py
@@ -2,31 +2,49 @@
 
 class Solution:
     def numSquarefulPerms(self, nums: List[int]) -> int:
-        @cache
-        def isSquare(num):
-            return (int(num**.5))**2 == num
-        
-        def helper(cnt, preNum):
-            nonlocal ans
-            if cnt == n:                 
-                ans += 1
+        def helper(cnt, prev):
+            nonlocal ans 
+            
+            if cnt == n: 
+                ans += 1 
+                
             else: 
-                for num in counter:
+                for num in arr: 
                     if counter[num] > 0:
-                        if isSquare(preNum + num):
-                            counter[num] -= 1
+                        tmp = prev + num
+                    
+                        if (int(tmp**.5))**2 == tmp: 
+                            counter[num] -= 1 
+                            
                             helper(cnt + 1, num)
-                            counter[num] += 1
+                
+                            counter[num] += 1 
+
                             
         n = len(nums)
-        ans = 0 
+        
         counter = defaultdict(lambda: 0)
+        
         for num in nums: 
-            counter[num] += 1
-
-        for num in counter:
-            counter[num] -= 1
+            counter[num] += 1 
+            
+        arr = [key for key in counter]
+        
+        ans = 0 
+        
+        for num in arr: 
+            counter[num] -= 1 
+            
             helper(1, num)
-            counter[num] += 1
-
-        return ans
+            
+            counter[num] += 1 
+        
+        return ans 
+            
+        
+        
+        
+        
+        
+        
+        
