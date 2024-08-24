970d70b62a07359b86fcb49ae079eb63aafac6cf
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-08-23 22:18:46 -0300

    Update 0564. [Hard] Find the Closest Palindrome.py

--- 0001 - 0999/0564. [Hard] Find the Closest Palindrome.py
+++ 0001 - 0999/0564. [Hard] Find the Closest Palindrome.py
@@ -2,55 +2,53 @@
 
 class Solution:
     def nearestPalindromic(self, n: str) -> str:
-        def helper(middle):
-            m = len(middle)
-            val = int(middle[:l//2]) if m % 2 == 0 else int(middle[:l//2 + 1])
+        m = len(n)
+        
+        if m == 1: 
+            if n == 0:
+                return "1"
             
-            if m % 2 == 0: 
-                if len(str(val - 1)) == len(str(val)):
-                    start = str(val - 1) + str(val - 1)[::-1]
-                else: 
-                    start = str(val - 1) + "9" + str(val - 1)[::-1]
-                
-                if len(str(val + 1)) == len(str(val)):
-                    end = str(val + 1) + str(val + 1)[::-1]
-                else: 
-                    end = str(val + 1) + str(val + 1)[:l//2][::-1]
-                    
             else: 
-                if len(str(val - 1)) == len(str(val)):
-                    start = str(val - 1) + str(val - 1)[:l//2][::-1]
-                else: 
-                    start = str(val - 1) + str(val - 1)[::-1]
-                
-                if len(str(val + 1)) == len(str(val)):
-                    end = str(val + 1) + str(val + 1)[:l//2][::-1]
-                else: 
-                    end = str(val + 1) + str(val + 1)[:l//2][::-1]
-                    
-            return (start, end)
+                return str(int(n) - 1)
         
-        l = len(n)
-        if int(n) <= 10: 
-            return str(int(n) - 1)
-        elif int(n) == 11: 
-            return "9"
-        
-        if l % 2 == 0: 
-            aux = n[:l//2]
-            middle = aux + aux[::-1]
-        else: 
-            aux = n[:l//2 + 1]
-            middle = aux + aux[:l//2][::-1]
+        elif m % 2 == 0: 
+            root = n[: m//2]
             
-        ans = (inf, inf)
-        (start, end) = helper(middle)
+            _prev = str(int(root) - 1)
+            _next = str(int(root) + 1)
+            
+            arr = [root + root[:: -1], _prev + _prev[:: -1], _next + _next[:: -1]]
+            
+            arr.append("9"*(m - 1))
+            arr.append("1" + "0"*(m - 1) + "1")
+        else:             
+            root = n[: m//2 + 1]
+            _prev = str(int(root) - 1)
+            _next = str(int(root) + 1)
+            
+            arr = [root[: -1] + root[:: -1], _prev[: -1] + _prev[:: -1], _next[: -1] + _next[:: -1]]
+            
+            arr.append("9"*(m - 1))
+            arr.append("1" + "0"*(m - 1) + "1")
+            
+        arr.sort(key = lambda item: (abs(int(item) - int(n)), int(item)))
         
-        for i in [start, end, middle]:
-            if i != n: 
-                ans = min(ans, (abs(int(i) - int(n)), int(i)))
-                
-        return str(ans[1])
+        if arr[0] == n: 
+            return arr[1]
+        
+        else: 
+            return arr[0]
+            
+            
+            
+            
+            
+        
+        
+        
+            
+        
+        
                 
         
         
