ac71a854887c161e426d0859e5e34b4cc646d7a5
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-08-29 12:07:39 -0300

    Update 0468. [Medium] Validate IP Address.py

--- 0001 - 0999/0468. [Medium] Validate IP Address.py
+++ 0001 - 0999/0468. [Medium] Validate IP Address.py
@@ -2,41 +2,38 @@
 
 class Solution:
     def validIPAddress(self, queryIP: str) -> str:
-        if "." in queryIP and queryIP.count(".") == 3: 
+        def helper(word):
+            if word.isdigit():
+                if len(word) == len(str(int(word))):
+                    if 0 <= int(word) <= 255: 
+                        return True 
+                    
+            return False 
+        
+        def collaborator(word):
+            if 1<= len(word) <= 4: 
+                return all([char in "0123456789abcdefABCDEF" for char in word])
+            
+            return False 
+            
+        if "." in queryIP:
             arr = queryIP.split(".")
-            for word in arr: 
-                if word.isdigit() and int(word) <= 255: 
-                    if word[0] == "0" and len(word) != 1: 
-                        return "Neither"
-                else: 
-                    return "Neither"
-                
-            return "IPv4"
-        
-        elif ":" in queryIP and queryIP.count(":") == 7:
-            arr = queryIP.split(":")
-            for word in arr: 
-                if 1 <= len(word) <= 4: 
-                    for char in word: 
-                        if char not in "0123456789abcdefABCDEF":
-                            return "Neither"
-                else: 
-                    return "Neither"
-
-            return "IPv6"
-        
+            
+            if len(arr) == 4: 
+                if all([helper(word) for word in arr]):
+                    return "IPv4"
+                        
         else: 
-            return "Neither"
+            arr = queryIP.split(":")
         
-class Solution2:
-    def validIPAddress(self, queryIP: str) -> str:
-
-        chunk_IPv4 = r'([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
-        patten_IPv4 = re.compile(r'^(' + chunk_IPv4 + r'\.){3}' + chunk_IPv4 + r'$')
-
-        chunk_IPv6 = r'([0-9a-fA-F]{1,4})'
-        patten_IPv6 = re.compile(r'^(' + chunk_IPv6 + r'\:){7}' + chunk_IPv6 + r'$')
-
-        if patten_IPv4.match(queryIP):
-            return "IPv4"
-        return "IPv6" if patten_IPv6.match(queryIP) else "Neither" 
+            if len(arr) == 8: 
+                if all([collaborator(word) for word in arr]):
+                    return "IPv6"
+            
+        return "Neither"
+            
+            
+            
+            
+            
+            
