f662086f07648ab072fd1f74740fbafc4ab5748a
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-09-02 12:43:45 -0300

    Update 3274. [Easy] Check if Two Chessboard Squares Have the Same Color.py

--- 3000 - 3999/3274. [Easy] Check if Two Chessboard Squares Have the Same Color.py
+++ 3000 - 3999/3274. [Easy] Check if Two Chessboard Squares Have the Same Color.py
@@ -4,8 +4,7 @@ class Solution:
     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
         value = {chr(ord("a") + i): i + 1 for i in range(8)}
         
-        (x1, y1) = (value[coordinate1[0]], int(coordinate1[1]))
-        
+        (x1, y1) = (value[coordinate1[0]], int(coordinate1[1]))        
         (x2, y2) = (value[coordinate2[0]], int(coordinate2[1]))
         
         return (x1 + y1) % 2 == (x2 + y2) % 2
