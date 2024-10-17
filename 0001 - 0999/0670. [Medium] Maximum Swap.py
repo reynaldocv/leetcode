4376b478416116b94905f8e5bef94d179cf1b4e7
Reynaldo C. V. <37712690+reynaldocv@users.noreply.github.com>
2024-10-17 12:34:57 -0300

    Update 0670. [Medium] Maximum Swap.py

--- 0001 - 0999/0670. [Medium] Maximum Swap.py
+++ 0001 - 0999/0670. [Medium] Maximum Swap.py
@@ -2,20 +2,27 @@
 
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        strN = str(num)
-        n = len(strN)        
-        visited = [False for _ in range(n)]
-        nums = [s for s in strN]
-        idx = 0
-        while idx < n:            
-            arr = [(nums[i], i) for i in range(n) if visited[i] == False]
-            maxElem, position = max(arr)
-            
-            if nums[idx] == maxElem: 
-                visited[idx] = True
-                idx += 1
-            else: 
-                nums[idx], nums[position] = nums[position], nums[idx] 
-                break 
+        n = len(str(num))
         
-        return int("".join(nums))
+        chars = []
+        maxElems = []
+        
+        for (ith, char) in enumerate(str(num)):
+            chars.append(char)
+            maxElems.append((char, ith))
+        
+        for i in range(n - 2, -1, -1):
+            maxElems[i] = max(maxElems[i], maxElems[i + 1])
+        
+        for (ith, char) in enumerate(str(num)):
+            (oldChar, jth) = maxElems[ith]
+            
+            if char != oldChar:
+                jth = abs(jth)
+                
+                chars[jth] = char
+                chars[ith] = oldChar
+                
+                break 
+             
+        return int("".join(chars))
